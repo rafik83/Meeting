975580ef8a48afd885feaa120e1bb31740be4fe3
Maxime COLIN <maxime.colin@elao.com>
2015-10-19 12:16:02 +0200

    Introduce form model and fixtures

---
+++ src/Domain/Model/Form.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model;
+
+use Proximum\Vimeet\Bundle\AppBundle\Form\Type;
+
+class Form
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $template;
+
+    /**
+     * @var Type
+     */
+    private $type;
+
+    /**
+     * Get id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get template
+     *
+     * @return mixed
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * Get type
+     *
+     * @return Type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+}
