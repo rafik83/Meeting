9ac85bf485653c7fd1347245bdc72dc1c5499eed
Richard Hanna <richard.hanna@elao.com>
2015-10-21 15:13:26 +0200

    27 - Add unit test for ChangePassword command

---
+++ tests/Application/Command/User/ChangePasswordHandlerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Tests\Application\Command\User;
+
+use Proximum\Vimeet\Application\Adapter\PasswordEncoderInterface;
+use Proximum\Vimeet\Application\Adapter\SaltGeneratorInterface;
+use Proximum\Vimeet\Application\Command\User\ChangePassword;
+use Proximum\Vimeet\Application\Command\User\ChangePasswordHandler;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\UserRepositoryInterface;
+
+class ChangePasswordHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $user = new User('test@test.com', '__salt__', 'encoded_password', 'fr');
+        $expectedUser = new User('test@test.com', '__new_salt__', 'encoded_new_password', 'fr');
+
+        $command = new ChangePassword($user);
+        $command->plainPassword = 'new-password';
+
+        $userRepository = $this->prophesize(UserRepositoryInterface::class);
+
+        $saltGenerator = $this->prophesize(SaltGeneratorInterface::class);
+        $saltGenerator->generate()->shouldBeCalled()->willReturn('__new_salt__');
+
+        $passwordEncoder = $this->prophesize(PasswordEncoderInterface::class);
+        $passwordEncoder
+            ->encode($command->plainPassword, '__new_salt__')
+            ->shouldBeCalled()
+            ->willReturn('encoded_new_password');
+
+        $userRepository->set($expectedUser)->shouldBeCalled();
+
+        $handler = new ChangePasswordHandler(
+            $userRepository->reveal(),
+            $passwordEncoder->reveal(),
+            $saltGenerator->reveal()
+        );
+        $handler->handle($command);
+    }
+}
