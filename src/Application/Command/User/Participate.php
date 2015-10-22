242bbdb3f59a608897b39f668964d1a0ca0df1ab
Maxime COLIN <maxime.colin@elao.com>
2015-10-22 10:23:15 +0200

    Update participate process

--- src/Application/Command/User/Participate.php
+++ src/Application/Command/User/Participate.php
@@ -11,7 +11,7 @@
 namespace Proximum\Vimeet\Application\Command\User;
 
 use Proximum\Vimeet\Domain\Model\Event;
-use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Model\Type;
 use Proximum\Vimeet\Domain\Model\User;
 
@@ -38,9 +38,9 @@ class Participate
     public $data;
 
     /**
-     * @var Participant
+     * @var Sheet
      */
-    public $participant;
+    public $sheet;
 
     /**
      * @param User  $user
---
+++ src/Domain/Repository/SheetRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository;
+
+use Proximum\Vimeet\Domain\Model\Sheet;
+
+interface SheetRepositoryInterface
+{
+    /**
+     * @param Sheet $sheet
+     */
+    public function add(Sheet $sheet);
+}
