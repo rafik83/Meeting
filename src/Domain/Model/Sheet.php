a6944385d9dda0051a54c73b6d652c8c99399869
Maxime COLIN <maxime.colin@elao.com>
2015-10-22 09:46:07 +0200

    Sheet model

--- src/Domain/Model/Sheet.php
+++ src/Domain/Model/Sheet.php
@@ -10,9 +10,87 @@
 
 namespace Proximum\Vimeet\Domain\Model;
 
+use Doctrine\Common\Collections\ArrayCollection;
+
 class Sheet
 {
+    /**
+     * @var integer
+     */
     private $id;
 
+    /**
+     * @var Event
+     */
+    private $event;
+
+    /**
+     * @var ArrayCollection
+     */
     private $participants;
+
+    /**
+     * @var string
+     */
+    private $data;
+
+    public function __construct(Event $event)
+    {
+        $this->event        = $event;
+        $this->participants = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get event
+     *
+     * @return Event
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * Get participants
+     *
+     * @return mixed
+     */
+    public function getParticipants()
+    {
+        return $this->participants;
+    }
+
+    /**
+     * Get data
+     *
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set data
+     *
+     * @param mixed $data
+     *
+     * @return Sheet
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
 }
