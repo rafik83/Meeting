a8b213d04115a1f4433f5528083862f2fdd9c248
Nico <nicolas.dievart@elao.com>
2015-10-22 15:19:46 +0200

    34 - fix useless dependencies

--- src/Bundle/AppBundle/Controller/EventController.php
+++ src/Bundle/AppBundle/Controller/EventController.php
@@ -11,17 +11,14 @@
 namespace Proximum\Vimeet\Bundle\AppBundle\Controller;
 
 use Proximum\Vimeet\Application\Command\Participant\Create;
-use Proximum\Vimeet\Application\Command\Participant\Update;
 use Proximum\Vimeet\Application\Command\User\Participate;
 use Proximum\Vimeet\Application\Command\User\Register;
 use Proximum\Vimeet\Application\Exception\User\EmailAlreadyExistsException;
 use Proximum\Vimeet\Bundle\AppBundle\Form\Type\Participant\ParticipantCreateType;
-use Proximum\Vimeet\Bundle\AppBundle\Form\Type\Participant\ParticipantUpdateType;
 use Proximum\Vimeet\Bundle\AppBundle\Form\Type\RegisterType;
 use Proximum\Vimeet\Domain\Model\EventView;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Model\TypeView;
-use Proximum\Vimeet\Domain\Model\ParticipantView;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Specification\Sheet\CanAccess;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
