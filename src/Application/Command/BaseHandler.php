cf522cda3d45271e875f003ee0cbb98d68f09a31
Nico <nicolas.dievart@elao.com>
2015-10-29 12:33:38 +0100

    48 - Add BaseHandler

---
+++ src/Application/Command/BaseHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command;
+
+use Proximum\Vimeet\Application\Exception\Data\RequiredDataEmptyException;
+
+class BaseHandler
+{
+    /**
+     * @param array   $data
+     * @param string  $template
+     * @param integer $block
+     *
+     * @throws RequiredDataEmptyException
+     */
+    public function checkDataConstraint(array $data, $template)
+    {
+        foreach ($data as $key => $value) {
+            if ($template[$key]['required'] === 'true' && $value === null) {
+                throw new RequiredDataEmptyException('A required field is empty');
+            }
+        }
+    }
+}
