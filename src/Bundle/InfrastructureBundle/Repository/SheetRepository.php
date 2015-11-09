80143428628d701617dad2c4e9669403daa2af03
Maxime COLIN <maxime.colin@elao.com>
2015-11-09 15:13:31 +0100

    Remove unsed statement

--- src/Bundle/InfrastructureBundle/Repository/SheetRepository.php
+++ src/Bundle/InfrastructureBundle/Repository/SheetRepository.php
@@ -12,7 +12,6 @@ namespace Proximum\Vimeet\Bundle\InfrastructureBundle\Repository;
 
 use Doctrine\ORM\EntityManager;
 use Proximum\Vimeet\Bundle\InfrastructureBundle\Doctrine\ORM\QueryBuilder\Sheet\SearchQueryBuilder;
-use Proximum\Vimeet\Domain\Model\Category;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
 
