2aac231c60ac6e8e310f03f0d1155a7e3f8d3a8f
Maxime COLIN <maxime.colin@elao.com>
2015-11-09 15:24:54 +0100

    Docblocks

--- src/Domain/Model/SheetView.php
+++ src/Domain/Model/SheetView.php
@@ -12,10 +12,22 @@ namespace Proximum\Vimeet\Domain\Model;
 
 class SheetView
 {
+    /**
+     * @var int
+     */
     public $id;
 
+    /**
+     * @var string
+     */
     public $typeTitle;
 
+    /**
+     * SheetView constructor.
+     *
+     * @param int    $id
+     * @param string $typeTitle
+     */
     public function __construct($id, $typeTitle)
     {
         $this->id        = $id;
