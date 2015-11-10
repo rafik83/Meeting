7342d8876402bf323199df9836bc7eca79ea3e71
Maxime COLIN <maxime.colin@elao.com>
2015-11-10 12:19:41 +0100

    See admin

---
+++ src/Bundle/InfrastructureBundle/Repository/SeeRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Bundle\InfrastructureBundle\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Query;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\See;
+use Proximum\Vimeet\Domain\Repository\SeeRepositoryInterface;
+
+class SeeRepository implements SeeRepositoryInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getByEvent(Event $event)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('see')
+            ->from('Entity:See', 'see')
+            ->where('see.event = :event')
+            ->setParameter('event', $event);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(See $see)
+    {
+        $this->entityManager->persist($see);
+        $this->entityManager->flush($see);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function remove(See $see)
+    {
+        $this->entityManager->remove($see);
+        $this->entityManager->flush($see);
+    }
+}
