c7880d67668922ac231f96aa6979411cc24e5612
Maxime COLIN <maxime.colin@elao.com>
2015-11-12 16:24:11 +0100

    Fix fixtures loading

--- src/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
+++ src/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
@@ -32,9 +32,9 @@ class FixturesLoader extends AbstractFixture implements ContainerAwareInterface
     public function load(ObjectManager $manager)
     {
         $files = [
-            __DIR__ . '/Template.yml',
             __DIR__ . '/Event.yml',
             __DIR__ . '/Nomenclature.yml',
+            __DIR__ . '/Template.yml',
             __DIR__ . '/Type.yml',
             __DIR__ . '/Category.yml',
             __DIR__ . '/Sheet.yml',
