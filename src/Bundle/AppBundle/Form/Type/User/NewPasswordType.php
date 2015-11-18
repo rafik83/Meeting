66cf70e485937b1e16371b9f0f5cbe21a3709aaf
Nico <nicolas.dievart@elao.com>
2015-11-18 18:52:52 +0100

    26 - Add FormType

---
+++ src/Bundle/AppBundle/Form/Type/User/NewPasswordType.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Bundle\AppBundle\Form\Type\User;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class NewPasswordType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', 'repeated', [
+                'type'            => 'password',
+                'invalid_message' => 'validators.password.mismatch',
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'new_password';
+    }
+}
