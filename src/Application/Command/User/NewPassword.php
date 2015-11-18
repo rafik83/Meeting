ea380e5e53d85701cb08ad478b3a180b46dcfe58
Nico <nicolas.dievart@elao.com>
2015-11-18 17:22:18 +0100

    26 - Add command user

---
+++ src/Application/Command/User/NewPassword.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\User;
+
+
+use Proximum\Vimeet\Domain\Model\User;
+
+class NewPassword
+{
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * @var string
+     */
+    public $password;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+}
