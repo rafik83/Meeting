94e64e058b8749cb69c4a83df8bb9145d538e5b9
Richard Hanna <richard.hanna@elao.com>
2015-11-20 16:06:48 +0100

    67 - Add nomenclature choices update unit test

--- src/Domain/Model/Event.php
+++ src/Domain/Model/Event.php
@@ -109,6 +109,16 @@ class Event
     }
 
     /**
+     * Set locales
+     *
+     * @param array $locales
+     */
+    public function setLocales(array $locales)
+    {
+        $this->locales = $locales;
+    }
+
+    /**
      * Get fallback
      *
      * @return string
