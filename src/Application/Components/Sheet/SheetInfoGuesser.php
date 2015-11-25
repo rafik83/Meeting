b361befef3b68b7ebf4981502aedc80a6d6e7e63
Nico <nicolas.dievart@elao.com>
2015-11-25 13:26:49 +0100

    Add SheetInfoGuesser

---
+++ src/Application/Components/Sheet/SheetInfoGuesser.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Components\Sheet;
+
+use Proximum\Vimeet\Application\Components\Participant\ParticipantInfoGuesser;
+use Proximum\Vimeet\Domain\Model\Sheet;
+
+class SheetInfoGuesser
+{
+    /**
+     * @var ParticipantInfoGuesser
+     */
+    private $participantInfoGuesser;
+
+    public function __construct(ParticipantInfoGuesser $participantInfoGuesser)
+    {
+        $this->participantInfoGuesser = $participantInfoGuesser;
+    }
+
+    /**
+     * @param Sheet $sheet
+     *
+     * @return string
+     */
+    public function guessOwnerInfo(Sheet $sheet)
+    {
+        $participants = $sheet->getParticipants();
+
+        foreach ($participants as $participant) {
+            if ($participant->isOwner()) {
+                return $this->participantInfoGuesser->guessParticipantInfo($participant);
+            }
+        }
+
+        return '';
+    }
+
+    /**
+     * @param Sheet $sheet
+     *
+     * @return string
+     */
+    public function guessSheetInfo(Sheet $sheet)
+    {
+        $sheetTemplate = $sheet->getTypeSheetTemplate();
+        $sheetData     = $sheet->getData();
+
+        foreach ($sheetTemplate as $blockKey => $block) {
+            if (isset($block['template'])){
+                foreach ($block['template'] as $templateKey => $template) {
+                    if (isset($template['type'])) {
+                        if ($template['type'] === 'lib_organisation') {
+                            if (isset($sheetData[$blockKey][$templateKey])) {
+                                return $sheetData[$blockKey][$templateKey];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        $ownerInfo = $this->guessOwnerInfo($sheet);
+
+        if ($ownerInfo === '') {
+            return sprintf('#%s', $sheet->getId());
+        } else {
+            return $ownerInfo;
+        }
+    }
+}
