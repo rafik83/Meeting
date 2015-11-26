beec17656fe1258f11c0500b57acbee55cd34a64
Nico <nicolas.dievart@elao.com>
2015-11-26 11:54:54 +0100

    80 - Add fixtures

--- src/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
+++ src/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
@@ -43,6 +43,7 @@ class FixturesLoader extends AbstractFixture implements ContainerAwareInterface
             __DIR__ . '/Participant.yml',
             __DIR__ . '/Schedule.yml',
             __DIR__ . '/Meeting/Request.yml',
+            __DIR__ . '/Meeting/Proposition.yml',
         ];
 
         $options = [
