b3c5122986a299dfb5bd17dfafc8963c0fe60154
Nico <nicolas.dievart@elao.com>
2015-11-26 11:54:32 +0100

    80 - Add action in controller and views

--- src/Bundle/AppBundle/Controller/Event/MeetingController.php
+++ src/Bundle/AppBundle/Controller/Event/MeetingController.php
@@ -40,4 +40,31 @@ class MeetingController extends BaseController
             'request_views' => $requestViews,
         ]);
     }
+
+    /**
+     * Sheet
+     *
+     * @param EventView $eventView
+     * @param Sheet     $sheet
+     *
+     * @return Response
+     */
+    public function listPropositionAction(EventView $eventView, Sheet $sheet)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessForNonParticipant($sheet->getParticipants());
+
+        $meetingRequestRepository = $this->get('vimeet_infrastructure.repository.meeting.request_repository');
+        $meetingProposition       = $meetingRequestRepository->getPropositionReceivedBySheet($sheet);
+
+        $propositionViews = $this
+            ->get('vimeet_infrastructure.application.components.meeting.request_views_builder')
+            ->generate($meetingProposition);
+
+        return $this->render('VimeetAppBundle:Event/Meeting:listProposition.html.twig', [
+            'eventView'         => $eventView,
+            'sheet'             => $sheet,
+            'proposition_views' => $propositionViews,
+        ]);
+    }
 }
