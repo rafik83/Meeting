372b92f18af573f7a6854b9a9d9f6a8fbccf398e
Nico <nicolas.dievart@elao.com>
2015-12-01 18:04:06 +0100

    83 - Add CancelRequest command and handler

---
+++ src/Application/Command/Meeting/CancelRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Meeting;
+
+use Proximum\Vimeet\Domain\Model\Meeting\Request;
+use Proximum\Vimeet\Domain\Model\User;
+
+class CancelRequest
+{
+    /**
+     * @var Request
+     */
+    public $request;
+
+    /**
+     * @var string
+     */
+    public $message;
+
+    /**
+     * @var User
+     */
+    public $emitter;
+
+    /**
+     * @param Request $request
+     * @param User $emitter
+     */
+    public function __construct(Request $request, User $emitter)
+    {
+        $this->request   = $request;
+        $this->emitter   = $emitter;
+    }
+}
