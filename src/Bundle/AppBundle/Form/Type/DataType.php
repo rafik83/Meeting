502f1039de402406644cc3476d02d7b052728fec
Maxime COLIN <maxime.colin@elao.com>
2015-12-09 10:48:01 +0100

    Define alias / type association in services

--- src/Bundle/AppBundle/Form/Type/DataType.php
+++ src/Bundle/AppBundle/Form/Type/DataType.php
@@ -18,36 +18,35 @@ use Proximum\Vimeet\Bundle\AppBundle\Form\Type\Library;
 class DataType extends AbstractType
 {
     /**
+     * @var array
+     */
+    private $types;
+
+    /**
+     * DataType constructor.
+     *
+     * @param array $types
+     */
+    public function __construct(array $types)
+    {
+        $this->types = $types;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $types = [
-            'choice_with_description' => Library\ChoiceWithDescriptionType::class,
-            'upload_with_choices'     => Library\UploadWithChoicesType::class,
-            'lib_text'                => Library\TextType::class,
-            'lib_textarea'            => Library\TextareaType::class,
-            'lib_email'               => Library\EmailType::class,
-            'lib_option'              => Library\OptionType::class,
-            'lib_participant'         => Library\ParticipantType::class,
-            'lib_planning'            => Library\PlanningType::class,
-            'lib_country'             => Library\CountryType::class,
-            'lib_choice'              => Library\ChoiceType::class,
-            'lib_last_name'           => Library\LastNameType::class,
-            'lib_first_name'          => Library\FirstNameType::class,
-            'lib_organisation'        => Library\OrganisationType::class,
-        ];
-
         $template = $options['template'];
         $locale   = $options['locale'];
         $sheet    = $options['sheet'];
 
         foreach ($template as $i => $field) {
-            if (!isset($types[$field['type']])) {
+            if (!isset($this->types[$field['type']])) {
                 throw new \RuntimeException('Type not found.');
             }
 
-            $builder->add($i, $types[$field['type']], [
+            $builder->add($i, $this->types[$field['type']], [
                 'label'    => $field['label'][$locale],
                 'help'     => isset($field['private']) && $field['private'] === true ? 'form.field.private' : null,
                 'required' => isset($field['required']) && $field['required'] === true,
