96cbdb39b591c395a725d2e46b2eb815e272a671
Nico <nicolas.dievart@elao.com>
2015-12-11 14:01:32 +0100

    111 - Update LibChoiceWithDescriptionCartTest

--- tests/Application/Components/Cart/Carts/LibChoiceWithDescriptionCartTest.php
+++ tests/Application/Components/Cart/Carts/LibChoiceWithDescriptionCartTest.php
@@ -12,24 +12,28 @@ namespace Tests\Application\Components\Cart\Carts;
 
 use Proximum\Vimeet\Application\Components\Cart\CartRow;
 use Proximum\Vimeet\Application\Components\Cart\Carts\LibChoiceWithDescriptionCart;
+use Proximum\Vimeet\Application\Components\Product\Products\LibChoiceProduct;
+use Proximum\Vimeet\Application\Components\Product\Products\LibChoiceWithDescriptionProduct;
 
 class LibChoiceWithDescriptionCartTest extends \PHPUnit_Framework_TestCase
 {
     public function testPrepareWithEmptyTemplateAndData()
     {
-        $template  = [];
+        $product   = new LibChoiceWithDescriptionProduct('key');
+        $product->setOptions([]);
         $dataValue = [];
         $locale    = 'fr';
 
         $optionCart = new LibChoiceWithDescriptionCart();
-        $cart       = $optionCart->prepare($template, $dataValue, $locale);
+        $cart       = $optionCart->prepare($product, $dataValue, $locale);
 
         $this->assertEquals(null, $cart);
     }
 
     public function testPrepareWithEmptyTemplate()
     {
-        $template  = [];
+        $product   = new LibChoiceWithDescriptionProduct('563cb15fbf353');
+        $product->setOptions([]);
         $dataValue = [
             "value" => '563cb15fbf353',
         ];
@@ -37,105 +41,96 @@ class LibChoiceWithDescriptionCartTest extends \PHPUnit_Framework_TestCase
         $locale = 'fr';
 
         $optionCart = new LibChoiceWithDescriptionCart();
-        $cart       = $optionCart->prepare($template, $dataValue, $locale);
+        $cart       = $optionCart->prepare($product, $dataValue, $locale);
 
         $this->assertEquals(null, $cart);
     }
 
     public function testPrepare()
     {
-        $template = [
+        $expectedProduct  = new LibChoiceWithDescriptionProduct('563cae8a5072d');
+        $expectedProductOption = [
             'label'       =>
-                [
-                    'fr' => 'Forfait',
-                    'en' => 'Package',
-                ],
+                ['fr' => 'Forfait'],
             'description' =>
-                [
-                    'fr' => 'Description forfait fr',
-                    'en' => 'Description package en',
-                ],
+                ['fr' => 'Description forfait fr'],
             'type'        => 'choice_with_description',
             'required'    => true,
             'choices'     => [
-                '563cb1257722e' => [
-                    'label'         =>
-                        [
-                            'fr' => 'Formule Exposant',
-                            'en' => 'Exhibitors package',
-                        ],
-                    'description'   =>
-                        [
-                            'fr' => 'Votre inscription aux Rendez-vous CARNOT comprend les prestations suivantes (valables pour 1 personne) :    - La publication de votre profil dans le catalogue envoyé aux participants par e-mail.    - L’accès au catalogue en ligne comprenant les « fiches de présentation » de chaque participant pour faire vos choix de rendez-vous.    - L’ouverture de votre « Espace Client » sur notre site internet.    - L’assistance logistique de notre équipe avant et pendant le salon.    - Un planning de rendez',
-                            'en' => 'Description Exhibitors',
-                        ],
-                    'unitPrice'     => 2190,
-                ],
-                '563cb12b41909' => [
-                    'label'         =>
-                        [
-                            'fr' => 'Formule Découverte',
-                            'en' => 'Discovery formula',
-                        ],
-                    'description'   =>
-                        [
-                            'fr' => 'Description forfait Découverte',
-                            'en' => 'Description Discovery',
-                        ],
-                    'unitPrice'     => 1090,
-                ],
                 '563cb13a4d3de' => [
-                    'label'         =>
+                    'label'       =>
                         [
                             'fr' => 'Forfait Silver',
                             'en' => 'Silver formula',
                         ],
-                    'description'   =>
+                    'description' =>
                         [
                             'fr' => 'Description forfait  Silver',
                             'en' => 'Description Silver',
                         ],
-                    'unitPrice'     => 3990,
+                    'unitPrice'   => 3990,
                 ],
                 '563cb1546dd70' => [
-                    'label'         =>
+                    'label'       =>
                         [
                             'fr' => 'Forfait Gold',
                             'en' => 'Gold formula',
                         ],
-                    'description'   =>
+                    'description' =>
                         [
                             'fr' => 'Description forfait  Gold',
                             'en' => 'Description Gold',
                         ],
-                    'unitPrice'     => 5990,
-                ],
-                '563cb15fbf353' => [
-                    'label'       =>
-                        [
-                            'fr' => 'Formule de sponsoring des CONFERENCES',
-                            'en' => 'Sponsoring formula for CONFERENCES',
-                        ],
-                    'description' =>
-                        [
-                            'fr' => 'Description forfait  sponsoring',
-                            'en' => 'Description sponsoring',
-                        ],
-                    'unitPrice'   => 11000,
+                    'unitPrice'   => 5990,
                 ],
             ],
         ];
+        $expectedProduct->setOptions($expectedProductOption);
+        $expectedChoiceProduct  = new LibChoiceProduct('563cb13a4d3de');
+        $expectedChoiceProductOption = [
+            'label'       =>
+                [
+                    'fr' => 'Forfait Silver',
+                    'en' => 'Silver formula',
+                ],
+            'description' =>
+                [
+                    'fr' => 'Description forfait  Silver',
+                    'en' => 'Description Silver',
+                ],
+            'unitPrice'   => 3990,
+        ];
+        $expectedChoiceProduct->setOptions($expectedChoiceProductOption);
+        $expectedChoiceProduct2 = new LibChoiceProduct('563cb1546dd70');
+        $expectedChoiceProduct2Option = [
+            'label'       =>
+                [
+                    'fr' => 'Forfait Gold',
+                    'en' => 'Gold formula',
+                ],
+            'description' =>
+                [
+                    'fr' => 'Description forfait  Gold',
+                    'en' => 'Description Gold',
+                ],
+            'unitPrice'   => 5990,
+        ];
+        $expectedChoiceProduct2->setOptions($expectedChoiceProduct2Option);
+        $expectedChoiceProduct2->setChoiceParent($expectedProduct);
+        $expectedChoiceProduct->setChoiceParent($expectedProduct);
+        $expectedProduct->addChoice($expectedChoiceProduct);
+        $expectedProduct->addChoice($expectedChoiceProduct2);
 
         $dataValue = [
-            'value'    => '563cb15fbf353',
+            'value'    => '563cb13a4d3de',
         ];
 
-        $result = new CartRow('Forfait : Formule de sponsoring des CONFERENCES', 1, 11000);
+        $result = new CartRow('Forfait : Forfait Silver', 1, 3990);
 
         $locale = 'fr';
 
         $optionCart = new LibChoiceWithDescriptionCart();
-        $cart       = $optionCart->prepare($template, $dataValue, $locale);
+        $cart       = $optionCart->prepare($expectedProduct, $dataValue, $locale);
 
         $this->assertEquals($result, $cart);
     }
