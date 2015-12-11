863a95197141f15cee3c2bb7c505be71bf32bd81
Nico <nicolas.dievart@elao.com>
2015-12-11 17:45:11 +0100

    103 - Add createOrder Command, handler and service declaration

---
+++ src/Application/Command/Billing/CreateOrder.php
@@ -0,0 +1,76 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Billing;
+
+use DateTimeInterface;
+use Proximum\Vimeet\Domain\Model\Sheet;
+
+class CreateOrder
+{
+    /**
+     * @var Sheet
+     */
+    public $sheet;
+
+    /**
+     * @var string
+     */
+    public $state;
+
+    /**
+     * @var string
+     */
+    public $proFormaTemplate;
+
+    /**
+     * @var array
+     */
+    public $packageData;
+
+    /**
+     * @var array
+     */
+    public $billingData;
+
+    /**
+     * @var DateTimeInterface
+     */
+    public $createdAt;
+
+    /**
+     * @var string
+     */
+    public $paymentMode;
+
+    /**
+     * @param Sheet             $sheet
+     * @param string            $state
+     * @param string            $proFormaTemplate
+     * @param array             $packageData
+     * @param array             $billingData
+     * @param DateTimeInterface $createdAt
+     */
+    public function __construct(
+        Sheet $sheet,
+        $state,
+        $proFormaTemplate,
+        array $packageData,
+        array $billingData,
+        DateTimeInterface $createdAt
+    ) {
+        $this->sheet            = $sheet;
+        $this->state            = $state;
+        $this->proFormaTemplate = $proFormaTemplate;
+        $this->packageData      = $packageData;
+        $this->billingData      = $billingData;
+        $this->createdAt        = $createdAt;
+    }
+}
