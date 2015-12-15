ed8ff468c5d5111bb1068ee83d318db24dac1ab9
Maxime COLIN <maxime.colin@elao.com>
2015-12-15 10:22:41 +0100

    Display meetings on schedule

---
+++ src/Domain/Repository/Meeting/MeetingRepositoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository\Meeting;
+
+use Proximum\Vimeet\Domain\Model\Meeting\Meeting;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\Schedule;
+
+interface MeetingRepositoryInterface
+{
+    /**
+     * @param Schedule    $schedule
+     * @param Participant $participant
+     *
+     * @return Meeting[]
+     */
+    public function findByScheduleAndParticipant(Schedule $schedule, Participant $participant);
+}
