84f155acc31c1dc0b2df4a18b2a2f3f8e26bfb03
Nico <nicolas.dievart@elao.com>
2015-12-17 10:17:53 +0100

    105 - Create Cart model

---
+++ src/Domain/Model/Cart.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model;
+
+use DateTimeInterface;
+
+class Cart
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @var array
+     */
+    private $template;
+
+    /**
+     * @var Sheet
+     */
+    private $sheet;
+
+    /**
+     * @var DateTimeInterface
+     */
+    private $createdAt;
+
+    /**
+     * @param array             $data
+     * @param array             $template
+     * @param Sheet             $sheet
+     * @param DateTimeInterface $createdAt
+     */
+    public function __construct(array $data, array $template, Sheet $sheet, DateTimeInterface $createdAt)
+    {
+        $this->data      = $data;
+        $this->template  = $template;
+        $this->sheet     = $sheet;
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param array $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * @return Sheet
+     */
+    public function getSheet()
+    {
+        return $this->sheet;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
