ee418dbfe08bfd645815061b86ea19c6c2fa6dec
Nico <nicolas.dievart@elao.com>
2016-01-25 10:09:38 +0100

    199 - update create to have event

--- src/Application/Command/Happening/Category/Create.php
+++ src/Application/Command/Happening/Category/Create.php
@@ -11,14 +11,13 @@
 namespace Proximum\Vimeet\Application\Command\Happening\Category;
 
 use Proximum\Vimeet\Domain\Model\Event;
-use Proximum\Vimeet\Domain\Model\Happening\Category;
 
 class Create
 {
     /**
-     * @var Category
+     * @var Event
      */
-    public $category;
+    public $event;
 
     /**
      * @var string
