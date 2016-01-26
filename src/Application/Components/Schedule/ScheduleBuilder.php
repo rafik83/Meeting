58b1c7d0e6b1e4a31dbf249955a345a73c4405f9
Nico <nicolas.dievart@elao.com>
2016-01-26 17:59:28 +0100

    204 - Modify ScheduleBuilder to remove Schedule

--- src/Application/Components/Schedule/ScheduleBuilder.php
+++ src/Application/Components/Schedule/ScheduleBuilder.php
@@ -13,22 +13,17 @@ namespace Proximum\Vimeet\Application\Components\Schedule;
 use Proximum\Vimeet\Application\Components\Participant\ParticipantInfoGuesser;
 use Proximum\Vimeet\Application\Components\Sheet\SheetInfoGuesser;
 use Proximum\Vimeet\Domain\Model\Participant;
-use Proximum\Vimeet\Domain\Model\Schedule;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Repository\HappeningParticipationRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\HappeningRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\MeetingRepositoryInterface;
-use Proximum\Vimeet\Domain\Repository\ScheduleRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\MeetingSlotRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\UnavailabilityRepositoryInterface;
 use Proximum\Vimeet\Domain\View\ScheduleSlotView;
+use Proximum\Vimeet\Domain\View\ScheduleView;
 
 class ScheduleBuilder
 {
-    /**
-     * @var ScheduleRepositoryInterface
-     */
-    private $scheduleRepository;
-
     /**
      * @var MeetingRepositoryInterface
      */
@@ -60,167 +55,272 @@ class ScheduleBuilder
     private $participantInfoGuesser;
 
     /**
+     * @var MeetingSlotRepositoryInterface
+     */
+    private $meetingSlotRepository;
+
+    /**
      * ScheduleBuilder constructor.
      *
-     * @param ScheduleRepositoryInterface               $scheduleRepository
      * @param MeetingRepositoryInterface                $meetingRepository
      * @param UnavailabilityRepositoryInterface         $unavailabilityRepository
      * @param HappeningRepositoryInterface              $happeningRepository
      * @param HappeningParticipationRepositoryInterface $happeningParticipationRepository
      * @param SheetInfoGuesser                          $sheetInfoGuesser
      * @param ParticipantInfoGuesser                    $participantInfoGuesser
+     * @param MeetingSlotRepositoryInterface            $meetingSlotRepository
      */
     public function __construct(
-        ScheduleRepositoryInterface $scheduleRepository,
         MeetingRepositoryInterface $meetingRepository,
         UnavailabilityRepositoryInterface $unavailabilityRepository,
         HappeningRepositoryInterface $happeningRepository,
         HappeningParticipationRepositoryInterface $happeningParticipationRepository,
         SheetInfoGuesser $sheetInfoGuesser,
-        ParticipantInfoGuesser $participantInfoGuesser
+        ParticipantInfoGuesser $participantInfoGuesser,
+        MeetingSlotRepositoryInterface $meetingSlotRepository
     ) {
-        $this->scheduleRepository               = $scheduleRepository;
         $this->meetingRepository                = $meetingRepository;
         $this->unavailabilityRepository         = $unavailabilityRepository;
         $this->happeningRepository              = $happeningRepository;
         $this->happeningParticipationRepository = $happeningParticipationRepository;
         $this->sheetInfoGuesser                 = $sheetInfoGuesser;
         $this->participantInfoGuesser           = $participantInfoGuesser;
+        $this->meetingSlotRepository            = $meetingSlotRepository;
     }
 
     /**
      * @param Participant $participant
+     * @param string      $locale
      *
      * @return array
      */
-    public function buildForParticipant(Participant $participant)
+    public function buildForParticipant(Participant $participant, $locale)
     {
-        $schedules = $this->scheduleRepository->findByEvent($participant->getSheet()->getEvent());
+        return $this->builScheduleViewForMeetingHappeningsAndUnavailabilities($participant, $locale);
+    }
 
-        $participantSchedule = [
+    /**
+     * @param Participant $participant
+     * @param string      $locale
+     *
+     * @return ScheduleView[]
+     */
+    private function builScheduleViewForMeetingHappeningsAndUnavailabilities(Participant $participant, $locale)
+    {
+        $scheduleViews = [];
+        $scheduleViews = $this->buildMeetingsForScheduleViews($scheduleViews, $participant);
+        $scheduleViews = $this->buildHappeningsForScheduleViews($scheduleViews, $participant, $locale);
+        $scheduleViews = $this->buildUnavailabilitiesForScheduleViews($scheduleViews, $participant);
+
+        return [
             'participant' => $participant,
             'name'        => $this->participantInfoGuesser->guessParticipantInfo($participant),
-            'schedules'   => [],
+            'schedules'   => $scheduleViews,
         ];
-
-        foreach ($schedules as $i => $schedule) {
-            $participantSchedule['schedules'][$i] = [
-                'id'      => $schedule->getId(),
-                'date'    => $schedule->getDate(),
-                'columns' => [
-                    'meeting'        => $this->buildMeetings($schedule, $participant),
-                    'happening'      => $this->buildHappenings($schedule, $participant),
-                    'unavailability' => $this->buildUnavailabilities($schedule, $participant),
-                ],
-            ];
-        }
-
-        return $participantSchedule;
     }
 
     /**
-     * @param Schedule    $schedule
+     * @param array       $scheduleViews
      * @param Participant $participant
      *
-     * @return array
+     * @return ScheduleView[]
      */
-    private function buildMeetings(Schedule $schedule, Participant $participant)
+    private function buildMeetingsForScheduleViews(array $scheduleViews, Participant $participant)
     {
-        $slots = [];
+        $meetingSlots = $this->meetingSlotRepository->findByEvent($participant->getSheet()->getEvent());
 
-        foreach ($schedule->getSlots() as $meetingSlot) {
-            $slots[$meetingSlot->getId()] = new ScheduleSlotView(
-                $meetingSlot->getId(),
-                'Vide',
-                $meetingSlot->getBegin(),
-                $meetingSlot->getEnd(),
-                false
-            );
+        foreach ($meetingSlots as $meetingSlot) {
+            $beginDate = clone $meetingSlot->getBegin();
+            $beginDate->setTimeZone(new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone()));
+
+            if (!empty($scheduleViews) && isset($scheduleViews[$beginDate->format('Y-m-d')])) {
+                $scheduleViews[$beginDate->format('Y-m-d')]->addMeeting(
+                    $meetingSlot->getId(),
+                    new ScheduleSlotView(
+                        $meetingSlot->getId(),
+                        'Vide',
+                        $meetingSlot->getBegin(),
+                        $meetingSlot->getEnd(),
+                        false
+                    )
+                );
+            } else {
+                $scheduleViews[$beginDate->format('Y-m-d')] = new ScheduleView(
+                    $beginDate->format('Y-m-d'),
+                    new \DateTime(
+                        $beginDate->format('Y-m-d 00:00:00'),
+                        new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone())
+                    )
+                );
+
+                $scheduleViews[$beginDate->format('Y-m-d')]->addMeeting(
+                    $meetingSlot->getId(),
+                    new ScheduleSlotView(
+                        $meetingSlot->getId(),
+                        'Vide',
+                        $meetingSlot->getBegin(),
+                        $meetingSlot->getEnd(),
+                        false
+                    )
+                );
+            }
         }
 
-        $meetings = $this->meetingRepository->findScheduledByScheduleAndParticipant($schedule, $participant);
+        $meetings = $this->meetingRepository->findByParticipant($participant);
 
         foreach ($meetings as $meeting) {
             $sheet = $meeting->getFromSheet() === $participant->getSheet() ? $meeting->getToSheet() : $meeting->getFromSheet();
+            $beginDate = clone $meetingSlot->getBegin();
+            $beginDate->setTimeZone(new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone()));
 
-            $slots[$meeting->getSlot()->getId()] = new ScheduleSlotView(
-                $meeting->getId(),
-                $this->sheetInfoGuesser->guessSheetInfo($sheet),
-                $meeting->getSlot()->getBegin(),
-                $meeting->getSlot()->getEnd(),
-                true
+            $scheduleViews[$beginDate->format('Y-m-d')]->addMeeting(
+                $meeting->getSlot()->getId(),
+                new ScheduleSlotView(
+                    $meeting->getId(),
+                    $this->sheetInfoGuesser->guessSheetInfo($sheet),
+                    $meeting->getSlot()->getBegin(),
+                    $meeting->getSlot()->getEnd(),
+                    true
+                )
             );
         }
 
-        $this->sort($slots);
-
-        return $slots;
-    }
-
-    /**
-     * @param Schedule    $schedule
-     * @param Participant $participant
-     *
-     * @return array
-     */
-    private function buildHappenings(Schedule $schedule, Participant $participant)
-    {
-        $slots = [];
-
-        $participations = $this->happeningRepository->findByScheduleAndParticipant($schedule, $participant);
-
-        foreach ($schedule->getHappenings() as $happening) {
-            $slots[] = new ScheduleSlotView(
-                $happening->getId(),
-                $happening->getTitle(),
-                $happening->getBegin(),
-                $happening->getEnd(),
-                in_array($happening, $participations)
-            );
+        foreach ($scheduleViews as $scheduleView) {
+            $this->sort($scheduleView->meetings);
         }
 
-        $this->sort($slots);
-
-        return $slots;
+        return $scheduleViews;
     }
 
     /**
-     * @param Schedule    $schedule
+     * @param array       $scheduleViews
+     * @param Participant $participant
+     * @param string      $locale
+     *
+     * @return ScheduleView[]
+     */
+    private function buildHappeningsForScheduleViews(array $scheduleViews, Participant $participant, $locale)
+    {
+        $happenings     = $this->happeningRepository->findByEvent($participant->getSheet()->getEvent(), $locale);
+
+        foreach ($happenings as $happening) {
+            $beginDate = clone $happening->getBegin();
+            $beginDate->setTimeZone(new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone()));
+            $present = $this->happeningParticipationRepository->findByHappeningAndParticipant($happening, $participant) !== null;
+
+            if (!empty($scheduleViews) && isset($scheduleViews[$beginDate->format('Y-m-d')])) {
+                $scheduleViews[$beginDate->format('Y-m-d')]->addHappening(
+                    new ScheduleSlotView(
+                        $happening->getId(),
+                        $happening->getTitle($locale),
+                        $happening->getBegin(),
+                        $happening->getEnd(),
+                        $present
+                    )
+                );
+
+                if ($present) {
+                    $scheduleViews[$beginDate->format('Y-m-d')]->addUnavailability(
+                        new ScheduleSlotView(
+                            $happening->getId(),
+                            $happening->getTitle($locale),
+                            $happening->getBegin(),
+                            $happening->getEnd(),
+                            false
+                        )
+                    );
+                }
+            } else {
+                $scheduleViews[$beginDate->format('Y-m-d')] = new ScheduleView(
+                    $beginDate->format('Y-m-d'),
+                    new \DateTime(
+                        $beginDate->format('Y-m-d 00:00:00'),
+                        new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone())
+                    )
+                );
+
+                $scheduleViews[$beginDate->format('Y-m-d')]->addHappening(
+                    new ScheduleSlotView(
+                        $happening->getId(),
+                        $happening->getTitle($locale),
+                        $happening->getBegin(),
+                        $happening->getEnd(),
+                        $present
+                    )
+                );
+
+                if ($present) {
+                    $scheduleViews[$beginDate->format('Y-m-d')]->addUnavailability(
+                        new ScheduleSlotView(
+                            $happening->getId(),
+                            $happening->getTitle($locale),
+                            $happening->getBegin(),
+                            $happening->getEnd(),
+                            false
+                        )
+                    );
+                }
+            }
+        }
+
+        foreach ($scheduleViews as $scheduleView) {
+            $this->sort($scheduleView->happenings);
+        }
+
+        return $scheduleViews;
+    }
+
+
+    /**
+     * @param array       $scheduleViews
      * @param Participant $participant
      *
-     * @return array
+     * @return ScheduleView[]
      */
-    private function buildUnavailabilities(Schedule $schedule, Participant $participant)
+    private function buildUnavailabilitiesForScheduleViews(array $scheduleViews, Participant $participant)
     {
-        $slots = [];
-
-        $unavailabilities = $this->unavailabilityRepository->findByScheduleAndParticipant($schedule, $participant);
+        $unavailabilities = $this->unavailabilityRepository->findByParticipant($participant);
 
         foreach ($unavailabilities as $unavailability) {
-            $slots[] = new ScheduleSlotView(
-                $unavailability->getId(),
-                'Indisponible',
-                $unavailability->getBegin(),
-                $unavailability->getEnd(),
-                true
-            );
+            $beginDate = clone $unavailability->getBegin();
+            $beginDate->setTimeZone(new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone()));
+
+            if (!empty($scheduleViews) && isset($scheduleViews[$beginDate->format('Y-m-d')])) {
+                $scheduleViews[$beginDate->format('Y-m-d')]->addUnavailability(
+                    new ScheduleSlotView(
+                        $unavailability->getId(),
+                        'Indisponible',
+                        $unavailability->getBegin(),
+                        $unavailability->getEnd(),
+                        true
+                    )
+                );
+            } else {
+                $scheduleViews[$beginDate->format('Y-m-d')] = new ScheduleView(
+                    $beginDate->format('Y-m-d'),
+                    new \DateTime(
+                        $beginDate->format('Y-m-d 00:00:00'),
+                        new \DateTimeZone($participant->getSheet()->getEvent()->getTimeZone())
+                    )
+                );
+
+                $scheduleViews[$beginDate->format('Y-m-d')]->addUnavailability(
+                    new ScheduleSlotView(
+                        $unavailability->getId(),
+                        'Indisponible',
+                        $unavailability->getBegin(),
+                        $unavailability->getEnd(),
+                        true
+                    )
+                );
+            }
         }
 
-        $blockingHappeningParticipations = $this->happeningParticipationRepository->findBlockingByScheduleAndParticipant($schedule, $participant);
-
-        foreach ($blockingHappeningParticipations as $blockingHappeningParticipation) {
-            $slots[] = new ScheduleSlotView(
-                $blockingHappeningParticipation->getId(),
-                $blockingHappeningParticipation->getHappening()->getTitle(),
-                $blockingHappeningParticipation->getHappening()->getBegin(),
-                $blockingHappeningParticipation->getHappening()->getEnd(),
-                false
-            );
+        foreach ($scheduleViews as $scheduleView) {
+            $this->sort($scheduleView->unavailabilities);
         }
 
-        $this->sort($slots);
-
-        return $slots;
+        return $scheduleViews;
     }
 
     /**
@@ -234,16 +334,17 @@ class ScheduleBuilder
     }
 
     /**
-     * @param Sheet $sheet
+     * @param Sheet  $sheet
+     * @param string $locale
      *
      * @return array
      */
-    public function buildForSheet(Sheet $sheet)
+    public function buildForSheet(Sheet $sheet, $locale)
     {
         $participantSchedules = [];
 
         foreach ($sheet->getParticipants() as $participant) {
-            $participantSchedules[] = $this->buildForParticipant($participant);
+            $participantSchedules[] = $this->buildForParticipant($participant, $locale);
         }
 
         return $participantSchedules;
