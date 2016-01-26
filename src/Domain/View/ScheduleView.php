30695c24c5f5ca8545467e391fd43a900ee7951d
Nico <nicolas.dievart@elao.com>
2016-01-26 17:54:38 +0100

    204 - update scheduleview

--- src/Domain/View/ScheduleView.php
+++ src/Domain/View/ScheduleView.php
@@ -25,19 +25,55 @@ class ScheduleView
     /**
      * @var ScheduleSlotView[]
      */
-    public $slots;
+    public $meetings;
+
+    /**
+     * @var ScheduleSlotView[]
+     */
+    public $happenings;
+
+    /**
+     * @var ScheduleSlotView[]
+     */
+    public $unavailabilities;
 
     /**
      * ScheduleView constructor.
      *
      * @param int       $id
-     * @param \DateTime $date
-     * @param array     $slots
+     * @param \DateTimeInterface $date
      */
-    public function __construct($id, \DateTime $date, array $slots)
+    public function __construct($id, \DateTimeInterface $date)
     {
-        $this->id    = $id;
-        $this->date  = $date;
-        $this->slots = $slots;
+        $this->id               = $id;
+        $this->date             = $date;
+        $this->meetings         = [];
+        $this->happenings       = [];
+        $this->unavailabilities = [];
+    }
+
+    /**
+     * @param int              $id
+     * @param ScheduleSlotView $scheduleSlotView
+     */
+    public function addMeeting($id, ScheduleSlotView $scheduleSlotView)
+    {
+        $this->meetings[$id] = $scheduleSlotView;
+    }
+
+    /**
+     * @param ScheduleSlotView $scheduleSlotView
+     */
+    public function addHappening(ScheduleSlotView $scheduleSlotView)
+    {
+        $this->happenings[] = $scheduleSlotView;
+    }
+
+    /**
+     * @param ScheduleSlotView $scheduleSlotView
+     */
+    public function addUnavailability(ScheduleSlotView $scheduleSlotView)
+    {
+        $this->unavailabilities[] = $scheduleSlotView;
     }
 }
