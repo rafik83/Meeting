cde7ac5bb49bb464f2db5a79bbc22c3a8a48cc84
Richard Hanna <richard.hanna@elao.com>
2016-01-27 20:30:43 +0100

    110 - add product to cart unit tests

--- tests/Application/Command/Order/EditProductHandlerTest.php
+++ tests/Application/Command/Order/EditProductHandlerTest.php
@@ -45,7 +45,7 @@ class EditProductHandlerTest extends \PHPUnit_Framework_TestCase
                             'min' => 0,
                             'max' => 4
                         ],
-                        'updatableUntil' => new \DateTime("+1 week"),
+                        'updatableUntil' => (new \DateTime("+1 week"))->format('Y-m-d H:i:s'),
                     ]
                 ]
             ]
@@ -105,18 +105,97 @@ class EditProductHandlerTest extends \PHPUnit_Framework_TestCase
             PaymentMode::NOPAYMENT
         );
 
+        // we have 2 quantities
         $editProduct = new EditProduct($sheet, $cart, $product, $createdAt, 'fr', 2);
+        // we select 1, we expect to have a negative order with -1 (2 - 3)
         $editProduct->productItem['quantity'] = 1;
 
         $orderRepository->add($expectedOrder)->shouldBeCalled();
-
-        $orderManager = new OrderManager();
 
         $handler = new EditProductHandler(
             $cartRepository->reveal(),
             $orderRepository->reveal(),
             $sheetRepository->reveal(),
-            $orderManager
+            new OrderManager()
+        );
+        $handler->handle($editProduct);
+    }
+
+    public function testAddUpdatedProductToCart()
+    {
+        $cartRepository  = $this->prophesize(CartRepositoryInterface::class);
+        $orderRepository = $this->prophesize(OrderRepositoryInterface::class);
+        $sheetRepository = $this->prophesize(SheetRepositoryInterface::class);
+
+        $packageTemplate = [
+            'group1' => [
+                'label' => 'group1-label',
+                'template' => [
+                    'row1' => [
+                        'label' => ['fr' => 'row1-label'],
+                        'description' => ['fr' => 'row1-description'],
+                        'type' => 'lib_option',
+                        'unitPrice' => 100,
+                        'quantity' => [
+                            'min' => 0,
+                            'max' => 4
+                        ],
+                        'updatableUntil' => (new \DateTime("+1 week"))->format('Y-m-d H:i:s'),
+                    ]
+                ]
+            ]
+        ];
+
+        $event = new Event();
+        $event->setBillingTemplate([]);
+        $type = new Type($event);
+        $type->setPackageTemplate($packageTemplate);
+
+        $sheet = new Sheet($event, $type, [], [
+            'group1' => [
+                'row1' => [
+                    'value'    => true,
+                    'quantity' => 2,
+                ]
+            ]
+        ]);
+        $sheet->setBillingData([]);
+        $sheet->setPackageData([]);
+
+        $createdAt = new \DateTime();
+        $cart      = new Cart([], $packageTemplate, $sheet, $createdAt);
+
+        $productBuilder = new ProductBuilder();
+        $template = $productBuilder->createFromType($type);
+        $step = $template->getStep('group1');
+        $product = $step->getProduct('row1');
+
+        $expectedCart =  new Cart(
+            [
+                'group1' => [
+                    'row1' => [
+                        'value' => true,
+                        'quantity' => 1,
+                    ]
+                ]
+            ],
+            $packageTemplate,
+            $sheet,
+            $createdAt
+        );
+
+        // we have 2 quantities
+        $editProduct = new EditProduct($sheet, $cart, $product, $createdAt, 'fr', 2);
+        // we select 3, we expect to have 1 (3 - 2) in cart
+        $editProduct->productItem['quantity'] = 3;
+
+        $cartRepository->set($expectedCart)->shouldBeCalled();
+
+        $handler = new EditProductHandler(
+            $cartRepository->reveal(),
+            $orderRepository->reveal(),
+            $sheetRepository->reveal(),
+            new OrderManager()
         );
         $handler->handle($editProduct);
     }
