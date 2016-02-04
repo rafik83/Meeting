d30909dedf40bfa7e7ffd2543625e5589c378deb
Maxime COLIN <contact@maximecolin.fr>
2016-02-04 13:43:27 +0100

    Speaker model

---
+++ src/Domain/Model/Happening/Speaker.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model\Happening;
+
+class Speaker
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $function;
+
+    /**
+     * @var string
+     */
+    private $organization;
+
+    /**
+     * @var string
+     */
+    private $logo;
+
+    /**
+     * @var string
+     */
+    private $photo;
+
+    /**
+     * Speaker constructor.
+     *
+     * @param string $name
+     * @param string $function
+     * @param string $organization
+     * @param string $logo
+     * @param string $photo
+     */
+    public function __construct($name, $function, $organization, $logo, $photo)
+    {
+        $this->name         = $name;
+        $this->function     = $function;
+        $this->organization = $organization;
+        $this->logo         = $logo;
+        $this->photo        = $photo;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Get function
+     *
+     * @return string
+     */
+    public function getFunction()
+    {
+        return $this->function;
+    }
+
+    /**
+     * Get organization
+     *
+     * @return string
+     */
+    public function getOrganization()
+    {
+        return $this->organization;
+    }
+
+    /**
+     * Get logo
+     *
+     * @return string
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Get photo
+     *
+     * @return string
+     */
+    public function getPhoto()
+    {
+        return $this->photo;
+    }
+}
