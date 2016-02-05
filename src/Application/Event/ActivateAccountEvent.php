1a69d2ce0bbaaea828bbef5d48f6087041b3061c
Nico <nicolas.dievart@elao.com>
2016-02-05 17:11:22 +0100

    159 - Remove useless use

--- src/Application/Event/ActivateAccountEvent.php
+++ src/Application/Event/ActivateAccountEvent.php
@@ -13,7 +13,6 @@ namespace Proximum\Vimeet\Application\Event;
 use Proximum\Vimeet\Domain\Model\ActivateAccountToken;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Model\Event as ProximumEvent;
-use Proximum\Vimeet\Domain\View\EventView;
 use Symfony\Component\EventDispatcher\Event;
 
 class ActivateAccountEvent extends Event
