34d3e8e3ed2be0155bdc6374f53309f56fc462da
Nico <nicolas.dievart@elao.com>
2016-02-09 11:04:47 +0100

    195 - Add components Mail"

---
+++ src/Application/Components/Mail/Mail.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Components\Mail;
+
+class Mail
+{
+    /**
+     * @var string
+     */
+    private $sender;
+
+    /**
+     * @var string
+     */
+    private $receiver;
+
+    /**
+     * @var string
+     */
+    private $template;
+
+    /**
+     * @var string
+     */
+    private $messageId;
+
+    /**
+     * @param string $sender
+     * @param string $receiver
+     * @param string $template
+     * @param string $messageId
+     */
+    public function __construct($sender, $receiver, $template, $messageId)
+    {
+        $this->sender    = $sender;
+        $this->receiver  = $receiver;
+        $this->template  = $template;
+        $this->messageId = $messageId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSender()
+    {
+        return $this->sender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getReceiver()
+    {
+        return $this->receiver;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessageId()
+    {
+        return $this->messageId;
+    }
+}
