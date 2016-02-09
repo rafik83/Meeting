097c671c044986266dc242956137a2bbec96e9d0
Nico <nicolas.dievart@elao.com>
2016-02-09 11:28:06 +0100

    195 -Add resetPasswordMail

---
+++ src/Bundle/AppBundle/Mail/ResetPasswordMail.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Bundle\AppBundle\Mail;
+
+use Proximum\Vimeet\Application\Components\Mail\Mail;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class ResetPasswordMail extends Mail
+{
+    /**
+     * @var Event
+     */
+    private $event;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @param string $sender
+     * @param string $receiver
+     * @param string $template
+     * @param string $messageId
+     * @param Event  $event
+     * @param string $token
+     */
+    public function __construct($sender, $receiver, $template, $messageId, Event $event, $token)
+    {
+        parent::__construct($sender, $receiver, $template, $messageId);
+        $this->event = $event;
+        $this->token = $token;
+    }
+
+    /**
+     * @return Event
+     */
+    public function getEvent()
+    {
+        return $this->event;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+}
