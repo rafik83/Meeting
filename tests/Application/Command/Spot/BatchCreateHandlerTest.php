ba915f20bebcb0072df7391927c3c267fc4bc26b
Maxime COLIN <maxime.colin@elao.com>
2016-02-11 16:06:18 +0100

    Fix test

--- tests/Application/Command/Spot/BatchCreateHandlerTest.php
+++ tests/Application/Command/Spot/BatchCreateHandlerTest.php
@@ -10,48 +10,49 @@
 
 namespace Tests\Application\Command\Spot;
 
+use Prophecy\Argument;
 use Proximum\Vimeet\Application\Command\Spot\BatchCreate;
 use Proximum\Vimeet\Application\Command\Spot\BatchCreateHandler;
 use Proximum\Vimeet\Application\Command\Spot\Create;
+use Proximum\Vimeet\Application\Command\Spot\CreateHandler;
 use Proximum\Vimeet\Application\Components\Spot\Recipe;
 use Proximum\Vimeet\Application\Components\Spot\ReferenceFactory;
 use Proximum\Vimeet\Domain\Model\Event;
-use Proximum\Vimeet\Domain\Model\Spot;
-use Proximum\Vimeet\Domain\Repository\SpotRepositoryInterface;
 
 class BatchCreateHandlerTest extends \PHPUnit_Framework_TestCase
 {
     public function testHandle()
     {
         //Context
-        $event            = new Event();
-        $recipe           = new Recipe('A', 1, 5);
-        $referenceFactory = new ReferenceFactory();
-        $spot1            = new Spot('A1', $event, 3, 1, 2, true);
-        $spot2            = new Spot('A2', $event, 3, 1, 2, true);
-        $spot3            = new Spot('A3', $event, 3, 1, 2, true);
-        $spot4            = new Spot('A4', $event, 3, 1, 2, true);
-        $spot5            = new Spot('A5', $event, 3, 1, 2, true);
-        $recipes          = $referenceFactory->createFromRecipe($recipe);
+        $event   = new Event();
+        $recipes = [new Recipe('A', 1, 3), new Recipe('B', 1, 3)];
 
         //Command
-        $command = new BatchCreate($event);
-        $command->recipes = $recipes;
-        $command->size = 3;
-        $command->active = true;
-        $command->seatCapacity = 2;
+        $command                  = new BatchCreate($event);
+        $command->recipes         = $recipes;
+        $command->size            = 3;
+        $command->active          = true;
+        $command->seatCapacity    = 2;
         $command->meetingCapacity = 1;
 
         //Mock
-        $spotRepository = $this->prophesize(SpotRepositoryInterface::class);
-        $spotRepository->add($spot1);
-        $spotRepository->add($spot2);
-        $spotRepository->add($spot3);
-        $spotRepository->add($spot4);
-        $spotRepository->add($spot5);
+        $referenceFactory = $this->prophesize(ReferenceFactory::class);
+        $referenceFactory->createFromRecipes($recipes)->shouldBeCalled()->willReturn([
+            'A1', 'A2', 'A3', 'B1', 'B2', 'B3'
+        ]);
+
+        $createHandler = $this->prophesize(CreateHandler::class);
+        $createHandler->handle(Argument::that(function (Create $create) {
+            return
+                in_array($create->reference, ['A1', 'A2', 'A3', 'B1', 'B2', 'B3']) &&
+                $create->size === 3 &&
+                $create->meetingCapacity === 1 &&
+                $create->seatCapacity === 2 &&
+                $create->active === true;
+        }))->shouldBeCalled();
 
         //Handler
-        $handle = new BatchCreateHandler();
+        $handle = new BatchCreateHandler($createHandler->reveal(), $referenceFactory->reveal());
         $handle->handle($command);
     }
 }
