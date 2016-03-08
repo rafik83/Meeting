e3edff03df628b8c11ae5813f51927c3429ca125
Nico <nicolas.dievart@elao.com>
2016-03-08 14:31:08 +0100

    247 - Add filter on admin list

--- src/Bundle/AppBundle/Controller/Admin/AdminController.php
+++ src/Bundle/AppBundle/Controller/Admin/AdminController.php
@@ -13,12 +13,27 @@ namespace Proximum\Vimeet\Bundle\AppBundle\Controller\Admin;
 use Proximum\Vimeet\Application\Command\Admin\Create;
 use Proximum\Vimeet\Application\Exception\User\EmailAlreadyExistsException;
 use Proximum\Vimeet\Bundle\AppBundle\Form\Type\Admin\CreateType;
+use Proximum\Vimeet\Bundle\AppBundle\Form\Type\Admin\FilterAdminType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormInterface;
 
 class AdminController extends Controller
 {
+    /**
+     * @param string $type
+     * @param string $data
+     * @param array  $options
+     *
+     * @return Form|FormInterface
+     */
+    private function createFilterForm($type, $data, array $options = [])
+    {
+        return $this->get('form.factory')->createNamed('', $type, $data, $options);
+    }
+
     /**
      * @param Request $request
      *
@@ -26,10 +41,26 @@ class AdminController extends Controller
      */
     public function listAction(Request $request)
     {
-        $admins = $this->get('repository.admin_repository')->listPaginated($request->query->get('page', 1), 20);
+        $filters = [];
+        $filtered   = false;
+        $filterForm = $this->createFilterForm(
+            FilterAdminType::class,
+            [
+                'role'  => $request->query->get('role'),
+                'event' => $request->query->get('event'),
+            ]
+        );
+
+        if ($filterForm->handleRequest($request)->isSubmitted() && $filterForm->isValid()) {
+            $filters   = $filterForm->getData();
+            $filtered = true;
+        }
+        $admins = $this->get('repository.admin_repository')->listPaginated($request->query->get('page', 1), 20, $filters);
 
         return $this->render('VimeetAppBundle:Admin/Admin:list.html.twig', [
-            'admins' => $admins
+            'admins'      => $admins,
+            'filter_form' => $filterForm->createView(),
+            'filtered'    => $filtered
         ]);
     }
 
