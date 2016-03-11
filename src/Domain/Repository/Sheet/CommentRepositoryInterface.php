cb2d4c191bde2c00a60897ae4b2661fc5183b80d
Nico <nicolas.dievart@elao.com>
2016-03-11 16:53:25 +0100

    311 - Add Sheet CommentRepository and Interface

---
+++ src/Domain/Repository/Sheet/CommentRepositoryInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository\Sheet;
+
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\Sheet\Comment;
+
+interface CommentRepositoryInterface
+{
+    /**
+     * @param Comment $comment
+     */
+    public function add(Comment $comment);
+
+    /**
+     * @param Sheet $sheet
+     *
+     * @return Comment[]
+     */
+    public function getCommentsBySheet(Sheet $sheet);
+}
