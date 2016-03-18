1c8f45e60a2768015dc305e745e4d59033817a0f
Nico <nicolas.dievart@elao.com>
2016-03-18 13:45:15 +0100

    265 - Add unit test

---
+++ tests/Application/Command/Admin/ChangePasswordHandlerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Tests\Application\Command\Admin;
+
+use Proximum\Vimeet\Application\Adapter\PasswordEncoderInterface;
+use Proximum\Vimeet\Application\Adapter\SaltGeneratorInterface;
+use Proximum\Vimeet\Application\Command\Admin\ChangePassword;
+use Proximum\Vimeet\Application\Command\Admin\ChangePasswordHandler;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Repository\AdminRepositoryInterface;
+
+class ChangePasswordHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $dateTime =  new \DateTime();
+        $admin = new Admin('test@test.com', '__salt__', 'encoded_password', 'fr', 'truc', 'muche', $dateTime);
+
+        // Command
+        $command = new ChangePassword($admin);
+        $command->plainPassword = 'new-password';
+
+        $expectedUser = new Admin(
+            'test@test.com',
+            '__new_salt__',
+            'encoded_new_password',
+            'fr',
+            'truc',
+            'muche',
+            $dateTime
+        );
+
+        $saltGenerator = $this->prophesize(SaltGeneratorInterface::class);
+        $saltGenerator->generate()->shouldBeCalled()->willReturn('__new_salt__');
+
+        $passwordEncoder = $this->prophesize(PasswordEncoderInterface::class);
+        $passwordEncoder
+            ->encode($admin, $command->plainPassword)
+            ->shouldBeCalled()
+            ->willReturn('encoded_new_password');
+
+        $adminRepository = $this->prophesize(AdminRepositoryInterface::class);
+        $adminRepository->set($expectedUser)->shouldBeCalled();
+
+        $handler = new ChangePasswordHandler(
+            $adminRepository->reveal(),
+            $passwordEncoder->reveal(),
+            $saltGenerator->reveal()
+        );
+        $handler->handle($command);
+    }
+}
