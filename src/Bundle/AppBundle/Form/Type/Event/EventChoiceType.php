d0f27d5cb43e4a92d3db7df588e6cee99a98c2f6
Nico <nicolas.dievart@elao.com>
2016-03-18 11:10:01 +0100

    343 - Update EventEntityType to EventChoiceType

---
+++ src/Bundle/AppBundle/Form/Type/Event/EventChoiceType.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Bundle\AppBundle\Form\Type\Event;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventChoiceType extends AbstractType
+{
+    /**
+     * @var EventRepositoryInterface
+     */
+    private $eventRepository;
+
+    /**
+     * @param EventRepositoryInterface $eventRepository
+     */
+    public function __construct(EventRepositoryInterface $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'class'        => Event::class,
+            'choice_label' => 'title',
+            'repositoryMethod' => function (EventRepositoryInterface $eventRepository) {
+                return $eventRepository->getAll();
+            },
+            'choices' => function (Options $options) {
+                return $options['repositoryMethod']($this->eventRepository);
+            },
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+}
