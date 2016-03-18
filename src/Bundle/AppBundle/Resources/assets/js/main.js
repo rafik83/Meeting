04e61f5101fa92d1db1c60f21e1186066bef6e20
Richard Hanna <richard.hanna@elao.com>
2016-03-18 13:54:46 +0100

    List sheets UI

--- src/Bundle/AppBundle/Resources/assets/js/main.js
+++ src/Bundle/AppBundle/Resources/assets/js/main.js
@@ -11,18 +11,18 @@ $(document).ready(function(){
     [].forEach.call(document.querySelectorAll('[data-update]'), function (element) { new Update(element); });
     // [].forEach.call(document.querySelectorAll('[data-check-all]'), function (element) { new CheckAll(element); });
 
-    $('.selected-row').on('change', function() {
+    $('tbody input[type="checkbox"]').on('change', function() {
         $(this).parents('tr').toggleClass('warning');
         showOrHideBatchActions();
     });
 
     // select a row by clicking wherever on row
     $('table.selectable-row tr').on('click', function(e) {
-        if (!$(e.target).is("td")) {
+        if (!$(e.target).is("td") && !$(e.target).is("small")) {
             return;
         }
 
-        var selectedRow = $(this).find('.selected-row');
+        var selectedRow = $(this).find('input[type="checkbox"]');
         if (selectedRow) {
             selectedRow.trigger('click');
         }
@@ -46,16 +46,16 @@ $(document).ready(function(){
 
     function showOrHideBatchActions()
     {
-        var rowSelectedCount = $('.selected-row:checked').length;
+        var rowSelectedCount = $('tbody input[type="checkbox"]:checked').length;
 
         if (rowSelectedCount) {
             $('.new-action').hide();
             $('.batch-actions').show();
 
             if (rowSelectedCount > 1) {
-                var batch_action_label = $('[data-batch-action-plural-label]').val().replace('%count%', rowSelectedCount);
+                var batch_action_label = $('[data-batch-action-plural-label]').data('batchActionPluralLabel').replace('%count%', rowSelectedCount);
             } else {
-                var batch_action_label = $('[data-batch-action-single-label]').val();
+                var batch_action_label = $('[data-batch-action-single-label]').data('batchActionSingleLabel');
             }
 
             $('.batch-actions-label').text(batch_action_label);
