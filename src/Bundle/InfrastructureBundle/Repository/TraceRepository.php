f12b54af7dc11778a6f38ab99f6cc193f34a4136
Nico <nicolas.dievart@elao.com>
2016-03-21 14:40:16 +0100

    324 - Add TraceRepository

---
+++ src/Bundle/InfrastructureBundle/Repository/TraceRepository.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Bundle\InfrastructureBundle\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Model\Trace;
+use Proximum\Vimeet\Domain\Model\TraceableInterface;
+use Proximum\Vimeet\Domain\Repository\TraceRepositoryInterface;
+
+class TraceRepository implements TraceRepositoryInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * SpotRepository constructor.
+     *
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager  = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(Trace $trace)
+    {
+        $this->entityManager->persist($trace);
+        $this->entityManager->flush($trace);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLastAcceptBySheet(TraceableInterface $sheet)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('trace')
+            ->from(Trace::class, 'trace', 'trace.id')
+            ->where('trace.object = :object')
+            ->setParameter('object', sprintf('%s%s', $sheet->getTraceableName(), $sheet->getId()))
+            ->andWhere('trace.action = :action')
+            ->setParameter('action', 'accept')
+            ->orderBy('trace.date', 'DESC')
+            ->setMaxResults(1);
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAllTracesByObject(TraceableInterface $traceable)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('trace')
+            ->from(Trace::class, 'trace', 'trace.id')
+            ->where('trace.object = :object')
+            ->setParameter('object', sprintf('%s%s', $traceable->getTraceableName(), $traceable->getId()));
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
