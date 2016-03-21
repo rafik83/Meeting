bc563bd39e93b0f2013e9426481bb834a991c88e
Nico <nicolas.dievart@elao.com>
2016-03-21 14:42:18 +0100

    324 - Add TraceableInterface

---
+++ src/Domain/Model/TraceableInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model;
+
+interface TraceableInterface
+{
+    /**
+     * @return int
+     */
+    public function getId();
+
+    /**
+     * @return string
+     */
+    public function getTraceableName();
+}
