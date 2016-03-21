9194cfee58b79d1bf044f6790d2c4f1d3b7b17d4
Richard Hanna <richard.hanna@elao.com>
2016-03-21 09:59:19 +0100

    Remove checkall functions

--- src/Bundle/AppBundle/Resources/assets/js/main.js
+++ src/Bundle/AppBundle/Resources/assets/js/main.js
@@ -1,6 +1,5 @@
 var $        = require('jquery'),
     Confirm  = require('./components/_Confirm'),
-    // CheckAll = require('./components/_CheckAll'),
     Update   = require('./components/_Update');
 
 require('elao-form.js');
@@ -9,7 +8,6 @@ $(document).ready(function(){
     $('[data-collection]').collection();
     $('[data-confirm]').each(function (key, element) { new Confirm(element); });
     [].forEach.call(document.querySelectorAll('[data-update]'), function (element) { new Update(element); });
-    // [].forEach.call(document.querySelectorAll('[data-check-all]'), function (element) { new CheckAll(element); });
 
     $('tbody input[type="checkbox"]').on('change', function() {
         $(this).parents('tr').toggleClass('warning');
