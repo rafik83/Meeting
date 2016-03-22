a8abf4c7e64254f23251a96072e9f1cb527a5357
Nico <nicolas.dievart@elao.com>
2016-03-22 13:55:52 +0100

    323 - Add SheetValidator component

---
+++ src/Application/Components/Sheet/SheetValidator.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Components\Sheet;
+
+use Proximum\Vimeet\Application\Components\Sheet\Validator\CriteriaValidatorInterface;
+use Proximum\Vimeet\Application\Components\Sheet\Validator\SheetAcceptedCriteriaValidator;
+use Proximum\Vimeet\Application\Exception\Sheet\NotValidException;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
+
+class SheetValidator
+{
+    /**
+     * @var SheetRepositoryInterface
+     */
+    private $sheetRepository;
+
+    /**
+     * @var CriteriaValidatorInterface[]
+     */
+    private $validators;
+
+    /**
+     * @param SheetRepositoryInterface $sheetRepository
+     */
+    public function __construct(SheetRepositoryInterface $sheetRepository)
+    {
+        $this->sheetRepository = $sheetRepository;
+        $this->validators      = [
+            new SheetAcceptedCriteriaValidator()
+        ];
+    }
+
+    /**
+     * @param Sheet $sheet
+     */
+    public function validate(Sheet $sheet)
+    {
+        try {
+            $yesCount = 0;
+
+            foreach ($this->validators as $validator) {
+                $result = $validator->isValid($sheet);
+
+                if ($result === CriteriaValidatorInterface::NO) {
+                    throw new NotValidException();
+                } elseif ($result === CriteriaValidatorInterface::YES) {
+                    $yesCount++;
+                }
+            }
+
+            if ($yesCount > 0) {
+                $sheet->markAsValidated();
+                $this->sheetRepository->set($sheet);
+            }
+        } catch (NotValidException $e) {
+            return;
+        }
+
+        return;
+    }
+}
