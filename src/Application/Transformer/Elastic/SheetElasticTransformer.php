c82e640266c9419159f324f476a53a09dc59b51f
Nico <nicolas.dievart@elao.com>
2016-03-29 11:11:16 +0200

    344 - Add SheetElasticTransformer and adapt it to fit the paginate sheet

--- src/Application/Transformer/Elastic/SheetElasticTransformer.php
+++ src/Application/Transformer/Elastic/SheetElasticTransformer.php
@@ -41,23 +41,35 @@ class SheetElasticTransformer implements ModelToElasticaTransformerInterface
         $id = $sheet->getId();
 
         if ($sheet instanceof Sheet) {
-            $sheetName  = $this->sheetInfoGuesser->guessSheetInfo($sheet);
-            $state      = $sheet->getState();
-            $type       = $sheet->getType()->getId();
-            $categories = array_map(function (Category $category) { return [$category->getId() => $category->getId()]; }, $sheet->getType()->getCategories()->toArray());
-            $followUp   = $sheet->getFollower() instanceof Admin ? $sheet->getFollower()->getId() : 0;
+            $sheetName         = $this->sheetInfoGuesser->guessSheetInfo($sheet);
+            $state             = $sheet->getState();
+            $type              = $sheet->getType()->getId();
+            $categories        = array_map(function (Category $category) { return ['id' => $category->getId()]; }, $sheet->getType()->getCategories()->toArray());
+            $followUp          = $sheet->getFollower() instanceof Admin ? $sheet->getFollower()->getId() : 0;
+            $participantNumber = count($sheet->getParticipants());
+            $event             = $sheet->getEvent()->getId();
+            $createdAt         = $sheet->getCreatedAt()->format('c');
 
+            try {
+                $owner = $sheet->getOwner()->getId();
+            } catch (\RuntimeException $e) {
+                $owner = 0;
+            }
         } else {
             throw new InvalidArgumentTypeException($sheet, 'Sheet');
         }
 
         return new Document($id, [
-            'id'         => $id,
-            'sheetName'  => $sheetName,
-            'state'      => $state,
-            'type'       => $type,
-            'categories' => $categories,
-            'followUp'   => $followUp,
+            'id'                => $id,
+            'sheetName'         => $sheetName,
+            'state'             => $state,
+            'type'              => $type,
+            'categories'        => $categories,
+            'followUp'          => $followUp,
+            'participantNumber' => $participantNumber,
+            'event'             => $event,
+            'owner'             => $owner,
+            'createdAt'         => $createdAt,
         ]);
     }
 }
