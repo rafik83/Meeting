7bf5253417e6dc2a4e5ecad05fc26c73cc88bc84
Maxime COLIN <maxime.colin@elao.com>
2016-03-31 17:37:01 +0200

    Folder refactorization

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/EventListener/MailEventSubscriber.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\EventListener;
+
+use Proximum\Vimeet\Application\Adapter\MailerInterface;
+use Proximum\Vimeet\Application\Event\Events;
+use Proximum\Vimeet\Application\Event\Sheet\SheetValidatedEvent;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Mail\SheetValidatedMail;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MailEventSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var MailerInterface
+     */
+    private $mailer;
+
+    /**
+     * @var string
+     */
+    private $sender;
+
+    /**
+     * MailEventSubscriber constructor.
+     *
+     * @param MailerInterface $mailer
+     * @param string          $sender
+     */
+    public function __construct(MailerInterface $mailer, $sender)
+    {
+        $this->mailer = $mailer;
+        $this->sender = $sender;
+    }
+
+    /**
+     * @param SheetValidatedEvent $event
+     */
+    public function onSheetValidated(SheetValidatedEvent $event)
+    {
+        $owner = $event->getSheet()->getOwner()->getUser();
+
+        $mail  = new SheetValidatedMail(
+            $event->getSheet(),
+            $this->sender,
+            $owner->getEmail(),
+            'VimeetAppBundle:Mail:Sheet/sheetValidated.html.twig',
+            'sheet_validated',
+            $owner->getLocale()
+        );
+
+        $this->mailer->send($mail);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            Events::SHEET_VALIDATED => 'onSheetValidated',
+        ];
+    }
+}
---
+++ src/Ui/Bundle/EventBundle/EventBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Proximum\Vimeet\Ui\Bundle\EventBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class EventBundle extends Bundle
+{
+}
