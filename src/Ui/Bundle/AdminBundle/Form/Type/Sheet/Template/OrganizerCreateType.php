24c273a22031e87b8285e6daa38b2249ab52ed22
Nico <nicolas.dievart@elao.com>
2016-04-05 14:55:21 +0200

    359 - Add organizer create and duplicate template type

---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/Template/OrganizerCreateType.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Sheet\Template;
+
+use Proximum\Vimeet\Application\Command\Sheet\Template\OrganizerCreate;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Event\EventChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganizerCreateType extends AbstractType
+{
+    /**
+     * @var EventRepositoryInterface
+     */
+    private $eventRepository;
+
+    /**
+     * @param EventRepositoryInterface $eventRepository
+     */
+    public function __construct(EventRepositoryInterface $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('event', EventChoiceType::class, [
+                'required'         => true,
+                'expanded'         => false,
+                'multiple'         => false,
+                'repositoryMethod' => function (EventRepositoryInterface $eventRepository) use ($options) {
+                    return $eventRepository->getEventsByAdmin($options['admin']);
+                },
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['admin']);
+        $resolver->setDefaults([
+            'data_class' => OrganizerCreate::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'organizer_sheet_template_create';
+    }
+}
---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/Template/OrganizerDuplicateType.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Sheet\Template;
+
+use Proximum\Vimeet\Application\Command\Sheet\Template\Duplicate;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Event\EventChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrganizerDuplicateType extends AbstractType
+{
+    /**
+     * @var EventRepositoryInterface
+     */
+    private $eventRepository;
+
+    /**
+     * @param EventRepositoryInterface $eventRepository
+     */
+    public function __construct(EventRepositoryInterface $eventRepository)
+    {
+        $this->eventRepository = $eventRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('event', EventChoiceType::class, [
+                'required'         => true,
+                'expanded'         => false,
+                'multiple'         => false,
+                'repositoryMethod' => function (EventRepositoryInterface $eventRepository) use ($options) {
+                    return $eventRepository->getEventsByAdmin($options['admin']);
+                },
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['admin']);
+        $resolver->setDefaults([
+            'data_class' => Duplicate::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'organizer_sheet_template_duplicate';
+    }
+}
