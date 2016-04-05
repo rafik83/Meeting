d00f605a31edf183f7305d292121ce31d839fc9e
Nico <nicolas.dievart@elao.com>
2016-04-05 14:55:47 +0200

    359 - Add OrganizerCreate command and handler

---
+++ src/Application/Command/Sheet/Template/OrganizerCreate.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Sheet\Template;
+
+use DateTimeInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class OrganizerCreate
+{
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var Event
+     */
+    public $event;
+
+    /**
+     * @var DateTimeInterface
+     */
+    public $createdAt;
+
+    /**
+     * @param DateTimeInterface $createdAt
+     */
+    public function __construct(DateTimeInterface $createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
---
+++ src/Application/Command/Sheet/Template/OrganizerCreateHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Sheet\Template;
+
+use Proximum\Vimeet\Domain\Model\Sheet\Template;
+use Proximum\Vimeet\Domain\Repository\Sheet\TemplateRepositoryInterface;
+
+class OrganizerCreateHandler
+{
+    /**
+     * @var TemplateRepositoryInterface
+     */
+    private $templateRepository;
+
+    /**
+     * CreateHandler constructor.
+     *
+     * @param TemplateRepositoryInterface $templateRepository
+     */
+    public function __construct(TemplateRepositoryInterface $templateRepository)
+    {
+        $this->templateRepository = $templateRepository;
+    }
+
+    /**
+     * @param OrganizerCreate $create
+     *
+     * @return CreateResult
+     */
+    public function handle(OrganizerCreate $create)
+    {
+        $template = new Template($create->title, '', $create->createdAt);
+        $template->setEvent($create->event);
+
+        $this->templateRepository->add($template);
+
+        return new CreateResult($template);
+    }
+}
