958f8fb12224968d0911a712c1e9a7409a74a727
Maxime COLIN <maxime.colin@elao.com>
2016-04-06 10:33:55 +0200

    Add test on Template

---
+++ tests/Domain/Model/Sheet/TemplateTest.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Tests\Domain\Model\Sheet;
+
+use Proximum\Vimeet\Domain\Model\Sheet\Template;
+
+class TemplateTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddLocale()
+    {
+        $template = new Template('My template', [
+            'ec74be5e' => [
+                'component' => 'object',
+                'type'      => 'text',
+                'config'    => [
+                    'content' => ['fr' => 'Lorem ipsum']
+                ],
+            ],
+            '211b2168' => [
+                'component' => 'block',
+                'type'      => '8-4',
+                'config'    => [
+                    [
+                        '0aea62b2' => [
+                            'component' => 'object',
+                            'type'      => 'editable-text',
+                            'config'    => [
+                                'label'       => ['fr' => 'Titre'],
+                                'placeholder' => ['fr' => 'Le titre'],
+                                'help'        => ['fr' => 'Ici le titre'],
+                                'length'      => 100,
+                                'required'    => true,
+                            ]
+                        ]
+                    ],
+                    [
+
+                    ]
+                ]
+            ],
+        ], ['fr']);
+
+        $expected = new Template('My template', [
+            'ec74be5e' => [
+                'component' => 'object',
+                'type'      => 'text',
+                'config'    => [
+                    'content' => ['en' => null, 'fr' => 'Lorem ipsum'],
+                ],
+            ],
+            '211b2168' => [
+                'component' => 'block',
+                'type'      => '8-4',
+                'config'    => [
+                    [
+                        '0aea62b2' => [
+                            'component' => 'object',
+                            'type'      => 'editable-text',
+                            'config'    => [
+                                'label'       => ['en' => null, 'fr' => 'Titre'],
+                                'placeholder' => ['en' => null, 'fr' => 'Le titre'],
+                                'help'        => ['en' => null, 'fr' => 'Ici le titre'],
+                                'length'      => 100,
+                                'required'    => true,
+                            ]
+                        ]
+                    ],
+                    [
+
+                    ]
+                ]
+            ],
+        ], ['fr', 'en']);
+
+        $this->assertEquals($expected, $template->addLocale('en'));
+    }
+
+    public function testGetFirstLocale()
+    {
+        $template = new Template('My template', [], ['fr', 'en']);
+
+        $this->assertEquals('fr', $template->getFirstLocale());
+        $this->assertEquals('fr', $template->addLocale('de')->getFirstLocale());
+    }
+}
