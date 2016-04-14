becd37528634f75fd9a6958d93b62f0a172bdb12
Nico <nicolas.dievart@elao.com>
2016-04-14 17:21:33 +0200

    401 - Fix registrationTemplateController

--- src/Ui/Bundle/AdminBundle/Controller/RegistrationTemplateController.php
+++ src/Ui/Bundle/AdminBundle/Controller/RegistrationTemplateController.php
@@ -11,23 +11,15 @@
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationTemplateController extends Controller
 {
     /**
-     * @param Request $request
-     *
      * @return Response
      */
-    public function listAction(Request $request)
+    public function listAction()
     {
-        $templates = $this->get('repository.template.registration_template_repository')->getBaseTemplate();
-
-        return $this->render('AdminBundle:Template:list.html.twig', [
-            'templates' => $templates,
-            'form'      => $form->createView(),
-        ]);
+        return $this->render('AdminBundle:Template:list.html.twig', []);
     }
 }
