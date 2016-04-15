b0524daeeb349f3c26df83920aacb18714543fd5
Nico <nicolas.dievart@elao.com>
2016-04-15 14:44:11 +0200

    401 - Add RegistrationTemplateRepository queries

---
+++ src/Domain/Repository/Template/RegistrationTemplateRepositoryInterface.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository\Template;
+
+use Proximum\Vimeet\Domain\Model\Template\RegistrationTemplate;
+
+interface RegistrationTemplateRepositoryInterface
+{
+    /**
+     * @return RegistrationTemplate[]
+     */
+    public function getBaseTemplates();
+
+    /**
+     * @return RegistrationTemplate[]
+     */
+    public function getAllOrganizersTemplates();
+
+    /**
+     * @param array $events
+     *
+     * @return RegistrationTemplate[]
+     */
+    public function getTemplateForGivenEvents(array $events);
+
+    /**
+     * @param RegistrationTemplate $registrationTemplate
+     * @return mixed
+     */
+    public function set(RegistrationTemplate $registrationTemplate);
+}
