6e5066cdbc740759e14d4c9bb5cb8fa05c774f6a
Richard Hanna <richard.hanna@elao.com>
2016-04-20 14:55:03 +0200

    312 - Add type description js

--- src/Ui/Bundle/EventBundle/Resources/assets/js/main.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/main.js
@@ -1,6 +1,7 @@
-var $         = require('jquery'),
-    bootstrap = require('bootstrap'),
-    Confirm   = require('./components/_Confirm');
+var $               = require('jquery'),
+    bootstrap       = require('bootstrap'),
+    Confirm         = require('./components/_Confirm'),
+    TypeDescription = require('./components/_TypeDescription');
 
 require('elao-form.js');
 
@@ -8,4 +9,5 @@ $(document).ready(function(){
     $('[data-collection]').collection();
     $('[data-toggle="tooltip"]').tooltip();
     $('[data-confirm]').each(function (key, element) { new Confirm(element); });
+    $('input[type=radio][data-description]').each(function (key, element) { new TypeDescription(element); });
 });
