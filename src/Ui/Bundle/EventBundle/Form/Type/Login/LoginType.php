f661e986f2e7e1f78c783d645fa6d3348c9273a7
Richard Hanna <richard.hanna@elao.com>
2016-04-22 15:07:58 +0200

    313 - Remember me login

--- src/Ui/Bundle/EventBundle/Form/Type/Login/LoginType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Login/LoginType.php
@@ -11,6 +11,7 @@
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Login;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,7 +26,10 @@ class LoginType extends AbstractType
         $builder
             ->add('username', HiddenType::class)
             ->add('password', PasswordType::class, [
-                'placeholder' => 'form.login.password.placeholder'
+                'placeholder' => 'form.login.children.password.placeholder'
+            ])
+            ->add('rememberMe', CheckboxType::class, [
+                'required' => false,
             ])
         ;
     }
