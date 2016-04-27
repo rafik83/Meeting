1f411388e3dab72e6fc492f7f362c3bed95ca51c
Nico <nicolas.dievart@elao.com>
2016-04-27 10:30:17 +0200

    Add image path to build sass

--- src/Application/Components/Guideline/Generator.php
+++ src/Application/Components/Guideline/Generator.php
@@ -38,6 +38,8 @@ class Generator
      */
     private $fontPath;
 
+    private $imagePath;
+
     /**
      * @var string
      */
@@ -49,19 +51,22 @@ class Generator
      * @param string            $webAssetsPath
      * @param string            $bundleGuidelinePath
      * @param string            $fontPath
+     * @param string            $imagePath
      */
     public function __construct(
         \Twig_Environment $twig,
         $rootPath,
         $webAssetsPath,
         $bundleGuidelinePath,
-        $fontPath
+        $fontPath,
+        $imagePath
     ) {
         $this->twig                = $twig;
         $this->rootPath            = $rootPath;
         $this->webAssetsPath       = $webAssetsPath;
         $this->bundleGuidelinePath = $bundleGuidelinePath;
         $this->fontPath            = $fontPath;
+        $this->imagePath           = $imagePath;
     }
 
     /**
@@ -90,6 +95,7 @@ class Generator
             'colorHighLighted'    => $colorHighlighted,
             'bundleGuidelinePath' => $this->bundleGuidelinePath,
             'fontPath'            => $this->fontPath,
+            'imagePath'           => $this->imagePath
         ]);
 
         $varsFileName = 'vars-' . sha1(uniqid()) . '.scss';
