cbbfb358ee6e2b6b676c0ba6766cddf37613f60d
Nico <nicolas.dievart@elao.com>
2016-05-10 17:10:25 +0200

    Add CustomValidator for block

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Validator/Constraint/Template/BlockConstraint.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Template;
+
+use Symfony\Component\Validator\Constraint;
+
+class BlockConstraint extends Constraint
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validatedBy()
+    {
+        return BlockValidator::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTargets()
+    {
+        return self::CLASS_CONSTRAINT;
+    }
+}
---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Validator/Constraint/Template/Object/UrlValidator.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Template\Object;
+
+use Proximum\Vimeet\Domain\Template\Object;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Template\ObjectValidator;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\Url as CoreUrl;
+
+class UrlValidator extends ObjectValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        parent::validate($value, $constraint);
+
+        if ($value instanceof Object\Url) {
+            if (null !== $value->getData()) {
+                $this->context->getValidator()->inContext($this->context)->atPath($constraint->key)->validate($value, new CoreUrl());
+            }
+        } else {
+            $this->context->buildViolation('validators.field.notValid.url')->atPath($constraint->key)->addViolation();
+        }
+    }
+}
