31025ef0d1ab76de37302269ffa0f9ea0653e9ad
Nico <nicolas.dievart@elao.com>
2016-05-20 13:57:55 +0200

    389 - Add Package Form Type

---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Package/CreateType.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Package;
+
+use Proximum\Vimeet\Application\Command\Package\Create;
+use Proximum\Vimeet\Domain\Model\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Intl\Intl;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('translations', CollectionType::class, [
+                'entry_type' => PackageTranslationsType::class,
+                'label'      => false,
+            ])
+            ->add('file', FileType::class, [
+                'required' => false,
+            ])
+            ->add('unitPrice', NumberType::class)
+            ->add('availabilityCurrent', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('availabilityMax', IntegerType::class, [
+                'required' => false,
+            ])
+            ->add('features', CollectionType::class, [
+                'entry_type'    => FeatureType::class,
+                'allow_add'     => true,
+                'allow_delete'  => true,
+                'entry_options' => [
+                    'label' => false,
+                    'event' => $options['event'],
+                ],
+            ])
+            ->add('participantIncluded', IntegerType::class, [
+                'required' => true,
+                'attr'     => [
+                    'min' => 0,
+                ]
+            ])
+            ->add('productIncluded', CollectionType::class, [
+                'entry_type'    => ProductIncludedType::class,
+                'allow_add'     => true,
+                'allow_delete'  => true,
+                'entry_options' => [
+                    'label' => false,
+                    'event' => $options['event'],
+                ],
+                'attr' => [
+                    'data-collection-product-included' => 'data-collection-product-included',
+                ],
+            ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        foreach ($view->children['translations'] as $translation) {
+            $translation->vars['label'] = ucfirst(Intl::getLocaleBundle()->getLocaleName($translation->vars['name']));
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['event']);
+        $resolver->setAllowedTypes('event', Event::class);
+        $resolver->setDefaults([
+            'data_class' => Create::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'package_create';
+    }
+}
