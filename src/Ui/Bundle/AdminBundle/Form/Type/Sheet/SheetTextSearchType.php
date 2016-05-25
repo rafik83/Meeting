e38c001a8937cabce9e99bcb7168bba3305daef8
Richard Hanna <richard.hanna@elao.com>
2016-05-25 11:16:20 +0200

    330 - Fix participant data fixture

--- src/Ui/Bundle/AdminBundle/Form/Type/Sheet/SheetTextSearchType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/SheetTextSearchType.php
@@ -14,7 +14,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class SheetNameType extends AbstractType
+class SheetTextSearchType extends AbstractType
 {
     /**
      * {@inheritdoc}
