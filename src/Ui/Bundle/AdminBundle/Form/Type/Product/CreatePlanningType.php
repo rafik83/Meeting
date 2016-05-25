38684cd3dcf30dac02db9dfda845acb012e4ebb3
Maxime COLIN <maxime.colin@elao.com>
2016-05-25 10:34:22 +0200

    Remove duplicate code

--- src/Ui/Bundle/AdminBundle/Form/Type/Product/CreatePlanningType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Product/CreatePlanningType.php
@@ -11,50 +11,10 @@
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Product;
 
 use Proximum\Vimeet\Application\Command\Product\CreatePlanning;
-use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Product\Planning\TranslationsType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
-use Symfony\Component\Intl\Intl;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CreatePlanningType extends AbstractType
+class CreatePlanningType extends CreateParticipantType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('name', TextType::class)
-            ->add('unitPrice', NumberType::class)
-            ->add('translations', CollectionType::class, [
-                'entry_type' => TranslationsType::class,
-                'label'      => false,
-            ])
-            ->add('quantityMax', IntegerType::class, [
-                'required' => false,
-                'attr'     => [
-                    'min' => 0,
-                ],
-            ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function finishView(FormView $view, FormInterface $form, array $options)
-    {
-        foreach ($view->children['translations'] as $translation) {
-            $translation->vars['label'] = ucfirst(Intl::getLocaleBundle()->getLocaleName($translation->vars['name']));
-        }
-    }
-
     /**
      * {@inheritdoc}
      */
