494278ff3c47cbf2047911f9fb607cef09e8b962
Nico <nicolas.dievart@elao.com>
2016-05-30 13:52:58 +0200

    28 - Add UpdateAvatar command and handler

---
+++ src/Application/Command/Participant/UpdateAvatar.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Participant;
+
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Template\TemplateData;
+
+class UpdateAvatar
+{
+    /**
+     * @var TemplateData
+     */
+    public $templateData;
+
+    /**
+     * @var Participant
+     */
+    public $participant;
+
+    /**
+     * @var string
+     */
+    public $locale;
+
+    /**
+     * @var User
+     */
+    public $user;
+
+    /**
+     * @param TemplateData $templateData
+     * @param Participant  $participant
+     * @param string       $locale
+     * @param User         $user
+     */
+    public function __construct(TemplateData $templateData, Participant $participant, $locale, User $user)
+    {
+        $this->templateData = $templateData;
+        $this->participant  = $participant;
+        $this->locale       = $locale;
+        $this->user         = $user;
+    }
+}
