7bc75e4f14adfb16deb9c0bb4c578cb45ea82306
Nico <nicolas.dievart@elao.com>
2016-05-31 13:50:58 +0200

    443 - add EditableTextIndicator component

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/_EditableTextIndicator.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/_EditableTextIndicator.js
@@ -1,25 +1,35 @@
-function EditableTextIndicator(element, maxIndicator)
+function EditableTextIndicator(element, maxIndicator, maxIndicatorTranslations)
 {
   this.element    = element;
   this.max        = maxIndicator;
   this.identifier = Math.random();
 
+  this.maxIndicatorArea = document.createElement('div');
+  this.maxIndicatorArea.classList.add('help-block');
+  this.element.parentNode.insertBefore(this.maxIndicatorArea, this.element.nextSibling);
+
+  var translations = maxIndicatorTranslations.split('|');
+  this.maxIndicatorRemainingPluralTranslation   = translations[0];
+  this.maxIndicatorRemainingSingularTranslation = translations[1];
+  this.maxIndicatorReachedTranslation           = translations[2];
+
   this.element.addEventListener('keyup', function () {
     this.count();
   }.bind(this));
+  this.count();
 }
 
 EditableTextIndicator.prototype.count = function ()
 {
-  var current = this.element.value.length;
+  var current   = this.element.value.length;
   var remaining = this.max - current;
 
-  if (remaining >= 0) {
-    this.element.parentNode.removeChild(document.getElementById(this.identifier));
-    this.element.parentNode.appendChild('<div id="' + this.identifier + '">' + remaining + ' caractères restants</div>');
+  if (remaining > 1) {
+    this.maxIndicatorArea.innerHTML = remaining + ' ' + this.maxIndicatorRemainingPluralTranslation;
+  } else if (remaining === 1) {
+    this.maxIndicatorArea.innerHTML = remaining + ' ' + this.maxIndicatorRemainingSingularTranslation;
   } else {
-    this.element.parentNode.removeChild(document.getElementById(this.identifier));
-    this.element.parentNode.appendChild('<div id="' + this.identifier + '">Nombre de caractères maximum atteint</div>');
+    this.maxIndicatorArea.innerHTML = this.maxIndicatorReachedTranslation;
   }
 };
 
