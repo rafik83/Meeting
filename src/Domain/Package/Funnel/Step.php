bb766021f5f576d83a6b4e93abb55a1dca0450f1
Nico <nicolas.dievart@elao.com>
2016-06-03 17:28:24 +0200

    407 - Add funnel factory

---
+++ src/Domain/Package/Funnel/Step.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Package\Funnel;
+
+class Step
+{
+    const TYPE_PLAN                 = 'plan';
+    const TYPE_PARTICIPANT_PLANNING = 'participant_planning';
+    const TYPE_OPTIONS              = 'options';
+
+    /**
+     * @var int
+     */
+    public $index;
+
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var bool
+     */
+    public $completed;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @param int    $index
+     * @param string $title
+     * @param string $type
+     * @param bool   $completed
+     */
+    public function __construct($index, $title, $type, $completed = false)
+    {
+        $this->index     = $index;
+        $this->title     = $title;
+        $this->type      = $type;
+        $this->completed = $completed;
+    }
+}
