cbfdbc78424c4746274c531bba475421618027bc
Richard Hanna <richard.hanna@elao.com>
2016-06-13 12:00:38 +0200

    409-410 Improve quantity selector

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/_QuantitySelector.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/_QuantitySelector.js
@@ -1,28 +1,37 @@
 function QuantitySelector(element)
 {
     this.input      = element.querySelector('input.qty');
-    var max         = this.input.getAttribute('data-max');
-    var min         = this.input.getAttribute('data-min');
+    this.max        = this.input.getAttribute('data-max');
+    this.min        = this.input.getAttribute('data-min');
+    this.min        = null !== this.min ? this.min : 0;
     this.unitPrice  = this.input.getAttribute('data-unit-price');
     this.totalPrice = element.querySelector('.product-total-price');
 
     element.querySelector('.qtyminus').addEventListener('click', function () {
-        if (null === min || min < this.input.value) {
-            this.input.value--;
-            this.updateTotalPrice();
-        }
+        this.input.value--;
+        this.updateTotalPrice();
     }.bind(this));
 
     element.querySelector('.qtyplus').addEventListener('click', function () {
-        if (null === max || max > this.input.value) {
-            this.input.value++;
-            this.updateTotalPrice();
-        }
+        this.input.value++;
+        this.updateTotalPrice();
+    }.bind(this));
+
+    this.input.addEventListener('change', function () {
+        this.updateTotalPrice();
     }.bind(this));
 }
 
 QuantitySelector.prototype.updateTotalPrice = function ()
 {
+    if (this.min >= this.input.value) {
+        this.input.value = this.min;
+    }
+
+    if (null === this.max || this.max <= this.input.value) {
+        this.input.value = this.max;
+    }
+
     this.totalPrice.innerHTML = this.unitPrice * this.input.value;
 };
 
