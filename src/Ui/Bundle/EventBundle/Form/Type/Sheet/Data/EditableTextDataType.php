ab3c97cb475bf404ed0f29e36cbb5be8b1771ed2
Nico <nicolas.dievart@elao.com>
2016-06-13 11:45:20 +0200

    455 - Remove useless use

--- src/Ui/Bundle/EventBundle/Form/Type/Sheet/Data/EditableTextDataType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Sheet/Data/EditableTextDataType.php
@@ -15,8 +15,6 @@ use Proximum\Vimeet\Infrastructure\Adapter\TranslatorAdapter;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EditableTextDataType extends AbstractType
