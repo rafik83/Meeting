52cbe68f72d346e04f2f7d02b1f0754f7691a5e8
Maxime COLIN <maxime.colin@elao.com>
2016-06-14 10:41:37 +0200

    Introduce slot generation

---
+++ src/Domain/Meeting/Slot/SlotGenerator.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Meeting\Slot;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\MeetingSlot;
+
+class SlotGenerator
+{
+    /**
+     * @param Event    $event
+     * @param Recipe[] $recipes
+     *
+     * @return MeetingSlot[]
+     */
+    public function generate(Event $event, array $recipes)
+    {
+        $slots = [];
+
+        foreach ($recipes as $recipe) {
+
+            $interval = new \DateInterval(sprintf('PT%sM', $recipe->duration + $recipe->interval));
+            $period   = new \DatePeriod($recipe->begin, $interval, $recipe->end);
+
+            foreach ($period as $date) {
+                $end = clone $date;
+                $end->modify(sprintf('+%sminutes', $recipe->duration));
+
+                $slots[] = new MeetingSlot($event, $date, $end, true);
+            }
+
+        }
+
+        return $slots;
+    }
+}
