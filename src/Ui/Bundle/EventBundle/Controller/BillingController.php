a5a7f0d56bd1eea9c880ff53a22fc5dc6014caba
Maxime COLIN <maxime.colin@elao.com>
2016-06-17 11:27:46 +0200

    Remove unused statement

--- src/Ui/Bundle/EventBundle/Controller/BillingController.php
+++ src/Ui/Bundle/EventBundle/Controller/BillingController.php
@@ -12,7 +12,6 @@ namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller;
 
 use Proximum\Vimeet\Application\Command\Billing\UpdateInfo;
 use Proximum\Vimeet\Domain\Model\BillingInfo;
-use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\View\EventView;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Billing\UpdateInfoType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
