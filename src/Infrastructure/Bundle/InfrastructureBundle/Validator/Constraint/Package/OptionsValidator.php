bb39075c040f0ee2cff84df6b7f855210d576487
Richard Hanna <richard.hanna@elao.com>
2016-06-21 14:19:59 +0200

    429 Add validator

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Validator/Constraint/Package/OptionsValidator.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Package;
+
+use Proximum\Vimeet\Application\Command\Package\Step\SelectOptions;
+use Proximum\Vimeet\Domain\Model\Product;
+use Proximum\Vimeet\Domain\Package\Product\QuantityMaxGuesser;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class OptionsValidator extends ConstraintValidator
+{
+    /**
+     * @var QuantityMaxGuesser
+     */
+    private $quantityMaxGuesser;
+
+    /**
+     * @param QuantityMaxGuesser $quantityMaxGuesser
+     */
+    public function __construct(QuantityMaxGuesser $quantityMaxGuesser)
+    {
+        $this->quantityMaxGuesser = $quantityMaxGuesser;
+    }
+
+    /**
+     * @param SelectOptions $selectOptions
+     * @param Constraint    $constraint
+     */
+    public function validate($selectOptions, Constraint $constraint)
+    {
+        $options = $selectOptions->sheet->getPackage()->getAvailablesOptions();
+        $options = array_combine(
+            array_map(
+                function (Product $product) {
+                    return $product->getId();
+                },
+                $options
+            ),
+            $options
+        );
+
+        foreach ($selectOptions->options as $id => $quantity) {
+            if (!isset($options[$id])) {
+                $this
+                    ->context
+                    ->buildViolation('package.product.notAvailable')
+                    ->addViolation();
+
+                continue;
+            }
+
+            $quantityMax = $this->quantityMaxGuesser->getMaxByProduct($selectOptions->sheet, $options[$id]);
+
+            if ($quantity < 0 || $quantity > $quantityMax) {
+                $this
+                    ->context
+                    ->buildViolation('package.product.quantity')
+                    ->setParameters(['%min%' => 0, '%max%' => $quantityMax])
+                    ->atPath($id)
+                    ->addViolation();
+            }
+        }
+    }
+}
