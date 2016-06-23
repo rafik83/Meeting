dafccd79f577ee1c4e884b6349a20d7fbb012dda
Nico <nicolas.dievart@elao.com>
2016-06-23 15:45:10 +0200

    485 - Fix unit test and add test for createHandler of event

---
+++ tests/Application/Command/Event/CreateHandlerTest.php
@@ -0,0 +1,280 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Event;
+
+use Prophecy\Argument;
+use Proximum\Vimeet\Application\Adapter\FileStorageInterface;
+use Proximum\Vimeet\Application\Command\Event\Create;
+use Proximum\Vimeet\Application\Command\Event\CreateHandler;
+use Proximum\Vimeet\Application\Components\Guideline\Generator;
+use Proximum\Vimeet\Application\Exception\Event\DomainAlreadyUsedException;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\EventTranslation;
+use Proximum\Vimeet\Domain\Repository\AdminRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+
+class CreateHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        // Actual user
+        $dateTime = new \DateTime();
+        $user = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_SUPER_ADMIN',
+            $dateTime
+        );
+
+        // Update command
+        $create             = new Create($user);
+        $create->title      = 'barfoo';
+        $create->locales    = ['fr', 'en'];
+        $create->fallback   = 'en';
+        $create->currency   = 'USD';
+        $create->leftColor  = '#FFFFFF';
+        $create->rightColor = '#000000';
+        $create->textColor  = '#CCCCCC';
+        $create->logo       = 'shouldBeUploadFile';
+        $create->domain     = 'hello.vimeet.proximum.dev';
+        $create->timeZone   = 'Europe/Paris';
+
+        // Expected event
+        $expectedEvent = new Event();
+        $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
+        $expectedEvent->update(
+            'barfoo',
+            ['fr', 'en'],
+            'en',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'USD',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
+        $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', ''));
+        $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', ''));
+        $expectedEvent->setLogo('toto.jpg');
+
+        $expectedUser = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_SUPER_ADMIN',
+            $dateTime
+        );
+
+        // Mock
+        $adminRepository = $this->prophesize(AdminRepositoryInterface::class);
+        $adminRepository->set($expectedUser)->shouldNotBeCalled();
+        $eventRepository = $this->prophesize(EventRepositoryInterface::class);
+        $eventRepository->add(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(null);
+        $guidelineGenerator = $this->prophesize(Generator::class);
+        $guidelineGenerator->generate(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldBeCalled();
+        $fileStorage = $this->prophesize(FileStorageInterface::class);
+        $fileStorage->upload('shouldBeUploadFile')->shouldBeCalled()->willReturn('toto.jpg');
+
+        // Handle
+        $handler = new CreateHandler(
+            $adminRepository->reveal(),
+            $eventRepository->reveal(),
+            $guidelineGenerator->reveal(),
+            $fileStorage->reveal()
+        );
+        $handler->handle($create);
+    }
+
+    public function testHandleWithOrganizer()
+    {
+        // Actual user
+        $dateTime = new \DateTime();
+        $user = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_ORGANIZER',
+            $dateTime
+        );
+
+        // Update command
+        $create             = new Create($user);
+        $create->title      = 'barfoo';
+        $create->locales    = ['fr', 'en'];
+        $create->fallback   = 'en';
+        $create->currency   = 'USD';
+        $create->leftColor  = '#FFFFFF';
+        $create->rightColor = '#000000';
+        $create->textColor  = '#CCCCCC';
+        $create->logo       = 'shouldBeUploadFile';
+        $create->domain     = 'hello.vimeet.proximum.dev';
+        $create->timeZone   = 'Europe/Paris';
+
+        // Expected event
+        $expectedEvent = new Event();
+        $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
+        $expectedEvent->update(
+            'barfoo',
+            ['fr', 'en'],
+            'en',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'USD',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
+        $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', ''));
+        $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', ''));
+        $expectedEvent->setLogo('toto.jpg');
+
+        $expectedUser = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_ORGANIZER',
+            $dateTime
+        );
+        $expectedUser->addEvent($expectedEvent);
+
+        // Mock
+        $adminRepository = $this->prophesize(AdminRepositoryInterface::class);
+        $adminRepository->set(Argument::that(function (Admin $admin) use ($expectedUser) {
+            return $admin->getEvents()->count() === $expectedUser->getEvents()->count();
+        }))->shouldBeCalled();
+
+        $eventRepository = $this->prophesize(EventRepositoryInterface::class);
+        $eventRepository->add(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(null);
+
+        $guidelineGenerator = $this->prophesize(Generator::class);
+        $guidelineGenerator->generate(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldBeCalled();
+
+        $fileStorage = $this->prophesize(FileStorageInterface::class);
+        $fileStorage->upload('shouldBeUploadFile')->shouldBeCalled()->willReturn('toto.jpg');
+
+        // Handle
+        $handler = new CreateHandler(
+            $adminRepository->reveal(),
+            $eventRepository->reveal(),
+            $guidelineGenerator->reveal(),
+            $fileStorage->reveal()
+        );
+        $handler->handle($create);
+    }
+
+    public function testHandleWithAlreadyUsedDomain()
+    {
+        $this->expectException(DomainAlreadyUsedException::class);
+
+        // Actual user
+        $dateTime = new \DateTime();
+        $user = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_SUPER_ADMIN',
+            $dateTime
+        );
+
+        // Update command
+        $create             = new Create($user);
+        $create->title      = 'barfoo';
+        $create->locales    = ['fr', 'en'];
+        $create->fallback   = 'en';
+        $create->currency   = 'USD';
+        $create->leftColor  = '#FFFFFF';
+        $create->rightColor = '#000000';
+        $create->textColor  = '#CCCCCC';
+        $create->logo       = 'shouldBeUploadFile';
+        $create->domain     = 'hello.vimeet.proximum.dev';
+        $create->timeZone   = 'Europe/Paris';
+
+        // Expected event
+        $expectedEvent = new Event();
+        $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
+        $expectedEvent->update(
+            'barfoo',
+            ['fr', 'en'],
+            'en',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'USD',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
+        $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', ''));
+        $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', ''));
+        $expectedEvent->setLogo('toto.jpg');
+
+        $expectedUser = new Admin(
+            'email@email.fr',
+            'salt',
+            'password',
+            'fr',
+            'toto',
+            'tata',
+            'ROLE_SUPER_ADMIN',
+            $dateTime
+        );
+
+        // Mock
+        $adminRepository = $this->prophesize(AdminRepositoryInterface::class);
+        $adminRepository->set($expectedUser)->shouldNotBeCalled();
+        $eventRepository = $this->prophesize(EventRepositoryInterface::class);
+        $eventRepository->add(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldNotBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(new Event());
+        $guidelineGenerator = $this->prophesize(Generator::class);
+        $guidelineGenerator->generate(Argument::that(function (Event $event) use ($expectedEvent) {
+            return $event->getTitle() === $expectedEvent->getTitle();
+        }))->shouldNotBeCalled();
+        $fileStorage = $this->prophesize(FileStorageInterface::class);
+        $fileStorage->upload('shouldBeUploadFile')->shouldNotBeCalled()->willReturn('toto.jpg');
+
+        // Handle
+        $handler = new CreateHandler(
+            $adminRepository->reveal(),
+            $eventRepository->reveal(),
+            $guidelineGenerator->reveal(),
+            $fileStorage->reveal()
+        );
+        $handler->handle($create);
+    }
+}
--- tests/Application/Command/Event/UpdateHandlerTest.php
+++ tests/Application/Command/Event/UpdateHandlerTest.php
@@ -14,10 +14,10 @@ use Proximum\Vimeet\Application\Adapter\FileStorageInterface;
 use Proximum\Vimeet\Application\Command\Event\Update;
 use Proximum\Vimeet\Application\Command\Event\UpdateHandler;
 use Proximum\Vimeet\Application\Components\Guideline\Generator;
+use Proximum\Vimeet\Application\Exception\Event\DomainAlreadyUsedException;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\EventTranslation;
 use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
 {
@@ -26,13 +26,23 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         // Actual event
         $event = new Event();
         $event->getConfiguration()->setColors('#111111', '#BBBBBB', '#333333');
-        $event->update('foobar', ['fr', 'en'], 'fr', Event::VAT_MODE_ATI, 20, 'FR', 'EUR');
+        $event->update(
+            'foobar',
+            ['fr', 'en'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Madrid',
+            'old.vimeet.proximum.dev'
+        );
         $event->getTranslations()->set('fr', new EventTranslation($event, 'fr', 'Bonjour'));
         $event->getTranslations()->set('en', new EventTranslation($event, 'en', 'Hello'));
         $event->setLogo('here.jpg');
 
         // Update command
-        $update = new Update($event);
+        $update               = new Update($event);
         $update->title        = 'barfoo';
         $update->locales      = ['fr', 'en'];
         $update->fallback     = 'en';
@@ -44,16 +54,28 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
                 'description' => 'Hello',
             ],
         ];
-        $update->currency   = 'USD';
-        $update->leftColor  = '#FFFFFF';
-        $update->rightColor = '#000000';
-        $update->textColor  = '#CCCCCC';
-        $update->logo       = 'shouldBeUploadFile';
+        $update->currency     = 'USD';
+        $update->leftColor    = '#FFFFFF';
+        $update->rightColor   = '#000000';
+        $update->textColor    = '#CCCCCC';
+        $update->logo         = 'shouldBeUploadFile';
+        $update->domain       = 'hello.vimeet.proximum.dev';
+        $update->timeZone     = 'Europe/Paris';
 
         // Expected event
         $expectedEvent = new Event();
         $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
-        $expectedEvent->update('barfoo', ['fr', 'en'], 'en', Event::VAT_MODE_ATI, 20, 'FR', 'USD');
+        $expectedEvent->update(
+            'barfoo',
+            ['fr', 'en'],
+            'en',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'USD',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
         $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', 'Salut'));
         $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', 'Hello'));
         $expectedEvent->setLogo('toto.jpg');
@@ -61,6 +83,7 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         // Mock
         $eventRepository = $this->prophesize(EventRepositoryInterface::class);
         $eventRepository->set($expectedEvent)->shouldBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(null);
         $guidelineGenerator = $this->prophesize(Generator::class);
         $guidelineGenerator->generate($event)->shouldBeCalled();
         $fileStorage = $this->prophesize(FileStorageInterface::class);
@@ -77,12 +100,22 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         // Actual event
         $event = new Event();
         $event->getConfiguration()->setColors('#111111', '#BBBBBB', '#333333');
-        $event->update('foobar', ['fr', 'en'], 'fr', Event::VAT_MODE_ATI, 20, 'FR', 'EUR');
+        $event->update(
+            'foobar',
+            ['fr', 'en'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Madrid',
+            'old.vimeet.proximum.dev'
+        );
         $event->getTranslations()->set('fr', new EventTranslation($event, 'fr', 'Bonjour'));
         $event->getTranslations()->set('en', new EventTranslation($event, 'en', 'Hello'));
 
         // Update command
-        $update = new Update($event);
+        $update               = new Update($event);
         $update->title        = 'foobar';
         $update->locales      = ['fr', 'en', 'de'];
         $update->fallback     = 'fr';
@@ -94,15 +127,27 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
                 'description' => 'Hello',
             ],
         ];
-        $update->currency   = 'EUR';
-        $update->leftColor  = '#FFFFFF';
-        $update->rightColor = '#000000';
-        $update->textColor  = '#CCCCCC';
+        $update->currency     = 'EUR';
+        $update->leftColor    = '#FFFFFF';
+        $update->rightColor   = '#000000';
+        $update->textColor    = '#CCCCCC';
+        $update->domain       = 'hello.vimeet.proximum.dev';
+        $update->timeZone     = 'Europe/Paris';
 
         // Expected event
         $expectedEvent = new Event();
         $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
-        $expectedEvent->update('foobar', ['fr', 'en', 'de'], 'fr', Event::VAT_MODE_ATI, 20, 'FR', 'EUR');
+        $expectedEvent->update(
+            'foobar',
+            ['fr', 'en', 'de'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
         $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', 'Bonjour'));
         $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', 'Hello'));
         $expectedEvent->getTranslations()->set('de', new EventTranslation($expectedEvent, 'de', ''));
@@ -110,6 +155,7 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         // Mock
         $eventRepository = $this->prophesize(EventRepositoryInterface::class);
         $eventRepository->set($expectedEvent)->shouldBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(null);
         $guidelineGenerator = $this->prophesize(Generator::class);
         $guidelineGenerator->generate($event)->shouldBeCalled();
         $fileStorage = $this->prophesize(FileStorageInterface::class);
@@ -124,12 +170,22 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         // Actual event
         $event = new Event();
         $event->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
-        $event->update('foobar', ['fr', 'en'], 'fr', Event::VAT_MODE_ATI, 20, 'FR', 'EUR');
+        $event->update(
+            'foobar',
+            ['fr', 'en'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Madrid',
+            'old.vimeet.proximum.dev'
+        );
         $event->getTranslations()->set('fr', new EventTranslation($event, 'fr', 'Bonjour'));
         $event->getTranslations()->set('en', new EventTranslation($event, 'en', 'Hello'));
 
         // Update command
-        $update = new Update($event);
+        $update               = new Update($event);
         $update->title        = 'foobar';
         $update->locales      = ['fr'];
         $update->fallback     = 'fr';
@@ -141,23 +197,112 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
                 'description' => 'Hello',
             ],
         ];
-        $update->currency   = 'EUR';
-        $update->leftColor  = '#FFFFFF';
-        $update->rightColor = '#000000';
-        $update->textColor  = '#CCCCCC';
+        $update->currency     = 'EUR';
+        $update->leftColor    = '#FFFFFF';
+        $update->rightColor   = '#000000';
+        $update->textColor    = '#CCCCCC';
+        $update->domain       = 'hello.vimeet.proximum.dev';
+        $update->timeZone     = 'Europe/Paris';
+
 
         // Expected event
         $expectedEvent = new Event();
         $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
-        $expectedEvent->update('foobar', ['fr'], 'fr', Event::VAT_MODE_ATI, 20, 'FR', 'EUR');
+        $expectedEvent->update(
+            'foobar',
+            ['fr'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
         $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', 'Bonjour'));
 
         // Mock
         $eventRepository = $this->prophesize(EventRepositoryInterface::class);
         $eventRepository->set($expectedEvent)->shouldBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(null);
         $guidelineGenerator = $this->prophesize(Generator::class);
         $guidelineGenerator->generate($event)->shouldNotBeCalled();
         $fileStorage = $this->prophesize(FileStorageInterface::class);
+
+        // Handle
+        $handler = new UpdateHandler($eventRepository->reveal(), $guidelineGenerator->reveal(), $fileStorage->reveal());
+        $handler->handle($update);
+    }
+
+    public function testHandleWithAlreadyUsedDomain()
+    {
+        $this->expectException(DomainAlreadyUsedException::class);
+        // Actual event
+        $event = new Event();
+        $event->getConfiguration()->setColors('#111111', '#BBBBBB', '#333333');
+        $event->update(
+            'foobar',
+            ['fr', 'en'],
+            'fr',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'EUR',
+            'Europe/Madrid',
+            'old.vimeet.proximum.dev'
+        );
+        $event->getTranslations()->set('fr', new EventTranslation($event, 'fr', 'Bonjour'));
+        $event->getTranslations()->set('en', new EventTranslation($event, 'en', 'Hello'));
+        $event->setLogo('here.jpg');
+
+        // Update command
+        $update               = new Update($event);
+        $update->title        = 'barfoo';
+        $update->locales      = ['fr', 'en'];
+        $update->fallback     = 'en';
+        $update->translations = [
+            'fr' => [
+                'description' => 'Salut',
+            ],
+            'en' => [
+                'description' => 'Hello',
+            ],
+        ];
+        $update->currency     = 'USD';
+        $update->leftColor    = '#FFFFFF';
+        $update->rightColor   = '#000000';
+        $update->textColor    = '#CCCCCC';
+        $update->logo         = 'shouldBeUploadFile';
+        $update->domain       = 'hello.vimeet.proximum.dev';
+        $update->timeZone     = 'Europe/Paris';
+
+        // Expected event
+        $expectedEvent = new Event();
+        $expectedEvent->getConfiguration()->setColors('#FFFFFF', '#000000', '#CCCCCC');
+        $expectedEvent->update(
+            'barfoo',
+            ['fr', 'en'],
+            'en',
+            Event::VAT_MODE_ATI,
+            20,
+            'FR',
+            'USD',
+            'Europe/Paris',
+            'hello.vimeet.proximum.dev'
+        );
+        $expectedEvent->getTranslations()->set('fr', new EventTranslation($expectedEvent, 'fr', 'Salut'));
+        $expectedEvent->getTranslations()->set('en', new EventTranslation($expectedEvent, 'en', 'Hello'));
+        $expectedEvent->setLogo('toto.jpg');
+
+        // Mock
+        $eventRepository = $this->prophesize(EventRepositoryInterface::class);
+        $eventRepository->set($expectedEvent)->shouldNotBeCalled();
+        $eventRepository->getEventByDomain('hello.vimeet.proximum.dev')->shouldBeCalled()->willReturn(new Event());
+        $guidelineGenerator = $this->prophesize(Generator::class);
+        $guidelineGenerator->generate($event)->shouldNotBeCalled();
+        $fileStorage = $this->prophesize(FileStorageInterface::class);
+        $fileStorage->remove('here.jpg')->shouldNotBeCalled();
+        $fileStorage->upload('shouldBeUploadFile')->shouldNotBeCalled();
 
         // Handle
         $handler = new UpdateHandler($eventRepository->reveal(), $guidelineGenerator->reveal(), $fileStorage->reveal());
