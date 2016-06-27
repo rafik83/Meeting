42bf2f0ba51489bcd4964d053bff15265382b8ab
Paul Boiseau <paulboiseau@gmail.com>
2016-06-27 11:56:06 +0200

    423 - add duplicate package form

---
+++ src/Application/Command/Package/Duplicate.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Package;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Package;
+
+class Duplicate
+{
+    /**
+     * @var string
+     */
+    public $title;
+
+    /**
+     * @var Event
+     */
+    public $event;
+
+    /**
+     * @var Package
+     */
+    public $package;
+}
---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Package/DuplicateType.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Package;
+
+use Proximum\Vimeet\Application\Command\Package\Duplicate;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\PackageRepositoryInterface;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Event\EventChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DuplicateType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'required' => true,
+            ])
+            ->add('event', EventChoiceType::class, [
+                'required'         => true,
+                'repositoryMethod' => function (EventRepositoryInterface $eventRepository) use ($options) {
+                    return $eventRepository->getEventsByAdmin($options['user']);
+                },
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['user']);
+        $resolver->setDefaults([
+            'data_class' => Duplicate::class,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'package_update';
+    }
+}
