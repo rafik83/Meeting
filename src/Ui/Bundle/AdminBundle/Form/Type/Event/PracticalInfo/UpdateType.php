acfcc2378bfbe440f184354b786ef35817b6dee5
Dimitri Fruit <dimitri@MacBook-Pro-de-elao.local>
2016-06-27 15:36:16 +0200

    286 - Add form type

---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Event/PracticalInfo/UpdateType.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Event\PracticalInfo;
+
+use Proximum\Vimeet\Application\Command\Event\PracticalInfo\Update;
+use Proximum\Vimeet\Domain\Model\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('organiserName', TextType::class,  [
+                'required' => true,
+            ])
+            ->add('contactLastName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('contactFirstName', TextType::class, [
+                'required' => false,
+            ])
+            ->add('phone', TextType::class, [
+                'required' => false,
+            ])
+            ->add('organiserEmail', TextType::class, [
+                'required' => false,
+            ])
+            ->add('website', TextType::class, [
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['event']);
+        $resolver->setAllowedTypes('event', Event::class);
+        $resolver->setDefaults([
+            'data-class' => Update::class,
+        ]);
+    }
+
+    /**
+     * @{inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'event_practical_info_update';
+    }
+}
