4dc3819e376ef82683b0643335c605d23b0222de
Paul Boiseau <paulboiseau@gmail.com>
2016-06-28 11:15:18 +0200

    503 - code styling fix

--- src/Application/Command/Product/AbstractCreate.php
+++ src/Application/Command/Product/AbstractCreate.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
 
 namespace Proximum\Vimeet\Application\Command\Product;
-
 
 use Proximum\Vimeet\Domain\Model\Event;
 
--- src/Application/Command/Product/AbstractHandler.php
+++ src/Application/Command/Product/AbstractHandler.php
@@ -1,8 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
 
 namespace Proximum\Vimeet\Application\Command\Product;
-
 
 use Proximum\Vimeet\Application\Adapter\FileStorageInterface;
 use Proximum\Vimeet\Domain\Repository\ProductRepositoryInterface;
@@ -21,13 +27,13 @@ abstract class AbstractHandler
 
     /**
      * @param ProductRepositoryInterface $productRepository
-     * @param FileStorageInterface $fileStorageInterface
+     * @param FileStorageInterface       $fileStorageInterface
      */
     public function __construct(
         ProductRepositoryInterface $productRepository,
         FileStorageInterface $fileStorageInterface = null
     ) {
-        $this->productRepository = $productRepository;
+        $this->productRepository    = $productRepository;
         $this->fileStorageInterface = $fileStorageInterface;
     }
 }
