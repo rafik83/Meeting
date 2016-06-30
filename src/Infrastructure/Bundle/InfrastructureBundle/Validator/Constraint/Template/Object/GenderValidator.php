e6fdbc520f5d768221b4b8aa93e2528bc0364b45
Dimitri Fruit <dimitri@MacBook-Pro-de-elao.local>
2016-06-30 11:07:45 +0200

    505 - Add constraint + validator + update block validator

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Validator/Constraint/Template/Object/GenderValidator.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Template\Object;
+
+
+use Symfony\Component\Validator\Constraint;
+use Proximum\Vimeet\Domain\Template\Object;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Template\ObjectValidator;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class GenderValidator extends ObjectValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function checkRequired(Object $object, Constraint $constraint)
+    {
+        if ($object instanceof Object\Gender && true === $object->getOption('required')) {
+            $this->context->getValidator()->inContext($this->context)->atPath($constraint->key . '.gender')->validate($object->getContentValue(), new NotBlank());
+        }
+    }
+}
