a06f3c74104ac47a8046f952a68200ef8591dd00
Nico <nicolas.dievart@elao.com>
2016-07-04 23:47:33 +0200

    467 - Add Payment utility

---
+++ src/Domain/Payment/Mode.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Payment;
+
+class Mode
+{
+    // Payment mode
+    const PAYMENT_BANK_CARD     = 'bank_card';
+    const PAYMENT_BANK_TRANSFER = 'bank_transfer';
+    const PAYMENT_BANK_CHECK    = 'bank_check';
+
+    // Payment mode for deposit
+    const PAYMENT_DEPOSIT_BANK_CARD     = 'deposit_bank_card';
+    const PAYMENT_DEPOSIT_BANK_TRANSFER = 'deposit_bank_transfer';
+    const PAYMENT_DEPOSIT_BANK_CHECK    = 'deposit_bank_check';
+
+    /**
+     * Return all the payment modes allowed by the platform
+     *
+     * @return array
+     */
+    public static function getPaymentModes()
+    {
+        return [
+            self::PAYMENT_BANK_CARD     => self::PAYMENT_BANK_CARD,
+            self::PAYMENT_BANK_TRANSFER => self::PAYMENT_BANK_TRANSFER,
+            self::PAYMENT_BANK_CHECK    => self::PAYMENT_BANK_CHECK,
+        ];
+    }
+
+    /**
+     * Return all the payment modes allowed as a deposit by the platform
+     *
+     * @return array
+     */
+    public static function getDepositPaymentModes()
+    {
+        return [
+            self::PAYMENT_DEPOSIT_BANK_CARD     => self::PAYMENT_DEPOSIT_BANK_CARD,
+            self::PAYMENT_DEPOSIT_BANK_TRANSFER => self::PAYMENT_DEPOSIT_BANK_TRANSFER,
+            self::PAYMENT_DEPOSIT_BANK_CHECK    => self::PAYMENT_DEPOSIT_BANK_CHECK,
+        ];
+    }
+
+    /**
+     * Return all the payment and deposit modes allowed by the platform
+     *
+     * @return array
+     */
+    public static function getAllPaymentModes()
+    {
+        return array_merge(self::getPaymentModes(), self::getDepositPaymentModes());
+    }
+}
---
+++ src/Domain/Payment/TotalToPay.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Payment;
+
+use Proximum\Vimeet\Domain\Cart\CartManager;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Package\Specification\VatApplicable;
+
+class TotalToPay
+{
+    /**
+     * @var CartManager
+     */
+    private $cartManager;
+
+    /**
+     * @var VatApplicable
+     */
+    private $vatApplicable;
+
+    /**
+     * @param CartManager   $cartManager
+     * @param VatApplicable $vatApplicable
+     */
+    public function __construct(CartManager $cartManager, VatApplicable $vatApplicable)
+    {
+        $this->cartManager   = $cartManager;
+        $this->vatApplicable = $vatApplicable;
+    }
+
+    /**
+     * @param Sheet $sheet
+     *
+     * @throws \Proximum\Vimeet\Domain\Package\Exception\MissingBillingInfoException
+     *
+     * @return float
+     */
+    public function getTotal(Sheet $sheet)
+    {
+        $cart          = $this->cartManager->getCart($sheet);
+        $vatApplicable = $this->vatApplicable->onCart($cart);
+        $total         = $cart->getTotal();
+        $vatToPay      = 0;
+
+        if ($vatApplicable) {
+            $vatToPay = ($total * $cart->getSheet()->getEvent()->getVat()) / 100;
+        }
+
+        return $total + $vatToPay;
+    }
+}
