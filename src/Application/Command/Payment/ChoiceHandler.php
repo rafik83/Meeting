693f8526766beb8b2d1221d7c017566c01b137f6
Nico <nicolas.dievart@elao.com>
2016-07-06 12:09:37 +0200

    467 - Create transaction in payment choice

--- src/Application/Command/Payment/ChoiceHandler.php
+++ src/Application/Command/Payment/ChoiceHandler.php
@@ -11,6 +11,11 @@
 namespace Proximum\Vimeet\Application\Command\Payment;
 
 use Proximum\Vimeet\Domain\Cart;
+use Proximum\Vimeet\Domain\Model\Transaction;
+use Proximum\Vimeet\Domain\Payment\DepositApplicable;
+use Proximum\Vimeet\Domain\Payment\Mode;
+use Proximum\Vimeet\Domain\Payment\TotalToPay;
+use Proximum\Vimeet\Domain\Repository\TransactionRepositoryInterface;
 
 class ChoiceHandler
 {
@@ -25,13 +30,39 @@ class ChoiceHandler
     private $cartManager;
 
     /**
-     * @param Cart\Converter   $converter
-     * @param Cart\CartManager $cartManager
+     * @var TotalToPay
      */
-    public function __construct(Cart\Converter $converter, Cart\CartManager $cartManager)
-    {
-        $this->converter   = $converter;
-        $this->cartManager = $cartManager;
+    private $totalToPay;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $datetime;
+
+    /**
+     * @var TransactionRepositoryInterface
+     */
+    private $transactionRepository;
+
+    /**
+     * @param TransactionRepositoryInterface $transactionRepository
+     * @param Cart\Converter                 $converter
+     * @param Cart\CartManager               $cartManager
+     * @param TotalToPay                     $totalToPay
+     * @param \DateTimeInterface             $datetime
+     */
+    public function __construct(
+        TransactionRepositoryInterface $transactionRepository,
+        Cart\Converter $converter,
+        Cart\CartManager $cartManager,
+        TotalToPay $totalToPay,
+        \DateTimeInterface $datetime
+    ) {
+        $this->transactionRepository = $transactionRepository;
+        $this->converter             = $converter;
+        $this->cartManager           = $cartManager;
+        $this->totalToPay            = $totalToPay;
+        $this->datetime              = $datetime;
     }
 
     /**
@@ -39,8 +70,25 @@ class ChoiceHandler
      */
     public function handle(Choice $choice)
     {
+        $total = $this->totalToPay->getTotal($choice->sheet);
+
+        if (Mode::isDeposit($choice->mode)) {
+            $total = DepositApplicable::calculateDeposit($choice->sheet->getEvent(), new \DateTime(), $total);
+        }
+
+        // Convert cart to order
         $this->converter->toOrder($this->cartManager->getCart($choice->sheet));
 
         // Create Transaction
+        $transaction = new Transaction(
+            $choice->sheet,
+            $total,
+            $this->datetime,
+            $choice->mode,
+            null,
+            Transaction::STATE_PENDING
+        );
+
+        $this->transactionRepository->add($transaction);
     }
 }
