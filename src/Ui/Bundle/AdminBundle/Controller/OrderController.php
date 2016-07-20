f1932c10f3c2519f524130ca6876584d1f7385f6
Dimitri Fruit <dimitrifruit@MacBook-Air-de-Dimitri.local>
2016-07-20 16:40:09 +0200

    156 - Update OrderController

--- src/Ui/Bundle/AdminBundle/Controller/OrderController.php
+++ src/Ui/Bundle/AdminBundle/Controller/OrderController.php
@@ -14,6 +14,7 @@ use Proximum\Vimeet\Application\Command\Order\AddRow;
 use Proximum\Vimeet\Application\Command\Order\RemoveRow;
 use Proximum\Vimeet\Application\Command\Order\UpdateRow;
 use Proximum\Vimeet\Application\Query\Order\PaginatedOrderListViewQuery;
+use Proximum\Vimeet\Application\Query\Order\SummaryQuery;
 use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Order\AddRowType;
 use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Order\FilterType;
 use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Order\UpdateRowType;
@@ -84,17 +85,20 @@ class OrderController extends Controller
             ->guessSheetName($order->getSheet(), $request->getLocale())
         ;
 
-        $orderView = null;
-
-        return $this->render(
-            'AdminBundle:Order:edit.html.twig',
-            [
-                'event'      => $event,
-                'sheet_info' => $sheetInfo,
-                'order'      => $order,
-                'order_view' => $orderView,
-            ]
+        $summaryView =  $this->get('tactician.commandbus.query')->handle(
+            new SummaryQuery(
+                $order->getSheet(),
+                $order,
+                $request->getLocale()
+            )
         );
+        
+        return $this->render('AdminBundle:Order:edit.html.twig', [
+            'event'      => $event,
+            'sheet_info' => $sheetInfo,
+            'order'      => $order,
+            'order_view' => $summaryView,
+        ]);
     }
 
     /**
@@ -105,7 +109,7 @@ class OrderController extends Controller
      *
      * @return RedirectResponse|Response
      */
-    public function addRowAction(Request $request, Event $event, Order $order, $group)
+    public function addRowToGroupAction(Request $request, Event $event, Order $order, $group)
     {
         $this->denyAccessIfOrderNotInEvent($event, $order);
 
@@ -114,11 +118,45 @@ class OrderController extends Controller
             ->guessSheetName($order->getSheet(), $request->getLocale())
         ;
 
+
         $addRow = new AddRow($order, $group);
         $form   = $this->createForm(AddRowType::class, $addRow);
 
         if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
             $this->get('tactician.commandbus')->handle($addRow);
+            $this->addFlash('success', 'flash.admin.order.add_row.success');
+
+            return $this->redirectToRoute('admin_sheet_order_edit', [
+                'event' => $event->getId(),
+                'order' => $order->getId(),
+            ]);
+        }
+
+        return $this->render(
+            'AdminBundle:Order:addRow.html.twig',
+            [
+                'event'      => $event,
+                'sheet_info' => $sheetInfo,
+                'order'      => $order,
+                'form'       => $form->createView(),
+            ]
+        );
+    }
+
+    public function editRowAction(Request $request, Event $event, Order $order, Order\Row $row)
+    {
+        $this->denyAccessIfOrderNotInEvent($event, $order);
+
+        $sheetInfo = $this
+            ->get('vimeet_infrastructure.application.components.sheet.sheet_info_guesser')
+            ->guessSheetName($order->getSheet(), $request->getLocale())
+        ;
+
+        $updateRow = new UpdateRow($order, $row, $request->query->get('groupId'));
+        $form   = $this->createForm(UpdateRowType::class, $updateRow);
+
+        if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+            $this->get('tactician.commandbus')->handle($updateRow);
             $this->addFlash('success', 'flash.admin.order.add_row.success');
 
             return $this->redirectToRoute('admin_sheet_order_edit', [
