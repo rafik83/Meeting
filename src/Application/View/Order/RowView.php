fb82960e6a7b38645d19cbf7f40175d057863553
Dimitri Fruit <dimitrifruit@MacBook-Air-de-Dimitri.local>
2016-07-25 18:00:56 +0200

    156 - Refacto : split AddRowHandler in two (add to group / add to product)

---
+++ src/Application/View/Order/RowView.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\View\Order;
+
+class RowView
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var int
+     */
+    public $quantity;
+
+    /**
+     * @var float
+     */
+    public $price;
+
+    /**
+     * @var float
+     */
+    public $total;
+
+    /**
+     * @var string
+     */
+    public $vatMode;
+
+    /**
+     * @var string
+     */
+    public $currency;
+
+    /**
+     * @var IncludedProductView[]
+     */
+    public $includedProducts = [];
+
+    /***
+     * @var null|CustomRowView[]
+     */
+    public $customRows = [];
+
+    /**
+     * @param int    $id
+     * @param int    $productId
+     * @param string $label
+     * @param float  $price
+     * @param int    $quantity
+     * @param string $vatMode
+     * @param string $currency
+     */
+    public function __construct(
+        $id,
+        $productId,
+        $label,
+        $price,
+        $quantity,
+        $vatMode,
+        $currency
+    ) {
+        $this->id        = $id;
+        $this->productId = $productId;
+        $this->label     = $label;
+        $this->price     = $price;
+        $this->quantity  = $quantity;
+        $this->total     = $price * $quantity;
+        $this->vatMode   = $vatMode;
+        $this->currency  = $currency;
+    }
+
+    /**
+     * @param IncludedProductView $productView
+     */
+    public function addIncludedProduct(IncludedProductView $productView)
+    {
+        $this->includedProducts[] = $productView;
+    }
+
+    /**
+     * @param CustomRowView $customRowView
+     */
+    public function addCustomRow(CustomRowView $customRowView)
+    {
+        $this->customRows[] = $customRowView;
+    }
+}
