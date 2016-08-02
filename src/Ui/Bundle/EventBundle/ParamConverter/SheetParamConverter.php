8593ea59ae1a6070b265a9cb37a49838e0d1bf30
Richard Hanna <supertanuki@gmail.com>
2016-08-02 14:09:06 +0200

    Add sheet param converter to check if sheet in event

---
+++ src/Ui/Bundle/EventBundle/ParamConverter/SheetParamConverter.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2015 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter;
+
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class SheetParamConverter implements ParamConverterInterface
+{
+    /**
+     * @var SheetRepositoryInterface
+     */
+    private $sheetRepository;
+
+    /**
+     * @param SheetRepositoryInterface $sheetRepository
+     */
+    public function __construct(SheetRepositoryInterface $sheetRepository)
+    {
+        $this->sheetRepository = $sheetRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        /** @var EventDomain $eventDomain */
+        $eventDomain = $request->attributes->get('eventDomain');
+        $sheetId     = $request->attributes->get('sheet');
+        $sheet       = $this->sheetRepository->getSheetById($sheetId);
+
+        if (null === $sheet) {
+            throw new NotFoundHttpException('Sheet not found');
+        }
+
+        if ($eventDomain->getEvent() !== $sheet->getEvent()) {
+            throw new NotFoundHttpException('Sheet not found in that event');
+        }
+
+        $request->attributes->set($configuration->getName(), $sheet);
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(ParamConverter $configuration)
+    {
+        return $configuration->getClass() === Sheet::class;
+    }
+}
