34a508ed9cced0b2aa67a97ba25786444927e445
DimitriMorgan <dim.fruit@gmail.com>
2016-08-03 11:56:05 +0200

    156 - Add custom row to recap in event

--- src/Ui/Bundle/EventBundle/Controller/OrderController.php
+++ src/Ui/Bundle/EventBundle/Controller/OrderController.php
@@ -120,6 +120,7 @@ class OrderController extends Controller
         return $this->render('EventBundle:Order/SummaryTotal:summaryTotal.html.twig', [
             'event' => $eventDomain->getEvent(),
             'sheet' => $sheet,
+            'order' => $order,
             'view'  => $view,
         ]);
     }
