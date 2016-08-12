a99b2b872c4128cf4bf0a6443aaa208fd88c28ae
Paul Boiseau <paulboiseau@gmail.com>
2016-08-12 14:10:48 +0200

    533 - fix crash when remove type with rules

--- src/Application/Command/Type/RemoveHandler.php
+++ src/Application/Command/Type/RemoveHandler.php
@@ -11,6 +11,7 @@
 namespace Proximum\Vimeet\Application\Command\Type;
 
 use Proximum\Vimeet\Application\Exception\Type\TypeUsedBySheetException;
+use Proximum\Vimeet\Domain\Repository\RuleRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
 
@@ -27,17 +28,25 @@ class RemoveHandler
     private $sheetRepository;
 
     /**
+     * @var RuleRepositoryInterface
+     */
+    private $ruleRepository;
+
+    /**
      * RemoveHandler constructor.
      *
      * @param TypeRepositoryInterface  $typeRepository
      * @param SheetRepositoryInterface $sheetRepository
+     * @param RuleRepositoryInterface  $ruleRepository
      */
     public function __construct(
         TypeRepositoryInterface $typeRepository,
-        SheetRepositoryInterface $sheetRepository
+        SheetRepositoryInterface $sheetRepository,
+        RuleRepositoryInterface $ruleRepository
     ) {
         $this->typeRepository  = $typeRepository;
         $this->sheetRepository = $sheetRepository;
+        $this->ruleRepository  = $ruleRepository;
     }
 
     /**
@@ -49,6 +58,15 @@ class RemoveHandler
     {
         if ($this->sheetRepository->isThereAtLeastOneByType($remove->type)) {
             throw new TypeUsedBySheetException();
+        }
+
+        // remove associated rule for type
+        $rules = $this->ruleRepository->getByType($remove->type);
+
+        if (!empty($rules)) {
+            foreach($rules as $rule) {
+                $this->ruleRepository->remove($rule);
+            }
         }
 
         $this->typeRepository->remove($remove->type);
