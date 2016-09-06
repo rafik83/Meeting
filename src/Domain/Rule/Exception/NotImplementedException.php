06cda92bd726abeb51b8d708d536d72bbca1507c
Nico <nicolas.dievart@elao.com>
2016-09-06 16:00:52 +0200

    483 - Add TagIdentifier

---
+++ src/Domain/Rule/Exception/NotImplementedException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 vimeet
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Rule\Exception;
+
+
+class NotImplementedException
+{
+
+}
