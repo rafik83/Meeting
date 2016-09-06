c6d31a9acc77147fab4aa6c7ce2e0b6a79777196
Nico <nicolas.dievart@elao.com>
2016-09-06 16:00:52 +0200

    483 - Add TagIdentifier

---
+++ src/Domain/Rule/TagIdentifier.php
@@ -0,0 +1,95 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Rule;
+
+use Proximum\Vimeet\Application\Components\Sheet\Template\Tag;
+use Proximum\Vimeet\Domain\Model\Category;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Model\Template\RegistrationTemplate;
+use Proximum\Vimeet\Domain\Template\TemplateObject;
+use Proximum\Vimeet\Domain\Model\WhoInterface;
+use Proximum\Vimeet\Domain\Rule\Exception\NotImplementedException;
+use Proximum\Vimeet\Domain\Template\TemplateDataFactory;
+
+class TagIdentifier
+{
+    /**
+     * @var TemplateDataFactory
+     */
+    private $templateFactory;
+
+    /**
+     * @param TemplateDataFactory $templateFactory
+     */
+    public function __construct(TemplateDataFactory $templateFactory)
+    {
+        $this->templateFactory = $templateFactory;
+    }
+
+    /**
+     * @param WhoInterface $who
+     *
+     * @return array
+     *
+     * @throws NotImplementedException
+     */
+    public function identify(WhoInterface $who)
+    {
+        if ($who instanceof Category) {
+            $templates = [];
+
+            /**
+             * @var Type $type
+             */
+            foreach ($who->getTypes() as $type) {
+                $templates[$type->getRegistrationTemplate()->getId()] = $type->getRegistrationTemplate();
+            }
+
+            $tags = [];
+
+            /**
+             * @var RegistrationTemplate $template
+             */
+            foreach ($templates as $template) {
+                $tags = array_merge($tags, $this->extractTags($template));
+            }
+
+            return $tags;
+        } elseif ($who instanceof Type) {
+            return $this->extractTags($who->getRegistrationTemplate());
+        } else {
+            throw new NotImplementedException(sprintf('No method for %', get_class($who)));
+        }
+    }
+
+    /**
+     * @param RegistrationTemplate $registrationTemplate
+     *
+     * @return array
+     */
+    private function extractTags(RegistrationTemplate $registrationTemplate)
+    {
+        $tags = [];
+
+        $template = $this->templateFactory->createFromTemplate($registrationTemplate, []);
+
+        /**
+         * @var TemplateObject $object
+         */
+        foreach ($template->getObjects() as $object) {
+            $tags = array_merge($tags, $object->getTags());
+        }
+
+        return array_filter($tags, function ($tag) {
+            return $tag !== Tag::SHEET_DATA && $tag !== Tag::PARTICIPANT_DATA;
+        });
+    }
+}
