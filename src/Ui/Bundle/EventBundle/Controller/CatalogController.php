f89831df8cd5fe6697cebb08950e292352f4f9d6
DimitriMorgan <dim.fruit@gmail.com>
2016-09-08 11:42:30 +0200

    487 - add key trans + remove useless use + add sheets in catalog fixtures

--- src/Ui/Bundle/EventBundle/Controller/CatalogController.php
+++ src/Ui/Bundle/EventBundle/Controller/CatalogController.php
@@ -10,7 +10,6 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller;
 
-use Proximum\Vimeet\Application\Components\Rule\Exception\NoRuleFoundException;
 use Proximum\Vimeet\Application\Components\Rule\Strategy\SetNullStrategy;
 use Proximum\Vimeet\Application\Exception\Paginator\UnavailableCurrentPageException;
 use Proximum\Vimeet\Application\Query\Participant\CardListViewQuery;
