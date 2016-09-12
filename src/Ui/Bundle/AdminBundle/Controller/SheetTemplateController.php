0278f0f8630d49daf521b4b7bdb52bc8e8a811b0
Nico <nicolas.dievart@elao.com>
2016-09-12 16:42:31 +0200

    539 - Disable add locale on template if assign to event

--- src/Ui/Bundle/AdminBundle/Controller/SheetTemplateController.php
+++ src/Ui/Bundle/AdminBundle/Controller/SheetTemplateController.php
@@ -267,7 +267,7 @@ class SheetTemplateController extends Controller
             'template'        => $template,
             'locale'          => $locale,
             'update_form'     => $updateForm->createView(),
-            'add_locale_form' => $addLocaleForm ? $addLocaleForm->createView() : null,
+            'add_locale_form' => null === $template->getEvent() && $addLocaleForm ? $addLocaleForm->createView() : null,
             'completeness'    => $completeness,
             'nomenclatures'   => $nomenclatures,
             'products'        => $products,
