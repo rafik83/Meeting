b77424ec641389b3592612dd6f2d006f393b1dc8
DimitriMorgan <dim.fruit@gmail.com>
2016-09-12 10:48:42 +0200

    580 - add catalog sheet access exception

---
+++ src/Domain/Exception/Catalog/SheetAccessDeniedException.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Exception\Catalog;
+
+use Proximum\Vimeet\Domain\Exception\Sheet\SheetException;
+
+class SheetAccessDeniedException extends SheetException
+{
+}
