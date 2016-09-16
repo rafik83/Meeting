c33fec953fe5b4aad426b8805c03ed632139c870
Richard Hanna <supertanuki@gmail.com>
2016-09-16 17:56:13 +0200

    Remove unused dependencies

--- src/Application/Components/Sheet/SheetManager.php
+++ src/Application/Components/Sheet/SheetManager.php
@@ -14,18 +14,11 @@ use Proximum\Vimeet\Domain\Model\Meeting\Request;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Repository\Meeting\RequestRepositoryInterface;
-use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
-use Proximum\Vimeet\Domain\View\SheetDataView;
 
 class SheetManager
 {
-    /**
-     * @var ParticipantRepositoryInterface
-     */
-    private $participantRepository;
-
     /**
      * @var TypeRepositoryInterface
      */
@@ -44,18 +37,15 @@ class SheetManager
     /**
      * SheetManager constructor.
      *
-     * @param ParticipantRepositoryInterface $participantRepository
      * @param TypeRepositoryInterface        $typeRepository
      * @param SheetRepositoryInterface       $sheetRepository
      * @param RequestRepositoryInterface     $requestRepository
      */
     public function __construct(
-        ParticipantRepositoryInterface $participantRepository,
         TypeRepositoryInterface $typeRepository,
         SheetRepositoryInterface $sheetRepository,
         RequestRepositoryInterface $requestRepository
     ) {
-        $this->participantRepository = $participantRepository;
         $this->typeRepository        = $typeRepository;
         $this->sheetRepository       = $sheetRepository;
         $this->requestRepository     = $requestRepository;
