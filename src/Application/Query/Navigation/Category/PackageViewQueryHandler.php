8d2eb4a7cc1a5109e8141350f2373f1fb3d26695
Richard Hanna <supertanuki@gmail.com>
2016-09-19 16:04:50 +0200

    273 - Fix condition on summary link in menu

--- src/Application/Query/Navigation/Category/PackageViewQueryHandler.php
+++ src/Application/Query/Navigation/Category/PackageViewQueryHandler.php
@@ -61,12 +61,17 @@ class PackageViewQueryHandler
                 ])
             );
 
-            $linksView[] = new LinkView(
-                'navigation.links.package.order_summary_total',
-                $this->navigationBuilder->getRoute('event_order_summary_total', [
-                    'sheet' => $packageQuery->sheet->getId()
-                ])
-            );
+            if ($packageQuery->sheet->hasNotCancelledOrders()) {
+                $linksView[] = new LinkView(
+                    'navigation.links.package.order_summary_total',
+                    $this->navigationBuilder->getRoute(
+                        'event_order_summary_total',
+                        [
+                            'sheet' => $packageQuery->sheet->getId()
+                        ]
+                    )
+                );
+            }
         } else {
             $funnel = $this->funnelFactory->create($packageQuery->sheet, $packageQuery->locale);
 
