1c91f2016ecaa50f83cc7cdfc9ce43757f1ba8de
Nico <nicolas.dievart@elao.com>
2016-09-19 12:05:31 +0200

    605 - Add boolean filters

--- src/Ui/Bundle/AdminBundle/Form/Type/Sheet/PredefinedFiltersChoiceType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/PredefinedFiltersChoiceType.php
@@ -10,23 +10,47 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Sheet;
 
+use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Sheet\Constant;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Service\BooleanFiltersBuilder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PredefinedFiltersChoiceType extends AbstractType
 {
     /**
+     * @var BooleanFiltersBuilder
+     */
+    private $booleanFilterBuilder;
+
+    /**
+     * @param BooleanFiltersBuilder $booleanFiltersBuilder
+     */
+    public function __construct(BooleanFiltersBuilder $booleanFiltersBuilder)
+    {
+        $this->booleanFilterBuilder = $booleanFiltersBuilder;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
+        $resolver->setRequired(['event']);
+        $resolver->setAllowedTypes('event', Event::class);
         $resolver->setDefaults([
-            'choices' => [
-                'admin.sheet.created_today'     => Constant::CREATED_TODAY,
-                'admin.sheet.created_this_week' => Constant::CREATED_THIS_WEEK,
-            ],
+            'choices' => function (Options $options) {
+                $filters = $this->booleanFilterBuilder->getFilters($options['event']);
+                $filters = array_flip($filters);
+                $predefinedFilters = [
+                    'admin.sheet.created_today'     => Constant::CREATED_TODAY,
+                    'admin.sheet.created_this_week' => Constant::CREATED_THIS_WEEK,
+                ];
+
+                return array_merge($predefinedFilters, $filters);
+            },
         ]);
     }
 
