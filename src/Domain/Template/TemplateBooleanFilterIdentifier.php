f680a587e511706d8150ba331135142c8f791656
Nico <nicolas.dievart@elao.com>
2016-09-19 12:06:17 +0200

    605 - Update elasticsearch to deal with boolean filter

--- src/Domain/Template/TemplateBooleanFilterIdentifier.php
+++ src/Domain/Template/TemplateBooleanFilterIdentifier.php
@@ -14,17 +14,66 @@ use Proximum\Vimeet\Domain\Template\TemplateObject\BooleanObject;
 
 class TemplateBooleanFilterIdentifier
 {
-    public function getBooleanFilterValues(TemplateData $templateData)
+    /**
+     * @param TemplateData $templateData
+     *
+     * @return array
+     */
+    public static function getBooleanFilterValues(TemplateData $templateData)
     {
-        foreach ($templateData->getObjects() as $object) {
-            if ($object instanceof BooleanObject) {
+        $filters = [];
 
+        foreach ($templateData->getObjects() as $object) {
+            if ($object instanceof BooleanObject
+                && $object->isFilter()
+                && null !== $object->getBoolean()
+                && false !== $object->getBoolean()
+            ) {
+                $filters[] = [
+                    'key' => $object->getKey(),
+                ];
             }
         }
+
+        return $filters;
     }
 
-    public function getBooleanFilters()
+    /**
+     * @param TemplateData $templateData
+     *
+     * @return array
+     */
+    public static function getBooleanFilterLabel(TemplateData $templateData)
     {
+        $filters = [];
 
+        foreach ($templateData->getObjects() as $object) {
+            if ($object instanceof BooleanObject
+                && $object->isFilter()
+            ) {
+                $filters[] = [
+                    'key'   => $object->getKey(),
+                    'value' => $object->getFilterLabel(),
+                ];
+            }
+        }
+
+        return $filters;
+    }
+
+    /**
+     * @param array $templateDatas
+     *
+     * @return array
+     */
+    public static function getBooleanFilters(array $templateDatas)
+    {
+        $filters = [];
+
+        foreach ($templateDatas as $templateData) {
+            $filters = array_merge($filters, self::getBooleanFilterLabel($templateData));
+        }
+
+        return $filters;
     }
 }
