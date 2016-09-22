034df1d2641214f6a196bc2727e9f796eb238ca4
DimitriMorgan <dim.fruit@gmail.com>
2016-09-22 16:41:28 +0200

    607 - add user filter type

---
+++ src/Ui/Bundle/AdminBundle/Form/Type/User/FilterType.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\User;
+
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\TypeChoiceType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type', TypeChoiceType::class, [
+                'label'       => 'form.user_filter.children.type.label',
+                'placeholder' => '',
+                'event'       => $options['event'],
+                'locale'      => $options['locale'],
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setRequired(['event', 'locale']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'user_filter';
+    }
+}
