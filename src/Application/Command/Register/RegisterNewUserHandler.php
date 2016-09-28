666d6922ae1e836037722979ea88c4591e010d98
DimitriMorgan <dim.fruit@gmail.com>
2016-09-28 17:11:02 +0200

    607 - add typerepository as param of registerNewUserHandler

--- src/Application/Command/Register/RegisterNewUserHandler.php
+++ src/Application/Command/Register/RegisterNewUserHandler.php
@@ -15,6 +15,7 @@ use Proximum\Vimeet\Application\Adapter\SaltGeneratorInterface;
 use Proximum\Vimeet\Application\Exception\User\EmailAlreadyExistsException;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Model\UserEvent;
+use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\UserEventRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\UserRepositoryInterface;
 
@@ -41,21 +42,29 @@ class RegisterNewUserHandler
     private $userEventRepository;
 
     /**
+     * @var TypeRepositoryInterface
+     */
+    private $typeRepository;
+
+    /**
      * @param UserRepositoryInterface      $userRepository
      * @param PasswordEncoderInterface     $encoder
      * @param SaltGeneratorInterface       $saltGenerator
      * @param UserEventRepositoryInterface $userEventRepository
+     * @param TypeRepositoryInterface      $typeRepository
      */
     public function __construct(
         UserRepositoryInterface $userRepository,
         PasswordEncoderInterface $encoder,
         SaltGeneratorInterface $saltGenerator,
-        UserEventRepositoryInterface $userEventRepository
+        UserEventRepositoryInterface $userEventRepository,
+        TypeRepositoryInterface $typeRepository
     ) {
         $this->userRepository      = $userRepository;
         $this->encoder             = $encoder;
         $this->saltGenerator       = $saltGenerator;
         $this->userEventRepository = $userEventRepository;
+        $this->typeRepository = $typeRepository;
     }
 
     /**
@@ -70,7 +79,7 @@ class RegisterNewUserHandler
             throw new EmailAlreadyExistsException(sprintf('"%s" already exists.', $register->email));
         }
 
-        $type     = $this->get('vimeet_infrastructure.repository.type_repository')->getById($register->typeView->id);
+        $type     = $this->typeRepository->getById($register->typeView->id);
         $salt     = $this->saltGenerator->generate();
         $user     = new User($register->email, $salt, null, $register->locale);
         $password = $this->encoder->encode($user, $register->password);
