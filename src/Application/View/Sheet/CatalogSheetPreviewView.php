932c69c5ef79b59f65c1f35e0b5adf0b66d5ba03
Nico <nicolas.dievart@elao.com>
2016-10-03 17:04:46 +0200

    567 - Update preview with request

--- src/Application/View/Sheet/CatalogSheetPreviewView.php
+++ src/Application/View/Sheet/CatalogSheetPreviewView.php
@@ -10,6 +10,7 @@
 
 namespace Proximum\Vimeet\Application\View\Sheet;
 
+use Proximum\Vimeet\Domain\Model\Meeting;
 use Proximum\Vimeet\Domain\Template\TemplateObject;
 
 class CatalogSheetPreviewView
@@ -39,16 +40,31 @@ class CatalogSheetPreviewView
     public $preview;
 
     /**
-     * @param int    $id
-     * @param string $title
-     * @param string $type
-     * @param array  $preview
+     * @var Meeting\Request|null
      */
-    public function __construct($id, $title, $type, array $preview)
+    public $meetingRequest;
+
+    /**
+     * @param int             $id
+     * @param string          $title
+     * @param string          $type
+     * @param array           $preview
+     * @param Meeting\Request $meetingRequest
+     */
+    public function __construct($id, $title, $type, array $preview, Meeting\Request $meetingRequest = null)
     {
-        $this->id      = $id;
-        $this->title   = $title;
-        $this->type    = $type;
-        $this->preview = $preview;
+        $this->id             = $id;
+        $this->title          = $title;
+        $this->type           = $type;
+        $this->preview        = $preview;
+        $this->meetingRequest = $meetingRequest;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasMeetingRequest()
+    {
+        return null !== $this->meetingRequest;
     }
 }
