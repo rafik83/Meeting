7baa10c865847745b11cec31691469f54b472e6e
DimitriMorgan <dim.fruit@gmail.com>
2016-10-06 16:11:42 +0200

    594 - add command + handler

---
+++ src/Application/Command/Event/SearchFacet/Update.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Event\SearchFacet;
+
+use Proximum\Vimeet\Domain\Model\Event;
+
+class Update
+{
+    /**
+     * @var array
+     */
+    public $searchFacets;
+
+    /**
+     * @var bool
+     */
+    public $enabled;
+
+    /**
+     * @var string
+     */
+    public $type;
+
+    /**
+     * @var array
+     */
+    public $translations = [];
+
+    /**
+     * @var Event
+     */
+    public $event;
+
+    /**
+     * Update constructor.
+     *
+     * @param array $searchFacets
+     */
+    public function __construct(array $searchFacets)
+    {
+        $this->searchFacets = $searchFacets;
+    }
+}
---
+++ src/Application/Command/Event/SearchFacet/UpdateHandler.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Event\SearchFacet;
+
+use Proximum\Vimeet\Domain\Model\SearchFacet;
+use Proximum\Vimeet\Domain\Repository\SearchFacetRepositoryInterface;
+
+class UpdateHandler
+{
+    /**
+     * @var SearchFacetRepositoryInterface
+     */
+    private $searchFacetRepository;
+
+    /**
+     * UpdateHandler constructor.
+     *
+     * @param SearchFacetRepositoryInterface $searchFacetRepository
+     */
+    public function __construct(SearchFacetRepositoryInterface $searchFacetRepository)
+    {
+        $this->searchFacetRepository = $searchFacetRepository;
+    }
+
+    /**
+     * @param Update $update
+     */
+    public function handle(Update $update)
+    {
+        /** @var SearchFacet $searchFacet */
+        foreach ($update->searchFacets as $searchFacet) {
+
+            $update->enabled = $searchFacet->isEnabled();
+            $update->type    = $searchFacet->getType();
+            $update->event   = $searchFacet->getEvent();
+
+            foreach ($searchFacet->getTranslations() as $locale => $translation) {
+                $searchFacet->translate($locale, $translation->getLabel(), $translation->getPlaceholder());
+            }
+
+            if (null === $searchFacet->getId()) {
+                $this->searchFacetRepository->add($searchFacet);
+            } else {
+                $this->searchFacetRepository->set($searchFacet);
+            }
+        }
+    }
+}
