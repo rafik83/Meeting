b7dbaed955269412958825096d3760ac13c689d9
Nico <nicolas.dievart@elao.com>
2016-10-06 17:11:59 +0200

    617 - update approve type

--- src/Ui/Bundle/EventBundle/Form/Type/Meeting/Request/MeetingRequestApproveType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Meeting/Request/MeetingRequestApproveType.php
@@ -11,47 +11,11 @@
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Meeting\Request;
 
 use Proximum\Vimeet\Application\Command\Meeting\ApproveRequest;
-use Proximum\Vimeet\Domain\Model\Participant;
 use Proximum\Vimeet\Domain\Model\Sheet;
-use Proximum\Vimeet\Domain\Template\ParticipantInfoGuesser;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MeetingRequestApproveType extends AbstractType
+class MeetingRequestApproveType extends AbstractMeetingRequestType
 {
-    /**
-     * @var ParticipantInfoGuesser
-     */
-    private $participantInfoGuesser;
-
-    /**
-     * @param ParticipantInfoGuesser $participantInfoGuesser
-     */
-    public function __construct(ParticipantInfoGuesser $participantInfoGuesser)
-    {
-        $this->participantInfoGuesser = $participantInfoGuesser;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('toParticipants', ChoiceType::class, [
-                'choices'      => $options['sheet']->getParticipants(),
-                'expanded'     => true,
-                'multiple'     => true,
-                'required'     => false,
-                'choice_label' => function (Participant $participant) use ($options) {
-                    return $this->participantInfoGuesser
-                        ->guessParticipantCompleteName($participant, $options['locale']);
-                },
-            ]);
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -61,7 +25,14 @@ class MeetingRequestApproveType extends AbstractType
         $resolver->setAllowedTypes('sheet', Sheet::class);
         $resolver->setDefaults([
             'data_class' => ApproveRequest::class,
-            'submit'     => true,
         ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'catalog_approve_meeting_request';
     }
 }
