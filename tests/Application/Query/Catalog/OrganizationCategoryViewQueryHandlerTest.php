fd3f28130fde7fdbeafd8d1c5613155ffab6b626
Paul Boiseau <paulboiseau@gmail.com>
2016-10-12 18:02:13 +0200

    598 - unit test filters views

---
+++ tests/Application/Query/Catalog/OrganizationCategoryViewQueryHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Query\Catalog;
+
+use Proximum\Vimeet\Application\Components\Sheet\Template\Tag;
+use Proximum\Vimeet\Application\Query\Catalog\OrganizationCategoryViewQuery;
+use Proximum\Vimeet\Application\Query\Catalog\OrganizationCategoryViewQueryHandler;
+use Proximum\Vimeet\Domain\Catalog\TaggedNomenclatureFilterGetter;
+use Proximum\Vimeet\Domain\View\Catalog\OrganizationCategoryView;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class OrganisationCategoryViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event = EventFactory::createEvent();
+        $query = new OrganizationCategoryViewQuery($event, 'fr');
+
+        // Expected
+        $expectedCategoriesViews = [
+            new OrganizationCategoryView('category8', 'Association'),
+            new OrganizationCategoryView('category5', 'ETI'),
+            new OrganizationCategoryView('category10', 'Expert'),
+        ];
+
+        // Mock
+        $taggedNomenclatureFilterGetter = $this->prophesize(TaggedNomenclatureFilterGetter::class);
+
+        $taggedNomenclatureFilterGetter->getNomenclaturesItems(
+            $event,
+            Tag::SHEET_ORGANIZATION_CATEGORY,
+            'fr'
+        )->shouldBeCalled()->willReturn([
+            "category8"  => "Association",
+            "category5"  => "ETI",
+            "category10" => "Expert",
+        ]);
+
+        $handler         = new OrganizationCategoryViewQueryHandler($taggedNomenclatureFilterGetter->reveal());
+        $categoriesViews = $handler->handle($query);
+
+        $this->assertEquals($expectedCategoriesViews, $categoriesViews);
+    }
+}
---
+++ tests/Application/Query/Catalog/TypeViewQueryHandlerTest.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Query\Catalog;
+
+use Proximum\Vimeet\Application\Query\Catalog\TypeViewQuery;
+use Proximum\Vimeet\Application\Query\Catalog\TypeViewQueryHandler;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
+use Proximum\Vimeet\Domain\View\Catalog\TypeView;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class TypeViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event  = EventFactory::createEvent();
+        $locale = 'fr';
+
+        $typeOne = new Type($event);
+        $typeOne->translate($locale, 'type1', 'description');
+
+        $typeTwo = new Type($event);
+        $typeTwo->translate($locale, 'type2', 'description');
+
+        $visiblesType = [
+            '1' => $typeOne,
+            '2' => $typeTwo,
+        ];
+
+        $query = new TypeViewQuery($event, $visiblesType, 'fr');
+
+        // Mock
+        $typeRepository = $this->prophesize(TypeRepositoryInterface::class);
+
+        $typeRepository->getTypesTitleByEventAndLocale(
+            $query->event,
+            $query->locale,
+            $query->visibleTypes
+        )->shouldBeCalled()->willReturn([
+            '1' => 'type1',
+            '2' => 'type2',
+        ]);
+
+        $handler   = new TypeViewQueryHandler($typeRepository->reveal());
+        $typeViews = $handler->handle($query);
+        
+        $exceptedTypeViews = [
+            '1' => new TypeView(1, 'type1', 0),
+            '2' => new TypeView(2, 'type2', 0),
+        ];
+
+        $this->assertEquals($exceptedTypeViews, $typeViews);
+    }
+}
