6f3fdbaa1e547c18fe0d70b901b9571ba661b167
Paul Boiseau <paulboiseau@gmail.com>
2016-10-25 16:18:34 +0200

    634 - remove unused import

--- src/Ui/Bundle/EventBundle/Form/Type/Catalog/SearchType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Catalog/SearchType.php
@@ -16,7 +16,6 @@ use Proximum\Vimeet\Domain\View\Catalog\TypeView;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
