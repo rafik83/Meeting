149708a16570f76ce4152ea77076f4389b29b6ef
Richard Hanna <supertanuki@gmail.com>
2016-11-01 21:26:15 +0100

    Truncate tables instead of recreate schema before running scenario

---
+++ src/Behat/DBAL/Driver/PDOMySql/Driver.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Behat\DBAL\Driver\PDOMySql;
+
+use Doctrine\DBAL\Driver\PDOMySql\Driver as BaseDriver;
+
+class Driver extends BaseDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getDatabasePlatform()
+    {
+        return new Platform();
+    }
+}
