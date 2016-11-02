24111ff27bb1c4c5deb3ca03d00c7d07f0994990
Nico <nicolas.dievart@elao.com>
2016-11-02 16:09:41 +0100

    619 - Improve UnRefuse to use permission manager

--- src/Application/Command/Meeting/UnRefuseMeetingRequestHandler.php
+++ src/Application/Command/Meeting/UnRefuseMeetingRequestHandler.php
@@ -10,6 +10,8 @@
 
 namespace Proximum\Vimeet\Application\Command\Meeting;
 
+use Proximum\Vimeet\Application\Components\Meeting\RequestPermissionManager;
+use Proximum\Vimeet\Application\Exception\MeetingRequest\IsNotAllowedToUnRefuseMeetingRequestException;
 use Proximum\Vimeet\Domain\Repository\Meeting;
 
 class UnRefuseMeetingRequestHandler
@@ -25,22 +27,39 @@ class UnRefuseMeetingRequestHandler
     private $dateTime;
 
     /**
+     * @var RequestPermissionManager
+     */
+    private $permissionManager;
+
+    /**
      * @param Meeting\RequestRepositoryInterface $meetingRequestRepository
+     * @param RequestPermissionManager           $permissionManager
      * @param \DateTimeInterface                 $dateTime
      */
     public function __construct(
         Meeting\RequestRepositoryInterface $meetingRequestRepository,
+        RequestPermissionManager $permissionManager,
         \DateTimeInterface $dateTime
     ) {
         $this->meetingRequestRepository = $meetingRequestRepository;
+        $this->permissionManager        = $permissionManager;
         $this->dateTime                 = $dateTime;
     }
 
     /**
      * @param UnRefuseMeetingRequest $unRefuseMeetingRequest
+     *
+     * @throws IsNotAllowedToUnRefuseMeetingRequestException
      */
     public function handle(UnRefuseMeetingRequest $unRefuseMeetingRequest)
     {
+        if (!$this->permissionManager->isAllowedToUnRefuse(
+            $unRefuseMeetingRequest->editor,
+            $unRefuseMeetingRequest->meetingRequest,
+            $unRefuseMeetingRequest->sheet
+        )) {
+            throw new IsNotAllowedToUnRefuseMeetingRequestException();
+        }
         $unRefuseMeetingRequest->meetingRequest->unRefuse($this->dateTime);
         $this->meetingRequestRepository->set($unRefuseMeetingRequest->meetingRequest);
     }
