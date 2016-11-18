62a97c5d7af77830d5dbc6be2768953f49450920
Nico <nicolas.dievart@elao.com>
2016-11-18 15:02:06 +0100

    708 - Fix validator and add validator unit test

--- src/Domain/Order/Numero/Validator.php
+++ src/Domain/Order/Numero/Validator.php
@@ -19,6 +19,10 @@ class Validator
      */
     public static function isValid($numero)
     {
+        if (!is_string($numero)) {
+            return false;
+        }
+
         // The stucture of the numero should contains strickly 2 hyphens
         if (2 !== substr_count($numero, '-')) {
             return false;
@@ -28,7 +32,7 @@ class Validator
 
         // Each element between the hyphen should contains numbers
         foreach ($numeroElements as $element) {
-            if (empty($element) && !preg_match("/^[0-9]+$/", $element)) {
+            if (empty($element) || !preg_match("/^[0-9]+$/", $element)) {
                 return false;
             }
         }
