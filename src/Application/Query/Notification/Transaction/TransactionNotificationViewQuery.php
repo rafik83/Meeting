06919447fbf229894c3988161ddc89f5a90214f2
Paul Boiseau <paulboiseau@gmail.com>
2016-11-21 17:18:51 +0100

    667 - remove unsued import

--- src/Application/Query/Notification/Transaction/TransactionNotificationViewQuery.php
+++ src/Application/Query/Notification/Transaction/TransactionNotificationViewQuery.php
@@ -11,7 +11,6 @@
 namespace Proximum\Vimeet\Application\Query\Notification\Transaction;
 
 use Proximum\Vimeet\Domain\Model\Sheet;
-use Proximum\Vimeet\Domain\Model\User;
 
 class TransactionNotificationViewQuery
 {
