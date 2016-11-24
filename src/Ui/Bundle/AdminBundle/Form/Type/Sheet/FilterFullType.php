3b0a699ae330db9f1153ab04cc5768006639595e
DimitriMorgan <dim.fruit@gmail.com>
2016-11-24 15:20:17 +0100

    692 - fix insight

--- src/Ui/Bundle/AdminBundle/Form/Type/Sheet/FilterFullType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/FilterFullType.php
@@ -13,7 +13,6 @@ namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Sheet;
 use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\CategoryChoiceType;
 use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\TypeChoiceType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
