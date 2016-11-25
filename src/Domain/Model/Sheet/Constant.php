1d6cbea1025f9aad2430f6bb6825d4784bb02582
DimitriMorgan <dim.fruit@gmail.com>
2016-11-25 11:35:36 +0100

    714 - add constant non order by

--- src/Domain/Model/Sheet/Constant.php
+++ src/Domain/Model/Sheet/Constant.php
@@ -21,4 +21,5 @@ final class Constant
     const ORDER_BY_DATE_ADDED_TO_CATALOG = 'dateAddedToCatalog';
     const ORDER_BY_CREATED_AT            = 'created_at';
     const ORDER_BY_RELEVANCE             = 'relevance';
+    const NONE_ORDER_BY                  = 'none';
 }
