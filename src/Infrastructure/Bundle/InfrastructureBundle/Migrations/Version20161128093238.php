2eab6fd1182aee0da5ea29203c28e783325a042b
Richard Hanna <supertanuki@gmail.com>
2016-11-28 10:33:36 +0100

    Fix migrations

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161128093238.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161128093238.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Add days of the event
  */
-class Version20161125115504 extends AbstractMigration
+class Version20161128093238 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -17,6 +17,7 @@ class Version20161125115504 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE event_day (id INT AUTO_INCREMENT NOT NULL, event_id INT DEFAULT NULL, day DATETIME NOT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, INDEX IDX_F46FEC4371F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event_day ADD CONSTRAINT FK_F46FEC4371F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
     }
 
@@ -27,6 +28,6 @@ class Version20161125115504 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE event_day DROP FOREIGN KEY FK_F46FEC4371F7E88B');
+        $this->addSql('DROP TABLE event_day');
     }
 }
