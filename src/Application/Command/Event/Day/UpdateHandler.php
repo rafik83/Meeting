5a893480ab14a8b980dec80c7db1370807685814
Nico <nicolas.dievart@elao.com>
2016-11-29 16:49:16 +0100

    727 - Refactor update day command to stop dealing with the day

--- src/Application/Command/Event/Day/UpdateHandler.php
+++ src/Application/Command/Event/Day/UpdateHandler.php
@@ -36,25 +36,15 @@ class UpdateHandler
         $this->dayRepository->removeFromEvent($update->event);
 
         foreach ($update->days as $day) {
-            /** @var \DateTimeInterface $eventDay */
-            $eventDay = $day['day'];
-            // set the time to 0 to avoid problem with timezone
-            date_time_set($eventDay, 0, 0);
-
             /** @var \DateTimeInterface $eventStartTime */
             $eventStartTime = $day['startTime'];
-            // set the date to be the same as the $day['day'] to get the info if needed
-            date_date_set($eventStartTime, $eventDay->format('Y'), $eventDay->format('n'), $eventDay->format('d'));
 
             /** @var \DateTimeInterface $eventEndTime */
             $eventEndTime = $day['endTime'];
-            // set the date to be the same as the $day['day'] to get the info if needed
-            date_date_set($eventEndTime, $eventDay->format('Y'), $eventDay->format('n'), $eventDay->format('d'));
 
             $this->dayRepository->add(
                 new Day(
                     $update->event,
-                    $eventDay,
                     $eventStartTime,
                     $eventEndTime
                 )
