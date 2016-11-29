e64ff19718ca0c3363ca2257cb54dfc71cd3451d
DimitriMorgan <dim.fruit@gmail.com>
2016-11-29 17:15:53 +0100

    725 - add dashboardview

---
+++ src/Application/View/Sheet/DashboardView.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\View\Sheet;
+
+class DashboardView
+{
+    /**
+     * @var float|int
+     */
+    public $totalOrders;
+
+    /**
+     * @var float|int
+     */
+    public $totalRemainingToPay;
+
+    /**
+     * @var float|int
+     */
+    public $totalPaid;
+}
