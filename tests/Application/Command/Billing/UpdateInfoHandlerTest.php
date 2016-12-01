b39c321b00117f0055d04695f15d54a899b4455b
DimitriMorgan <dim.fruit@gmail.com>
2016-12-01 16:39:15 +0100

    billing/UpdateInfoHandlerTest

---
+++ tests/Application/Command/Billing/UpdateInfoHandlerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Billing;
+
+use DateTime;
+use Proximum\Vimeet\Domain\Model\Address;
+use Proximum\Vimeet\Domain\Model\BillingInfo;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\BillingInfoRepositoryInterface;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class UpdateInfoHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event       = EventFactory::createEvent();
+        $type        = new Type($event);
+        $user        = new User('test@test.fr', 'test', 'test', 'fr');
+        $dateTime    = new DateTime();
+        $sheet       = new Sheet($event, $type, [], $user, $dateTime);
+        $billingInfo = new BillingInfo($sheet);
+
+        // Command
+        $update = new UpdateInfo($billingInfo);
+        $update->gender    = 'male';
+        $update->lastname  = 'jean';
+        $update->firstname = 'maurice';
+        $update->function  = 'dj';
+        $update->company   = 'bestsound';
+        $update->phone     = '0197676545';
+        $update->mobile    = '0697676545';
+        $update->email     = 'jean.maurice@test.com';
+        $update->street    = '25 rue de la tarte';
+        $update->zipcode   = '75002';
+        $update->city      = 'Paris';
+        $update->country   = 'France';
+
+        // Expected =
+        $expectedBilling = new BillingInfo($sheet);
+        $expectedBilling->prefill(
+            'male',
+            'maurice',
+            'jean',
+            'dj',
+            'bestsound',
+            '0197676545',
+            '0697676545',
+            'jean.maurice@test.com',
+            new Address('25 rue de la tarte', '75002', 'Paris', 'France')
+        );
+
+        $billingInfoRepository = $this->prophesize(BillingInfoRepositoryInterface::class);
+        $billingInfoRepository->add($billingInfo)->shouldBeCalled();
+
+        $handler    = new UpdateInfoHandler($billingInfoRepository->reveal());
+        $updateInfo = $handler->handle($update);
+
+        $this->assertEquals($expectedBilling, $updateInfo->billingInfo);
+    }
+}
