8362b7c1a2694d5a150a010985d50ced176f81c4
Nico <nicolas.dievart@elao.com>
2016-12-01 17:10:12 +0100

    727 - fix migrations

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161128093238.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161128093238.php
@@ -17,7 +17,7 @@ class Version20161128093238 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE event_day (id INT AUTO_INCREMENT NOT NULL, event_id INT DEFAULT NULL, day DATETIME NOT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, INDEX IDX_F46FEC4371F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event_day (id INT AUTO_INCREMENT NOT NULL, event_id INT DEFAULT NULL, start_time DATETIME NOT NULL, end_time DATETIME NOT NULL, INDEX IDX_F46FEC4371F7E88B (event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event_day ADD CONSTRAINT FK_F46FEC4371F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
     }
 
