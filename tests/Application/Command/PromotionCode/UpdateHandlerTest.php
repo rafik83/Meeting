ef812cdfc3cf342fa4bd407029ef044d26d74195
DimitriMorgan <dim.fruit@gmail.com>
2016-12-01 15:56:53 +0100

    promotioncode/UpdateHandlerTest

---
+++ tests/Application/Command/PromotionCode/UpdateHandlerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\PromotionCode;
+
+use Proximum\Vimeet\Domain\Model\PromotionCode;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Promotion\Checker\UniqueCodeChecker;
+use Proximum\Vimeet\Domain\Repository\PromotionCodeRepositoryInterface;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event = EventFactory::createEvent();
+
+        // Expected
+        $promotionCode = new PromotionCode(
+            $event,
+            'promotionCodeTitle',
+            'TESTCODE',
+            10
+        );
+
+        $update = new Update($promotionCode);
+
+        $promotionCodeRepository = $this->prophesize(PromotionCodeRepositoryInterface::class);
+        $uniqueCodeChecker       = $this->prophesize(UniqueCodeChecker::class);
+
+        $promotionCodeRepository->set($promotionCode)->shouldBeCalled();
+        $uniqueCodeChecker->hasUniqueCode($promotionCode)->shouldBeCalled()->willReturn(true);
+
+        $handler = new UpdateHandler(
+            $promotionCodeRepository->reveal(),
+            $uniqueCodeChecker->reveal()
+        );
+
+        $handler->handle($update);
+    }
+}
