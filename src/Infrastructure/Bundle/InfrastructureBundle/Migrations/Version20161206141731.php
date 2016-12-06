e5b5475d58fdb16925c43f5151e421d1625200a7
DimitriMorgan <dim.fruit@gmail.com>
2016-12-06 15:54:49 +0100

    728 - clean commented lines + add description in migration file

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161206141731.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20161206141731.php
@@ -6,7 +6,12 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Migration related to feature 728 :
+ * To be able to set left color and right color configuration
+ * to a category regrouping sub-events
+ *
+ * Migration does :
+ * Adding right_color and left_color to table Happening_category
  */
 class Version20161206141731 extends AbstractMigration
 {
@@ -15,7 +20,6 @@ class Version20161206141731 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE happening_category ADD left_color VARCHAR(255) NOT NULL, ADD right_color VARCHAR(255) NOT NULL');
@@ -26,7 +30,6 @@ class Version20161206141731 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE happening_category DROP left_color, DROP right_color');
