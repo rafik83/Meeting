8933f438204631f40ffb1b16fcb605ac5216a615
Nico <nicolas.dievart@elao.com>
2016-12-06 16:32:27 +0100

    729 - Refacto category and add category mass unavailability

--- src/Domain/Model/Happening/Category.php
+++ src/Domain/Model/Happening/Category.php
@@ -11,139 +11,71 @@
 namespace Proximum\Vimeet\Domain\Model\Happening;
 
 use Doctrine\Common\Collections\ArrayCollection;
+use Proximum\Vimeet\Domain\Model\AbstractCategory;
 use Proximum\Vimeet\Domain\Model\Event;
 
-class Category
+class Category extends AbstractCategory
 {
     /**
      * @var int
      */
-    private $id;
-
-    /**
-     * @var Event
-     */
-    private $event;
-
-    /**
-     * @var string
-     */
-    private $picto;
-
-    /**
-     * @var int
-     */
-    private $position;
+    private $rank;
 
     /**
      * @var ArrayCollection
      */
-    private $translations;
-
-    /**
-     * @var string
-     */
-    private $leftColor;
-
-    /**
-     * @var string
-     */
-    private $rightColor;
+    protected $translations;
 
     /**
      * Category constructor.
      *
      * @param Event  $event
      * @param string $picto
-     * @param int    $position
+     * @param int    $rank
      * @param string $leftColor
      * @param string $rightColor
      */
-    public function __construct(
-        Event $event,
-        $picto,
-        $position,
-        $leftColor,
-        $rightColor
-    ) {
-        $this->position     = $position;
-        $this->event        = $event;
-        $this->picto        = $picto;
+    public function __construct(Event $event, $picto, $rank, $leftColor, $rightColor)
+    {
+        parent::__construct($event, $picto, $leftColor, $rightColor);
+
+        $this->rank         = $rank;
         $this->translations = new ArrayCollection();
-        $this->leftColor    = $leftColor;
-        $this->rightColor   = $rightColor;
     }
 
     /**
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return Event
-     */
-    public function getEvent()
-    {
-        return $this->event;
-    }
-
-    /**
-     * @param Event $event
-     */
-    public function setEvent($event)
-    {
-        $this->event = $event;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPicto()
-    {
-        return $this->picto;
-    }
-
-    /**
-     * Get position
+     * Get rank
      *
      * @return int
      */
-    public function getPosition()
+    public function getRank()
     {
-        return $this->position;
+        return $this->rank;
     }
 
     /**
-     * Set position
+     * Set rank
      *
-     * @param int $position
+     * @param int $rank
      *
      * @return Category
      */
-    public function setPosition($position)
+    public function setRank($rank)
     {
-        $this->position = $position;
+        $this->rank = $rank;
 
         return $this;
     }
 
     /**
-     * @param string $picto
+     * @param string $locale
+     * @param string $title
      */
-    public function setPicto($picto)
+    public function update($locale, $title)
     {
-        $this->picto = $picto;
-    }
-
-    /**
-     * @return ArrayCollection
-     */
-    public function getTranslations()
-    {
-        return $this->translations;
+        $this->translations->containsKey($locale)
+            ? $this->translations->get($locale)->setTitle($title)
+            : $this->translations->set($locale, new CategoryTranslation($this, $locale, $title));
     }
 
     /**
@@ -155,34 +87,22 @@ class Category
     }
 
     /**
+     * @return array
+     */
+    public function getTranslations()
+    {
+        return $this->translations->toArray();
+    }
+
+    /**
+     * @param string $locale
+     *
      * @return string
      */
-    public function getLeftColor()
+    public function getTitle($locale)
     {
-        return $this->leftColor;
-    }
-
-    /**
-     * @return string
-     */
-    public function getRightColor()
-    {
-        return $this->rightColor;
-    }
-
-    /**
-     * @param string $leftColor
-     */
-    public function setLeftColor($leftColor)
-    {
-        $this->leftColor = $leftColor;
-    }
-
-    /**
-     * @param string $rightColor
-     */
-    public function setRightColor($rightColor)
-    {
-        $this->rightColor = $rightColor;
+        return $this->translations->containsKey($locale)
+            ? $this->translations->get($locale)->getTitle()
+            : '';
     }
 }
