6f4f490c2aa06deb8bbb2f5e81b3e9af3989cc0c
Nico <nicolas.dievart@elao.com>
2016-12-06 16:31:32 +0100

    729 - Update Happening Category type

--- src/Ui/Bundle/AdminBundle/Form/Type/Picto/CategoryPictoType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Picto/CategoryPictoType.php
@@ -8,7 +8,7 @@
  * @author Elao <contact@elao.com>
  */
 
-namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Happening\Category;
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Picto;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
