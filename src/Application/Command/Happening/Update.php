bee0cae2625d23db76e23307b6df14a9878dea18
Nico <nicolas.dievart@elao.com>
2016-12-09 10:44:54 +0100

    758-730 - Update Happening model to deal with allowQuestion and limitParticipant

--- src/Application/Command/Happening/Update.php
+++ src/Application/Command/Happening/Update.php
@@ -45,6 +45,17 @@ class Update
      */
     public $talkings = [];
 
+
+    /**
+     * @var bool
+     */
+    public $allowQuestion;
+
+    /**
+     * @var int|null
+     */
+    public $limitParticipant;
+
     /**
      * @var bool
      */
