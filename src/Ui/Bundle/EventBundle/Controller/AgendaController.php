4296c2d5ed5384cb03764c809e087c3c5fc9bd91
Nico <nicolas.dievart@elao.com>
2016-12-09 19:00:22 +0100

    179 - Fix old index on agenda

--- src/Ui/Bundle/EventBundle/Controller/AgendaController.php
+++ src/Ui/Bundle/EventBundle/Controller/AgendaController.php
@@ -10,45 +10,24 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller;
 
-use Proximum\Vimeet\Application\Exception\Happening\HappeningException;
-use Proximum\Vimeet\Application\Query\Happening\HappeningViewQuery;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AgendaController extends Controller
 {
     /**
-     * @param Request     $request
      * @param EventDomain $eventDomain
      *
      * @return Response
      */
-    public function indexAction(Request $request, EventDomain $eventDomain)
+    public function indexAction(EventDomain $eventDomain)
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
         $this->denyAccessUnlessGranted('PERMISSION_HAPPENING_ACCESS', $eventDomain->getEvent());
 
-        if ($request->attributes->get('day') === null) {
-            return $this->redirectToRoute('happening_program_day', ['day' => 1]);
-        }
-
-        try {
-            $happeningListView = $this->get('tactician.commandbus.query')->handle(
-                new HappeningViewQuery(
-                    $eventDomain->getEvent(),
-                    $request->getLocale(),
-                    $request->attributes->get('day')
-                )
-            );
-        } catch (HappeningException $exception) {
-            return $this->redirectToRoute('event_sheet');
-        }
-
         return $this->render('EventBundle:Agenda:index.html.twig', [
-            'event'         => $eventDomain->getEvent(),
-            'happeningList' => $happeningListView,
+            'event' => $eventDomain->getEvent(),
         ]);
     }
 }
