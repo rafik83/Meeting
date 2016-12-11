29ed1e42d0fb5964bf2cc7c083cf80c15e96882f
Nico <nicolas.dievart@elao.com>
2016-12-11 17:03:18 +0100

    735 - Add mass unavailbility model and translation and repository

---
+++ src/Domain/Model/Unavailability/MassTranslation.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model\Unavailability;
+
+class MassTranslation
+{
+    /** @var int */
+    private $id;
+
+    /** @var string */
+    private $locale;
+
+    /** @var Mass */
+    private $mass;
+
+    /** @var string */
+    private $title;
+
+    /** @var string */
+    private $description;
+
+    /**
+     * @param Mass   $mass
+     * @param string $locale
+     * @param string $title
+     * @param string $description
+     */
+    public function __construct(
+        Mass $mass,
+        $locale,
+        $title,
+        $description
+    ) {
+        $this->mass        = $mass;
+        $this->locale      = $locale;
+        $this->title       = $title;
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale()
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @return Mass
+     */
+    public function getMass()
+    {
+        return $this->mass;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
---
+++ src/Infrastructure/Repository/Unavailability/MassRepository.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Repository\Unavailability;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Unavailability\Mass;
+use Proximum\Vimeet\Domain\Repository\Unavailability\MassRepositoryInterface;
+
+class MassRepository implements MassRepositoryInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(Mass $mass)
+    {
+        $this->entityManager->persist($mass);
+        $this->entityManager->flush($mass);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function update(Mass $mass)
+    {
+        $this->entityManager->flush($mass);
+
+        foreach ($mass->getTranslations() as $translation) {
+            $this->entityManager->flush($translation);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByEvent(Event $event, $locale)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('mass')
+            ->from(Mass::class, 'mass')
+            ->join('mass.translations', 'translation', 'WITH', 'translation.locale = :locale')
+            ->where('category.event = :event')
+            ->setParameter('event', $event)
+            ->setParameter('locale', $locale);
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+}
