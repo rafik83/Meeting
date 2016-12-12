b132dccbc8db136ba8107930bb30f137b4edcc9c
DimitriMorgan <dim.fruit@gmail.com>
2016-12-12 11:55:00 +0100

    776 - query + handler

---
+++ src/Application/Query/Navigation/Category/ProgramViewQueryHandler.php
@@ -0,0 +1,94 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Navigation\Category;
+
+use Proximum\Vimeet\Application\Components\Navigation\Category;
+use Proximum\Vimeet\Application\View\Navigation\CategoryView;
+use Proximum\Vimeet\Application\View\Navigation\LinkView;
+use Proximum\Vimeet\Domain\KeyDates\Checker\HappeningsAccessChecker;
+use Proximum\Vimeet\Domain\Model\Happening;
+use Proximum\Vimeet\Domain\Navigation\NavigationBuilderInterface;
+use Proximum\Vimeet\Infrastructure\Repository\HappeningRepository;
+
+class ProgramViewQueryHandler
+{
+    /**
+     * @var HappeningRepository
+     */
+    private $happeningRepository;
+
+    /**
+     * @var HappeningsAccessChecker
+     */
+    private $happeningsAccessChecker;
+
+    /**
+     * @var NavigationBuilderInterface
+     */
+    private $navigationBuilder;
+
+    /**
+     * ProgramViewQueryHandler constructor.
+     *
+     * @param NavigationBuilderInterface $navigationBuilder
+     * @param HappeningsAccessChecker    $happeningsAccessChecker
+     * @param HappeningRepository        $happeningRepository
+     */
+    public function __construct(
+        NavigationBuilderInterface $navigationBuilder,
+        HappeningsAccessChecker $happeningsAccessChecker,
+        HappeningRepository $happeningRepository
+    ) {
+        $this->happeningRepository     = $happeningRepository;
+        $this->happeningsAccessChecker = $happeningsAccessChecker;
+        $this->navigationBuilder       = $navigationBuilder;
+    }
+
+    /**
+     * @param ProgramViewQuery $programViewQuery
+     *
+     * @return null|CategoryView
+     */
+    public function handle(ProgramViewQuery $programViewQuery)
+    {
+        if (!$this->happeningsAccessChecker->allowedToAccess($programViewQuery->sheet->getEvent())) {
+            return null;
+        }
+
+        $linksView = [];
+
+        $happenings = $this->happeningRepository->findListByEvent(
+            $programViewQuery->sheet->getEvent(),
+            $programViewQuery->locale
+        );
+
+        /** @var Happening $happening */
+        foreach ($happenings as $happening) {
+            $categoryTitle = $happening->getCategory()->getTitle($programViewQuery->locale);
+            $categories[$categoryTitle] = $happening->getCategory();
+        }
+
+        usort($categories, function($previousCategory, $nextCategory) {
+            return strcmp($previousCategory->getRank(), $nextCategory->getRank());
+        });
+
+        /** @var Happening\Category $category */
+        foreach ($categories as $category) {
+            $linksView[] = new LinkView(
+                $category->getTitle($programViewQuery->locale),
+                $this->navigationBuilder->getRoute('event_sheet_locale', ['locale' => $programViewQuery->locale]),
+                null
+            );
+        }
+
+        return new CategoryView(Category::PROGRAM, Category::PROGRAM_ICON, $linksView);
+    }
+}
