21261b8d6439ee5d562c36f69c59454e53774865
Nico <nicolas.dievart@elao.com>
2016-12-20 00:24:39 +0100

    180 - Add unavailability exception

---
+++ src/Application/Exception/Unavailability/TimeOutOfRangeException.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Exception\Unavailability;
+
+use Proximum\Vimeet\Domain\Model\Event\Day;
+
+class TimeOutOfRangeException extends UnavailabilityException
+{
+    const BEGIN = 'begin';
+    const END   = 'end';
+
+    /**
+     * @var Day
+     */
+    public $day;
+
+    /**
+     * @var string
+     */
+    public $period;
+
+    /**
+     * @param Day    $day
+     * @param string $period
+     */
+    public function __construct(Day $day, $period)
+    {
+        parent::__construct('Time selected is out of range');
+        $this->day    = $day;
+        $this->period = $period;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOutOfRangeAtEndOfDay()
+    {
+        return $this->period === TimeOutOfRangeException::END;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOutOfRangeAtBeginOfDay()
+    {
+        return $this->period === TimeOutOfRangeException::BEGIN;
+    }
+}
