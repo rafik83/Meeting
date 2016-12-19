dc6afd73c4875176fdf926e5a3423344b268565d
Nico <nicolas.dievart@elao.com>
2016-12-20 00:26:10 +0100

    180 - Fix interface and add method in repository for unavailability

--- src/Infrastructure/Repository/ParticipantRepository.php
+++ src/Infrastructure/Repository/ParticipantRepository.php
@@ -245,7 +245,8 @@ class ParticipantRepository implements ParticipantRepositoryInterface
         \DateTimeInterface $begin,
         \DateTimeInterface $end,
         Meeting $exceptedMeeting = null,
-        Happening $exceptedHappening = null
+        Happening $exceptedHappening = null,
+        $exceptAllUnavailabilities = false
     ) {
         $queryBuilder = $this
             ->entityManager
@@ -274,19 +275,6 @@ class ParticipantRepository implements ParticipantRepositoryInterface
                             OR :end BETWEEN slot.begin AND slot.end
                         )
                 )",
-                // Participant have not unavailability during this period
-                "NOT EXISTS (
-                    SELECT u.id
-                    FROM Entity:Unavailability u
-                    WHERE
-                        u.participant = participant
-                        AND (
-                            u.begin BETWEEN :begin AND :end
-                            OR u.end BETWEEN :begin AND :end
-                            OR :begin BETWEEN u.begin AND u.end
-                            OR :end BETWEEN u.begin AND u.end
-                        )
-                )",
                 // Participant have not happening during this period
                 "NOT EXISTS (
                     SELECT hp.id
@@ -304,6 +292,26 @@ class ParticipantRepository implements ParticipantRepositoryInterface
                 )"
             )
         );
+
+        if (false === $exceptAllUnavailabilities) {
+            $queryBuilder->andWhere(
+                $queryBuilder->expr()->andX(
+                     // Participant have not unavailability during this period
+                    "NOT EXISTS (
+                        SELECT u.id
+                        FROM Entity:Unavailability u
+                        WHERE
+                            u.participant = participant
+                            AND (
+                                u.begin BETWEEN :begin AND :end
+                                OR u.end BETWEEN :begin AND :end
+                                OR :begin BETWEEN u.begin AND u.end
+                                OR :end BETWEEN u.begin AND u.end
+                            )
+                    )"
+                )
+            );
+        }
 
         if (null !== $exceptedMeeting) {
             $queryBuilder->setParameter('exceptedMeeting', $exceptedMeeting);
@@ -366,5 +374,16 @@ class ParticipantRepository implements ParticipantRepositoryInterface
         ;
 
         return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParticipantsWithoutMeetingAndHappening(
+        array $participants,
+        \DateTimeInterface $begin,
+        \DateTimeInterface $end
+    ) {
+        return $this->getAvailableParticipants($participants, $begin, $end, null, null, true);
     }
 }
