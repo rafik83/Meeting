f1104b771065c2db056e2024354c796badadcff7
Nico <nicolas.dievart@elao.com>
2016-12-22 19:01:46 +0100

    180 - do a correct check for null

--- src/Infrastructure/Repository/UnavailabilityRepository.php
+++ src/Infrastructure/Repository/UnavailabilityRepository.php
@@ -95,7 +95,7 @@ class UnavailabilityRepository implements UnavailabilityRepositoryInterface
             ->setParameter('begin', $unavailability->getBegin())
             ->setParameter('end', $unavailability->getEnd());
 
-        if ($unavailability->getId()) {
+        if (null !== $unavailability->getId()) {
             $queryBuilder
                 ->andWhere('unavailability.id != :id')
                 ->setParameter('id', $unavailability->getId());
