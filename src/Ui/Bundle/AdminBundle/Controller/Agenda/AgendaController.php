983e8c3c78e70ae0b4ac0d3bacd4750b6df64a13
Richard Hanna <supertanuki@gmail.com>
2016-12-23 15:17:37 +0100

    812 - Init agenda

---
+++ src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2016 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Agenda;
+
+use Proximum\Vimeet\Application\Query\Sheet\PaginatedSheetListViewQuery;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\PaginatedResult;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AgendaController extends Controller
+{
+    /**
+     * @param Request $request
+     * @param Event   $event
+     *
+     * @return Response
+     */
+    public function indexAction(Request $request, Event $event)
+    {
+        $this->denyAccessUnlessGranted('PERMISSION_EVENT_ACCESS', $event);
+
+        $query = new PaginatedSheetListViewQuery(
+            $event,
+            ['enable' => true],
+            $request->query->getInt('page', 1),
+            1000,
+            $request->getLocale(),
+            $this->getUser()
+        );
+
+        /** @var PaginatedResult $sheets */
+        $sheets = $this->get('tactician.commandbus.query')->handle($query);
+
+        return $this->render('AdminBundle:Agenda:index.html.twig', [
+            'event'  => $event,
+            'sheets' => $sheets,
+        ]);
+    }
+}
