af5ce2f0a45efc0394270639b88711935cd74216
DimitriMorgan <dim.fruit@gmail.com>
2016-12-28 14:35:04 +0100

    812 - update admin agenda controller

--- src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
+++ src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
@@ -10,10 +10,11 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Agenda;
 
-use Proximum\Vimeet\Application\Query\Sheet\PaginatedSheetListViewQuery;
+use Proximum\Vimeet\Application\Query\Agenda\SheetListViewQuery;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\PaginatedResult;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -27,19 +28,26 @@ class AgendaController extends Controller
      */
     public function indexAction(Request $request, Event $event)
     {
+        $test = 0;
         $this->denyAccessUnlessGranted('PERMISSION_EVENT_ACCESS', $event);
 
-        $query = new PaginatedSheetListViewQuery(
-            $event,
-            ['enable' => true],
-            $request->query->getInt('page', 1),
-            1000,
-            $request->getLocale(),
-            $this->getUser()
-        );
+        $sheets = [];
 
-        /** @var PaginatedResult $sheets */
-        $sheets = $this->get('tactician.commandbus.query')->handle($query);
+        if ($request->isXmlHttpRequest() && $test == 0) {
+
+            $query = new SheetListViewQuery($event, $request->getLocale());
+
+            /** @var PaginatedResult $sheets */
+            $sheets = $this->get('query.agenda.sheet_list_view_query_handler')->handle($query);
+
+            return new JsonResponse(
+                [
+                    'html' => $this->renderView('AdminBundle:Agenda:sheets-list.html.twig', [
+                        'sheets' => $sheets,
+                    ]),
+                ]
+            );
+        }
 
         return $this->render('AdminBundle:Agenda:index.html.twig', [
             'event'  => $event,
