4ce8bc857bb5398473bf637a36caff6a0d73c95d
Richard Hanna <supertanuki@gmail.com>
2017-01-06 15:02:11 +0100

    Clean controller

--- src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
+++ src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
@@ -12,7 +12,6 @@ namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Agenda;
 
 use Proximum\Vimeet\Application\Query\Agenda\SheetListViewQuery;
 use Proximum\Vimeet\Domain\Model\Event;
-use Proximum\Vimeet\Domain\Model\PaginatedResult;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,25 +28,18 @@ class AgendaController extends Controller
     public function indexAction(Request $request, Event $event)
     {
         $this->denyAccessUnlessGranted('PERMISSION_EVENT_ACCESS', $event);
-        $sheets = [];
 
         if ($request->isXmlHttpRequest()) {
-            $query = new SheetListViewQuery($event, $event->getAvailableLocale($request->getLocale()));
+            $sheets = $this->get('query.agenda.sheet_list_view_query_handler')->handle(
+                new SheetListViewQuery($event, $event->getAvailableLocale($request->getLocale()))
+            );
 
-            /** @var PaginatedResult $sheets */
-            $sheets = $this->get('query.agenda.sheet_list_view_query_handler')->handle($query);
-
-            return new JsonResponse([
-                'html' => $this->renderView('AdminBundle:Agenda:sheets-list.html.twig', [
-                    'event'  => $event,
-                    'sheets' => $sheets,
-                ]),
-            ]);
+            return new JsonResponse($sheets);
         }
 
         return $this->render('AdminBundle:Agenda:index.html.twig', [
             'event'  => $event,
-            'sheets' => $sheets,
+            'sheets' => [],
         ]);
     }
 }
