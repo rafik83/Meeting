5c74bbff90cbc5553895d5848671c88b5a91dabc
Nico <nicolas.dievart@elao.com>
2017-01-06 17:36:21 +0100

    747 - add route for export button

--- src/Ui/Bundle/AdminBundle/Controller/Planner/ExportController.php
+++ src/Ui/Bundle/AdminBundle/Controller/Planner/ExportController.php
@@ -14,6 +14,7 @@ use Proximum\Vimeet\Domain\Model\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class ExportController extends Controller
 {
@@ -27,8 +28,13 @@ class ExportController extends Controller
     {
         $this->denyAccessUnlessGranted('PERMISSION_EVENT_ACCESS', $event);
 
-        $content  = $this->get('service_planner.exporter')->getXML($event, $request->getLocale());
-        $response = new Response($content);
+        $content     = $this->get('service_planner.exporter')->getXML($event, $request->getLocale());
+        $response    = new Response($content);
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            sprintf("export_planner_%s_%s.xml", $event->getId(), date("Y_m_d_His"))
+        );
+        $response->headers->set('Content-Disposition', $disposition);
         $response->headers->set('Content-Type', 'xml');
 
         return $response;
