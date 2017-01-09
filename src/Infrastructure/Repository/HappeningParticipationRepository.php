a5718bb93c97b8f53ac9773e5638c0871a01675a
DimitriMorgan <dim.fruit@gmail.com>
2017-01-09 15:59:45 +0100

    671 - add checkAnyParticipation in happening participation repo

--- src/Infrastructure/Repository/HappeningParticipationRepository.php
+++ src/Infrastructure/Repository/HappeningParticipationRepository.php
@@ -91,6 +91,25 @@ class HappeningParticipationRepository implements HappeningParticipationReposito
     /**
      * {@inheritdoc}
      */
+    public function checkAnyParticipation(Participant $participant)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('participation.id')
+            ->from(HappeningParticipation::class, 'participation')
+            ->join('participation.happening', 'happening')
+            ->where('participation.participant = :participant')
+            ->setParameter('participant', $participant)
+        ;
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
     public function countParticipationByHappening(Happening $happening)
     {
         $queryBuilder = $this
