b6deab09ea07618fd00d1710e8fc7ce878fd84e7
DimitriMorgan <dim.fruit@gmail.com>
2017-01-11 15:30:55 +0100

    800 - add request view query/handler + service declaration

---
+++ src/Application/Query/Agenda/Admin/RequestViewQuery.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Agenda\Admin;
+
+use Proximum\Vimeet\Domain\Model\Meeting\Request;
+use Proximum\Vimeet\Domain\Model\Sheet;
+
+class RequestViewQuery
+{
+    /**
+     * @var Request
+     */
+    public $request;
+
+    /**
+     * @var Sheet
+     */
+    public $sheet;
+
+    /**
+     * @var string
+     */
+    public $locale;
+
+    /**
+     * RequestViewQuery constructor.
+     *
+     * @param Request $request
+     * @param Sheet   $sheet
+     * @param string  $locale
+     */
+    public function __construct(Request $request, Sheet $sheet, $locale)
+    {
+        $this->request = $request;
+        $this->sheet   = $sheet;
+        $this->locale  = $locale;
+    }
+}
---
+++ src/Application/Query/Agenda/Admin/RequestViewQueryHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Agenda\Admin;
+
+use Proximum\Vimeet\Application\Components\Sheet\SheetInfoGuesser;
+use Proximum\Vimeet\Application\View\Agenda\Admin\ParticipantView;
+use Proximum\Vimeet\Application\View\Agenda\Admin\RequestView;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Meeting\Request;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Template\ParticipantInfoGuesser;
+
+class RequestViewQueryHandler
+{
+    /**
+     * @var SheetInfoGuesser
+     */
+    private $sheetInfoGuesser;
+
+    /**
+     * @var ParticipantInfoGuesser
+     */
+    private $participantInfoGuesser;
+
+    /**
+     * RequestViewQueryHandler constructor.
+     *
+     * @param SheetInfoGuesser       $sheetInfoGuesser
+     * @param ParticipantInfoGuesser $participantInfoGuesser
+     */
+    public function __construct(
+        SheetInfoGuesser $sheetInfoGuesser,
+        ParticipantInfoGuesser $participantInfoGuesser
+    ) {
+
+        $this->sheetInfoGuesser       = $sheetInfoGuesser;
+        $this->participantInfoGuesser = $participantInfoGuesser;
+    }
+
+    /**
+     * @param RequestViewQuery $query
+     *
+     * @return RequestView
+     */
+    public function handle(RequestViewQuery $query)
+    {
+        $sheetMet = $query->request->getSheetMet($query->sheet);
+
+        return new RequestView(
+            $this->sheetInfoGuesser->guessSheetTitle($sheetMet, $query->locale),
+            $sheetMet->getId(),
+            $this->getParticipantViews($query->request, $query->sheet, $query->locale)
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param Sheet   $sheet
+     * @param string  $locale
+     *
+     * @return array
+     */
+    public function getParticipantViews(Request $request, Sheet $sheet, $locale)
+    {
+        $participantViews = [];
+        $participants     = $request->getParticipants($sheet);
+
+        foreach ($participants as $participant) {
+
+            $participantViews[] = new ParticipantView(
+                $participant->getId(),
+                $this->participantInfoGuesser->guessParticipantCompleteName($participant, $locale)
+            );
+        }
+
+        return $participantViews;
+    }
+}
