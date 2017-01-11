ba277f4d43ac56134856e05ecbde1d9f4dde2eea
DimitriMorgan <dim.fruit@gmail.com>
2017-01-11 15:30:00 +0100

    800 - add getUnassignedRequestsBySheetAndEvent in request repo

--- src/Infrastructure/Repository/Meeting/RequestRepository.php
+++ src/Infrastructure/Repository/Meeting/RequestRepository.php
@@ -310,6 +310,30 @@ class RequestRepository implements RequestRepositoryInterface
     /**
      * {@inheritdoc}
      */
+    public function getUnassignedRequestsBySheetAndEvent(Sheet $sheet, Event $event, $state)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('request')
+            ->from(Request::class, 'request')
+            ->join('request.to', 'toSheet', 'WITH', 'toSheet.event = :event')
+            ->join('request.from', 'fromSheet', 'WITH', 'fromSheet.event = :event')
+//            ->join('toSheet.participants', 'participant', 'WITH', 'participant.user = :user')
+            ->where('request.meeting IS NULL')
+            ->andWhere('request.to = :sheet OR request.from = :sheet')
+            ->andWhere('request.state = :state')
+            ->setParameter('sheet', $sheet)
+            ->setParameter('event', $event)
+            ->setParameter('state', $state);
+        ;
+
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     public function getRequestBetweenSheetsWithStates(Sheet $one, Sheet $another, array $states)
     {
         $queryBuilder = $this
