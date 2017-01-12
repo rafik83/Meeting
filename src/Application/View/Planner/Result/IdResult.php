adb260c03cd6a90ef7c140dc055c6a47dc5b3f20
Nico <nicolas.dievart@elao.com>
2017-01-12 14:18:50 +0100

    835 - Add planner result

---
+++ src/Application/View/Planner/Result/IdResult.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\View\Planner\Result;
+
+class IdResult
+{
+    /** @var int */
+    public $id;
+}
