d584cc9ee5f662712248767d9f45ea10968a3f93
Nico <nicolas.dievart@elao.com>
2017-01-13 12:54:11 +0100

    847 - Fix deprecated

--- src/Application/Query/Meeting/MeetingRequestViewQueryHandler.php
+++ src/Application/Query/Meeting/MeetingRequestViewQueryHandler.php
@@ -53,7 +53,7 @@ class MeetingRequestViewQueryHandler
 
         return new MeetingRequestView(
             $sheet,
-            $this->sheetInfoGuesser->guessSheetName($sheet, $query->locale),
+            $this->sheetInfoGuesser->guessSheetTitle($sheet, $query->locale),
             $this->getFilterState($query),
             $sheet->getType()->getTitle($query->locale),
             $query->meetingRequest->getCreatedAt(),
