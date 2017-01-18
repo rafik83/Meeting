8770f1a5ce98e9ac14c87585cd53f4c05419ee5a
Nico <nicolas.dievart@elao.com>
2017-01-18 14:40:32 +0100

    877 - fix unit test

--- tests/Application/Query/Agenda/AgendaViewQueryHandlerTest.php
+++ tests/Application/Query/Agenda/AgendaViewQueryHandlerTest.php
@@ -15,8 +15,11 @@ use Proximum\Vimeet\Application\Query\Agenda\AgendaViewQuery;
 use Proximum\Vimeet\Application\Query\Agenda\AgendaViewQueryHandler;
 use Proximum\Vimeet\Application\Query\Agenda\DayViewQuery;
 use Proximum\Vimeet\Application\Query\Agenda\DayViewQueryHandler;
+use Proximum\Vimeet\Application\Query\Agenda\ParticipantViewQuery;
+use Proximum\Vimeet\Application\Query\Agenda\ParticipantViewQueryHandler;
 use Proximum\Vimeet\Application\View\Agenda\AgendaView;
 use Proximum\Vimeet\Application\View\Agenda\DayView;
+use Proximum\Vimeet\Application\View\Agenda\ParticipantView;
 use Proximum\Vimeet\Domain\Model\Event\Day;
 use Proximum\Vimeet\Domain\Model\Happening;
 use Proximum\Vimeet\Domain\Model\HappeningParticipation;
@@ -55,12 +58,8 @@ class AgendaViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
         $unavailability = new Unavailability($participant, $begin, $end);
 
         // Mock
-        $sheetGuesser = $this->prophesize(SheetGuesser::class);
-        $sheetGuesser->getUserSheet($user, $event, 'fr')->shouldBeCalled()->willReturn($sheet);
-
         $dayRepository  = $this->prophesize(DayRepositoryInterface::class);
         $dayRepository->findByEvent($event)->shouldBeCalled()->willReturn([$day]);
-
 
         $happeningParticipationRepository = $this->prophesize(HappeningParticipationRepositoryInterface::class);
         $happeningParticipationRepository->findByParticipant($participant)->shouldBeCalled()->willReturn([
@@ -80,20 +79,101 @@ class AgendaViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
             ->willReturn($dayView)
         ;
 
+        $participantHandler = $this->prophesize(ParticipantViewQueryHandler::class);
+        $participantHandler->handle(new ParticipantViewQuery([$participant], 'fr'))->shouldBeCalled()->willReturn([new ParticipantView(1, 'fullName')]);
+
+        // Reflection
+        $reflection = new \ReflectionClass(User::class);
+        $property   = $reflection->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($user, 1);
+        $property->setAccessible(false);
+
         // Handler
         $handler = new AgendaViewQueryHandler(
-            $sheetGuesser->reveal(),
             $dayRepository->reveal(),
             $dayViewQueryHandler->reveal(),
             $happeningParticipationRepository->reveal(),
             $unavailabilityRepository->reveal(),
-            $massUnavailabilityRepository->reveal()
+            $massUnavailabilityRepository->reveal(),
+            $participantHandler->reveal()
         );
-        $result = $handler->handle(new AgendaViewQuery($event, $user, 'fr'));
-
+        $result = $handler->handle(new AgendaViewQuery($event, $sheet, $participant, $user, 'fr'));
 
         // Expected
-        $expected = new AgendaView([$dayView], $sheet);
+        $expected = new AgendaView([$dayView], $sheet, $participant, true, [new ParticipantView(1, 'fullName')]);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testHandleMultipleParticipant()
+    {
+        $event        = EventFactory::createEvent();
+        $user         = new User('user@vimeet.com', 'salt', 'password', 'fr');
+        $user2        = new User('user@vimeet.com2', 'salt2', 'password2', 'fr');
+        $sheet        = SheetFactory::create($event, $user);
+        $participant  = ParticipantFactory::create($sheet, $user);
+        $participant2 = ParticipantFactory::create($sheet, $user2);
+
+        $begin = new \DateTime('2016-10-12 10:00:00');
+        $end   = new \DateTime('2016-10-12 18:00:00');
+        $day   = new Day($event, $begin, $end);
+
+        $category = new Category($event, 'picto', 'title', 'leftColor', 'rightColor');
+        $mass     = new Mass($event, $category, 'name', $begin, $end, true);
+
+        $categoryH = new Happening\Category($event, 'picto', 1, 'leftColor', 'rightColor');
+        $happening = new Happening($event, $begin, $end, $categoryH, false, 100);
+        $happeningParticipation = new HappeningParticipation($happening, $participant2);
+
+        $unavailability = new Unavailability($participant2, $begin, $end);
+
+        // Mock
+        $dayRepository  = $this->prophesize(DayRepositoryInterface::class);
+        $dayRepository->findByEvent($event)->shouldBeCalled()->willReturn([$day]);
+
+        $happeningParticipationRepository = $this->prophesize(HappeningParticipationRepositoryInterface::class);
+        $happeningParticipationRepository->findByParticipant($participant2)->shouldBeCalled()->willReturn([
+            $happeningParticipation
+        ]);
+
+        $unavailabilityRepository = $this->prophesize(UnavailabilityRepositoryInterface::class);
+        $unavailabilityRepository->findByParticipant($participant2)->shouldBeCalled()->willReturn([$unavailability]);
+
+        $massUnavailabilityRepository     = $this->prophesize(MassRepositoryInterface::class);
+        $massUnavailabilityRepository->findByEvent($event, 'fr')->shouldBeCalled()->willReturn([$mass]);
+        $dayViewQueryHandler              = $this->prophesize(DayViewQueryHandler::class);
+        $dayView = new DayView($begin, $end, $event->getConfiguration()->getScheduleScale(), [], [], []);
+        $dayViewQueryHandler
+            ->handle(new DayViewQuery($day, 'fr', [$happeningParticipation], [$unavailability], [$mass]))
+            ->shouldBeCalled()
+            ->willReturn($dayView)
+        ;
+
+        $participantHandler = $this->prophesize(ParticipantViewQueryHandler::class);
+        $participantHandler->handle(new ParticipantViewQuery([$participant, $participant2], 'fr'))->shouldBeCalled()->willReturn([new ParticipantView(1, 'fullName'), new ParticipantView(2, 'fullName2')]);
+
+        // Reflection
+        $reflection = new \ReflectionClass(User::class);
+        $property   = $reflection->getProperty('id');
+        $property->setAccessible(true);
+        $property->setValue($user, 1);
+        $property->setValue($user2, 2);
+        $property->setAccessible(false);
+
+        // Handler
+        $handler = new AgendaViewQueryHandler(
+            $dayRepository->reveal(),
+            $dayViewQueryHandler->reveal(),
+            $happeningParticipationRepository->reveal(),
+            $unavailabilityRepository->reveal(),
+            $massUnavailabilityRepository->reveal(),
+            $participantHandler->reveal()
+        );
+        $result = $handler->handle(new AgendaViewQuery($event, $sheet, $participant2, $user2, 'fr'));
+
+        // Expected
+        $expected = new AgendaView([$dayView], $sheet, $participant2, false, [new ParticipantView(1, 'fullName'), new ParticipantView(2, 'fullName2')]);
 
         $this->assertEquals($expected, $result);
     }
