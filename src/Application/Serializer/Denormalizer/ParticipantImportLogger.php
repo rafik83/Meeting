f03dfbf01fb38a4abdf182475706909d7488a801
LamGui <guillaume.webdev@gmail.com>
2017-01-19 11:18:48 +0100

    820-Retrieve the good csv file number on logger

--- src/Application/Serializer/Denormalizer/ParticipantImportLogger.php
+++ src/Application/Serializer/Denormalizer/ParticipantImportLogger.php
@@ -91,7 +91,7 @@ class ParticipantImportLogger
     public function addError($row, ValidatorError $validatorError, $data, $locale)
     {
         $newError = [
-            $row,
+            $row + 2, // We've removed first and last line, increment 2 to get original file line number
             $this->translatorAdapter->trans($validatorError->getMessage(), [], 'validators', $locale),
         ];
 
