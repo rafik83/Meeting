9f8151a3e24bf3874a6875e3b120e1c3651786d9
Nico <nicolas.dievart@elao.com>
2017-01-19 21:34:47 +0100

    Add markdown formatter service

---
+++ src/Domain/Service/MarkdownFormatter.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Service;
+
+use Proximum\Vimeet\Domain\Exception\Service\Markdown\HeadingNotSupportedException;
+
+final class MarkdownFormatter
+{
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function bold($string)
+    {
+        return sprintf('**%s**', $string);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function italic($string)
+    {
+        return sprintf('_%s_', $string);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function strikethrough($string)
+    {
+        return sprintf('~~%s~~', $string);
+    }
+
+    /**
+     * @param array $list
+     *
+     * @return string
+     */
+    public static function lists(array $list)
+    {
+        return sprintf('%s%s',
+            implode(PHP_EOL, array_map(function ($item) {
+                return sprintf('- %s', $item);
+            }, $list)),
+            PHP_EOL
+        );
+    }
+
+    /**
+     * @param string      $string
+     * @param string|null $format
+     *
+     * @return string
+     */
+    public static function highlight($string, $format = null)
+    {
+        return sprintf('```%s%s%s%s```%s', $format, PHP_EOL, $string, PHP_EOL, PHP_EOL);
+    }
+
+    /**
+     * @param string $string
+     *
+     * @return string
+     */
+    public static function newLine($string)
+    {
+        return sprintf('%s%s', $string, PHP_EOL . PHP_EOL);
+    }
+
+    /**
+     * @param string      $link
+     * @param string|null $label
+     * @param string|null $title
+     *
+     * @return string
+     */
+    public static function link($link, $label = null, $title = null)
+    {
+        return sprintf(
+            '[%s](%s%s)',
+            null !== $label ? $label : $link,
+            $link,
+            null !== $title ? sprintf(' "%s"', $title) : null
+        );
+    }
+
+    /**
+     * @param string      $link
+     * @param string|null $label
+     * @param string|null $title
+     *
+     * @return string
+     */
+    public static function image($link, $label = null, $title = null)
+    {
+        return sprintf('!%s', self::link($link, $label, $title));
+    }
+
+    /**
+     * @param string $string
+     * @param int    $head
+     *
+     * @return string
+     *
+     * @throws HeadingNotSupportedException
+     */
+    public static function heading($string, $head)
+    {
+        if ($head > 6 || $head < 1) {
+            throw new HeadingNotSupportedException(
+                sprintf('The given heading (%s) is not supported, should be between 1 and 6', $head)
+            );
+        }
+
+        return sprintf('%s %s', str_repeat('#', $head), $string);
+    }
+}
