f7015c06e1efb3de215ec10119b27c33176be84b
Paul Boiseau <paulboiseau@gmail.com>
2017-01-27 16:34:33 +0100

    829 - migration spot unavailability

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170127153413.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170127153413 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE spot_unavailability (id INT AUTO_INCREMENT NOT NULL, spot_id INT DEFAULT NULL, slot_id INT DEFAULT NULL, INDEX IDX_7262A9442DF1D37C (spot_id), INDEX IDX_7262A94459E5119C (slot_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE spot_unavailability ADD CONSTRAINT FK_7262A9442DF1D37C FOREIGN KEY (spot_id) REFERENCES spot (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE spot_unavailability ADD CONSTRAINT FK_7262A94459E5119C FOREIGN KEY (slot_id) REFERENCES meeting_slot (id) ON DELETE CASCADE');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE spot_unavailability');
+    }
+}
