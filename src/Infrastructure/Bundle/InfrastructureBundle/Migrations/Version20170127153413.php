4bad166eb89aaab08da6acf6fd4eed17e25fa7f2
Paul Boiseau <paulboiseau@gmail.com>
2017-01-27 17:35:05 +0100

    829 - migration comment

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170127153413.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170127153413.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Migration create spot_unavailability table
  */
 class Version20170127153413 extends AbstractMigration
 {
