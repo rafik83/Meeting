cababd279c1387e9fedaddeb94ec6df227578678
LamGui <guillaume.webdev@gmail.com>
2017-01-27 17:00:37 +0100

    903 - Create unit test

--- tests/Application/Command/Spot/DeleteBatchHandlerTest.php
+++ tests/Application/Command/Spot/DeleteBatchHandlerTest.php
@@ -12,25 +12,104 @@ namespace Proximum\Vimeet\Tests\Application\Command\Spot;
 
 use Proximum\Vimeet\Application\Command\Spot\DeleteBatch;
 use Proximum\Vimeet\Application\Command\Spot\DeleteBatchHandler;
+use Proximum\Vimeet\Application\View\Spot\Batch\DeleteBatchView;
+use Proximum\Vimeet\Domain\Model\Meeting;
+use Proximum\Vimeet\Domain\Model\Spot;
 use Proximum\Vimeet\Domain\Repository\SpotRepositoryInterface;
 use Proximum\Vimeet\Tests\Factory\EventFactory;
+use Proximum\Vimeet\Tests\Factory\SheetFactory;
 
 class DeleteBatchHandlerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testHandle()
+    public function testDeleteSpot()
     {
         //Context
         $event = EventFactory::createEvent();
         $ids = [1, 2, 3, 4, 5];
+        $spots = [new Spot('REF', $event, 20.0, 1, 1, true)];
+
+        $expectedView = new DeleteBatchView(
+            ['REF']
+        );
 
         //Command
         $command = new DeleteBatch($ids, $event);
 
         $spotRepository = $this->prophesize(SpotRepositoryInterface::class);
-        $spotRepository->removeBatchSpot($ids, $event)->shouldBeCalled();
+
+        $spotRepository->getSpotsByIds($ids)->willReturn($spots);
+
+        $spotRepository->hasMeeting($spots[0])->shouldBeCalled()->willReturn(false);
+        
+        $spotRepository->removeBatchSpot(['REF'], $event)->shouldBeCalled();
 
         //Handler
         $handle = new DeleteBatchHandler($spotRepository->reveal());
-        $handle->handle($command);
+        $view = $handle->handle($command);
+
+        $this->assertEquals($view, $expectedView);
+    }
+    
+    public function testCannotDeleteSpotWithMeeting()
+    {
+        //Context
+        $event = EventFactory::createEvent();
+        $ids = [1, 2, 3, 4, 5];
+        $spots = [new Spot('REF', $event, 20.0, 1, 1, true)];
+
+        $expectedView = new DeleteBatchView(
+            [],
+            ['REF']
+        );
+
+        //Command
+        $command = new DeleteBatch($ids, $event);
+
+        $spotRepository = $this->prophesize(SpotRepositoryInterface::class);
+
+        $spotRepository->getSpotsByIds($ids)->willReturn($spots);
+
+        $spotRepository->hasMeeting($spots[0])->shouldBeCalled()->willReturn(true);
+
+        $spotRepository->removeBatchSpot([], $event)->shouldBeCalled();
+
+        //Handler
+        $handle = new DeleteBatchHandler($spotRepository->reveal());
+        $view = $handle->handle($command);
+
+        $this->assertEquals($view, $expectedView);
+    }
+
+    public function testCannotDeleteSpotWithSheet()
+    {
+        //Context
+        $event = EventFactory::createEvent();
+        $ids = [1, 2, 3, 4, 5];
+        $spot = new Spot('REF', $event, 20.0, 1, 1, true);
+        $sheet = SheetFactory::create($event);
+        $spot->addSheet($sheet);
+
+        $expectedView = new DeleteBatchView(
+            [],
+            [],
+            ['REF']
+        );
+
+        //Command
+        $command = new DeleteBatch($ids, $event);
+
+        $spotRepository = $this->prophesize(SpotRepositoryInterface::class);
+
+        $spotRepository->getSpotsByIds($ids)->willReturn([$spot]);
+
+        $spotRepository->hasMeeting($spot)->shouldBeCalled()->willReturn(false);
+
+        $spotRepository->removeBatchSpot([], $event)->shouldBeCalled();
+
+        //Handler
+        $handle = new DeleteBatchHandler($spotRepository->reveal());
+        $view = $handle->handle($command);
+
+        $this->assertEquals($view, $expectedView);
     }
 }
