0a1df9a6afb572209485cc1f0a9bee154aed7bb5
Nico <nicolas.dievart@elao.com>
2017-01-30 11:49:00 +0100

    738 - Update spot model with priority and visio

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170130095809.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add properties visio and priority to the Spot entity
+ */
+class Version20170130095809 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE spot ADD visio TINYINT(1) NOT NULL, ADD priority INT DEFAULT 12 NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE spot DROP visio, DROP priority');
+    }
+}
