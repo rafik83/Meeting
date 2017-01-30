8f3079e7b8237a0b115e9693bdab86628f60ae66
Nico <nicolas.dievart@elao.com>
2017-01-30 14:31:28 +0100

    738 - Fix unit test and add unit test

---
+++ tests/Application/Command/Spot/Action/UnVisioHandlerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Spot\Action;
+
+use Proximum\Vimeet\Application\Command\Spot\Action\UnVisio;
+use Proximum\Vimeet\Application\Command\Spot\Action\UnVisioHandler;
+use Proximum\Vimeet\Domain\Model\Spot;
+use Proximum\Vimeet\Domain\Repository\SpotRepositoryInterface;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class UnVisioHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        // Data
+        $event = EventFactory::createEvent();
+        $spot  = new Spot('ref', $event, 10, 3, 3, true, 10, true);
+
+        // Expected
+        $expectedSpot = new Spot('ref', $event, 10, 3, 3, true, 10, false);
+
+        // Mock
+        $spotRepository = $this->prophesize(SpotRepositoryInterface::class);
+        $spotRepository->set($expectedSpot)->shouldBeCalled();
+
+        // Handler
+        $handler = new UnVisioHandler($spotRepository->reveal());
+        $handler->handle(new UnVisio($spot));
+    }
+}
