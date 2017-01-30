4c0267dedd1078ed9926370b017a9997cfe88432
LamGui <guillaume.webdev@gmail.com>
2017-01-30 09:51:20 +0100

    903 - Enforce hasMeeting query check

--- src/Infrastructure/Repository/SpotRepository.php
+++ src/Infrastructure/Repository/SpotRepository.php
@@ -213,7 +213,7 @@ class SpotRepository implements SpotRepositoryInterface
             ->groupBy('meeting.spot')
         ;
 
-        return $queryBuilder->getQuery()->getResult() ? false : true;
+        return $queryBuilder->getQuery()->getResult() === 0 ? false : true;
     }
 
     /**
