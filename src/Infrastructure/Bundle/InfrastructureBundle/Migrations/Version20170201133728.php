f4d6d06160c14df442dec384506cd9689567cdc8
DimitriMorgan <dim.fruit@gmail.com>
2017-02-01 14:42:36 +0100

    848 - migration

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170201133728.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add disabled field into meeting_request
+ */
+class Version20170201133728 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting_request ADD disabled TINYINT(1) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting_request DROP disabled');
+    }
+}
