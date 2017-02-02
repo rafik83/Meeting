e0dcf4c37d711fcbc6fd2c64a375faacb1fdab84
DimitriMorgan <dim.fruit@gmail.com>
2017-02-02 11:20:50 +0100

    923 - add migration

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170202101935.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add disabled field in happening participation table
+ */
+class Version20170202101935 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE happening_participation ADD disabled INT DEFAULT  NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE happening_participation DROP disabled');
+    }
+}
