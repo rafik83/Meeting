4c1bdcbecb6b19f4c5392ddceb2a86a07b0cd1df
Maxime COLIN <maxime.colin@elao.com>
2017-02-02 17:40:44 +0100

    Add imported filter

---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Messaging/Campaign/ImportedChoiceType.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet website.
+ *
+ * Copyright © Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Messaging\Campaign;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImportedChoiceType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'placeholder' => 'form.sheet_filter.children.imported.all.label',
+            'choices' => [
+                'form.sheet_filter.children.imported.imported.label'                    => 'imported',
+                'form.sheet_filter.children.imported.imported_with_connection.label'    => 'imported_with_connection',
+                'form.sheet_filter.children.imported.imported_without_connection.label' => 'imported_without_connection',
+                'form.sheet_filter.children.imported.not_imported.label'                => 'not_imported',
+            ],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'messaging_campaign_imported_choice';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return ChoiceType::class;
+    }
+}
