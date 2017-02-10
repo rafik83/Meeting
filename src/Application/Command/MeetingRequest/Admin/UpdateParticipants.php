76c7263aeab7c596f2c80f4ded8b16442cbb8dca
Nico <nicolas.dievart@elao.com>
2017-02-10 13:30:42 +0100

    846 - Add UpdateParticipants command handler

---
+++ src/Application/Command/MeetingRequest/Admin/UpdateParticipants.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\MeetingRequest\Admin;
+
+use Proximum\Vimeet\Domain\Model\Meeting\Request;
+
+class UpdateParticipants
+{
+    /**
+     * @var Request
+     */
+    public $request;
+
+    /**
+     * @var array
+     */
+    public $fromParticipants;
+
+    /**
+     * @var array
+     */
+    public $toParticipants;
+
+    /**
+     * @param Request $request
+     * @param array   $fromParticipants array of from Participant ids
+     * @param array   $toParticipants   array of to Participant ids
+     */
+    public function __construct(Request $request, array $fromParticipants, array $toParticipants)
+    {
+        $this->request          = $request;
+        $this->fromParticipants = $fromParticipants;
+        $this->toParticipants   = $toParticipants;
+    }
+}
