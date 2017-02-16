193b958326fcf40543999d991012cd860c9c7bf6
DimitriMorgan <dim.fruit@gmail.com>
2017-02-16 14:48:40 +0100

    910 - add invoice prefix setting in create/update event

--- src/Application/Command/Event/Update.php
+++ src/Application/Command/Event/Update.php
@@ -45,6 +45,7 @@ class Update extends AbstractEvent
         $this->textColor     = $event->getConfiguration()->getTextColor();
         $this->organiserName = $event->getOrganiserName();
         $this->emailTeam     = $event->getEmailTeam();
+        $this->invoicePrefix = $event->getInvoicePrefix();
 
         foreach ($event->getTranslations() as $translation) {
             $this->translations[$translation->getLocale()] = [
