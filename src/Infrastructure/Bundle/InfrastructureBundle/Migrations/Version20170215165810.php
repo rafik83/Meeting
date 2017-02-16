64641e7629c9da337d4316cd467bf8486e2adcbb
DimitriMorgan <dim.fruit@gmail.com>
2017-02-16 14:50:44 +0100

    910 - migration

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170215165810.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170215165810 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE invoice_prefix (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, prefix VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE event ADD invoice_prefix_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA739FABE62 FOREIGN KEY (invoice_prefix_id) REFERENCES invoice_prefix (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_3BAE0AA739FABE62 ON event (invoice_prefix_id)');
+        $this->addSql('UPDATE event SET invoice_prefix_id = 1');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA739FABE62');
+        $this->addSql('DROP TABLE invoice_prefix');
+        $this->addSql('DROP INDEX IDX_3BAE0AA739FABE62 ON event');
+        $this->addSql('ALTER TABLE event DROP invoice_prefix_id');
+    }
+}
