24613215b636897d212d444152695b5a2e794b97
DimitriMorgan <dim.fruit@gmail.com>
2017-02-16 14:45:24 +0100

    910 - invoice prefix repository

---
+++ src/Domain/Repository/Invoice/PrefixRepositoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository\Invoice;
+
+use Proximum\Vimeet\Domain\Model\Invoice\Prefix;
+
+interface PrefixRepositoryInterface
+{
+    /**
+     * @param Prefix $prefix
+     */
+    public function add(Prefix $prefix);
+
+    /**
+     * @return array|null
+     */
+    public function getAll();
+}
