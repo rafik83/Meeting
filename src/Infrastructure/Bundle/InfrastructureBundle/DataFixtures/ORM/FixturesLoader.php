d6d13bb95dfa2e2ad522c20e43a9888525f405c5
Paul Boiseau <paulboiseau@gmail.com>
2017-02-17 15:53:00 +0100

    910 - fix behat test + add fixtures

--- src/Infrastructure/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/DataFixtures/ORM/FixturesLoader.php
@@ -71,6 +71,7 @@ class FixturesLoader extends AbstractFixture implements ContainerAwareInterface
             __DIR__ . '/Meeting/ASDDays2016-Meeting.yml',
             __DIR__ . '/Admin.yml',
             __DIR__ . '/AdminWithType.yml',
+            __DIR__ . '/InvoicePrefix.yml',
         ];
 
         $options = [
