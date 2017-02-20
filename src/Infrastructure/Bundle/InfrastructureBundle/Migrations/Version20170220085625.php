a0e7cdc93bcf7259d752bf11d19a4994b2b21855
LamGui <guillaume.webdev@gmail.com>
2017-02-20 10:11:06 +0100

    952 - Create migration for analyticsCode field

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170220085625.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add optional field `analyticsCode` to event table
+ */
+class Version20170220085625 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event ADD configuration_analytics_code VARCHAR(255) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE event DROP configuration_analytics_code');
+    }
+}
