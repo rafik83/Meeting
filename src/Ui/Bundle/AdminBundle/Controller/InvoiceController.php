fd9d5bf36409612be29f0e934e10d447d0cd6a5c
DimitriMorgan <dim.fruit@gmail.com>
2017-02-23 11:54:52 +0100

    765 - add invoice controller + export action/route

---
+++ src/Ui/Bundle/AdminBundle/Controller/InvoiceController.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller;
+
+use Proximum\Vimeet\Application\Serializer\Charset;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\View\Normalizer\InvoicesNormalizerView;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+
+class InvoiceController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function exportAction(Request $request)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ALLOWED_TO_ORGANIZE');
+
+        $charset        = Charset::WINDOWS_1252;
+        $normaliserView = new InvoicesNormalizerView($this->getUser());
+
+        $serializer    = $this->get('serializer');
+        $exportContent = $serializer->serialize($normaliserView, 'csv', [
+            'locale'  => $request->getLocale(),
+            'charset' => $charset,
+        ]);
+
+        $response    = new Response($exportContent);
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            "export_invoices_" . date("Y_m_d_His") . ".csv"
+        );
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->headers->set('Content-Type', sprintf('text/csv; charset=%s', $charset));
+
+        return $response;
+    }
+}
