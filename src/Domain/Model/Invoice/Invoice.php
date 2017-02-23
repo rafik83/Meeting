c59d393b0ae4c27939fc992c1e32172e70ccf419
LamGui <guillaume.webdev@gmail.com>
2017-02-23 11:21:16 +0100

    750 - Convert float to int on invoice model

--- src/Domain/Model/Invoice/Invoice.php
+++ src/Domain/Model/Invoice/Invoice.php
@@ -54,17 +54,17 @@ class Invoice
     private $invoiceIncrement;
 
     /**
-     * @var float
+     * @var int
      */
     private $total;
 
     /**
-     * @var float
+     * @var int
      */
     private $totalWithVat;
 
     /**
-     * @var float
+     * @var int
      */
     private $vatAmount;
 
@@ -76,15 +76,15 @@ class Invoice
     /**
      * Invoice constructor.
      *
-     * @param Event  $event
-     * @param Sheet  $sheet
-     * @param Prefix $prefix
-     * @param string $invoicePrefix
-     * @param string $invoiceYear
-     * @param string $invoiceIncrement
-     * @param float  $total
-     * @param float  $totalWithVat
-     * @param float  $vatAmount
+     * @param Event     $event
+     * @param Sheet     $sheet
+     * @param Prefix    $prefix
+     * @param string    $invoicePrefix
+     * @param string    $invoiceYear
+     * @param string    $invoiceIncrement
+     * @param int       $total
+     * @param int       $totalWithVat
+     * @param int       $vatAmount
      * @param \DateTime $createdAt
      */
     public function __construct(
@@ -144,7 +144,7 @@ class Invoice
     }
 
     /**
-     * @return float
+     * @return int
      */
     public function getTotal()
     {
@@ -152,7 +152,7 @@ class Invoice
     }
 
     /**
-     * @return float
+     * @return int
      */
     public function getTotalWithVat()
     {
@@ -160,7 +160,7 @@ class Invoice
     }
 
     /**
-     * @return float
+     * @return int
      */
     public function getVatAmount()
     {
