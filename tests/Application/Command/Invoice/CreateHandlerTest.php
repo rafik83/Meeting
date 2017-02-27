f5457a007d140eea4ce10612b4fa6deb79bd9c97
LamGui <guillaume.webdev@gmail.com>
2017-02-27 17:09:15 +0100

    750 - Add unit test for Invoice CreateHandler

---
+++ tests/Application/Command/Invoice/CreateHandlerTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Invoice;
+
+use Proximum\Vimeet\Application\Command\Invoice\Create;
+use Proximum\Vimeet\Application\Command\Invoice\CreateHandler;
+use Proximum\Vimeet\Domain\Model\Address;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Model\Invoice\Invoice;
+use Proximum\Vimeet\Domain\Model\Invoice\Prefix;
+use Proximum\Vimeet\Domain\Model\Order;
+use Proximum\Vimeet\Domain\Model\Order\BillingInfo;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\Invoice\InvoiceRepositoryInterface;
+use Proximum\Vimeet\Domain\View\Invoice\OrdersToInvoiceView;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class CreateHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event       = EventFactory::createEvent();
+        $date        = new \DateTime();
+        $admin       = new Admin('email@email.com', 'test', 'test', 'fr', 'test', 'test', 'ROLE_SUPER_ADMIN', $date);
+        $type        = new Type($event);
+        $user        = new User('test@test.com', 'salt', 'password', 'fr');
+        $sheet       = new Sheet($event, $type, [], $user, $date);
+        $address     = new Address('test', 'test', 'test', 'test');
+        $billingInfo = new BillingInfo('test', 'test', 'test','test', '0', '0', 'test@test.com', 'test', $address, 'FR42');
+        $order       = new Order($sheet, true, $billingInfo, 'test', $date);
+        $prefix      = new Prefix('Vimeet', 'Vi');
+        $invoice     = new Invoice($event, $sheet, $prefix, 'Vi', 2017, 1, 1000, 200, 1200, $date);
+        $orderToInvoiceView = new OrdersToInvoiceView([$order], [], 4200, 504, 4704);
+    
+        $invoiceRepository = $this->prophesize(InvoiceRepositoryInterface::class);
+        
+        $invoiceRepository->add($invoice)->shouldBeCalled()->willReturn($invoice);
+        
+        $create = new Create($event, $sheet, $prefix, 'Vi', 2017, 1, 1000, 200, 1200, $date);
+        
+        $handler = new CreateHandler($invoiceRepository->reveal());
+        
+        $handler->handle($create);
+    }
+}
