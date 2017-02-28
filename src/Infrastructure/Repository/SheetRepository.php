b5ab665b06abb0e89998c7f650381e58c11d859c
DimitriMorgan <dim.fruit@gmail.com>
2017-02-28 10:07:06 +0100

    963 - opti sql request

--- src/Infrastructure/Repository/SheetRepository.php
+++ src/Infrastructure/Repository/SheetRepository.php
@@ -433,7 +433,8 @@ class SheetRepository implements SheetRepositoryInterface
             ->createQueryBuilder()
             ->select('sheet')
             ->from(Sheet::class, 'sheet')
-            ->where('sheet.owner = :user OR EXISTS (SELECT p.id FROM Entity:Participant p WHERE p.user = :user AND p.sheet = sheet)')
+            ->leftJoin('sheet.participants', 'participant')
+            ->andWhere('sheet.owner = :user OR participant.user = :user')
             ->setParameter('user', $user)
         ;
 
