fc00e6fc1d78a79089fb4445119c2de7bf230389
DimitriMorgan <dim.fruit@gmail.com>
2017-03-01 17:11:59 +0100

    855 - replace callback by constraint/validator

--- src/Application/Command/Event/Day/Update.php
+++ src/Application/Command/Event/Day/Update.php
@@ -10,9 +10,7 @@
 
 namespace Proximum\Vimeet\Application\Command\Event\Day;
 
-use DateTimeZone;
 use Proximum\Vimeet\Domain\Model\Event;
-use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class Update
 {
@@ -39,30 +37,6 @@ class Update
                 'startTime' => $day->getStartTime(),
                 'endTime'   => $day->getEndTime(),
             ];
-        }
-    }
-
-    /**
-     * @param ExecutionContextInterface $context
-     */
-    public function validateSameDay(ExecutionContextInterface $context)
-    {
-        foreach ($this->days as $day) {
-            $startTime = new \DateTime(
-                $day['startTime']->format('Y-m-d'),
-                new DateTimeZone($this->event->getTimeZone())
-            );
-
-            $endTime = new \DateTime(
-                $day['endTime']->format('Y-m-d'),
-                new DateTimeZone($this->event->getTimeZone())
-            );
-
-            if ($startTime === $endTime) {
-                $context->buildViolation('validators.schedule_day.shouldBeTheSameDay')
-                    ->atPath('days')
-                    ->addViolation();
-            }
         }
     }
 }
---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Validator/Constraint/Event/HoursOnSameDayValidator.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Validator\Constraint\Event;
+
+use DateTimeZone;
+use Proximum\Vimeet\Application\Command\Event\Day\Update;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class HoursOnSameDayValidator extends ConstraintValidator
+{
+    /**
+     * @param Update      $update
+     * @param Constraint $constraint
+     */
+    public function validate($update, Constraint $constraint)
+    {
+        foreach ($update->days as $day) {
+            $startTime = new \DateTime($day['startTime']->format('Y-m-d H:i:s'));
+            $startTime->setTimezone(new DateTimeZone($update->event->getTimeZone()));
+
+            $endTime = new \DateTime($day['endTime']->format('Y-m-d H:i:s'));
+            $endTime->setTimezone(new DateTimeZone($update->event->getTimeZone()));
+
+            if ($startTime->format('Y-m-d') != $endTime->format('Y-m-d')) {
+                $this
+                    ->context
+                    ->buildViolation('validators.schedule_day.shouldBeTheSameDay')
+                    ->atPath('days')
+                    ->addViolation();
+            }
+        }
+    }
+}
