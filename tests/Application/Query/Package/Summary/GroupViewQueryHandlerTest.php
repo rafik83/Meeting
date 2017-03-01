dbd6fa65c86cbc7bb4d71766a33b0e433f145df7
Paul Boiseau <paulboiseau@gmail.com>
2017-03-01 14:38:10 +0100

    1068 - add summary group view query handler test

---
+++ tests/Application/Query/Package/Summary/GroupViewQueryHandlerTest.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Query\Package\Summary;
+
+use Prophecy\Argument;
+use Proximum\Vimeet\Application\Query\Package\Summary\GroupViewQuery;
+use Proximum\Vimeet\Application\Query\Package\Summary\GroupViewQueryHandler;
+use Proximum\Vimeet\Application\Query\Package\Summary\ProductViewQuery;
+use Proximum\Vimeet\Application\Query\Package\Summary\ProductViewQueryHandler;
+use Proximum\Vimeet\Application\View\Package\Summary\GroupView;
+use Proximum\Vimeet\Application\View\Package\Summary\PlanGroupView;
+use Proximum\Vimeet\Application\View\Package\Summary\ProductView;
+use Proximum\Vimeet\Domain\Cart\Cart;
+use Proximum\Vimeet\Domain\Model\CartRow;
+use Proximum\Vimeet\Domain\Model\Package;
+use Proximum\Vimeet\Domain\Model\PackageGroup;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+use Proximum\Vimeet\Tests\Factory\ProductFactory;
+use Proximum\Vimeet\Tests\Factory\SheetFactory;
+
+class GroupViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $datetime = new \DateTime();
+        $locale   = 'fr';
+        $event    = EventFactory::createEvent();
+        $type     = new Type($event);
+        $package  = new Package($event, 'Package1', $datetime);
+        $package->enable(false, false, true);
+        $sheet   = SheetFactory::create($event, null, $datetime, $type);
+        $product = ProductFactory::create($event, 'option');
+
+        $package->setPlanning($product);
+        $type->setPackage($package);
+
+        $cartRow = new CartRow($sheet, $product, 1);
+        $cart    = new Cart($sheet, [$cartRow], []);
+        $group   = new PackageGroup($package, 1);
+        $group->setOptions([$product]);
+
+        $planGroupView = new PlanGroupView('label', [], 0.0);
+
+        $productView = new ProductView(
+            1,
+            'Option1',
+            25,
+            1, // quantity
+            25, // total
+            $event->getMode(),
+            $event->getCurrency()
+        );
+
+        // Expected
+        $expectedGroupView = new GroupView('', [$productView], 25);
+
+        // Mock
+        $productViewQueryHandler = $this->prophesize(ProductViewQueryHandler::class);
+
+        $productViewQueryHandler->handle(Argument::that(function (ProductViewQuery $query) {
+            return true;
+        }))->shouldBeCalled()->willReturn($productView);
+
+        $handler           = new GroupViewQueryHandler($productViewQueryHandler->reveal());
+        $query             = new GroupViewQuery($sheet, $group, $cart, $locale, $planGroupView);
+        $planningGroupView = $handler->handle($query);
+
+        $this->assertEquals($planningGroupView, $expectedGroupView);
+    }
+}
