25038a6c7ab5259204976c8df0beea89e69b61e2
Paul Boiseau <paulboiseau@gmail.com>
2017-03-02 16:05:17 +0100

    961 - remove unused import

--- src/Ui/Bundle/AdminBundle/Form/Type/Sheet/BatchType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Sheet/BatchType.php
@@ -13,7 +13,6 @@ namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Sheet;
 use Proximum\Vimeet\Application\Command\Sheet\Batch;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
