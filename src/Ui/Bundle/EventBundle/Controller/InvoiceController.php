36aa47e1015d670ec65bc20b310d774417d11f63
Richard Hanna <supertanuki@gmail.com>
2017-03-10 15:43:34 +0100

    954 - init invoice route

---
+++ src/Ui/Bundle/EventBundle/Controller/InvoiceController.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller;
+
+use Proximum\Vimeet\Domain\Model\Invoice\Invoice;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class InvoiceController extends Controller
+{
+    /**
+     * @param EventDomain $eventDomain
+     * @param Sheet       $sheet
+     * @param Invoice     $invoice
+     * @param string      $hash
+     *
+     * @return Response
+     */
+    public function showAction(EventDomain $eventDomain, Sheet $sheet, Invoice $invoice, $hash)
+    {
+        dump($sheet, $invoice, $hash);
+
+        if ($invoice->getSheet() !== $sheet) {
+            throw $this->createNotFoundException();
+        }
+
+        $event = $eventDomain->getEvent();
+
+        exit;
+
+        return $this->render('EventBundle:Invoice:show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+}
