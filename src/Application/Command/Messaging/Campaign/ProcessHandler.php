7708e9b4b8dc3cdd0eed256f0adf9b868551062e
Nico <nicolas.dievart@elao.com>
2017-03-13 15:00:00 +0100

    Fix mailer locale

--- src/Application/Command/Messaging/Campaign/ProcessHandler.php
+++ src/Application/Command/Messaging/Campaign/ProcessHandler.php
@@ -98,12 +98,13 @@ class ProcessHandler
      */
     private function getReceivers(array $sheets, array $recipients, Message $message)
     {
+        $event                 = $message->getEvent();
         $sendToParticipants    = in_array(Campaign::RECIPIENT_PARTICIPANTS, $recipients, true);
         $sendToOwners          = in_array(Campaign::RECIPIENT_SHEET_OWNER, $recipients, true);
         $sendToBillingContacts = in_array(Campaign::RECIPIENT_BILLING_CONTACT, $recipients, true);
 
         $receivers    = [];
-        $addReceivers = function (Sheet $sheet, $newReceivers) use (&$receivers, $message) {
+        $addReceivers = function (Sheet $sheet, $newReceivers) use (&$receivers, $message, $event) {
             if (!is_array($newReceivers) && !$newReceivers instanceof \Traversable) {
                 return;
             }
@@ -123,7 +124,8 @@ class ProcessHandler
                         $sheet,
                         $receiver->getLocale(),
                         $placeholders
-                    )
+                    ),
+                    $event->getAvailableLocale($receiver->getLocale())
                 );
 
                 $receivers[$receiver->getEmail()] = $receiverView;
