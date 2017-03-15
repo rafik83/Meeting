93d12abad8eddadb3298b40973c91cb77b344fff
Richard Hanna <supertanuki@gmail.com>
2017-03-15 14:24:21 +0100

    954 - Fix country in billinginfo

--- src/Application/Query/Sheet/BillingInfos/BillingInfosQueryHandler.php
+++ src/Application/Query/Sheet/BillingInfos/BillingInfosQueryHandler.php
@@ -55,7 +55,7 @@ class BillingInfosQueryHandler
             $billingInfo->getAddress()->getStreet(),
             $billingInfo->getAddress()->getZipcode(),
             $billingInfo->getAddress()->getCity(),
-            $billingInfo->getAddress()->getCity(),
+            $billingInfo->getAddress()->getCountry(),
             $billingInfo->getVatNumber(),
             $billingInfo->getReference()
         );
