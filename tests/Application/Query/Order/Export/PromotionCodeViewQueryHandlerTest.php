8a41514e616fbf0866baf8016b8538b6856eaab0
Nico <nicolas.dievart@elao.com>
2017-03-20 11:17:59 +0100

    948 - Add PromotionCodeViewQueryHandlerTest

---
+++ tests/Application/Query/Order/Export/PromotionCodeViewQueryHandlerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Query\Order\Export;
+
+use Proximum\Vimeet\Application\Adapter\TranslatorInterface;
+use Proximum\Vimeet\Application\Query\Order\Export\PromotionCodeViewQuery;
+use Proximum\Vimeet\Application\Query\Order\Export\PromotionCodeViewQueryHandler;
+use Proximum\Vimeet\Application\View\Order\Export\PromotionCodeView;
+use Proximum\Vimeet\Domain\Model\PromotionCode;
+
+class PromotionCodeViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $adminLocale = 'fr';
+        $translator  = $this->prophesize(TranslatorInterface::class);
+        $promotionCode = $this->prophesize(PromotionCode::class);
+        $promotionCode->getTitle()->willReturn('promotion code title');
+        $promotionCode->getId()->willReturn(8);
+
+        $translator->trans('order.column.promotionCode.quantity', ['%promotionCodeTitle%' => 'promotion code title'], 'export', $adminLocale)->shouldBeCalled()->willReturn('promotion code title quantity');
+        $translator->trans('order.column.promotionCode.total', ['%promotionCodeTitle%' => 'promotion code title'], 'export', $adminLocale)->shouldBeCalled()->willReturn('promotion code title total');
+
+        $query = new PromotionCodeViewQuery($promotionCode->reveal(), $adminLocale);
+
+        $handler = new PromotionCodeViewQueryHandler($translator->reveal());
+        $result = $handler->handle($query);
+
+        $expected = new PromotionCodeView(8, 'promotion code title', 'promotion code title quantity', 'promotion code title total');
+
+        $this->assertEquals($expected, $result);
+    }
+}
