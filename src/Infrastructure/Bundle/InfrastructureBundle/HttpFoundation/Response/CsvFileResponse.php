7820d12c6a80171aad2673d4b7f9a2f77b8d9b9c
LamGui <guillaume.webdev@gmail.com>
2017-03-20 17:43:03 +0100

    969 - Add parameters for transactions export path

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/HttpFoundation/Response/CsvFileResponse.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elao
+ * Date: 20/03/2017
+ * Time: 17:15
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\HttpFoundation\Response;
+
+
+class CsvFileResponse
+{
+    
+}
