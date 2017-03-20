e35ee2be87eca2a8876bd57be2aa019cfba4b35f
LamGui <guillaume.webdev@gmail.com>
2017-03-20 17:43:39 +0100

    969 - Get CsvFileRepsonse from #1132

--- src/Infrastructure/Bundle/InfrastructureBundle/HttpFoundation/Response/CsvFileResponse.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/HttpFoundation/Response/CsvFileResponse.php
@@ -1,15 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: elao
- * Date: 20/03/2017
- * Time: 17:15
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
  */
 
 namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\HttpFoundation\Response;
 
+use Proximum\Vimeet\Application\Serializer\Charset;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
-class CsvFileResponse
+class CsvFileResponse extends Response
 {
-    
+    /**
+     * @param mixed  $file
+     * @param string $filename
+     * @param int    $status
+     * @param array  $headers
+     * @param string $charset
+     */
+    public function __construct(
+        $file,
+        $filename,
+        $status = 200,
+        $headers = [],
+        $charset = Charset::WINDOWS_1252
+    ) {
+        parent::__construct($file, $status, $headers);
+        
+        $disposition = $this->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename);
+        $this->headers->set('Content-Disposition', $disposition);
+        $this->headers->set('Content-Type', sprintf('text/csv; charset=%s', $charset));
+    }
 }
