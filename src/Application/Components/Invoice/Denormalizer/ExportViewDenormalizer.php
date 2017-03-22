d55e174a4ec992940056396c5beaf747b9c3e8d6
DimitriMorgan <dim.fruit@gmail.com>
2017-03-22 16:04:13 +0100

    765 - refacto generate view in denormalizer

--- src/Application/Components/Invoice/Denormalizer/ExportViewDenormalizer.php
+++ src/Application/Components/Invoice/Denormalizer/ExportViewDenormalizer.php
@@ -10,23 +10,51 @@
 
 namespace Proximum\Vimeet\Application\Components\Invoice\Denormalizer;
 
+use Proximum\Vimeet\Application\Command\Planning\SheetInfoGuesserCache;
+use Proximum\Vimeet\Application\Components\Sheet\SheetInfoGuesser;
 use Proximum\Vimeet\Application\View\Invoice\BillingInfosView;
 use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Invoice\Invoice;
+use Proximum\Vimeet\Domain\Money\AmountFormatter;
+use Proximum\Vimeet\Domain\Order\Balance;
 use Proximum\Vimeet\Domain\View\Invoice\ExportView;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+use IntlDateFormatter;
 
 class ExportViewDenormalizer implements DenormalizerInterface, DenormalizerAwareInterface
 {
     use DenormalizerAwareTrait;
 
     /**
+     * @var SheetInfoGuesser
+     */
+    private $sheetInfoGuesser;
+
+    /**
+     * @var Balance
+     */
+    private $balance;
+
+    /**
+     * @param SheetInfoGuesserCache $sheetInfoGuesser
+     * @param Balance               $balance
+     */
+    public function __construct(
+        SheetInfoGuesserCache $sheetInfoGuesser,
+        Balance $balance
+    ) {
+        $this->sheetInfoGuesser = $sheetInfoGuesser;
+        $this->balance          = $balance;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function denormalize($data, $class, $format = null, array $context = [])
     {
-        /** @var ExportView $invoice */
+        /** @var Invoice $invoice */
         $invoice = $context['invoice'];
 
         /** @var BillingInfosView $billingInfo */
@@ -34,10 +62,33 @@ class ExportViewDenormalizer implements DenormalizerInterface, DenormalizerAware
             ->denormalizer
             ->denormalize($data['billingInfosView'], BillingInfosView::class, $format, $context);
 
-        $invoice->vatNumber          = $billingInfo->vatNumber;
-        $invoice->billingInfoCountry = $billingInfo->country;
+        $sheetTitle = $this->sheetInfoGuesser->guessSheetTitle(
+            $invoice->getSheet(),
+            $invoice->getEvent()->getAvailableLocale($context['locale'])
+        );
 
-        return $invoice;
+        $invoiceDate = $this->getFormattedDate(
+            $context['dateFormatters'][$invoice->getEvent()->getId()],
+            $invoice->getCreatedAt()
+        );
+
+        $invoiceExportView = new ExportView(
+            $invoice->getEvent()->getId(),
+            $invoice->getEvent()->getTitle(),
+            $invoice->getSheet()->getOwner()->getId(),
+            $sheetTitle,
+            $invoice->getNumber(),
+            $invoiceDate,
+            AmountFormatter::centsToDecimalAmount($invoice->getTotal()),
+            AmountFormatter::centsToDecimalAmount($invoice->getTotalWithVat()),
+            $invoice->getVatAmount(),
+            AmountFormatter::centsToDecimalAmount($this->balance->getBalance($invoice->getSheet())),
+            $invoice->getEvent()->getConfiguration()->getAnalyticsCode(),
+            $billingInfo->vatNumber,
+            $billingInfo->country
+        );
+
+        return $invoiceExportView;
     }
 
     /**
@@ -45,6 +96,19 @@ class ExportViewDenormalizer implements DenormalizerInterface, DenormalizerAware
      */
     public function supportsDenormalization($data, $type, $format = null)
     {
-        return $type === ExportView::class && isset($data['billingInfosView']);
+        return $type === ExportView::class
+               && isset($data['billingInfosView'])
+               && isset($data['invoice']);
+    }
+
+    /**
+     * @param IntlDateFormatter  $dateFormatter
+     * @param \DateTimeInterface $date
+     *
+     * @return bool|string
+     */
+    private function getFormattedDate(IntlDateFormatter $dateFormatter, \DateTimeInterface $date)
+    {
+        return $dateFormatter->format($date) ? $dateFormatter->format($date) : '';
     }
 }
