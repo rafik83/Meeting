f1d3a947e86af8bdf6ae03e391b822a5b3615c39
Paul Boiseau <paulboiseau@gmail.com>
2017-04-03 17:16:16 +0200

    1010 - Add unit test for post batch validate

---
+++ tests/Application/Command/Sheet/PostBatch/PostBatchValidateHandlerTest.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Sheet\PostBatch;
+
+use Prophecy\Argument;
+use Proximum\Vimeet\Application\Command\Sheet\BatchCatalog;
+use Proximum\Vimeet\Application\Command\Sheet\BatchCatalogHandler;
+use Proximum\Vimeet\Application\Command\Sheet\PostBatch\PostBatchEnableDisable;
+use Proximum\Vimeet\Application\Command\Sheet\PostBatch\PostBatchEnableDisableHandler;
+use Proximum\Vimeet\Application\Components\Sheet\Request\EnableDisableManager;
+use Proximum\Vimeet\Application\Event\Events;
+use Proximum\Vimeet\Application\Event\Sheet\SheetEnableDisableEvent;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+use Proximum\Vimeet\Tests\Factory\SheetFactory;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class PostBatchValidateHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event  = EventFactory::createEvent();
+        $sheet1 = SheetFactory::create($event);
+        $sheet2 = SheetFactory::create($event);
+        $sheet3 = SheetFactory::create($event);
+        $admin  = new Admin('john@doe.com', 'salt', 'password', 'fr', 'john', 'doh', 'ROLE_ADMIN', new \DateTime());
+
+        // Mock
+        $batchCatalogHandler  = $this->prophesize(BatchCatalogHandler::class);
+        $eventDispatcher      = $this->prophesize(EventDispatcherInterface::class);
+        $datetime             = new \DateTime();
+        $enableDisableManager = $this->prophesize(EnableDisableManager::class);
+
+        $batchCatalogHandler->handle(Argument::type(BatchCatalog::class))->shouldNotBeCalled();
+
+        $enableDisableManager
+            ->update(Argument::type(Sheet::class), true)
+            ->shouldBeCalledTimes(3);
+
+        $eventDispatcher->dispatch(
+            Events::SHEET_ENABLE_DISABLE,
+            Argument::type(SheetEnableDisableEvent::class)
+        )->shouldBeCalledTimes(3);
+
+        $query   = new PostBatchEnableDisable([$sheet1, $sheet2, $sheet3], [1, 2, 3], $admin, true);
+        $handler = new PostBatchEnableDisableHandler(
+            $batchCatalogHandler->reveal(),
+            $eventDispatcher->reveal(),
+            $datetime,
+            $enableDisableManager->reveal()
+        );
+
+        $handler->handle($query);
+    }
+
+    public function testUnvalidateHandle()
+    {
+        $event  = EventFactory::createEvent();
+        $sheet1 = SheetFactory::create($event);
+        $sheet2 = SheetFactory::create($event);
+        $sheet3 = SheetFactory::create($event);
+        $admin  = new Admin('john@doe.com', 'salt', 'password', 'fr', 'john', 'doh', 'ROLE_ADMIN', new \DateTime());
+
+        // Mock
+        $batchCatalogHandler  = $this->prophesize(BatchCatalogHandler::class);
+        $eventDispatcher      = $this->prophesize(EventDispatcherInterface::class);
+        $datetime             = new \DateTime();
+        $enableDisableManager = $this->prophesize(EnableDisableManager::class);
+
+        $batchCatalogHandler->handle(Argument::type(BatchCatalog::class))->shouldBeCalled();
+
+        $enableDisableManager
+            ->update(Argument::type(Sheet::class), false)
+            ->shouldBeCalledTimes(3);
+
+        $eventDispatcher->dispatch(
+            Events::SHEET_ENABLE_DISABLE,
+            Argument::type(SheetEnableDisableEvent::class)
+        )->shouldBeCalledTimes(3);
+
+        $query   = new PostBatchEnableDisable([$sheet1, $sheet2, $sheet3], [1, 2, 3], $admin, false);
+        $handler = new PostBatchEnableDisableHandler(
+            $batchCatalogHandler->reveal(),
+            $eventDispatcher->reveal(),
+            $datetime,
+            $enableDisableManager->reveal()
+        );
+
+        $handler->handle($query);
+    }
+}
