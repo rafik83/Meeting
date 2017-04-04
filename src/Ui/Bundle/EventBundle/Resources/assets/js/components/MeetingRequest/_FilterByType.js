cc5e637eb4e859dfd9e1cc492f8c80904735e961
Nico <nicolas.dievart@elao.com>
2017-04-04 17:46:51 +0200

    973 - Filter by proposition or request on front GDR

---
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/MeetingRequest/_FilterByType.js
@@ -0,0 +1,76 @@
+function FilterByType(element, buttonsZone)
+{
+    // constant
+    this.requestConstant     = 'request';
+    this.propositionConstant = 'proposition';
+
+    this.filterByAllConstant = 'all';
+    this.filterByRequestsConstant = 'requests';
+    this.filterByPropositionsConstant = 'propositions';
+
+    this.element      = element;
+    this.buttonsZone  = buttonsZone;
+    this.buttons      = [];
+    this.requests     = [];
+    this.propositions = [];
+
+    [].forEach.call(this.element.querySelectorAll('[data-meeting-request]'), function (element) {
+        var attribute = element.getAttribute('data-meeting-request');
+
+        if (attribute === this.requestConstant) {
+            this.requests.push(element);
+        } else if (attribute === this.propositionConstant) {
+            this.propositions.push(element);
+        }
+    }.bind(this));
+
+    [].forEach.call(this.buttonsZone.querySelectorAll('button'), function (element) {
+        var attribute = element.getAttribute('data-meeting-request-filter-by');
+
+        if (attribute === this.filterByAllConstant
+            || attribute === this.filterByRequestsConstant
+            || attribute === this.filterByPropositionsConstant
+        ) {
+            element.addEventListener('click', function () {
+                this.filterByType(attribute, element);
+            }.bind(this));
+        }
+
+        this.buttons.push(element);
+    }.bind(this));
+}
+
+FilterByType.prototype.filterByType = function (type, buttonClicked)
+{
+    this.buttons.forEach(function (element) {
+        element.classList.remove('btn-primary');
+        element.classList.add('btn-default');
+    });
+
+    buttonClicked.classList.add('btn-primary');
+
+    if (type === this.filterByAllConstant) {
+        this.requests.forEach(function (element) {
+            element.style.display = 'flex';
+        });
+        this.propositions.forEach(function (element) {
+            element.style.display = 'flex';
+        });
+    } else if (type === this.filterByRequestsConstant) {
+        this.requests.forEach(function (element) {
+            element.style.display = 'flex';
+        });
+        this.propositions.forEach(function (element) {
+            element.style.display = 'none';
+        });
+    } else if (type === this.filterByPropositionsConstant) {
+        this.requests.forEach(function (element) {
+            element.style.display = 'none';
+        });
+        this.propositions.forEach(function (element) {
+            element.style.display = 'flex';
+        });
+    }
+};
+
+module.exports = FilterByType;
