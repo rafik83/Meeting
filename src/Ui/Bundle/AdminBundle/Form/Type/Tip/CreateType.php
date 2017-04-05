86677f37ddb3fda242811dab4b4084d8dd01b20e
LamGui <guillaume.webdev@gmail.com>
2017-04-05 14:33:41 +0200

    1013 - Refactor updateType and CreateType

--- src/Ui/Bundle/AdminBundle/Form/Type/Tip/CreateType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/Tip/CreateType.php
@@ -11,39 +11,10 @@
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Tip;
 
 use Proximum\Vimeet\Application\Command\Tip\Create;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CreateType extends AbstractType
+class CreateType extends TipType
 {
-    /** {@inheritdoc} */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title', TextType::class)
-            ->add('onMeetingManagement', CheckboxType::class, [
-                'required' => false,
-            ])
-            ->add('onCatalog', CheckboxType::class, [
-                'required' => false,
-            ])
-            ->add('onPrintPlanning', CheckboxType::class, [
-                'required' => false,
-            ])
-            ->add('translations', CollectionType::class, [
-                'entry_type'    => TipTranslationType::class,
-                'allow_add'     => true,
-                'allow_delete'  => true,
-                'label'         => false,
-            ])
-            ->add('submit', SubmitType::class);
-    }
-    
     /** {@inheritdoc} */
     public function configureOptions(OptionsResolver $resolver)
     {
---
+++ src/Ui/Bundle/AdminBundle/Form/Type/Tip/TipType.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Tip;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class TipType extends AbstractType
+{
+    /** {@inheritdoc} */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('onMeetingManagement', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('onCatalog', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('onPrintPlanning', CheckboxType::class, [
+                'required' => false,
+            ])
+            ->add('translations', CollectionType::class, [
+                'entry_type'    => TipTranslationType::class,
+                'allow_add'     => true,
+                'allow_delete'  => true,
+                'label'         => false,
+            ])
+            ->add('submit', SubmitType::class);
+    }
+}
