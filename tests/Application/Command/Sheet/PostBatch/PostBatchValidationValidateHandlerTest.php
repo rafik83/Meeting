c0bd15e7ca1a933a7f39a36f41984108f6c64222
Paul Boiseau <paulboiseau@gmail.com>
2017-04-05 15:25:16 +0200

    1010 - Add unit test post batch validation validate

---
+++ tests/Application/Command/Sheet/PostBatch/PostBatchValidationValidateHandlerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Sheet\PostBatch;
+
+use Prophecy\Argument;
+use Proximum\Vimeet\Application\Command\Sheet\PostBatch\PostBatchValidationValidate;
+use Proximum\Vimeet\Application\Command\Sheet\PostBatch\PostBatchValidationValidateHandler;
+use Proximum\Vimeet\Application\Event\Events;
+use Proximum\Vimeet\Application\Event\Sheet\SheetValidationValidateEvent;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+use Proximum\Vimeet\Tests\Factory\SheetFactory;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class PostBatchValidationValidateHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $event  = EventFactory::createEvent();
+        $sheet1 = SheetFactory::create($event);
+        $sheet2 = SheetFactory::create($event);
+        $sheet3 = SheetFactory::create($event);
+        $admin  = new Admin('john@doe.com', 'salt', 'password', 'fr', 'john', 'doh', 'ROLE_ADMIN', new \DateTime());
+
+        // Mock
+        $eventDispatcher = $this->prophesize(EventDispatcherInterface::class);
+        $datetime        = new \DateTime();
+
+        $eventDispatcher->dispatch(
+            Events::SHEET_VALIDATION_VALIDATE,
+            Argument::type(SheetValidationValidateEvent::class)
+        )->shouldBeCalledTimes(3);
+
+        $query   = new PostBatchValidationValidate([$sheet1, $sheet2, $sheet3], $admin);
+        $handler = new PostBatchValidationValidateHandler(
+            $eventDispatcher->reveal(),
+            $datetime
+        );
+
+        $handler->handle($query);
+    }
+}
