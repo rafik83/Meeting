de438c47e1e0b8e889c20f50b5d953933e988bba
Nico <nicolas.dievart@elao.com>
2017-04-06 12:25:33 +0200

    1079 - Add behat test for invoice numero

---
+++ src/Behat/Context/Domain/InvoiceContext.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Behat\Context\Domain;
+
+use Behat\Behat\Context\Context;
+use Proximum\Vimeet\Behat\Context\Domain\Proxy\InvoiceContextProxyInterface;
+
+class InvoiceContext implements Context
+{
+    /** @var InvoiceContextProxyInterface */
+    private $invoiceContextProxy;
+
+    /**
+     * @param InvoiceContextProxyInterface $invoiceContextProxy
+     */
+    public function __construct(InvoiceContextProxyInterface $invoiceContextProxy)
+    {
+        $this->invoiceContextProxy = $invoiceContextProxy;
+    }
+
+    /**
+     * @Given /^there is an invoice with the numero "(?P<numero>[^"]+)" for this sheet$/
+     *
+     * @param string $numero
+     */
+    public function thereIsAnInvoiceWithNumero($numero)
+    {
+        $sheet = $this->invoiceContextProxy->getStorage()->get('sheet');
+
+        if (null === $sheet) {
+            throw new \InvalidArgumentException('Missing Sheet');
+        }
+
+        $invoice = $this->invoiceContextProxy->getInvoiceManager()->create($sheet->getEvent(), $numero, $sheet);
+        $this->invoiceContextProxy->getStorage()->set('invoice', $invoice);
+    }
+}
