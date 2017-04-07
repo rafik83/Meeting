0ebc580cdb8fae7d003f37e351386f8619747f19
Nico <nicolas.dievart@elao.com>
2017-04-07 11:46:01 +0200

    1028 - redirect to a new page with message

--- src/Ui/Bundle/EventBundle/Controller/ActivateAccountController.php
+++ src/Ui/Bundle/EventBundle/Controller/ActivateAccountController.php
@@ -104,14 +104,31 @@ class ActivateAccountController extends Controller
 
         if ($request->isMethod('POST')) {
             $this->get('tactician.commandbus')->handle($command);
-            $this->addFlash('success', 'flash.user.activate_account.token_re_send');
+            $this->addFlash('reSendActivateAccountToken', 'confirm');
 
-            return $this->redirectToRoute('event');
+            return $this->redirectToRoute('event_actiavet_account_re_send_token_confirm');
         }
 
         return $this->render('EventBundle:ActivateAccount:expired.html.twig', [
             'event' => $eventDomain->getEvent(),
             'token' => $activateAccountToken->getToken(),
+        ]);
+    }
+
+    /**
+     * @param Request     $request
+     * @param EventDomain $eventDomain
+     *
+     * @return Response
+     */
+    public function confirmReSendTokenAction(Request $request, EventDomain $eventDomain)
+    {
+        if (empty($this->container->get('session')->getFlashBag()->get('reSendActivateAccountToken'))) {
+            throw $this->createNotFoundException('Not allowed');
+        }
+
+        return $this->render('EventBundle:ActivateAccount:confirmReSendToken.html.twig', [
+            'event' => $eventDomain->getEvent(),
         ]);
     }
 
