dce86976e76778a7f2f05d2df859e090c358a9f4
Nico <nicolas.dievart@elao.com>
2017-04-14 10:35:59 +0200

    1051 - Split method

--- src/Application/Query/Group/Request/SheetListViewQueryHandler.php
+++ src/Application/Query/Group/Request/SheetListViewQueryHandler.php
@@ -14,6 +14,7 @@ use Proximum\Vimeet\Application\Command\Planning\SheetInfoGuesserCache;
 use Proximum\Vimeet\Application\Exception\Group\Request\NoResultException;
 use Proximum\Vimeet\Application\View\Group\Request\SheetListView;
 use Proximum\Vimeet\Application\View\Group\Request\SheetView;
+use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Meeting\Request;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Repository\Meeting\RequestRepositoryInterface;
@@ -84,6 +85,9 @@ class SheetListViewQueryHandler
             return strcasecmp($sheetOne['title'], $sheetTwo['title']);
         });
 
+        // split the sheetsWithTitle into multiple chunk with the limit provided
+        // it is used as the pagination
+        // the chunks array is used after to count the total page possible
         $chunks = array_chunk($sheetsWithTitle, $query->limit);
 
         if (!isset($chunks[$query->page - 1])) {
@@ -106,36 +110,74 @@ class SheetListViewQueryHandler
             return $sheetMet['sheet'];
         }, $chunks[$query->page - 1]);
 
+        $sheetViews = $this->getSheetViewsWithRequest(
+            $query->group->getEvent(),
+            $groupSheets,
+            $sheetsMet,
+            $query->locale
+        );
+
+        return new SheetListView(
+            $query->group->getId(),
+            $query->group->getTitle(),
+            $sheetViews,
+            $query->page, // current page
+            count($chunks) // total page
+        );
+    }
+
+    /**
+     * Creates the SheetViews and returns them with there requests
+     *
+     * @param Event   $event
+     * @param Sheet[] $groupSheets
+     * @param Sheet[] $sheetsMet
+     * @param string  $locale
+     *
+     * @return SheetView[]
+     */
+    private function getSheetViewsWithRequest(Event $event, array &$groupSheets, array &$sheetsMet, $locale)
+    {
         /** @var SheetView[] $sheetViews */
         $sheetViews = [];
 
         foreach ($sheetsMet as $sheetMet) {
             $sheetViews[$sheetMet->getId()] = $this->sheetViewQueryHandler->handle(
-                new SheetViewQuery($sheetMet, $query->locale)
+                new SheetViewQuery($sheetMet, $locale)
             );
         }
 
-        $requests = $this->requestRepository->getRequestsOfSheetsWithSheets($query->group->getEvent(), $sheetsMet, $groupSheets);
+        $requests = $this->requestRepository->getRequestsOfSheetsWithSheets(
+            $event,
+            $sheetsMet,
+            $groupSheets
+        );
 
+        $this->addRequestToSheetViews($requests, $groupSheets, $sheetViews, $locale);
+
+        return $sheetViews;
+    }
+
+    /**
+     * Add RequestView to SheetView
+     * @param Request[]   $requests
+     * @param Sheet[]     $groupSheets
+     * @param SheetView[] $sheetViews
+     * @param string      $locale
+     */
+    private function addRequestToSheetViews(array &$requests, array &$groupSheets, array &$sheetViews, $locale)
+    {
         foreach ($requests as $request) {
             $sheetMet = $this->getSheetMet($request, $groupSheets);
 
             $requestView = $this->requestViewQueryHandler->handle(
-                new RequestViewQuery($sheetMet, $request, $query->locale)
+                new RequestViewQuery($sheetMet, $request, $locale)
             );
 
             if (isset($sheetViews[$sheetMet->getId()])) {
                 $sheetViews[$sheetMet->getId()]->addRequest($requestView);
             }
         }
-
-        return new SheetListView(
-            $query->group->getId(),
-            $query->group->getTitle(),
-            $sheetViews,
-            $query->page,
-            count($chunks)
-        );
     }
 
     /**
