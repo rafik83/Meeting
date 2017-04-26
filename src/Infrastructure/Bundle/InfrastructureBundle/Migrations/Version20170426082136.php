d6ff2f25ace216a31a629bb9d230ac7f357017f2
Nico <nicolas.dievart@elao.com>
2017-04-26 13:57:24 +0200

    942 - Add postUp migration to set hasMessage

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170426082136.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170426082136.php
@@ -25,4 +25,17 @@ class Version20170426082136 extends AbstractMigration
     {
         $this->addSql('ALTER TABLE meeting_request DROP has_message');
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function postUp(Schema $schema)
+    {
+        $this->connection->executeQuery(
+            "UPDATE meeting_request
+             SET has_message = 1
+             WHERE meeting_request.id IN (SELECT DISTINCT m.request_id FROM meeting_message m)
+             "
+        );
+    }
 }
