764f253f25db5df7b33c0c87683b1e834dfecdf1
LamGui <guillaume.webdev@gmail.com>
2017-04-28 16:58:08 +0200

    1093 - Create GroupFactory

---
+++ tests/Factory/GroupFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) 2017 Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Factory;
+
+use DateTime;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Sheet\Group;
+use Proximum\Vimeet\Domain\Model\User;
+
+class GroupFactory
+{
+    /**
+     * @param Event|null    $event
+     * @param User|null     $user
+     * @param DateTime|null $dateTime
+     * @param string|null   $title
+     *
+     * @return Group
+     */
+    public static function createGroup(
+        Event $event = null,
+        User $user = null,
+        DateTime $dateTime = null,
+        $title = null
+    ) {
+        $event    = ($event !== null) ? $event : EventFactory::createEvent();
+        $user     = ($user !== null) ? $user : new User('user@vimeet.com', 'salt', 'password', 'fr');
+        $dateTime = ($dateTime !== null) ? $dateTime : new DateTime();
+        $title    = ($title !== null) ? $title : 'GroupTitle';
+
+        return new Group($event, $user, $title, $dateTime);
+    }
+}
