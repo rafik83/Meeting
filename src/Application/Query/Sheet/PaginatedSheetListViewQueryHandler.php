55523cfc27c5add17884d8b274fd19b13e7c6a0d
Paul Boiseau <paulboiseau@gmail.com>
2017-04-28 17:17:20 +0200

    issue - Add constant for trace name

--- src/Application/Query/Sheet/PaginatedSheetListViewQueryHandler.php
+++ src/Application/Query/Sheet/PaginatedSheetListViewQueryHandler.php
@@ -22,6 +22,7 @@ use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\TraceRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
 use Proximum\Vimeet\Domain\Template\ParticipantInfoGuesser;
+use Proximum\Vimeet\Domain\Trace\TraceableName;
 use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Security\Impersonate\Impersonate;
 
 class PaginatedSheetListViewQueryHandler
@@ -111,8 +112,18 @@ class PaginatedSheetListViewQueryHandler
             false
         );
 
-        $lastAccepts     = $this->traceRepository->getLastByTraceableObjectsAndAction($sheets->results, 'sheet', Trace::ACCEPT);
-        $lastValidates   = $this->traceRepository->getLastByTraceableObjectsAndAction($sheets->results, 'sheet', Trace::VALIDATE);
+        $lastAccepts = $this->traceRepository->getLastByTraceableObjectsAndAction(
+            $sheets->results,
+            TraceableName::SHEET_TRACEABLE_NAME,
+            Trace::ACCEPT
+        );
+
+        $lastValidates = $this->traceRepository->getLastByTraceableObjectsAndAction(
+            $sheets->results,
+            TraceableName::SHEET_TRACEABLE_NAME,
+            Trace::VALIDATE
+        );
+
         $sheets->results = $this->sheetRepository->findFullSheets($sheets->results);
 
         $sheets->results = array_map(function (Sheet $sheet) use ($query, $lastAccepts, $lastValidates) {
