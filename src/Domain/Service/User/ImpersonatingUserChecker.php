1512d2faf4fae6da712ccf70bdbffe427d2ad159
LamGui <guillaume.webdev@gmail.com>
2017-05-04 17:45:30 +0200

    972 - Create service to check if user is impersonating

---
+++ src/Domain/Service/User/ImpersonatingUserChecker.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Service\User;
+
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+
+class ImpersonatingUserChecker
+{
+    /** @var AuthorizationCheckerInterface */
+    private $authorizationChecker;
+
+    /**
+     * ImpersonatingUserChecker constructor.
+     *
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     */
+    public function __construct(AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isImpersonated()
+    {
+        return ($this->isUserWasPreviousAdmin() && $this->isUserWasPreviousUser());
+    }
+
+    /**
+     * Return true if current user have ROLE_PREVIOUS_ADMIN
+     *
+     * @return bool
+     */
+    public function isUserWasPreviousAdmin()
+    {
+        return $this->authorizationChecker->isGranted('ROLE_PREVIOUS_ADMIN');
+    }
+
+    /**
+     * Return true if current user have ROLE_PREVIOUS_USER
+     *
+     * @return bool
+     */
+    public function isUserWasPreviousUser()
+    {
+        return $this->authorizationChecker->isGranted('ROLE_PREVIOUS_USER');
+    }
+}
