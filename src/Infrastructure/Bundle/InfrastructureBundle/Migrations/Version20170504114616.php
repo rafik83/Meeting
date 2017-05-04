f4d2ee11c7cde52ab0f1f0b013a4372a084109ef
LamGui <guillaume.webdev@gmail.com>
2017-05-04 15:54:58 +0200

    972 - Make sheetViewed fields NOT NULL

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170504114616.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170504114616.php
@@ -17,7 +17,7 @@ class Version20170504114616 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE sheet_viewed (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, sheet_id INT DEFAULT NULL, created_at DATETIME NOT NULL, INDEX IDX_58E1575AA76ED395 (user_id), INDEX IDX_58E1575A8B1206A5 (sheet_id), UNIQUE INDEX unique_user_sheet (user_id, sheet_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE sheet_viewed (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, sheet_id INT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_58E1575AA76ED395 (user_id), INDEX IDX_58E1575A8B1206A5 (sheet_id), UNIQUE INDEX unique_user_sheet (user_id, sheet_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE sheet_viewed ADD CONSTRAINT FK_58E1575AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE sheet_viewed ADD CONSTRAINT FK_58E1575A8B1206A5 FOREIGN KEY (sheet_id) REFERENCES sheet (id)');
     }
