450a49f5421349fc6088d42470b697b4a5ac9427
Richard Hanna <richard.hanna@elao.com>
2017-05-12 15:04:17 +0200

    1144 - Check user availability instead of participant

--- src/Infrastructure/Repository/ParticipantRepository.php
+++ src/Infrastructure/Repository/ParticipantRepository.php
@@ -352,16 +352,18 @@ class ParticipantRepository implements ParticipantRepositoryInterface
 
         $queryBuilder->andWhere(
             $queryBuilder->expr()->andX(
-            // Participant have not already a meeting during this period
+                // Participant have not already a meeting during this period
                 "NOT EXISTS (
                     SELECT m.id
                     FROM Entity:Meeting m
                     JOIN m.slot slot
                     LEFT JOIN m.fromParticipants fp
+                    LEFT JOIN fp.user fpUser
                     LEFT JOIN m.toParticipants tp
+                    LEFT JOIN tp.user tpUser
                     WHERE
                         " . (null !== $exceptedMeeting ? 'm != :exceptedMeeting' : '1=1') . "
-                        AND (fp.id = participant OR tp.id = participant)
+                        AND (fpUser = user OR tpUser = user)
                         AND (
                             slot.begin BETWEEN :begin AND :end
                             OR slot.end BETWEEN :begin AND :end
@@ -374,9 +376,10 @@ class ParticipantRepository implements ParticipantRepositoryInterface
                     SELECT hp.id
                     FROM Entity:HappeningParticipation hp
                     JOIN hp.happening h
+                    JOIN hp.participant p
                     WHERE
                         " . (null !== $exceptedHappening ? 'h != :exceptedHappening' : '1=1') . "
-                        AND hp.participant = participant
+                        AND p.user = user
                         AND (
                             h.begin BETWEEN :begin AND :end
                             OR h.end BETWEEN :begin AND :end
