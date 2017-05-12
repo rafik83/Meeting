2c07488836415e34494a0a6b9558da28ffac314b
Richard Hanna <richard.hanna@elao.com>
2017-05-12 12:07:42 +0200

    Fix restarting php7.1-fpm on deploy

--- app/config/deploy/preprod.rb
+++ app/config/deploy/preprod.rb
@@ -15,7 +15,7 @@ role :app, domain, :primary => true
 namespace :app_tasks do
   task :php do
     capifony_pretty_print "--> Restarting PHP"
-    invoke_command "sudo /usr/sbin/service php5.6-fpm reload", :via => run_method
+    invoke_command "sudo /usr/sbin/service php7.1-fpm reload", :via => run_method
     capifony_puts_ok
   end
 end
