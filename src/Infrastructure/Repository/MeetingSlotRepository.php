3f04e2f04cd7386664ce4875721ac8b6f6792a6a
Richard Hanna <richard.hanna@elao.com>
2017-05-13 11:24:16 +0200

    1144 - Check user availability instead of participant for each slot

--- src/Infrastructure/Repository/MeetingSlotRepository.php
+++ src/Infrastructure/Repository/MeetingSlotRepository.php
@@ -160,10 +160,12 @@ class MeetingSlotRepository implements MeetingSlotRepositoryInterface
                         'NOT EXISTS (
                             SELECT m.id FROM Entity:Meeting m
                             LEFT JOIN m.fromParticipants fp
+                            LEFT JOIN fp.user fpUser
                             LEFT JOIN m.toParticipants tp
+                            LEFT JOIN tp.user tpUser
                             WHERE m.slot = slot
                             AND m.state = :meetingState
-                            AND (fp.id IN (:participants) OR tp.id IN (:participants))
+                            AND (fpUser.id IN (:userIds) OR tpUser.id IN (:userIds))
                             %s
                         )',
                         null !== $exceptedMeeting ? 'AND m != :exceptedMeeting' : ''
@@ -179,14 +181,14 @@ class MeetingSlotRepository implements MeetingSlotRepositoryInterface
         // Participants have not unavailability during this slot
         $queryBuilder
             ->andWhere('NOT EXISTS (
-                SELECT u.id FROM Entity:Unavailability u
-                WHERE u.user IN (:userIds)
+                SELECT unavailability.id FROM Entity:Unavailability unavailability
+                WHERE unavailability.user IN (:userIds)
                 AND (
-                    slot.begin = u.begin
-                    OR u.end = slot.end
-                    OR u.begin < slot.begin AND slot.begin < u.end
-                    OR u.begin < slot.end AND slot.end < u.end
-                    OR slot.begin < u.begin AND u.begin < slot.end
+                    slot.begin = unavailability.begin
+                    OR unavailability.end = slot.end
+                    OR unavailability.begin < slot.begin AND slot.begin < unavailability.end
+                    OR unavailability.begin < slot.end AND slot.end < unavailability.end
+                    OR slot.begin < unavailability.begin AND unavailability.begin < slot.end
                 )
             )');
 
@@ -194,15 +196,15 @@ class MeetingSlotRepository implements MeetingSlotRepositoryInterface
         $queryBuilder
             ->andWhere('NOT EXISTS (
                 SELECT hp.id FROM Entity:HappeningParticipation hp
-                JOIN hp.happening h
-                WHERE hp.participant IN (:participants)
-                AND (
-                    slot.begin = h.begin
-                    OR h.end = slot.end
-                    OR h.begin < slot.begin AND slot.begin < h.end
-                    OR h.begin < slot.end AND slot.end < h.end
-                    OR slot.begin < h.begin AND h.begin < slot.end
-                )
+                JOIN hp.participant hpParticipant
+                    WITH hpParticipant.user IN (:userIds)
+                JOIN hp.happening happening
+                WHERE
+                    slot.begin = happening.begin
+                    OR happening.end = slot.end
+                    OR happening.begin < slot.begin AND slot.begin < happening.end
+                    OR happening.begin < slot.end AND slot.end < happening.end
+                    OR slot.begin < happening.begin AND happening.begin < slot.end
             )');
 
         // No blocking mass unvailabilities during this slot
@@ -224,21 +226,19 @@ class MeetingSlotRepository implements MeetingSlotRepositoryInterface
         $queryBuilder
             ->andWhere('NOT EXISTS (
                 SELECT assignment.id FROM Entity:Unavailability\MassAssignment assignment
+                JOIN assignment.participant assignmentParticipant
+                    WITH assignment.enabled = true AND assignmentParticipant.user IN (:userIds)
                 JOIN assignment.mass massUnavailability
-                WHERE assignment.participant IN (:participants)
-                AND massUnavailability.blocking = true
-                AND assignment.enabled = true
-                AND (
+                    WITH massUnavailability.blocking = true
+                WHERE
                     slot.begin = assignment.begin
                     OR assignment.end = slot.end
                     OR assignment.begin < slot.begin AND slot.begin < assignment.end
                     OR assignment.begin < slot.end AND slot.end < assignment.end
                     OR slot.begin < assignment.begin AND assignment.begin < slot.end
-                )
-        )');
+            )');
 
         $queryBuilder
-            ->setParameter('participants', $participants)
             ->setParameter('userIds', $userIds)
         ;
 
