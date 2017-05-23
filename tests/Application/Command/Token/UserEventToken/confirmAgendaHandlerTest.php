98f810aa459e660c4cb657ceb9f7b03e16bb2afc
Nico <nicolas.dievart@elao.com>
2017-05-23 17:07:41 +0200

    1168 - Add confirmAgendaHandlerTest

---
+++ tests/Application/Command/Token/UserEventToken/confirmAgendaHandlerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Token\UserEventToken;
+
+use Proximum\Vimeet\Domain\Model\Token\UserEventToken;
+use Proximum\Vimeet\Domain\Repository\Token\UserEventTokenRepositoryInterface;
+
+class confirmAgendaHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHandle()
+    {
+        $dateTime = new \DateTime();
+        $userEventToken = $this->prophesize(UserEventToken::class);
+        $userEventToken->confirm($dateTime)->shouldBeCalled();
+
+        $userEventTokenRepository = $this->prophesize(UserEventTokenRepositoryInterface::class);
+        $userEventTokenRepository->set($userEventToken->reveal())->shouldBeCalled();
+
+        $command = new ConfirmAgenda($userEventToken);
+        $handler = new ConfirmAgendaHandler($userEventTokenRepository->reveal(), $dateTime);
+    }
+}
