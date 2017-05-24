93c36d70bda9f51bf968d8acd08c6bc7f60b0ef8
LamGui <guillaume.webdev@gmail.com>
2017-05-24 16:32:45 +0200

    1124 - Create TypeListViewQueryHandler

---
+++ src/Application/Query/Tip/Event/TypeListViewQuery.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Tip\Event;
+
+use Proximum\Vimeet\Domain\Model\Event;
+
+class TypeListViewQuery
+{
+    /** @var Event */
+    public $event;
+
+    /** @var string */
+    public $locale;
+
+    /**
+     * TypeListViewQuery constructor.
+     *
+     * @param Event  $event
+     * @param string $locale
+     */
+    public function __construct(Event $event, $locale)
+    {
+        $this->event  = $event;
+        $this->locale = $locale;
+    }
+}
