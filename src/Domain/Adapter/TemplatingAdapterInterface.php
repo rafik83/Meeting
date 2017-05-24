39aeeb458eb8c57981220830a79a57fb81e754bb
Nico <nicolas.dievart@elao.com>
2017-05-24 15:36:44 +0200

    1168 - Add templating adapter interface

---
+++ src/Domain/Adapter/TemplatingAdapterInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Adapter;
+
+interface TemplatingAdapterInterface
+{
+    /**
+     * Renders a template
+     *
+     * @param string $template
+     * @param array  $context The arguments
+     *
+     * @return string
+     */
+    public function render($template, array $context);
+}
