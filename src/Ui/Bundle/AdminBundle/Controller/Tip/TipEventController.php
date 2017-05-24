662386a5f4e485585a340ac69b26162a8c7eab19
LamGui <guillaume.webdev@gmail.com>
2017-05-24 16:38:19 +0200

    1124 - Display Affect tip view on event admin

--- src/Ui/Bundle/AdminBundle/Controller/Tip/TipEventController.php
+++ src/Ui/Bundle/AdminBundle/Controller/Tip/TipEventController.php
@@ -10,9 +10,13 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Tip;
 
-use Proximum\Vimeet\Application\Query\Tip\Event\TipListViewQuery;
+use Proximum\Vimeet\Application\Command\Tip\Event\Affect;
+use Proximum\Vimeet\Application\Exception\Tip\NoTipAvailableException;
 use Proximum\Vimeet\Application\Query\Tip\Event\TipListViewQuery as EventTipListViewQuery;
+use \Proximum\Vimeet\Application\Query\Tip\TipListViewQuery;
+use Proximum\Vimeet\Application\Query\Tip\Event\TypeListViewQuery;
 use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Form\Type\Tip\Event\AffectType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,7 +33,6 @@ class TipEventController extends Controller
     {
         $this->denyAccessUnlessGranted('ROLE_ALLOWED_TO_ORGANIZE');
 
-        $tipListViewQuery = new TipListViewQuery($event, $request->query->get('page', 1));
         $tipListViewQuery = new EventTipListViewQuery($event, $request->query->get('page', 1));
 
         $tipListView = $this->get('tactician.commandbus')->handle($tipListViewQuery);
@@ -40,8 +43,35 @@ class TipEventController extends Controller
         ]);
     }
 
-    public function affectAction()
+    public function affectAction(Request $request, Event $event)
     {
-        return '';
+        $this->denyAccessUnlessGranted('ROLE_ALLOWED_TO_ORGANIZE');
+
+        $locale = $event->getAvailableLocale($request->getLocale());
+
+        try {
+            $tipListViewQuery = new TipListViewQuery($locale);
+            $tipViews = $this->get('tactician.commandbus')->handle($tipListViewQuery);
+        } catch (NoTipAvailableException $exception) {
+            $this->addFlash('error', $this->get('translator')->trans($exception->getMessage(), [], 'flashes'));
+        }
+
+        $typeListViewQuery = new TypeListViewQuery($event, $locale);
+        $typeViews         = $this->get('tactician.commandbus')->handle($typeListViewQuery);
+        $affect            = new Affect($tipViews, $typeViews);
+
+        $form = $this->createForm(AffectType::class, null, [
+                'tipViews'  => $affect->tips,
+                'typeViews' => $affect->types,
+            ]);
+
+         if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+
+         }
+
+        return $this->render('@Admin/Tip/Event/affect.html.twig', [
+            'event' => $event,
+            'form'  => $form->createView()
+        ]);
     }
 }
