0113065fda73e6892ed55e21361ed4941f20822d
Nico <nicolas.dievart@elao.com>
2017-05-24 10:40:28 +0200

    1168 - Add UserEventTokenGenerator

--- src/Domain/Token/UserEventTokenGenerator.php
+++ src/Domain/Token/UserEventTokenGenerator.php
@@ -10,7 +10,59 @@
 
 namespace Proximum\Vimeet\Domain\Token;
 
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Token\UserEventToken;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\Token\UserEventTokenRepositoryInterface;
+
 class UserEventTokenGenerator
 {
+    /** @var UserEventTokenRepositoryInterface */
+    private $userEventTokenRepository;
 
+    /** @var UniqidGenerator */
+    private $uniqidGenerator;
+
+    /** @var \DateTimeInterface */
+    private $dateTime;
+
+    /**
+     * @param UserEventTokenRepositoryInterface $userEventTokenRepository
+     * @param UniqidGenerator                   $uniqidGenerator
+     * @param \DateTimeInterface                $dateTime
+     */
+    public function __construct(
+        UserEventTokenRepositoryInterface $userEventTokenRepository,
+        UniqidGenerator $uniqidGenerator,
+        \DateTimeInterface $dateTime
+    ) {
+        $this->userEventTokenRepository = $userEventTokenRepository;
+        $this->uniqidGenerator = $uniqidGenerator;
+        $this->dateTime = $dateTime;
+    }
+
+    /**
+     * @param Event  $event
+     * @param User   $user
+     * @param string $type
+     *
+     * @return UserEventToken
+     */
+    public function getUserEventTokenForConfirmAgenda(Event $event, User $user, $type)
+    {
+        $userEventToken = $this->userEventTokenRepository->findByEventAndUser($event, $user, $type);
+
+        if ($userEventToken !== null) {
+            return $userEventToken;
+        }
+
+        $uniqid = $this->uniqidGenerator->generate();
+        $token = sha1(sprintf('%s%s%s%s%s', $event->getId(), $user->getId(), $type, $this->dateTime->format('c'), $uniqid));
+
+        $userEventToken = new UserEventToken($event, $user, $type, $token, $this->dateTime);
+
+        $this->userEventTokenRepository->add($userEventToken);
+
+        return $userEventToken;
+    }
 }
