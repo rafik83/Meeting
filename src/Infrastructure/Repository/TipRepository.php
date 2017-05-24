629ea54e0741e9db148f199e63ac72d76bd64a14
LamGui <guillaume.webdev@gmail.com>
2017-05-24 16:36:17 +0200

    1124 - Add findAll() method to tipRepository

--- src/Infrastructure/Repository/TipRepository.php
+++ src/Infrastructure/Repository/TipRepository.php
@@ -105,4 +105,20 @@ class TipRepository implements TipRepositoryInterface
 
         return $this->paginator->paginate($queryBuilder, $page, $limit, 'tip');
     }
+
+    /** {@inheritdoc} */
+    public function findAll($locale)
+    {
+        $queryBuilder = $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('new \Proximum\Vimeet\Application\View\Tip\TipTranslationView(tipTranslation.id, tipTranslation.title, tipTranslation.content)')
+            ->from(Tip::class, 'tip')
+            ->join('tip.translations', 'tipTranslation', 'WITH', 'tipTranslation.locale = :locale')
+            ->orderBy('tip.title')
+            ->setParameter('locale', $locale);
+
+        return $queryBuilder->getQuery()->getResult();
+
+    }
 }
