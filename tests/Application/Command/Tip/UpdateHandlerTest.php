8f3e9690c8d985925628afec0685b9d0361a404a
Nico <nicolas.dievart@elao.com>
2017-05-29 12:25:56 +0200

    1179 - Add unit test

--- tests/Application/Command/Tip/UpdateHandlerTest.php
+++ tests/Application/Command/Tip/UpdateHandlerTest.php
@@ -39,4 +39,24 @@ class UpdateHandlerTest extends \PHPUnit_Framework_TestCase
         $handler = new UpdateHandler($tipRepository->reveal());
         $handler->handle($command);
     }
+
+    public function testHandleUpdate()
+    {
+        $dateTime = new \DateTime();
+        $tip = new Tip('tipTitle', true, true, true, true, true, true, $dateTime);
+
+        $tipRepository = $this->prophesize(TipRepositoryInterface::class);
+        $expectedTip = new Tip('newTipTitle', false, true, false, true, false, true, $dateTime);
+        $tipRepository->set($expectedTip)->shouldBeCalled();
+
+        $command                      = new Update($tip);
+        $command->title               = 'newTipTitle';
+        $command->onMeetingManagement = false;
+        $command->onPrintPlanning     = false;
+        $command->onAgenda            = false;
+        $command->translations        = [];
+
+        $handler = new UpdateHandler($tipRepository->reveal());
+        $handler->handle($command);
+    }
 }
