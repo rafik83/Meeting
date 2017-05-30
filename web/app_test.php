4dbe40f71494d3c1c1708dfff9ce6994573281fe
Nico <nicolas.dievart@elao.com>
2017-05-30 14:40:23 +0200

    Update dependencies

--- web/app_test.php
+++ web/app_test.php
@@ -22,7 +22,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
 /**
  * @var Composer\Autoload\ClassLoader $loader
  */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 $kernel = new AppKernel('test', false);
 if (PHP_VERSION_ID < 70000) {
