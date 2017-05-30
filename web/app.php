0b58370b4e32dc1dacf81ef70d4b5a70c21ed038
Nico <nicolas.dievart@elao.com>
2017-05-30 11:27:35 +0200

    Apply patch

--- web/app.php
+++ web/app.php
@@ -3,11 +3,15 @@
 use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+$loader = require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 $kernel->boot();
 
 Request::setTrustedProxies(
