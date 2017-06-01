4d7de90d0166f038708cae7530144334917f0ac3
DimitriMorgan <dim.fruit@gmail.com>
2017-06-01 13:56:59 +0200

    1180 - improve unit tests

--- tests/Application/Query/Tip/TipViewQueryHandlerTest.php
+++ tests/Application/Query/Tip/TipViewQueryHandlerTest.php
@@ -14,9 +14,12 @@ use Proximum\Vimeet\Application\Query\Tip\TipViewQuery;
 use Proximum\Vimeet\Application\Query\Tip\TipViewQueryHandler;
 use Proximum\Vimeet\Application\View\Tip\PaginatedTipView;
 use Proximum\Vimeet\Application\View\Tip\TipView;
+use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\PaginatedResult;
 use Proximum\Vimeet\Domain\Model\Tip\Tip;
+use Proximum\Vimeet\Domain\Model\Type;
 use Proximum\Vimeet\Domain\Repository\TipRepositoryInterface;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
 
 class TipViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,14 +27,35 @@ class TipViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
     {
         $tipRepository = $this->prophesize(TipRepositoryInterface::class);
         $dateTime = new \DateTime();
+        $event1 = $this->prophesize(Event::class);
+        $event1->getId()->shouldBeCalled()->willReturn(1);
+        $event2 = $this->prophesize(Event::class);
+        $event2->getId()->shouldBeCalled()->willReturn(2);
 
-        $tips = [
-            new Tip('tip_1', false, true, false, false, false, false, $dateTime),
-            new Tip('tip_2', false, false, true, false, false, false, $dateTime),
-            new Tip('tip_3', true, false, true, false, false, false, $dateTime),
-        ];
+        $type1event1 = new Type($event1->reveal());
+        $type2event1 = new Type($event1->reveal());
+        $type3event1 = new Type($event1->reveal());
 
-        $results = new PaginatedResult([$tips], 1, 10, 3);
+        $type1event2 = new Type($event2->reveal());
+        $type2event2 = new Type($event2->reveal());
+
+        $tip1 = new Tip('tip_1', false, true, false, false, false, false, $dateTime);
+        $tip2 = new Tip('tip_2', false, true, false, false, false, false, $dateTime);
+        $tip3 = new Tip('tip_3', false, true, false, false, false, false, $dateTime);
+
+        $tip1->addType($type1event1);
+        $tip1->addType($type2event1);
+        $tip1->addType($type3event1);
+
+        $tip2->addType($type1event1);
+        $tip2->addType($type2event1);
+        $tip2->addType($type3event1);
+        $tip2->addType($type1event2);
+        $tip2->addType($type2event2);
+
+        $tips = [$tip1, $tip2, $tip3];
+
+        $results = new PaginatedResult($tips, 1, 10, 3);
 
         $expectedTipListView = new PaginatedTipView($results);
 
@@ -41,6 +65,9 @@ class TipViewQueryHandlerTest extends \PHPUnit_Framework_TestCase
 
         $handler = new TipViewQueryHandler($tipRepository->reveal());
         $tipListView = $handler->handle($query);
+
+        $this->assertCount(1, $tipListView->results->results[0]->getUnduplicatedEvents());
+        $this->assertCount(2, $tipListView->results->results[1]->getUnduplicatedEvents());
 
         $this->assertEquals($expectedTipListView, $tipListView);
     }
