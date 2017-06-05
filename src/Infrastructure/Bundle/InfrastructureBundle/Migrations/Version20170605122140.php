73c1a52c897a530db134319c405d9245c8025862
LamGui <guillaume.webdev@gmail.com>
2017-06-05 14:35:58 +0200

    1331 - Add event field to meeting table
    - Create migration
    - Update Model
    - Update Entity

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170605122140.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add `event` to table `meeting`
+ * Add INDEX `state` to table `meeting`
+ */
+class Version20170605122140 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting ADD event_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE meeting ADD CONSTRAINT FK_F515E13971F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_F515E13971F7E88B ON meeting (event_id)');
+        $this->addSql('CREATE INDEX state ON meeting_request (state)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting DROP FOREIGN KEY FK_F515E13971F7E88B');
+        $this->addSql('DROP INDEX IDX_F515E13971F7E88B ON meeting');
+        $this->addSql('ALTER TABLE meeting DROP event_id');
+        $this->addSql('DROP INDEX state ON meeting_request');
+    }
+}
