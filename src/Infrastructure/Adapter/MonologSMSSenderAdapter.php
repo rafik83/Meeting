e0cb764884b791ae1c0e075a04d5fb0275d2c291
Nico <nicolas.dievart@elao.com>
2017-06-08 15:51:30 +0200

    1177 - Add MonologSMSSenderAdapter service

--- src/Infrastructure/Adapter/MonologSMSSenderAdapter.php
+++ src/Infrastructure/Adapter/MonologSMSSenderAdapter.php
@@ -12,11 +12,30 @@ namespace Proximum\Vimeet\Infrastructure\Adapter;
 
 use Proximum\Vimeet\Application\Adapter\SMSSenderInterface;
 use Proximum\Vimeet\Domain\Messaging\SMS\SMS;
+use Psr\Log\LoggerInterface;
 
 class MonologSMSSenderAdapter implements SMSSenderInterface
 {
+    const MESSAGE_LOGGED = 'SMS send to %s with message: %s';
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @param SMS $sms
+     */
     public function send(SMS $sms)
     {
-
+        $this->logger->info(
+            sprintf(self::MESSAGE_LOGGED, $sms->getReceiver(), $sms->getMessage())
+        );
     }
 }
