59742297402f0c61b33e1742744e46fba898dd4d
Nico <nicolas.dievart@elao.com>
2017-06-08 16:41:16 +0200

    1177 - Add service declaration with alias

---
+++ src/Infrastructure/Adapter/SMSSenderAdapter.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Adapter;
+
+use Proximum\Vimeet\Application\Adapter\SMSSenderInterface;
+use Proximum\Vimeet\Domain\Messaging\SMS\SMS;
+
+class SMSSenderAdapter implements SMSSenderInterface
+{
+    /**
+     * @param SMS $sms
+     */
+    public function send(SMS $sms)
+    {
+    }
+}
