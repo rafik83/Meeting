10aec1c29e4e3aae38dcd7ee5e194a226181638d
Richard Hanna <richard.hanna@elao.com>
2017-06-14 13:40:31 +0200

    1099 - Set event_id and user_id not nullable

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170614101111.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170614101111.php
@@ -17,7 +17,7 @@ class Version20170614101111 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_event_phone (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, event_id INT DEFAULT NULL, phone VARCHAR(255) NOT NULL, code VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, validated_at DATETIME DEFAULT NULL, validated TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_AC4FEF64A76ED395 (user_id), INDEX IDX_AC4FEF6471F7E88B (event_id), UNIQUE INDEX unique_user_event (user_id, event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_event_phone (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, event_id INT NOT NULL, phone VARCHAR(255) NOT NULL, code VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, validated_at DATETIME DEFAULT NULL, validated TINYINT(1) DEFAULT \'0\' NOT NULL, INDEX IDX_AC4FEF64A76ED395 (user_id), INDEX IDX_AC4FEF6471F7E88B (event_id), UNIQUE INDEX unique_user_event (user_id, event_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_event_phone ADD CONSTRAINT FK_AC4FEF64A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_event_phone ADD CONSTRAINT FK_AC4FEF6471F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
     }
