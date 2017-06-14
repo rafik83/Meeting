2385647840d4889adb9e828a2c69779d67e73d81
Nico <nicolas.dievart@elao.com>
2017-06-14 15:43:11 +0200

    1099 - improve code for digit code generator

--- src/Domain/Code/DigitCodeGenerator.php
+++ src/Domain/Code/DigitCodeGenerator.php
@@ -29,12 +29,23 @@ class DigitCodeGenerator
             $code .= (string) mt_rand(0, 9);
         }
 
+        return $this->isExcluded($code) ? $this->generateCode($length) : $code;
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return bool
+     */
+    private function isExcluded($code)
+    {
         $exclude = [];
+        $codeLength = strlen($code);
 
         for ($digit = 0; $digit <= 9; $digit++) {
-            $exclude[] = str_repeat((string) $digit, $length);
+            $exclude[] = str_repeat((string) $digit, $codeLength);
         }
 
-        return in_array($code, $exclude) ? $this->generateCode($length) : $code;
+        return in_array($code, $exclude);
     }
 }
--- tests/Domain/Code/DigitCodeGeneratorTest.php
+++ tests/Domain/Code/DigitCodeGeneratorTest.php
@@ -20,23 +20,44 @@ class DigitCodeGeneratorTest extends TestCase
         $generator = new DigitCodeGenerator();
 
         for ($i = 0; $i < 100; $i++) {
-            $code = $generator->generateCode(4);
+            $code4chars = $generator->generateCode(4);
             $code9chars = $generator->generateCode(9);
 
-            $this->assertEquals(4, strlen($code));
+            $this->assertEquals(4, strlen($code4chars));
             $this->assertEquals(9, strlen($code9chars));
-            $this->assertNotContains($code, [
-                '0000',
-                '1111',
-                '2222',
-                '3333',
-                '4444',
-                '5555',
-                '6666',
-                '7777',
-                '8888',
-                '9999',
-            ]);
+
         }
+    }
+
+    public function testIsExcluded()
+    {
+        $generator = new DigitCodeGenerator();
+
+        $reflection = new \ReflectionClass(DigitCodeGenerator::class);
+        $method = $reflection->getMethod('isExcluded');
+        $method->setAccessible(true);
+
+        $this->assertTrue($method->invokeArgs($generator, ['0000']));
+        $this->assertTrue($method->invokeArgs($generator, ['1111']));
+        $this->assertTrue($method->invokeArgs($generator, ['2222']));
+        $this->assertTrue($method->invokeArgs($generator, ['3333']));
+        $this->assertTrue($method->invokeArgs($generator, ['4444']));
+        $this->assertTrue($method->invokeArgs($generator, ['5555']));
+        $this->assertTrue($method->invokeArgs($generator, ['6666']));
+        $this->assertTrue($method->invokeArgs($generator, ['7777']));
+        $this->assertTrue($method->invokeArgs($generator, ['8888']));
+        $this->assertTrue($method->invokeArgs($generator, ['9999']));
+        $this->assertTrue($method->invokeArgs($generator, ['00000000000']));
+        $this->assertFalse($method->invokeArgs($generator, ['1234']));
+        $this->assertFalse($method->invokeArgs($generator, ['4321']));
+        $this->assertFalse($method->invokeArgs($generator, ['9876']));
+        $this->assertFalse($method->invokeArgs($generator, ['6789']));
+        $this->assertFalse($method->invokeArgs($generator, ['4567']));
+        $this->assertFalse($method->invokeArgs($generator, ['123456789']));
+        $this->assertFalse($method->invokeArgs($generator, ['987654321']));
+        $this->assertFalse($method->invokeArgs($generator, ['0000000000000001']));
+        $this->assertFalse($method->invokeArgs($generator, ['1111111111121111']));
+
+        $method->setAccessible(false);
     }
 }
