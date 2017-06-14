a0c5b16a4545d92ab1bb27be0b7765bbb33e7d7a
Nico <nicolas.dievart@elao.com>
2017-06-14 19:11:59 +0200

    1099 - Add query handler for UserEventPhone

--- src/Application/Query/User/Phone/UserEventPhoneQueryHandler.php
+++ src/Application/Query/User/Phone/UserEventPhoneQueryHandler.php
@@ -10,7 +10,44 @@
 
 namespace Proximum\Vimeet\Application\Query\User\Phone;
 
+use Proximum\Vimeet\Application\Exception\User\Phone\UserEventPhoneNotFound;
+use Proximum\Vimeet\Domain\Model\User\UserEventPhone;
+use Proximum\Vimeet\Domain\Repository\User\UserEventPhoneRepositoryInterface;
+
 class UserEventPhoneQueryHandler
 {
+    /** @var UserEventPhoneRepositoryInterface */
+    private $userEventPhoneRepository;
 
+    /**
+     * @param UserEventPhoneRepositoryInterface $userEventPhoneRepository
+     */
+    public function __construct(UserEventPhoneRepositoryInterface $userEventPhoneRepository)
+    {
+        $this->userEventPhoneRepository = $userEventPhoneRepository;
+    }
+
+    /**
+     * @param UserEventPhoneQuery $query
+     *
+     * @return UserEventPhone
+     *
+     * @throws UserEventPhoneNotFound
+     */
+    public function handle(UserEventPhoneQuery $query)
+    {
+        $userEventPhone = $this->userEventPhoneRepository->find($query->user, $query->event);
+
+        if ($userEventPhone === null) {
+            throw new UserEventPhoneNotFound(
+                sprintf(
+                    'UserEventPhone not found for User %s and Event %s',
+                    $query->user->getId(),
+                    $query->event->getId()
+                )
+            );
+        }
+
+        return $userEventPhone;
+    }
 }
