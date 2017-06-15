6f7d48cc003b3ddf397c4d3ad5c9ccb58e90ed08
Richard Hanna <richard.hanna@elao.com>
2017-06-15 15:51:56 +0200

    1099 - Add ParticipantInfoSetter

---
+++ tests/Domain/Participant/ParticipantInfoSetterTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+* This file is part of the vimeet project.
+*
+* Copyright (C) Proximum
+*
+* @author Elao <contact@elao.com>
+*/
+
+namespace Proximum\Vimeet\Tests\Domain\Participant;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Participant\ParticipantInfoSetter;
+use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
+use Proximum\Vimeet\Domain\Template\Block;
+use Proximum\Vimeet\Domain\Template\TemplateData;
+use Proximum\Vimeet\Domain\Template\TemplateDataFactory;
+use Proximum\Vimeet\Domain\Template\TemplateObject\Telephone;
+
+class ParticipantInfoSetterTest extends TestCase
+{
+    public function testSetPhone()
+    {
+        $templateData = new TemplateData('root', [], 'fr', 'fr');
+        $block = new Block('12', [], 'fr', 'fr');
+        $phoneObject = new Telephone(
+            'phone', 'telephone', ['tags' => ['participant_phone', 'participant_data']], 'fr', 'fr'
+        );
+        $phoneObject->setTelephone('+33 666 999 000');
+        $block->addChild(1, "3ad4b72f", $phoneObject);
+        $templateData->addChild(0, '67019e4a', $block);
+
+        $participant = $this->prophesize(Participant::class);
+        $templateDataFactory = $this->prophesize(TemplateDataFactory::class);
+        $participantRepository = $this->prophesize(ParticipantRepositoryInterface::class);
+
+        $participantInfoSetter = new ParticipantInfoSetter(
+            $templateDataFactory->reveal(),
+            $participantRepository->reveal()
+        );
+
+        $templateDataFactory->createProfileTemplate($participant->reveal(), 'fr')->willReturn($templateData);
+        $phoneObject->setTelephone('+3311223344');
+        $participant->setData($templateData->getData())->shouldBeCalled();
+
+        $participantRepository->set($participant->reveal())->shouldBeCalled();
+        $participantInfoSetter->setPhone($participant->reveal(), '+3311223344', 'fr');
+    }
+
+    public function testNotFoundPhoneTaggedObject()
+    {
+        $templateData = new TemplateData('root', [], 'fr', 'fr');
+
+        $participant = $this->prophesize(Participant::class);
+        $templateDataFactory = $this->prophesize(TemplateDataFactory::class);
+        $participantRepository = $this->prophesize(ParticipantRepositoryInterface::class);
+
+        $participantInfoSetter = new ParticipantInfoSetter(
+            $templateDataFactory->reveal(),
+            $participantRepository->reveal()
+        );
+
+        $templateDataFactory->createProfileTemplate($participant->reveal(), 'fr')->willReturn($templateData);
+        $participant->setData($templateData->getData())->shouldNotBeCalled();
+
+        $participantRepository->set($participant->reveal())->shouldBeCalled();
+        $participantInfoSetter->setPhone($participant->reveal(), '+3311223344', 'fr');
+    }
+}
