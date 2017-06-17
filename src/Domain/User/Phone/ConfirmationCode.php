906a8bff9049b0b651d7dd4f0ed54ee8a5f31baf
Richard Hanna <richard.hanna@elao.com>
2017-06-17 21:41:17 +0200

    1099 - Set translations

--- src/Domain/User/Phone/ConfirmationCode.php
+++ src/Domain/User/Phone/ConfirmationCode.php
@@ -12,6 +12,6 @@ namespace Proximum\Vimeet\Domain\User\Phone;
 
 class ConfirmationCode
 {
-    const MESSAGE_TRANSLATION_KEY = 'to define';
+    const MESSAGE_TRANSLATION_KEY = 'user.phone.confirmationCode.message';
     const CODE_LENGTH = 4;
 }
