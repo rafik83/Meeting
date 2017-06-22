d0999e87ac3ad1755f234e779c281a0059272e67
ludovic <ludovic.moreau@elao.com>
2017-06-22 17:16:41 +0200

    Fix errors and empty line

--- tests/Domain/Money/AmountFormatterTest.php
+++ tests/Domain/Money/AmountFormatterTest.php
@@ -18,11 +18,10 @@ class AmountFormatterTest extends TestCase
     public function provideDecimalToCentsAmount()
     {
         return [
-            ['1259', '12.59'],
-            ['10','0.1'],
-            ['1200','12'],
-            ['100','1'],
-
+            [1259, 12.59],
+            [10,0.1],
+            [1200,12],
+            [100,1],
         ];
     }
 
@@ -41,10 +40,10 @@ class AmountFormatterTest extends TestCase
     public function provideCentsToDecimalAmount()
     {
         return [
-            ['12.59', '1259'],
-            ['1.2', '120'],
-            ['1000', '100000'],
-            ['1', '100'],
+            [12.59, 1259],
+            [1.2, 120],
+            [1000, 100000],
+            [1, 100],
         ];
     }
 
@@ -63,9 +62,9 @@ class AmountFormatterTest extends TestCase
     public function provideCalculateRateAmount()
     {
         return [
-            ['4','200','2'],
-            ['12','400','3'],
-            ['600','5000','12'],
+            [4,200,2],
+            [12,400,3],
+            [600,5000,12],
         ];
     }
 
