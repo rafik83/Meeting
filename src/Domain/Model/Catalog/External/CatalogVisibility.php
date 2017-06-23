418995f9a6b04b60e68ce6db9e7105f5e8f55c08
LamGui <guillaume.webdev@gmail.com>
2017-06-23 11:44:35 +0200

    1222 - Create Model CatalogVisibility + migration

---
+++ src/Domain/Model/Catalog/External/CatalogVisibility.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model\Catalog\External;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class CatalogVisibility
+{
+    /** @var int */
+    private $id;
+
+    /** @var Event */
+    private $event;
+
+    /** @var ArrayCollection of Type */
+    private $types;
+
+    /** @var ArrayCollection of Category */
+    private $categories;
+
+    /**
+     * CatalogVisibility constructor.
+     * @param Event $event
+     * @param array $types
+     * @param array $categories
+     */
+    public function __construct(Event $event, array $types, array $categories)
+    {
+        $this->event = $event;
+        $this->types = $types;
+        $this->categories = $categories;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Event
+     */
+    public function getEvent(): Event
+    {
+        return $this->event;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTypes(): array
+    {
+        return $this->types->toArray();
+    }
+
+    /**
+     * @return array
+     */
+    public function getCategories(): array
+    {
+        return $this->categories->toArray();
+    }
+}
