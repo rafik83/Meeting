73d56d1b5ea70d81600728c64281675027c80b8b
ludovic <ludovic.moreau@elao.com>
2017-06-26 11:19:54 +0200

    Fix Code Style

--- tests/Domain/Money/AmountFormatterTest.php
+++ tests/Domain/Money/AmountFormatterTest.php
@@ -26,7 +26,6 @@ class AmountFormatterTest extends TestCase
 
     /**
      * @dataProvider provideCentsToDecimalAmount
-     *
      */
     public function testCentsToDecimalAmount($expected, $amount)
     {
@@ -36,7 +35,6 @@ class AmountFormatterTest extends TestCase
 
     /**
      * @dataProvider provideCalculateRateAmount
-     *
      */
     public function testCalculateRateAmount($expected, $amount, $rate)
     {
