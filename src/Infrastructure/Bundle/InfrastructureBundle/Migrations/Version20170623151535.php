79fa7e2caea14e6e29fec260b3fa072b4110e1ec
Nico <nicolas.dievart@elao.com>
2017-06-26 10:40:05 +0200

    1188 - Add nullable false on joinColumn

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170623151535.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170623151535.php
@@ -17,7 +17,7 @@ class Version20170623151535 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE user_agenda_version (id INT AUTO_INCREMENT NOT NULL, event_id INT DEFAULT NULL, user_id INT DEFAULT NULL, version LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', created_at DATETIME NOT NULL, INDEX IDX_575D6EDB71F7E88B (event_id), INDEX IDX_575D6EDBA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_agenda_version (id INT AUTO_INCREMENT NOT NULL, event_id INT NOT NULL, user_id INT NOT NULL, version LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', created_at DATETIME NOT NULL, INDEX IDX_575D6EDB71F7E88B (event_id), INDEX IDX_575D6EDBA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_agenda_version ADD CONSTRAINT FK_575D6EDB71F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_agenda_version ADD CONSTRAINT FK_575D6EDBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
