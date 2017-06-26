e604ce6adeff1613f5196c4cbe8c6e3c801b2062
LamGui <guillaume.webdev@gmail.com>
2017-06-26 10:21:03 +0200

    1222 - Remove useless constructor

--- src/Domain/Model/Catalog/External/SearchFacet.php
+++ src/Domain/Model/Catalog/External/SearchFacet.php
@@ -11,19 +11,7 @@
 namespace Proximum\Vimeet\Domain\Model\Catalog\External;
 
 use Proximum\Vimeet\Domain\Model\Catalog\AbstractSearchFacet;
-use Proximum\Vimeet\Domain\Model\Event;
 
 class SearchFacet extends AbstractSearchFacet
 {
-    /**
-     * SearchFacet constructor.
-     *
-     * @param Event  $event
-     * @param string $type
-     * @param bool   $enabled
-     */
-    public function __construct(Event $event, string $type, bool $enabled = false)
-    {
-        parent::__construct($event, $type, $enabled);
-    }
 }
