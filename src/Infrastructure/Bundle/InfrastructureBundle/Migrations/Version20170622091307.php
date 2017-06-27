f87c120c03026af4c19c2d9625ca92dd377c5e5c
Nico <nicolas.dievart@elao.com>
2017-06-27 16:06:08 +0200

    1191 - remove useless comment

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170622091307.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20170622091307.php
@@ -15,7 +15,6 @@ class Version20170622091307 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE sheet ADD agenda_confirmed_status VARCHAR(255) DEFAULT \'agenda_not_concerned\' NOT NULL');
@@ -26,7 +25,6 @@ class Version20170622091307 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE sheet DROP agenda_confirmed_status');
