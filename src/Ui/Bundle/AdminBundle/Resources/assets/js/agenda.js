11c744db61decb0a88eba08ede5e723cc8879d0f
Nico <nicolas.dievart@elao.com>
2017-07-03 11:13:25 +0200

    1188 - Remove old nl2br

--- src/Ui/Bundle/AdminBundle/Resources/assets/js/agenda.js
+++ src/Ui/Bundle/AdminBundle/Resources/assets/js/agenda.js
@@ -2,8 +2,7 @@ var Vue                    = require('vue'),
     store                  = require('./agenda/store/index'),
     options                = require('./vueComponents/options'),
     spot                   = require('./agenda/spot/Spot'),
-    meeting                = require('./agenda/meeting/Meeting'),
-    Nl2br                  = require('vue-nl2br');
+    meeting                = require('./agenda/meeting/Meeting');
 
 var tabs = require('./vueComponents/tabs');
 
@@ -18,7 +17,6 @@ Vue.component('Modal', {
 });
 Vue.component('spot', spot);
 Vue.component('meeting', meeting);
-Vue.component('nl2br', Nl2br);
 
 new Vue({
     el: '#agenda',
