885fd8f1d8e9f264f0fc923a0320f67788fbdcdc
Nico <nicolas.dievart@elao.com>
2017-07-06 17:55:37 +0200

    1226 - Add archive on event

--- src/Domain/Model/Event.php
+++ src/Domain/Model/Event.php
@@ -109,6 +109,9 @@ class Event implements EventInterface, TraceableInterface
     /** @var bool */
     private $userAgendaVersionsGenerated;
 
+    /** @var bool */
+    private $archived;
+
     /**
      * @param string      $title
      * @param string      $fallback
@@ -156,6 +159,7 @@ class Event implements EventInterface, TraceableInterface
         $this->assetPath      = '';
 
         $this->userAgendaVersionsGenerated = false;
+        $this->archived = false;
     }
 
     /**
@@ -692,5 +696,29 @@ class Event implements EventInterface, TraceableInterface
     public function setUserAgendaVersionsGenerated(bool $userAgendaVersionsGenerated)
     {
         $this->userAgendaVersionsGenerated = $userAgendaVersionsGenerated;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isArchived(): bool
+    {
+        return $this->archived;
+    }
+
+    /**
+     * Archive the event
+     */
+    public function archive()
+    {
+        $this->archived = true;
+    }
+
+    /**
+     * Un archive the event
+     */
+    public function unArchive()
+    {
+        $this->archived = false;
     }
 }
