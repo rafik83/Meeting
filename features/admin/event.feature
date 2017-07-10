313f25e7c1672e2c0b5592279174c85b24a66444
DimitriMorgan <dim.fruit@gmail.com>
2017-07-10 23:42:37 +0200

    1228 - add method to handle 404 on url instead of base url page

--- features/admin/event.feature
+++ features/admin/event.feature
@@ -95,5 +95,4 @@ Feature: See, create and update event
     And I press "form.event_update.children.submit.label"
     Then I go to this page "/en/event"
     And I should see "Invisible"
-    When I go to "http://rdv-carnot-2016.vimeet.proximum.dev/app_test.php/fr"
-    Then the response status code should be 404
+    Then this event page "http://rdv-carnot-2016.vimeet.proximum.dev/app_test.php/fr" returns 404
--- src/Behat/Context/FeatureContext.php
+++ src/Behat/Context/FeatureContext.php
@@ -626,6 +626,17 @@ class FeatureContext extends MinkContext implements KernelAwareContext, SnippetA
     }
 
     /**
+     * Event page returns 404
+     *
+     * @Then /^this event page "(?P<page>[^"]+)" returns 404$/
+     */
+    public function eventPageReturns404(string $eventPageUrl)
+    {
+        $this->visit($eventPageUrl);
+        $this->assertResponseStatus(404);
+    }
+
+    /**
      * Checks, that current page PATH is equal to specified.
      *
      * @Then /^(?:|I )should be on this page "(?P<page>[^"]+)"$/
