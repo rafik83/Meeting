a5d3f7d4036ee27c92bfea74a0fbd7966bf29c8e
Nico <nicolas.dievart@elao.com>
2017-07-10 11:42:21 +0200

    Improve test

--- tests/Domain/Template/TemplateRemoveFieldTest.php
+++ tests/Domain/Template/TemplateRemoveFieldTest.php
@@ -15,6 +15,7 @@ use Proximum\Vimeet\Domain\Model\Template\SheetTemplate;
 use Proximum\Vimeet\Domain\Template\Block;
 use Proximum\Vimeet\Domain\Template\TemplateData;
 use Proximum\Vimeet\Domain\Template\TemplateDataFactory;
+use Proximum\Vimeet\Domain\Template\TemplateObject\Image;
 use Proximum\Vimeet\Domain\Template\TemplateObject\Telephone;
 use Proximum\Vimeet\Domain\Template\TemplateRemoveField;
 
@@ -38,19 +39,24 @@ class TemplateRemoveFieldTest extends TestCase
     /** @var Block */
     private $block;
 
+    /** @var Image */
+    private $image;
+
     public function setUp()
     {
         $this->templateDataFactory = $this->prophesize(TemplateDataFactory::class);
         $this->dateTime            = new \DateTime('2017-12-01 10:00:00');
         $this->phoneObject         = new Telephone('phone', 'telephone', ['tags' => ['participant_mobile']], 'fr', 'fr');
+        $this->image               = new Image('image', 'image', ['products' => [1, 2]], 'fr', 'fr');
         $this->sheetTemplate       = new SheetTemplate('title', [], ['fr'], 'fr', $this->dateTime, []);
         $this->templateData        = new TemplateData('root', [], 'fr', 'fr');
         $this->block               = new Block(12, [], 'fr', 'fr');
     }
 
-    public function testRemove()
+    public function testRemoveProducts()
     {
         $this->block->addChild(0, 'a34e56d', $this->phoneObject);
+        $this->block->addChild(0, '12345', $this->image);
         $this->templateData->addChild(0, 'a34e56d', $this->block);
 
         $this->templateDataFactory
@@ -70,15 +76,67 @@ class TemplateRemoveFieldTest extends TestCase
                         'a34e56d' => [
                             'component' => 'object',
                             'type'      => 'telephone',
-                            'config' => [
+                            'config'    => [
                                 'tags' => ['participant_mobile']
                             ]
+                        ],
+                        '12345' => [
+                            'component' => 'object',
+                            'type'      => 'image',
+                            'config'    => [
+                                'products' => [],
+                            ],
                         ]
                     ]
                 ]
             ]
         ];
 
-        $this->assertEquals($expected, $templater->remove($this->sheetTemplate, 'title',[]));
+        $this->assertEquals($expected, $templater->remove($this->sheetTemplate, 'products',[]));
+    }
+
+    public function testRemoveTags()
+    {
+        $this->block->addChild(0, 'a34e56d', $this->phoneObject);
+        $this->block->addChild(0, '12345', $this->image);
+        $this->templateData->addChild(0, 'a34e56d', $this->block);
+
+        $this->templateDataFactory
+            ->createFromTemplate($this->sheetTemplate)
+            ->shouldBeCalled()
+            ->willReturn($this->templateData);
+
+        $templater = new TemplateRemoveField($this->templateDataFactory->reveal());
+
+        $expected = [
+            'a34e56d' => [
+                'component' => 'block',
+                'type'      => 12,
+                'config'    => [],
+                'children'  => [
+                    [
+                        'a34e56d' => [
+                            'component' => 'object',
+                            'type'      => 'telephone',
+                            'config'    => [
+                                'tags' => []
+                            ]
+                        ],
+                        '12345' => [
+                            'component' => 'object',
+                            'type'      => 'image',
+                            'config'    => [
+                                'products' => [
+                                    0 => 1,
+                                    1 => 2,
+                                ],
+                            ],
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $templater->remove($this->sheetTemplate, 'tags',[]));
     }
 }
