1e35b8f3992b01062bbcaea2a6155b3c8caf573f
DimitriMorgan <dim.fruit@gmail.com>
2017-07-31 17:29:52 +0200

    1230 - handle CGV duplication

---
+++ src/Domain/Event/Content/Duplicator.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Proximum\Vimeet\Domain\Event\Content;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Repository\Event\ContentRepositoryInterface;
+
+class Duplicator
+{
+    /**
+     * @var ContentRepositoryInterface
+     */
+    private $contentRepository;
+
+    /**
+     * Duplicator constructor.
+     *
+     * @param ContentRepositoryInterface $contentRepository
+     */
+    public function __construct(ContentRepositoryInterface $contentRepository)
+    {
+        $this->contentRepository = $contentRepository;
+    }
+
+    /**
+     * @param Event  $event
+     * @param string $type
+     */
+    public function duplicate(Event $event, string $type)
+    {
+        $content = $this->contentRepository->findByEventAndType($event, $type);
+        $contentDuplicatedFrom = $this
+            ->contentRepository
+            ->findByEventAndType($event->getDuplicatedFrom(), $type)
+        ;
+
+        foreach ($contentDuplicatedFrom->getEvent()->getLocales() as $locale) {
+            $content->translate($locale, $contentDuplicatedFrom->getValue($locale));
+        }
+
+        $this->contentRepository->set($content);
+    }
+}
---
+++ tests/Domain/Event/Content/DuplicatorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Proximum\Vimeet\Tests\Domain\Event\Content;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Domain\Event\Content\Duplicator;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Event\Content;
+use Proximum\Vimeet\Domain\Repository\Event\ContentRepositoryInterface;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+
+class DuplicatorTest extends TestCase
+{
+    public function testDuplicate()
+    {
+        $eventDuplicatedFrom = EventFactory::createEvent('second event');
+        $event               = EventFactory::createEvent(
+            'first event',
+            EventFactory::FALLBACK_LOCALE_DEFAULT,
+            ['fr', 'en',],
+            Event::VAT_MODE_ET,
+            $eventDuplicatedFrom
+        );
+
+        $emptyContent = new Content($event, Content::TYPE_TERMS_OF_SALE);
+        $content      = new Content($eventDuplicatedFrom, Content::TYPE_TERMS_OF_SALE);
+        $content->translate('fr', 'test fr');
+        $content->translate('en', 'test en');
+
+        $contentRepository = $this->prophesize(ContentRepositoryInterface::class);
+        $contentRepository
+            ->findByEventAndType($event, Content::TYPE_TERMS_OF_SALE)
+            ->shouldBeCalled()
+            ->willReturn($emptyContent);
+
+        $contentRepository
+            ->findByEventAndType($eventDuplicatedFrom, Content::TYPE_TERMS_OF_SALE)
+            ->shouldBeCalled()
+            ->willReturn($content);
+
+        $contentRepository->set($emptyContent)->shouldBeCalled();
+
+        (new Duplicator($contentRepository->reveal()))->duplicate($event, Content::TYPE_TERMS_OF_SALE);
+    }
+}
+
+
