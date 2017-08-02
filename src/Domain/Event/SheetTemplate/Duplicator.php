c8dfd4d2664856988fb9aa662021919a63dab916
DimitriMorgan <dim.fruit@gmail.com>
2017-08-02 15:13:54 +0200

    1230 - handle sheet template duplication

---
+++ src/Domain/Event/SheetTemplate/Duplicator.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Proximum\Vimeet\Domain\Event\SheetTemplate;
+
+use Proximum\Vimeet\Application\Template\Sheet\SheetTemplateCloner;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Repository\Template\SheetTemplateRepositoryInterface;
+use Proximum\Vimeet\Domain\Template\TemplateDataFactory;
+
+class Duplicator
+{
+    /**
+     * @var SheetTemplateRepositoryInterface
+     */
+    private $sheetTemplateRepository;
+
+    /**
+     * @var SheetTemplateCloner
+     */
+    private $sheetTemplateCloner;
+
+    /**
+     * @var TemplateDataFactory
+     */
+    private $templateDataFactory;
+
+    /**
+     * Duplicator constructor.
+     *
+     * @param SheetTemplateRepositoryInterface $sheetTemplateRepository
+     * @param SheetTemplateCloner              $sheetTemplateCloner
+     * @param TemplateDataFactory              $templateDataFactory
+     */
+    public function __construct(
+        SheetTemplateRepositoryInterface $sheetTemplateRepository,
+        SheetTemplateCloner $sheetTemplateCloner,
+        TemplateDataFactory $templateDataFactory
+    ) {
+        $this->sheetTemplateRepository = $sheetTemplateRepository;
+        $this->sheetTemplateCloner = $sheetTemplateCloner;
+        $this->templateDataFactory = $templateDataFactory;
+    }
+
+    /**
+     * @param Event $event
+     * @param array $duplicationHelper
+     *
+     * @return array
+     */
+    public function duplicate(Event $event, array $duplicationHelper)
+    {
+        $sheetTemplates = $this
+            ->sheetTemplateRepository
+            ->getTemplateForGivenEvent($event->getDuplicatedFrom())
+        ;
+
+        foreach ($sheetTemplates as $sheetTemplate) {
+            $clonedTemplate = $this->sheetTemplateCloner->duplicate(
+                $sheetTemplate,
+                $event,
+                $sheetTemplate->getTitle()
+            );
+            $templateData        = $this->templateDataFactory->createFromTemplate($sheetTemplate);
+            $nomenclatureObjects = $templateData->getNomenclatureObjects();
+
+            foreach ($nomenclatureObjects as $nomenclatureObject) {
+                $nomenclatureId  = $nomenclatureObject->getNomenclatureId();
+                $newNomenclature = $duplicationHelper['nomenclature'][$nomenclatureId];
+                $nomenclatureObject->setNomenclature($newNomenclature);
+            }
+
+            $clonedTemplate->setValue($templateData->getConfig());
+            $this->sheetTemplateRepository->set($clonedTemplate);
+            $duplicationHelper['registrationTemplate'][$sheetTemplate->getId()] = $clonedTemplate;
+        }
+
+        return $duplicationHelper;
+    }
+}
