fd894eeaf9c7f844c3edf83ef3e72d4038f38ca2
DimitriMorgan <dim.fruit@gmail.com>
2017-08-24 11:00:17 +0200

    669 - who see who categories service

---
+++ src/Domain/Catalog/VisibleParticipationCategories.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) vimeet
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Catalog;
+
+use Proximum\Vimeet\Domain\Model\Category;
+use Proximum\Vimeet\Domain\Model\Rule;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Repository\RuleRepositoryInterface;
+
+class VisibleParticipationCategories
+{
+    /**
+     * @var RuleRepositoryInterface
+     */
+    private $ruleRepository;
+
+    /**
+     * @param RuleRepositoryInterface $ruleRepository
+     */
+    public function __construct(RuleRepositoryInterface $ruleRepository)
+    {
+        $this->ruleRepository = $ruleRepository;
+    }
+
+    /**
+     * @param Sheet $sheet
+     *
+     * @return Category[]
+     */
+    public function getAllowedCategoriesList(Sheet $sheet)
+    {
+        $visibleTypes = [];
+        $categoryTypes = $sheet->getType()->getCategories();
+
+        $rules = $this->ruleRepository->getByEvent($sheet->getEvent());
+
+        $filteredRules = [];
+
+        foreach ($categoryTypes as $categoryType) {
+            $filteredRules = array_filter($rules, function (Rule $rule) use ($categoryType) {
+                return $rule->getSeerCategory() === $categoryType;
+            });
+        }
+
+        foreach ($filteredRules as $rule) {
+            /** @var Category $categoryType */
+            foreach ($rule->getSeeableCategory() as $categoryType) {
+                $visibleTypes[$categoryType->getId()] = $categoryType;
+            }
+        }
+
+        return $visibleTypes;
+    }
+}
