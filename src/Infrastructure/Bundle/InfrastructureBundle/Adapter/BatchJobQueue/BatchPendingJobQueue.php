c429f14ae43d83565a692a978465345929d3dcaa
Paul Boiseau <paulboiseau@gmail.com>
2017-08-30 10:30:39 +0200

    1046 - Add trace event for sheet batch pending

--- src/Infrastructure/Bundle/InfrastructureBundle/Adapter/BatchJobQueue/BatchPendingJobQueue.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Adapter/BatchJobQueue/BatchPendingJobQueue.php
@@ -14,6 +14,7 @@ use JMS\JobQueueBundle\Entity\Job;
 use Proximum\Vimeet\Application\Adapter\BatchJobQueueInterface;
 use Proximum\Vimeet\Domain\Model\Admin;
 use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Adapter\AbstractJobQueueAdapter;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command\Batch\BatchValidationValidateCommand;
 
 class BatchPendingJobQueue extends AbstractJobQueueAdapter implements BatchJobQueueInterface
 {
