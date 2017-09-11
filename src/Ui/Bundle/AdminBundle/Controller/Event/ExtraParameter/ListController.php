1d4f8169f01280abeb6ec7f6aa66c30c801d883b
Nico <nicolas.dievart@elao.com>
2017-09-11 14:27:53 +0200

    1286 - Add list of extra parameters

---
+++ src/Ui/Bundle/AdminBundle/Controller/Event/ExtraParameter/ListController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Event\ExtraParameter;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class ListController extends Controller
+{
+    /**
+     * @param Event $event
+     *
+     * @return Response
+     */
+    public function listAction(Event $event): Response
+    {
+        $extraParameters = $this->get('repository.event.extra_parameter_repository')->findByEvent($event);
+
+        return $this->render('AdminBundle:Event/ExtraParameter:list.html.twig', [
+            'event'           => $event,
+            'extraParameters' => $extraParameters,
+        ]);
+    }
+}
