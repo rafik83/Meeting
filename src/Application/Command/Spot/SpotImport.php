3e8d5f919dbd40d6be76d697a8ff3a4facd9c22c
LamGui <guillaume.webdev@gmail.com>
2017-09-11 12:06:35 +0200

    1015 - Init form type

---
+++ src/Application/Command/Spot/SpotImport.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Spot;
+
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class SpotImport
+{
+    /** @var UploadedFile */
+    public $file;
+
+    /** @var string */
+    public $charset;
+}
