cea84c625a548dfe08f45be20c4ecbc27c41a5c0
Nico <nicolas.dievart@elao.com>
2017-09-14 12:31:14 +0200

    1261 - Add AvailableSlot query handler without day

---
+++ src/Application/Query/Agenda/AvailableSheets/AvailableSlotsByParticipantQuery.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Agenda\AvailableSheets;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Participant;
+
+class AvailableSlotsByParticipantQuery
+{
+    /** @var Event */
+    public $event;
+
+    /** @var Participant */
+    public $participant;
+
+    /**
+     * @param Event       $event
+     * @param Participant $participant
+     */
+    public function __construct(Event $event, Participant $participant)
+    {
+        $this->event = $event;
+        $this->participant = $participant;
+    }
+}
