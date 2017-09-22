bcec307223c716f4653cfce5c24f4f5513bc8959
Nico <nicolas.dievart@elao.com>
2017-09-22 12:09:48 +0200

    1286 - Add ExtraData model and repository

---
+++ src/Domain/User/Event/ExtraData/Type.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\User\Event\ExtraData;
+
+final class Type
+{
+    const LENI_FINGERPRINT = 'leni_fingerprint';
+}
