9102e5b8d3796f8582b341727915d238b042b763
Richard Hanna <richard.hanna@elao.com>
2017-09-28 11:55:04 +0200

    Remove sms blacklist api call

--- src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
+++ src/Ui/Bundle/AdminBundle/Controller/Agenda/AgendaController.php
@@ -10,7 +10,6 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Agenda;
 
-use Proximum\Vimeet\Application\Command\User\Phone\UpdateBlackList;
 use Proximum\Vimeet\Application\Query\Agenda\Admin\AgendaSheetViewQuery;
 use Proximum\Vimeet\Application\Query\Agenda\Admin\Indicator\SheetIndicatorsLazyLoadViewQuery;
 use Proximum\Vimeet\Application\Query\Agenda\Admin\Spot\AgendaSpotViewQuery;
@@ -35,8 +34,6 @@ class AgendaController extends Controller
     public function indexAction(Event $event)
     {
         $this->denyAccessUnlessGranted('PERMISSION_EVENT_ACCESS', $event);
-
-        $this->get('tactician.commandbus')->handle(new UpdateBlackList());
 
         return $this->render('AdminBundle:Agenda:index.html.twig', [
             'event'                        => $event,
