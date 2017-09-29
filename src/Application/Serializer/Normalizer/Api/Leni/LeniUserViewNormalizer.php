d0fbdfa1f3031c09f8660aa4d49c107f955a4038
Nico <nicolas.dievart@elao.com>
2017-09-29 16:34:14 +0200

    1286 - Update normalizer with new keys

--- src/Application/Serializer/Normalizer/Api/Leni/LeniUserViewNormalizer.php
+++ src/Application/Serializer/Normalizer/Api/Leni/LeniUserViewNormalizer.php
@@ -15,18 +15,20 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class LeniUserViewNormalizer implements NormalizerInterface
 {
-    const LENI_COL_PARTICIPANT_ID = 'participantId';
-    const LENI_COL_COMPANY_NAME = 'companyName';
-    const LENI_COL_TYPE = 'type';
-    const LENI_COL_TITLE = 'title';
-    const LENI_COL_FIRST_NAME = 'firstName';
-    const LENI_COL_LAST_NAME = 'lastName';
-    const LENI_COL_POSITION = 'position';
-    const LENI_COL_PHONE_NUMBER = 'phoneNumber';
-    const LENI_COL_EMAIL = 'email';
-    const LENI_COL_MOBILE_PHONE = 'mobilePhone';
-    const LENI_COL_UNALLOCATED = 'ZL_RDVNONORGANISES';
-    const LENI_COL_DAY = 'ZL_JOURNEE';
+    const LENI_COL_CAB_2        = 'CAB2';
+    const LENI_COL_EXTERNAL_KEY = 'CleExterne';
+    const LENI_COL_COMPANY_NAME = 'Societe';
+    const LENI_COL_TYPE         = 'ZL_SOUSCATEGORIE';
+    const LENI_COL_CATEGORY     = 'CategorieIndividuEvt';
+    const LENI_COL_TITLE        = 'Civilite';
+    const LENI_COL_FIRST_NAME   = 'Prenom';
+    const LENI_COL_LAST_NAME    = 'Nom';
+    const LENI_COL_POSITION     = 'Fonction';
+    const LENI_COL_PHONE_NUMBER = 'TelephoneFixe';
+    const LENI_COL_EMAIL        = 'Email';
+    const LENI_COL_MOBILE_PHONE = 'TelephoneMobile';
+    const LENI_COL_UNALLOCATED  = 'ZL_RDVNONORGANISES';
+    const LENI_COL_DAY          = 'ZL_JOURNEE';
 
     /**
      * {@inheritdoc}
@@ -36,17 +38,19 @@ class LeniUserViewNormalizer implements NormalizerInterface
         /** @var LeniUserView $userView */
         $userView = $object;
         $data = [
-            self::LENI_COL_PARTICIPANT_ID => $userView->id,
-            self::LENI_COL_COMPANY_NAME   => $userView->sheetName,
-            self::LENI_COL_TYPE           => $userView->typeName,
-            self::LENI_COL_TITLE          => $userView->gender,
-            self::LENI_COL_FIRST_NAME     => $userView->firstName,
-            self::LENI_COL_LAST_NAME      => $userView->lastName,
-            self::LENI_COL_POSITION       => $userView->position,
-            self::LENI_COL_EMAIL          => $userView->email,
-            self::LENI_COL_MOBILE_PHONE   => $userView->mobile,
-            self::LENI_COL_PHONE_NUMBER   => $userView->phone,
-            self::LENI_COL_UNALLOCATED    => $userView->planning->unallocated,
+            self::LENI_COL_CAB_2        => $userView->id,
+            self::LENI_COL_EXTERNAL_KEY => $userView->id,
+            self::LENI_COL_COMPANY_NAME => $userView->sheetName,
+            self::LENI_COL_CATEGORY     => $userView->categoryName,
+            self::LENI_COL_TYPE         => $userView->typeName,
+            self::LENI_COL_TITLE        => $userView->gender,
+            self::LENI_COL_FIRST_NAME   => $userView->firstName,
+            self::LENI_COL_LAST_NAME    => $userView->lastName,
+            self::LENI_COL_POSITION     => $userView->position,
+            self::LENI_COL_EMAIL        => $userView->email,
+            self::LENI_COL_MOBILE_PHONE => $userView->mobile,
+            self::LENI_COL_PHONE_NUMBER => $userView->phone,
+            self::LENI_COL_UNALLOCATED  => $userView->planning->unallocated,
         ];
 
         $dayNumber = 1;
