79bcc90fe5e83d5c00cc28268d72e8b6f797b412
DimitriMorgan <dim.fruit@gmail.com>
2017-10-10 11:30:52 +0200

    1286 - remove participant limit send

--- src/Application/ThirdParty/LENI/Command/PrepareLeniApiCallHandler.php
+++ src/Application/ThirdParty/LENI/Command/PrepareLeniApiCallHandler.php
@@ -119,7 +119,6 @@ class PrepareLeniApiCallHandler
             );
 
             $userFingerPrints = $this->indexExtraDataByUserId($usersExtraData);
-            $numberUsersSend = 0;
 
             foreach ($users as $user) {
                 $leniUserView = $this->leniUserViewQueryHandler->handle(new LeniUserViewQuery($event, $user));
@@ -152,11 +151,6 @@ class PrepareLeniApiCallHandler
                         $this->dateTime
                     )
                 );
-
-                $numberUsersSend++;
-                if ($numberUsersSend === 100) {
-                    break;
-                }
             }
         }
     }
