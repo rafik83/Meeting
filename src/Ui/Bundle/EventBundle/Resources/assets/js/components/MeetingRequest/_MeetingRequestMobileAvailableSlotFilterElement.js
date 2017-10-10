28c371ed6ceeeb9e9ef7d86e9ff10bd2e8a360c7
Nico <nicolas.dievart@elao.com>
2017-10-10 14:45:16 +0200

    1283 - add mobile filter on gdr

---
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/MeetingRequest/_MeetingRequestMobileAvailableSlotFilterElement.js
@@ -0,0 +1,15 @@
+function MeetingRequestMobileAvailableSlotFilterElement(element, filterId, content) {
+    this.element = element;
+    this.filterId = filterId;
+    this.content = content;
+}
+
+MeetingRequestMobileAvailableSlotFilterElement.prototype.inactive = function() {
+    this.element.getElementsByTagName('a')[0].classList.add('btn-inactive');
+};
+
+MeetingRequestMobileAvailableSlotFilterElement.prototype.active = function() {
+    this.element.getElementsByTagName('a')[0].classList.remove('btn-inactive');
+};
+
+module.exports = MeetingRequestMobileAvailableSlotFilterElement;
---
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/MeetingRequest/_MeetingRequestMobileStateFilterElement.js
@@ -0,0 +1,16 @@
+function MeetingRequestMobileStateTypeFilterElement(element, filterId, content, count) {
+    this.element = element;
+    this.filterId = filterId;
+    this.content = content;
+    this.count = parseInt(count);
+}
+
+MeetingRequestMobileStateTypeFilterElement.prototype.inactive = function() {
+    this.element.getElementsByTagName('a')[0].classList.add('btn-inactive');
+};
+
+MeetingRequestMobileStateTypeFilterElement.prototype.active = function() {
+    this.element.getElementsByTagName('a')[0].classList.remove('btn-inactive');
+};
+
+module.exports = MeetingRequestMobileStateTypeFilterElement;
