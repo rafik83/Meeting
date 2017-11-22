1b7455174c6890b2fac6be468e12339236b7bd5b
Nico <nicolas.dievart@elao.com>
2017-11-22 15:18:34 +0100

    1386 - Add unit test for availability confirmation

---
+++ tests/Application/Command/User/Availability/ConfirmationHandlerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\User\Availability;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Proximum\Vimeet\Application\Command\User\Availability\Confirmation;
+use Proximum\Vimeet\Application\Command\User\Availability\ConfirmationHandler;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\User\Event\ExtraDataRepositoryInterface;
+use Proximum\Vimeet\Domain\Model\User\Event\ExtraData;
+use Proximum\Vimeet\Domain\User\Event\ExtraData\Type;
+
+class ConfirmationHandlerTest extends TestCase
+{
+    public function testHandle()
+    {
+        $event = $this->prophesize(Event::class);
+        $user = $this->prophesize(User::class);
+        $dateTime = new \DateTime();
+
+        $extraDataRepository = $this->prophesize(ExtraDataRepositoryInterface::class);
+        $extraDataRepository
+            ->getExtraDataForEventNameAndUser($event->reveal(), Type::AVAILABILITY_CONFIRMATION, $user->reveal())
+            ->shouldBeCalled()
+            ->willReturn(null)
+        ;
+        $extraDataRepository
+            ->add(new ExtraData($user->reveal(), $event->reveal(), Type::AVAILABILITY_CONFIRMATION, 'confirmed', $dateTime))
+            ->shouldBeCalled()
+        ;
+
+        $handler = new ConfirmationHandler(
+            $extraDataRepository->reveal(),
+            $dateTime
+        );
+        $handler->handle(new Confirmation($event->reveal(), $user->reveal()));
+    }
+
+    public function testHandleAlreadyCreated()
+    {
+        $event = $this->prophesize(Event::class);
+        $user = $this->prophesize(User::class);
+        $extraData = $this->prophesize(ExtraData::class);
+        $dateTime = new \DateTime();
+
+        $extraDataRepository = $this->prophesize(ExtraDataRepositoryInterface::class);
+        $extraDataRepository
+            ->getExtraDataForEventNameAndUser($event->reveal(), Type::AVAILABILITY_CONFIRMATION, $user->reveal())
+            ->shouldBeCalled()
+            ->willReturn($extraData->reveal())
+        ;
+        $extraDataRepository->add(Argument::any())->shouldNotBeCalled();
+
+        $handler = new ConfirmationHandler(
+            $extraDataRepository->reveal(),
+            $dateTime
+        );
+        $handler->handle(new Confirmation($event->reveal(), $user->reveal()));
+    }
+}
