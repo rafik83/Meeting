c3b1610e38652a96a8802b8bd9eac56198f09f8f
Nico <nicolas.dievart@elao.com>
2017-11-23 13:31:48 +0100

    1401 - Add filter by availabilityConfirmationStatus

--- src/Domain/Sheet/Availability/ConfirmationStatus.php
+++ src/Domain/Sheet/Availability/ConfirmationStatus.php
@@ -15,4 +15,10 @@ final class ConfirmationStatus
     const NONE_CONFIRMED = 'none_confirmed';
     const AT_LEAST_ONE_CONFIRMED = 'at_least_one_confirmed';
     const ALL_CONFIRMED = 'all_confirmed';
+
+    const ALL_STATUS = [
+        self::ALL_CONFIRMED,
+        self::AT_LEAST_ONE_CONFIRMED,
+        self::NONE_CONFIRMED,
+    ];
 }
