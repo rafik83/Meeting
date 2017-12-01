967dbffb4b4218774a9ba1a9229f7e9d25cf015c
DimitriMorgan <dim.fruit@gmail.com>
2017-12-01 14:39:20 +0100

    1253 + add handler/action unit tests

---
+++ tests/Ui/Bundle/EventBundle/Controller/User/Phone/IgnoreConfirmationActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) vimeet
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Ui\Bundle\EventBundle\Controller\User\Phone;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\Command\User\Phone\IgnoreConfirmation;
+use Proximum\Vimeet\Application\Command\User\Phone\IgnoreConfirmationHandler;
+use Proximum\Vimeet\Tests\Factory\EventFactory;
+use Proximum\Vimeet\Tests\Factory\ParticipantFactory;
+use Proximum\Vimeet\Tests\Factory\SheetFactory;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\User\Phone\IgnoreConfirmationAction;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class IgnoreConfirmationActionTest extends TestCase
+{
+    public function testAction()
+    {
+        $event = EventFactory::createEvent();
+        $participant = ParticipantFactory::create(SheetFactory::create($event));
+
+        $ignoreConfirmation = new IgnoreConfirmation($event, $participant);
+        $ignoreConfirmationHandler = $this->prophesize(IgnoreConfirmationHandler::class);
+        $ignoreConfirmationHandler->handle($ignoreConfirmation)->shouldBeCalled();
+
+        $action = new IgnoreConfirmationAction($ignoreConfirmationHandler->reveal());
+        $response = $action(SheetFactory::create($event, $participant->getUser()), $participant);
+        $expectedResponse = new JsonResponse([]);
+
+        $this->assertEquals($expectedResponse, $response);
+
+    }
+}
