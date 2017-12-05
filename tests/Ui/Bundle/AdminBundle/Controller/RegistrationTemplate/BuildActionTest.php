97b678d07edac682b70e60fd59f5bea60294e043
Richard Hanna <richard.hanna@elao.com>
2017-12-05 15:55:09 +0100

    1406 - Add BuildAction unit test

---
+++ tests/Ui/Bundle/AdminBundle/Controller/RegistrationTemplate/BuildActionTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Ui\Bundle\AdminBundle\Controller\RegistrationTemplate;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Template\RegistrationTemplate;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Security\Voter\AdminTemplateAccessVoter;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\RegistrationTemplate\BuildAction;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BuildActionTest extends TestCase
+{
+    public function testInvoke()
+    {
+        $authorizationChecker = $this->prophesize(AuthorizationCheckerAdapterInterface::class);
+        $engine = $this->prophesize(EngineInterface::class);
+        $request = $this->prophesize(Request::class);
+        $response = $this->prophesize(Response::class);
+        $event = $this->prophesize(Event::class);
+        $registrationTemplate = $this->prophesize(RegistrationTemplate::class);
+
+        $registrationTemplate->getEvent()->willReturn($event->reveal());
+
+        $authorizationChecker->isGranted('ROLE_ALLOWED_TO_ORGANIZE')->shouldBeCalled()->willReturn(true);
+        $authorizationChecker
+            ->isGranted(
+                AdminTemplateAccessVoter::PERMISSION_TEMPLATE_EDIT,
+                $registrationTemplate
+            )
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
+
+        $engine
+            ->renderResponse('AdminBundle:RegistrationTemplate:builder.html.twig', [
+                'registrationTemplate' => $registrationTemplate->reveal(),
+                'event' => $event->reveal(),
+            ])
+            ->shouldBeCalled()
+            ->willReturn($response->reveal())
+        ;
+
+        $buildAction = new BuildAction($authorizationChecker->reveal(), $engine->reveal());
+        $response = $buildAction($request->reveal(), $registrationTemplate->reveal(), 'fr');
+
+        $this->assertInstanceOf(Response::class, $response);
+    }
+}
