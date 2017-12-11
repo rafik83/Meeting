6bd34e1d6259bd8a12cd5d9bf27baec91fca0f59
Richard Hanna <richard.hanna@elao.com>
2017-12-11 14:04:31 +0100

    1410 - Add save and loading button

--- src/Ui/Bundle/AdminBundle/Resources/assets/js/components/_RegistrationTemplateBuilder.js
+++ src/Ui/Bundle/AdminBundle/Resources/assets/js/components/_RegistrationTemplateBuilder.js
@@ -1,4 +1,5 @@
-var TemplateBlock = require('./_TemplateBlock');
+var LoadingButton = require('./_LoadingButton'),
+  TemplateBlock = require('./_TemplateBlock');
 
 /**
  * TemplateBuilder
@@ -9,6 +10,14 @@ var TemplateBlock = require('./_TemplateBlock');
 function RegistrationTemplateBuilder(element) {
   this.element = element;
   this.templateContainer = element.querySelector('#template-container');
+
+  var saveButton = element.querySelector('#template-save-button');
+  this.saveButton = new LoadingButton(saveButton, saveButton.getAttribute('data-loading-button'));
+  this.saveButton.element.addEventListener('click', function (event) {
+    event.preventDefault();
+    this.save();
+  }.bind(this));
+
   this.init(this.templateContainer);
 }
 
@@ -35,6 +44,30 @@ RegistrationTemplateBuilder.prototype.children = function (item) {
 };
 
 RegistrationTemplateBuilder.prototype.closeMenu = function () {
+  // nothing to do; closeMenu() is needed by TemplateBlock
+};
+
+RegistrationTemplateBuilder.prototype.save = function () {
+  this.saveButton.start();
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    var DONE = 4;
+    var OK   = 200;
+
+    if (xhr.readyState === DONE) {
+      if (xhr.status === OK) {
+        var config = JSON.parse(xhr.response);
+      } else {
+        alert('Error');
+      }
+
+      this.saveButton.stop();
+    }
+  }.bind(this);
+
+  xhr.open('POST', this.url);
+  xhr.send(JSON.stringify(this.normalize(this.templateContainer)));
 };
 
 module.exports = RegistrationTemplateBuilder;
