23c403aa3512a39b985aae5b9e65fec39f3983c2
Richard Hanna <richard.hanna@elao.com>
2017-12-13 17:34:43 +0100

    1412 - Toggle tags display

--- src/Ui/Bundle/AdminBundle/Resources/assets/js/components/templateObjects/_EditableTextObject.js
+++ src/Ui/Bundle/AdminBundle/Resources/assets/js/components/templateObjects/_EditableTextObject.js
@@ -13,7 +13,37 @@ function EditableTextObject(element, locale)
   this.locale  = locale;
   this.form    = new Form(element);
   this.config  = JSON.parse(this.element.getAttribute('data-config'));
+
+  this.sheetDataField = element.querySelector('input[name="sheet_data"]');
+
+  if (this.sheetDataField) {
+    this.participantDataField = element.querySelector('input[name="participant_data"]');
+    this.sheetDataField.onchange = this.handleDataTypeChanged.bind(this);
+    this.participantDataField.onchange = this.handleDataTypeChanged.bind(this);
+
+    this.sheetTagNode = element.querySelector('[data-sheet-tag]');
+    this.participantTagNode = element.querySelector('[data-participant-tag]');
+
+    this.toggleDisplay(this.sheetTagNode, false);
+    this.toggleDisplay(this.participantTagNode, false);
+  }
 }
+
+EditableTextObject.prototype.handleDataTypeChanged = function (event)
+{
+  if (event.target === this.sheetDataField) {
+    this.toggleDisplay(this.sheetTagNode, event.target.checked);
+
+    return;
+  }
+
+  this.toggleDisplay(this.participantTagNode, event.target.checked);
+};
+
+EditableTextObject.prototype.toggleDisplay = function (element, displayed)
+{
+  element.style.display = displayed ? 'block' : 'none';
+};
 
 EditableTextObject.prototype.fill = function ()
 {
