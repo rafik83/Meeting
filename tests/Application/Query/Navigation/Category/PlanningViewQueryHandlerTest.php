f6fb845449fc2c3b74364b08746b9435a31f401b
Nico <nicolas.dievart@elao.com>
2017-12-21 11:03:03 +0100

    970 - Fix date of agenda opening

--- tests/Application/Query/Navigation/Category/PlanningViewQueryHandlerTest.php
+++ tests/Application/Query/Navigation/Category/PlanningViewQueryHandlerTest.php
@@ -18,7 +18,7 @@ use Proximum\Vimeet\Application\Query\Navigation\Category\PlanningViewQueryHandl
 use Proximum\Vimeet\Application\View\Navigation\CategoryView;
 use Proximum\Vimeet\Application\View\Navigation\LinkView;
 use Proximum\Vimeet\Application\View\Navigation\StateButtonView;
-use Proximum\Vimeet\Domain\KeyDates\Checker\HappeningsAccessChecker;
+use Proximum\Vimeet\Domain\KeyDates\Checker\AgendaAccessChecker;
 use Proximum\Vimeet\Domain\KeyDates\Checker\MeetingPublishedAccessChecker;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Navigation\NavigationBuilderInterface;
@@ -29,25 +29,19 @@ use PHPUnit\Framework\TestCase;
 
 class PlanningViewQueryHandlerTest extends TestCase
 {
-    /**
-     * @var ObjectProphecy
-     */
+    /** @var ObjectProphecy */
     private $navigationBuilder;
 
-    /**
-     * @var ObjectProphecy
-     */
-    private $happeningsAccessChecker;
+    /** @var ObjectProphecy */
+    private $agendaAccessChecker;
 
-    /**
-     * @var ObjectProphecy
-     */
+    /** @var ObjectProphecy */
     private $meetingPublishedAccessChecker;
 
     public function setUp()
     {
         $this->navigationBuilder             = $this->prophesize(NavigationBuilderInterface::class);
-        $this->happeningsAccessChecker       = $this->prophesize(HappeningsAccessChecker::class);
+        $this->agendaAccessChecker           = $this->prophesize(AgendaAccessChecker::class);
         $this->meetingPublishedAccessChecker = $this->prophesize(MeetingPublishedAccessChecker::class);
     }
 
@@ -58,12 +52,12 @@ class PlanningViewQueryHandlerTest extends TestCase
         $sheet = SheetFactory::create($event, $user);
 
         $this->navigationBuilder->getRoute(Argument::any())->shouldNotBeCalled();
-        $this->happeningsAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
+        $this->agendaAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
         $this->meetingPublishedAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
 
         $handler = new PlanningViewQueryHandler(
             $this->navigationBuilder->reveal(),
-            $this->happeningsAccessChecker->reveal(),
+            $this->agendaAccessChecker->reveal(),
             $this->meetingPublishedAccessChecker->reveal()
         );
         $result = $handler->handle(new PlanningViewQuery($sheet, $user, 'fr'));
@@ -80,15 +74,15 @@ class PlanningViewQueryHandlerTest extends TestCase
         $user  = UserFactory::create();
         $sheet = SheetFactory::create($event, $user);
         $date  = new \DateTime('2016-10-12 10:00:00.000');
-        $event->getConfiguration()->setDates(null, $date, null);
+        $event->getConfiguration()->setDates(null, null, null, null, null, null, $date);
 
         $this->navigationBuilder->getRoute(Argument::any())->shouldNotBeCalled();
-        $this->happeningsAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(false);
+        $this->agendaAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(false);
         $this->meetingPublishedAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
 
         $handler = new PlanningViewQueryHandler(
             $this->navigationBuilder->reveal(),
-            $this->happeningsAccessChecker->reveal(),
+            $this->agendaAccessChecker->reveal(),
             $this->meetingPublishedAccessChecker->reveal()
         );
         $result = $handler->handle(new PlanningViewQuery($sheet, $user, 'fr'));
@@ -113,15 +107,15 @@ class PlanningViewQueryHandlerTest extends TestCase
         $sheet->getId()->shouldBeCalled()->willReturn(1);
 
         $date  = new \DateTime('2016-10-12 10:00:00.000');
-        $event->getConfiguration()->setDates(null, $date, null);
+        $event->getConfiguration()->setDates(null, null, null, null, null, null, $date);
 
         $this->navigationBuilder->getRoute('event_agenda', ['sheet' => 1])->shouldBeCalled()->willReturn('route');
-        $this->happeningsAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(true);
+        $this->agendaAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(true);
         $this->meetingPublishedAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
 
         $handler = new PlanningViewQueryHandler(
             $this->navigationBuilder->reveal(),
-            $this->happeningsAccessChecker->reveal(),
+            $this->agendaAccessChecker->reveal(),
             $this->meetingPublishedAccessChecker->reveal()
         );
         $result = $handler->handle(new PlanningViewQuery($sheet->reveal(), $user, 'fr'));
@@ -146,12 +140,12 @@ class PlanningViewQueryHandlerTest extends TestCase
         $event->getConfiguration()->setDates(null, null, $date);
 
         $this->navigationBuilder->getRoute(Argument::any())->shouldNotBeCalled();
-        $this->happeningsAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
+        $this->agendaAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
         $this->meetingPublishedAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(false);
 
         $handler = new PlanningViewQueryHandler(
             $this->navigationBuilder->reveal(),
-            $this->happeningsAccessChecker->reveal(),
+            $this->agendaAccessChecker->reveal(),
             $this->meetingPublishedAccessChecker->reveal()
         );
         $result = $handler->handle(new PlanningViewQuery($sheet, $user, 'fr'));
@@ -185,12 +179,12 @@ class PlanningViewQueryHandlerTest extends TestCase
         $sheet->getId()->shouldBeCalled()->willReturn(1);
 
         $this->navigationBuilder->getRoute('event_agenda', ['sheet' => 1])->shouldBeCalled()->willReturn('route');
-        $this->happeningsAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
+        $this->agendaAccessChecker->allowedToAccess(Argument::any())->shouldNotBeCalled();
         $this->meetingPublishedAccessChecker->allowedToAccess($event)->shouldBeCalled()->willReturn(true);
 
         $handler = new PlanningViewQueryHandler(
             $this->navigationBuilder->reveal(),
-            $this->happeningsAccessChecker->reveal(),
+            $this->agendaAccessChecker->reveal(),
             $this->meetingPublishedAccessChecker->reveal()
         );
         $result = $handler->handle(new PlanningViewQuery($sheet->reveal(), $user, 'fr'));
