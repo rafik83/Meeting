d41cf892e53f121ee45f666ffbdf7459e50a5983
Richard Hanna <richard.hanna@elao.com>
2018-01-09 10:54:51 +0100

    1437 - Cascade delete for CartRowParticipant

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180104153743.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180104153743.php
@@ -19,7 +19,7 @@ class Version20180104153743 extends AbstractMigration
 
         $this->addSql('CREATE TABLE cart_row_participant (cart_row_id INT NOT NULL, participant_id INT NOT NULL, INDEX IDX_EF46B0A98D260BAD (cart_row_id), INDEX IDX_EF46B0A99D1C3019 (participant_id), PRIMARY KEY(cart_row_id, participant_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE cart_row_participant ADD CONSTRAINT FK_EF46B0A98D260BAD FOREIGN KEY (cart_row_id) REFERENCES cart_row (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE cart_row_participant ADD CONSTRAINT FK_EF46B0A99D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id)');
+        $this->addSql('ALTER TABLE cart_row_participant ADD CONSTRAINT FK_EF46B0A99D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE participant ADD participant_product_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE participant ADD CONSTRAINT FK_D79F6B1179448044 FOREIGN KEY (participant_product_id) REFERENCES product (id)');
         $this->addSql('CREATE INDEX IDX_D79F6B1179448044 ON participant (participant_product_id)');
