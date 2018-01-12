a8865897012f99704915f28ce94e8f11f178a637
Richard Hanna <richard.hanna@elao.com>
2018-01-12 18:00:29 +0100

    1437 - Finalize product widget

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/_PackageParticipantRow.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/_PackageParticipantRow.js
@@ -5,17 +5,76 @@ function PackageParticipantRow(parent, element)
 {
   this.parent = parent;
   this.element = element;
+  this.selectElement = this.element.querySelector('select.participant-product');
+  this.productTitleTextElement = this.element.querySelector('[data-product-title-text]');
+  this.productTitleElement = this.element.querySelector('[data-product-title]');
+  this.productDescriptionElement = this.element.querySelector('[data-product-description]');
+  this.productIncludedElement = this.element.querySelector('[data-product-included]');
+  this.productPriceElement = this.element.querySelector('[data-product-price]');
 
-  this.select();
+  this.productTitleElement.querySelector('a').addEventListener('click', this.editRow.bind(this));
+
+  if (this.selectElement.value) {
+    this.hide(this.selectElement);
+    this.updateRow(this.selectElement.value);
+  } else {
+    this.initSelect();
+  }
+
+  $(this.selectElement).on('select2:close', function (event) {
+    if (event.target.value) {
+      $(this.selectElement).select2('destroy');
+      this.updateRow(event.target.value);
+    }
+  }.bind(this));
 }
 
-PackageParticipantRow.prototype.select = function () {
-  [].forEach.call(this.element.querySelectorAll('select.participant-product'), function (selectElement) {
-    $(selectElement).select2({
-      templateResult: this.formatState.bind(this),
-      minimumResultsForSearch: 10
-    });
-  }.bind(this));
+PackageParticipantRow.prototype.show = function (element) {
+  element.classList.remove('hidden');
+};
+
+PackageParticipantRow.prototype.hide = function (element) {
+  element.classList.add('hidden');
+};
+
+PackageParticipantRow.prototype.initSelect = function () {
+  this.hide(this.productTitleElement);
+  this.hide(this.productDescriptionElement);
+  this.hide(this.productIncludedElement);
+  this.hide(this.productPriceElement);
+
+  this.show(this.selectElement);
+
+  $(this.selectElement).select2({
+    templateResult: this.formatState.bind(this),
+    minimumResultsForSearch: 10
+  });
+
+  this.hide(this.selectElement);
+};
+
+PackageParticipantRow.prototype.editRow = function (event) {
+  event.preventDefault();
+  this.initSelect();
+  $(this.selectElement).select2('open');
+};
+
+PackageParticipantRow.prototype.updateRow = function (productId)
+{
+  var participantProduct = this.parent.getParticipantProduct(productId);
+
+  if (!participantProduct) {
+    return;
+  }
+
+  var price = this.parent.getParticipantProductPrice(productId);
+
+  this.productTitleTextElement.innerHTML = participantProduct.title;
+  this.productDescriptionElement.innerHTML = participantProduct.description;
+  this.productPriceElement.innerHTML = price;
+  this.show(this.productTitleElement);
+  this.show(this.productDescriptionElement);
+  this.show(this.productPriceElement);
 };
 
 PackageParticipantRow.prototype.formatState = function (state)
