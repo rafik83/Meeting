6b1e0d02205676109471ea434489ff54a2c752a0
DimitriMorgan <dim.fruit@gmail.com>
2018-02-05 15:47:50 +0100

    1415 - remove useless code + move button in list

--- src/Ui/Bundle/AdminBundle/Resources/assets/js/components/templateObjects/_TextObject.js
+++ src/Ui/Bundle/AdminBundle/Resources/assets/js/components/templateObjects/_TextObject.js
@@ -14,10 +14,6 @@ function TextObject(element, locale)
   this.locale  = locale;
   this.form    = new Form(element);
   this.config  = JSON.parse(this.element.getAttribute('data-config'));
-
-  if (element.querySelector('[data-template-tags-select]')) {
-    this.templateTaggableObject = new TemplateTaggableObject(element);
-  }
 }
 
 TextObject.prototype.fill = function ()
@@ -28,8 +24,6 @@ TextObject.prototype.fill = function ()
     this.form.bind('content', this.config.content[this.locale]);
   }
 
-  this.form.set('tag', this.config.tag);
-  this.form.set('tags', this.config.tags);
   this.form.set('type', this.config.type);
 
 };
@@ -37,16 +31,10 @@ TextObject.prototype.fill = function ()
 TextObject.prototype.save = function ()
 {
   this.config.style                = this.form.get('style');
-  if (this.config.content) {
-    this.config.content[this.locale] = this.form.get('content');
-  }
+  this.config.content[this.locale] = this.form.get('content');
   this.config.type                 = this.form.get('type');
-  this.config.tag                  = this.form.get('tag');
-  this.config.tags                 = this.form.get('tags');
 
-  if (this.config.content) {
-    this.form.bind('content', this.config.content[this.locale]);
-  }
+  this.form.bind('content', this.config.content[this.locale]);
 
   return true;
 };
