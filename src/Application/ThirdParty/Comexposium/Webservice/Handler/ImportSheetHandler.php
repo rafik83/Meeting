405508d855dc4d99a051dceee193090f8686826a
Richard Hanna <richard.hanna@elao.com>
2018-02-15 08:51:39 +0100

    1486 - Add ImportSheetHandler unit test

---
+++ src/Application/ThirdParty/Comexposium/Webservice/Handler/ImportSheetHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\ThirdParty\Comexposium\Webservice\Handler;
+
+use Proximum\Vimeet\Domain\Model\Event;
+
+class ImportSheetHandler
+{
+    public function __construct()
+    {
+    }
+
+    public function handle(Event $event, string $registratioReference): void
+    {
+    }
+}
---
+++ tests/Application/ThirdParty/Comexposium/Webservice/Handler/ImportSheetHandlerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\ThirdParty\Comexposium\Webservice\Handler;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\ThirdParty\Comexposium\Webservice\Handler\ImportSheetHandler;
+
+class ImportSheetHandlerTest extends TestCase
+{
+    public function testHandle()
+    {
+        $importSheetHandler = new ImportSheetHandler();
+    }
+}
