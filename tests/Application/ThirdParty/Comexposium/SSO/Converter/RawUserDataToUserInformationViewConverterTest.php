393999f457f08084e6f2f5bd5f22d4e387baaf3c
Richard Hanna <richard.hanna@elao.com>
2018-03-06 10:52:04 +0100

    1505 - Add RawUserDataToUserInformationViewConverter unit test

---
+++ tests/Application/ThirdParty/Comexposium/SSO/Converter/RawUserDataToUserInformationViewConverterTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: richard
+ * Date: 06/03/2018
+ * Time: 10:40
+ */
+
+namespace Proximum\Vimeet\Tests\Application\ThirdParty\Comexposium\SSO\Converter;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\ThirdParty\Comexposium\SSO\Converter\RawUserDataToUserInformationViewConverter;
+use Proximum\Vimeet\Application\ThirdParty\Comexposium\SSO\View\UserInformationView;
+
+class RawUserDataToUserInformationViewConverterTest extends TestCase
+{
+    public function testConvert()
+    {
+        $rawUserDataToUserInformationViewConverter = new RawUserDataToUserInformationViewConverter();
+        $userInformationView = $rawUserDataToUserInformationViewConverter->convert(
+            'myemail@example.com',
+            [
+                'civility' => '0',
+                'firstname' => 'Bruce',
+                'lastname' => 'Willis',
+                'mobilephone:indicatif' => '+33',
+                'mobilephone:content' => '699887766',
+                'country' => 'FRA',
+            ]
+        );
+
+        $expectedUserInformationView = new UserInformationView(
+            'myemail@example.com',
+            'man',
+            'Bruce',
+            'Willis',
+            '+33699887766',
+            'FR'
+        );
+
+        $this->assertEquals($expectedUserInformationView, $userInformationView);
+    }
+
+    public function testConvertWithNull()
+    {
+        $rawUserDataToUserInformationViewConverter = new RawUserDataToUserInformationViewConverter();
+        $userInformationView = $rawUserDataToUserInformationViewConverter->convert(
+            'myemail@example.com',
+            [
+                'civility' => '2',
+                'firstname' => 'Bruce',
+                'lastname' => 'Willis',
+            ]
+        );
+
+        $expectedUserInformationView = new UserInformationView(
+            'myemail@example.com',
+            null,
+            'Bruce',
+            'Willis',
+            null,
+            null
+        );
+
+        $this->assertEquals($expectedUserInformationView, $userInformationView);
+    }
+}
