7002c9201798a55b2da1c59b09054591461796ad
Richard Hanna <richard.hanna@elao.com>
2018-03-07 17:22:32 +0100

    Add event extra data repository

---
+++ src/Domain/Repository/Event/ExtraDataRepositoryInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository\Event;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Event\ExtraData;
+
+interface ExtraDataRepositoryInterface
+{
+    /**
+     * @param ExtraData $extraData
+     */
+    public function add(ExtraData $extraData);
+
+    /**
+     * @param ExtraData $extraData
+     */
+    public function set(ExtraData $extraData);
+
+    /**
+     * @param Event  $event
+     * @param string $name
+     *
+     * @return ExtraData
+     */
+    public function getExtraDataForEvent(Event $event, string $name): ExtraData;
+}
