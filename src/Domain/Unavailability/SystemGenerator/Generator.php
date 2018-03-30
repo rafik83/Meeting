e068d843c381e04bcdcec0bc0c16af1863aabdac
Nico <nicolas.dievart@elao.com>
2018-03-30 16:26:19 +0200

    1521 - Init system unavailability generator

---
+++ src/Domain/Unavailability/SystemGenerator/Generator.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Proximumn Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Unavailability\SystemGenerator;
+
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\User;
+
+class Generator
+{
+    public function generateSystemUnavailability(Event $event, User $user): void
+    {
+
+    }
+}
---
+++ tests/Domain/Unavailability/SystemGenerator/GeneratorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Domain\Unavailability\SystemGenerator;
+
+use PHPUnit\Framework\TestCase;
+
+class GeneratorTest extends TestCase
+{
+
+}
