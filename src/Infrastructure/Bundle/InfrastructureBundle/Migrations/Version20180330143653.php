f931538e99d5ce5dced3898a0f95dd5acbd542be
Nico <nicolas.dievart@elao.com>
2018-03-30 16:38:37 +0200

    1521 - Add index on createdBy

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180330143653.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add index on field createdBy on Unavailability
+ */
+class Version20180330143653 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE INDEX createdByIndex ON unavailability (created_by)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX createdByIndex ON unavailability');
+    }
+}
