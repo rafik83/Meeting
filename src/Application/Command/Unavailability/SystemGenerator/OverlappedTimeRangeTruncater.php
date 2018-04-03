0b65e84465e9a3f874c027f6052d063c457e3d40
Nico <nicolas.dievart@elao.com>
2018-04-03 16:50:28 +0200

    1521 - Truncate overlapped time range

---
+++ src/Application/Command/Unavailability/SystemGenerator/OverlappedTimeRangeTruncater.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Command\Unavailability\SystemGenerator;
+
+use Proximum\Vimeet\Domain\Time\TimeOverlap;
+
+/**
+ * This class is used to truncate array of timeRanges that overlaps other array of timeRanges
+ * The timeRange are cut of the part that overlaps the other timeRange
+ */
+class OverlappedTimeRangeTruncater
+{
+    /**
+     * @param AbstractTimeRange   $needle
+     * @param AbstractTimeRange[] $haystack
+     *
+     * @return AbstractTimeRange[]
+     */
+    public function truncate(AbstractTimeRange $needle, array $haystack): array
+    {
+        $result = [];
+        $needleToCheck = $needle;
+
+        foreach ($haystack as $timeRange) {
+            if (TimeOverlap::beginIn($needleToCheck, $timeRange)) {
+                $needleToCheck->begin = $timeRange->end;
+
+                continue;
+            }
+
+            if (TimeOverlap::endIn($needleToCheck, $timeRange)) {
+                $needleToCheck->end = $timeRange->begin;
+
+                continue;
+            }
+
+            // If the needle contains a timeRange
+            // We can the needle in two, and continue the check with the second part
+            if (TimeOverlap::contains($timeRange, $needleToCheck)) {
+                $firstNeedle = clone $needleToCheck;
+                $firstNeedle->end = $timeRange->begin;
+
+                $result[] = $firstNeedle;
+
+                $secondNeedle = clone $needleToCheck;
+                $secondNeedle->begin = $timeRange->end;
+                $needleToCheck = $secondNeedle;
+            }
+        }
+
+        $result[] = $needleToCheck;
+
+        return $result;
+    }
+}
