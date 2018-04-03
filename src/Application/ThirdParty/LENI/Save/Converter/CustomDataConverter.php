734436326fad372c5817cc123614a9365bc8e1c7
Richard Hanna <richard.hanna@elao.com>
2018-04-03 15:21:07 +0200

    1543 - Convert Sheet state to LENI state

--- src/Application/ThirdParty/LENI/Save/Converter/CustomDataConverter.php
+++ src/Application/ThirdParty/LENI/Save/Converter/CustomDataConverter.php
@@ -14,12 +14,12 @@ class CustomDataConverter
 {
     /**
      * @param array $customDataMapping LENI field indexed by tag,
-     *              example: ['sheet_organization_staff' => 'ZL_Effectif', 'sheet_generic_tag_20' => 'ZL_TypePrestation']
+     *              example: ['sheet_state' => 'ZL_MODERATION', 'sheet_organization_staff' => 'ZL_Effectif', 'sheet_generic_tag_20' => 'ZL_TypePrestation']
      * @param array $taggedData data indexed by tag,
-     *              example: ['sheet_organization_staff' => 'A1', 'sheet_generic_tag_20' => ['P12', 'P3', 'P5']]
+     *              example: ['sheet_state' => 'Y', 'sheet_organization_staff' => 'A1', 'sheet_generic_tag_20' => ['P12', 'P3', 'P5']]
      *
      * @return array indexed by LENI field
-     *               exemple: ['ZL_Effectif' => 'A1', 'ZL_TypePrestation' => ['P12', 'P3', 'P5']]
+     *               exemple: ['ZL_MODERATION' => 'Y', 'ZL_Effectif' => 'A1', 'ZL_TypePrestation' => ['P12', 'P3', 'P5']]
      */
     public function convert(array $customDataMapping, array $taggedData): array
     {
