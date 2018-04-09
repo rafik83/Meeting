6880f4fe0765f615757788694758249bb9aceb96
Nico <nicolas.dievart@elao.com>
2018-04-09 16:15:30 +0200

    1532 - Add other check for package passable and other participant time range

--- src/Domain/AvailabilityTimeRange/Product/ParticipantProductWithAvailabilityTimeRangeChecker.php
+++ src/Domain/AvailabilityTimeRange/Product/ParticipantProductWithAvailabilityTimeRangeChecker.php
@@ -10,8 +10,12 @@
 
 namespace Proximum\Vimeet\Domain\AvailabilityTimeRange\Product;
 
+use Proximum\Vimeet\Domain\Exception\Package\PackageNotPassableException;
+use Proximum\Vimeet\Domain\Model\AvailabilityTimeRange;
+use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Participant;
 use Proximum\Vimeet\Domain\Model\Product;
+use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
 use Proximum\Vimeet\Domain\Unavailability\SystemGenerator\OverlappedTimeRangeMerger;
 use Proximum\Vimeet\Domain\Unavailability\SystemGenerator\OverlappedTimeRangeTruncater;
@@ -64,32 +68,23 @@ class ParticipantProductWithAvailabilityTimeRangeChecker
             return true;
         }
 
+        $user = $participant->getUser();
+        $event = $participant->getSheet()->getEvent();
+
+        try {
+            $othersAvailabilityTimeRanges = $this->getOtherAvailabilityTimeRangeBought($event, $user, $participant);
+        } catch (PackageNotPassableException $exception) {
+            // If the user has a package not passable, it means he/she has full access on the event
+            return true;
+        }
+
         $newAvailabilityTimeRanges = $product->getAvailabilityTimeRanges();
 
-        $newAvailabilityTimeRangesById = [];
-        $timeRangeViews = [];
-        $timeRangeNotAccessibleViews = [];
-        $removedAvailabilityTimeRanges = [];
-
-        foreach ($newAvailabilityTimeRanges as $newAvailabilityTimeRange) {
-            $newAvailabilityTimeRangesById[$newAvailabilityTimeRange->getId()] = $newAvailabilityTimeRange;
-
-            $timeRangeViews[] = new TimeRangeView(
-                $newAvailabilityTimeRange->getBegin(),
-                $newAvailabilityTimeRange->getEnd()
-            );
-        }
-
-        foreach ($currentAvailabilityTimeRanges as $currentAvailabilityTimeRange) {
-            if (!isset($newAvailabilityTimeRangesById[$currentAvailabilityTimeRange->getId()])) {
-                $removedAvailabilityTimeRanges[$currentAvailabilityTimeRange->getId()] = $currentAvailabilityTimeRange;
-
-                $timeRangeNotAccessibleViews[] = new TimeRangeNotAccessibleView(
-                    $currentAvailabilityTimeRange->getBegin(),
-                    $currentAvailabilityTimeRange->getEnd()
-                );
-            }
-        }
+        list ($timeRangeViews, $timeRangeNotAccessibleViews) = $this->getTimeRangeAccessibleAndNotAccessible(
+            $newAvailabilityTimeRanges,
+            $othersAvailabilityTimeRanges,
+            $currentAvailabilityTimeRanges
+        );
 
         // If the new product add more or the same time range, it is ok to set it
         if (!empty($timeRangeNotAccessibleViews)) {
@@ -125,5 +120,104 @@ class ParticipantProductWithAvailabilityTimeRangeChecker
         }
 
         return true;
+    }
+
+    /**
+     * @param Event       $event
+     * @param User        $user
+     * @param Participant $participant
+     *
+     * @return AvailabilityTimeRange[]
+     *
+     * @throws PackageNotPassableException
+     */
+    private function getOtherAvailabilityTimeRangeBought(Event $event, User $user, Participant $participant): array
+    {
+        $availabilityTimeRangesBought = [];
+
+        $participants = $this->participantRepository->getAllParticipantForUser($event, $user);
+
+        if (\count($participants) === 1) {
+            return $availabilityTimeRangesBought;
+        }
+
+        foreach ($participants as $participantToChecker) {
+            // We do not handle the current participant
+            if ($participant->getId() === $participantToChecker->getId()) {
+                continue;
+            }
+
+            if (!$participant->getSheet()->getPackage()->isPassable()) {
+                throw new PackageNotPassableException('Package not passable');
+            }
+
+            $product = $participant->getParticipantProduct();
+
+            if ($product instanceof Product) {
+                foreach ($product->getAvailabilityTimeRanges() as $availabilityTimeRange) {
+                    $availabilityTimeRangesBought[$availabilityTimeRange->getId()] = $availabilityTimeRange;
+                }
+            }
+        }
+
+        return $availabilityTimeRangesBought;
+    }
+
+    /**
+     * @param array $newAvailabilityTimeRanges
+     * @param array $othersAvailabilityTimeRanges
+     * @param array $currentAvailabilityTimeRanges
+     *
+     * @return array of [
+     *    TimeRangeView[],
+     *    TimeRangeNotAccessibleView[],
+     * ]
+     */
+    private function getTimeRangeAccessibleAndNotAccessible(
+        array $newAvailabilityTimeRanges,
+        array $othersAvailabilityTimeRanges,
+        array $currentAvailabilityTimeRanges
+    ): array {
+        $newAvailabilityTimeRangesById = [];
+        $timeRangeViews = [];
+        $timeRangeNotAccessibleViews = [];
+
+        foreach ($newAvailabilityTimeRanges as $newAvailabilityTimeRange) {
+            if (!isset($newAvailabilityTimeRangesById[$newAvailabilityTimeRange->getId()])) {
+                $newAvailabilityTimeRangesById[$newAvailabilityTimeRange->getId()] = $newAvailabilityTimeRange;
+
+                $timeRangeViews[] = new TimeRangeView(
+                    $newAvailabilityTimeRange->getBegin(),
+                    $newAvailabilityTimeRange->getEnd()
+                );
+            }
+        }
+
+        foreach ($othersAvailabilityTimeRanges as $othersAvailabilityTimeRange) {
+            if (!isset($newAvailabilityTimeRangesById[$othersAvailabilityTimeRange->getId()])) {
+                $newAvailabilityTimeRangesById[$othersAvailabilityTimeRange->getId()] = $othersAvailabilityTimeRange;
+
+                $timeRangeViews[] = new TimeRangeView(
+                    $othersAvailabilityTimeRange->getBegin(),
+                    $othersAvailabilityTimeRange->getEnd()
+                );
+            }
+        }
+
+        foreach ($currentAvailabilityTimeRanges as $currentAvailabilityTimeRange) {
+            if (!isset($newAvailabilityTimeRangesById[$currentAvailabilityTimeRange->getId()])) {
+                $removedAvailabilityTimeRanges[$currentAvailabilityTimeRange->getId()] = $currentAvailabilityTimeRange;
+
+                $timeRangeNotAccessibleViews[] = new TimeRangeNotAccessibleView(
+                    $currentAvailabilityTimeRange->getBegin(),
+                    $currentAvailabilityTimeRange->getEnd()
+                );
+            }
+        }
+
+        return [
+            $timeRangeViews,
+            $timeRangeNotAccessibleViews
+        ];
     }
 }
