1be5295871c32403efed8655f37c15ac604a7f28
Nico <nicolas.dievart@elao.com>
2018-04-13 10:42:26 +0200

    1561 - Add CartRowParticipantRepository

---
+++ src/Infrastructure/Repository/CartRowParticipantRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Model\CartRowParticipant;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Repository\CartRowParticipantRepositoryInterface;
+
+class CartRowParticipantRepository implements CartRowParticipantRepositoryInterface
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findByParticipant(Participant $participant): ?CartRowParticipant
+    {
+        $queryBuilder = $this->entityManager->createQueryBuilder()
+            ->select('cartRowParticipant, cartRow, product')
+            ->from(CartRowParticipant::class, 'cartRowParticipant')
+            ->join('cartRowParticipant.participant', 'participant', 'WITH', 'participant.id = :participant')
+            ->join('cartRowParticipant.cartRow', 'cartRow')
+            ->join('cartRow.product', 'product')
+            ->setParameter('participant', $participant->getId())
+            ->setMaxResults(1)
+        ;
+
+        return $queryBuilder->getQuery()->getOneOrNullResult();
+    }
+}
