f3d2a4dccfd0a3c5e5d1e0a0c8b0148b9eebb729
Richard Hanna <richard.hanna@elao.com>
2018-04-13 09:54:23 +0200

    1511 - Add Notify mail

---
+++ src/Ui/Bundle/MailBundle/Mail/Spot/NotifyExportMail.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the vimeet project.
+ *
+ * Copyright (C) vimeet
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\MailBundle\Mail\Spot;
+
+use Proximum\Vimeet\Application\Components\Mail\AbstractMail;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class NotifyExportMail extends AbstractMail
+{
+    /** @var string */
+    protected $subject = 'mail.spot.export.subject';
+
+    /** @var string */
+    protected $template = 'MailBundle:Mail:Spot/export.html.twig';
+
+    /** @var string */
+    protected $messageId = 'export_spot';
+
+    /** @var Event */
+    public $event;
+
+    /** @var string */
+    public $fileHash;
+
+    /** @var int */
+    public $fileId;
+
+    /**
+     * @param Event  $event
+     * @param string $sender
+     * @param string $receiver
+     * @param string $locale
+     * @param string $fileHash
+     * @param int    $fileId
+     */
+    public function __construct(
+        Event $event,
+        $sender,
+        $receiver,
+        $locale,
+        $fileHash,
+        $fileId
+    ) {
+        parent::__construct($sender, $receiver, $locale);
+
+        $this->event     = $event;
+        $this->fileHash  = $fileHash;
+        $this->fileId    = $fileId;
+    }
+}
