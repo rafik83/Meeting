1695aca713ec7b03f8ade87412dd59a910e624b2
Nico <nicolas.dievart@elao.com>
2018-04-13 10:42:49 +0200

    1561 - On participant added, create system unavailability

--- src/Domain/Unavailability/SystemGenerator/Generator.php
+++ src/Domain/Unavailability/SystemGenerator/Generator.php
@@ -14,12 +14,14 @@ use Proximum\Vimeet\Application\Adapter\DelayedEventDispatcherInterface;
 use Proximum\Vimeet\Application\Event\Events;
 use Proximum\Vimeet\Application\Event\Unavailability\System\SystemUnavailabilityForUserGeneratedEvent;
 use Proximum\Vimeet\Domain\Model\AvailabilityTimeRange;
+use Proximum\Vimeet\Domain\Model\CartRowParticipant;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Participant;
 use Proximum\Vimeet\Domain\Model\Product;
 use Proximum\Vimeet\Domain\Model\Unavailability;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Repository\AvailabilityTimeRangeRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\CartRowParticipantRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\UnavailabilityRepositoryInterface;
 
@@ -43,13 +45,17 @@ class Generator
     /** @var DelayedEventDispatcherInterface */
     private $eventDispatcher;
 
+    /** @var CartRowParticipantRepositoryInterface */
+    private $cartRowParticipantRepository;
+
     public function __construct(
         UnavailabilityRepositoryInterface $unavailabilityRepository,
         AvailabilityTimeRangeRepositoryInterface $availabilityTimeRangeRepository,
         ParticipantRepositoryInterface $participantRepository,
         OverlappedTimeRangeMerger $overlappedTimeRangeMerger,
         OverlappedTimeRangeTruncater $overlappedTimeRangeTruncater,
-        DelayedEventDispatcherInterface $eventDispatcher
+        DelayedEventDispatcherInterface $eventDispatcher,
+        CartRowParticipantRepositoryInterface $cartRowParticipantRepository
     ) {
         $this->unavailabilityRepository = $unavailabilityRepository;
         $this->availabilityTimeRangeRepository = $availabilityTimeRangeRepository;
@@ -57,6 +63,7 @@ class Generator
         $this->overlappedTimeRangeMerger = $overlappedTimeRangeMerger;
         $this->overlappedTimeRangeTruncater = $overlappedTimeRangeTruncater;
         $this->eventDispatcher = $eventDispatcher;
+        $this->cartRowParticipantRepository = $cartRowParticipantRepository;
     }
 
     public function generateSystemUnavailability(Event $event, User $user): void
@@ -152,6 +159,16 @@ class Generator
             $product = $participant->getParticipantProduct();
 
             if ($product instanceof Product) {
+                $products[$product->getId()] = $product;
+
+                continue;
+            }
+
+            $cartRowParticipant = $this->cartRowParticipantRepository->findByParticipant($participant);
+
+            if ($cartRowParticipant instanceof CartRowParticipant) {
+                $product = $cartRowParticipant->getCartRow()->getProduct();
+
                 $products[$product->getId()] = $product;
             }
         }
--- src/Infrastructure/Bundle/InfrastructureBundle/EventListener/Unavailability/System/SystemUnavailabilityEventSubscriber.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/EventListener/Unavailability/System/SystemUnavailabilityEventSubscriber.php
@@ -10,6 +10,7 @@
 
 namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\EventListener\Unavailability\System;
 
+use Proximum\Vimeet\Application\Event\Cart\ParticipantCartRowAddedEvent;
 use Proximum\Vimeet\Application\Event\Events;
 use Proximum\Vimeet\Application\Event\Participant\ParticipantRemovedEvent;
 use Proximum\Vimeet\Application\Event\Participant\ProductSetOnParticipantEvent;
@@ -34,7 +35,22 @@ class SystemUnavailabilityEventSubscriber implements EventSubscriberInterface
         return [
             Events::PARTICIPANT_PRODUCT_SET => 'onParticipantProductSet',
             Events::PARTICIPANT_REMOVED => 'onParticipantRemoved',
+            Events::PARTICIPANT_CART_ROW_ADDED => 'onParticipantCartRowAdded',
         ];
+    }
+
+    public function onParticipantCartRowAdded(ParticipantCartRowAddedEvent $participantCartRowAddedEvent): void
+    {
+        // We generate the system unavailability only when the participant added a product to the cart
+        // Without a product already set on him/her and the previous product in the cart is different
+        if (false === $participantCartRowAddedEvent->hadAlreadyThisProductInCart
+            && false === $participantCartRowAddedEvent->hadPreviousProduct
+        ) {
+            $this->generator->generateSystemUnavailability(
+                $participantCartRowAddedEvent->participant->getSheet()->getEvent(),
+                $participantCartRowAddedEvent->participant->getUser()
+            );
+        }
     }
 
     public function onParticipantProductSet(ProductSetOnParticipantEvent $event): void
