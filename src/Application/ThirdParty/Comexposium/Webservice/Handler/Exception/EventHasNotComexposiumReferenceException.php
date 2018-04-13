7c7c8e9fc02d2b71a76f5bd5343d297408ff6d01
Richard Hanna <richard.hanna@elao.com>
2018-04-13 09:49:41 +0200

    1511 - Add Comexposium Exceptions

---
+++ src/Application/ThirdParty/Comexposium/Webservice/Handler/Exception/EventHasNotComexposiumReferenceException.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\ThirdParty\Comexposium\Webservice\Handler\Exception;
+
+class EventHasNotComexposiumReferenceException extends ComexposiumException
+{
+}
