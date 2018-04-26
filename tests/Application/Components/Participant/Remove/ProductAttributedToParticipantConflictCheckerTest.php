f6b742241c038878054f5fe4b174f9bc85dd02b8
Nico <nicolas.dievart@elao.com>
2018-04-26 16:20:11 +0200

    1534 - wip unit tesdt

---
+++ tests/Application/Components/Participant/Remove/ProductAttributedToParticipantConflictCheckerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Components\Participant\Remove;
+
+use Proximum\Vimeet\Application\Components\Participant\Remove\ProductAttributedToParticipantConflictChecker;
+use PHPUnit\Framework\TestCase;
+
+class ProductAttributedToParticipantConflictCheckerTest extends TestCase
+{
+    public function testWithConflictOnAttributedProduct(): void
+    {
+
+    }
+
+    public function testWithConflictOnCartRowParticipant(): void
+    {
+
+    }
+
+    public function testWithoutConflict(): void
+    {
+
+    }
+}
