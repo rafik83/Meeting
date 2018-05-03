7be7bb32429a8dced44bda157916118da227515a
Richard Hanna <richard.hanna@elao.com>
2018-05-03 10:33:00 +0200

    1572 - Add ProtectedKey interface and adapter implementation

---
+++ src/Application/Adapter/ProtectedKeyInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Adapter;
+
+interface ProtectedKeyInterface
+{
+    public function getKeyProtectedByPassword(string $password): string;
+}
