8ca30b59a532a26122d7db5631743a3143b4c755
Richard Hanna <richard.hanna@elao.com>
2018-05-03 16:29:00 +0200

    1572 - Add User event defuse crypto key getter

---
+++ src/Infrastructure/Encryption/UserEventKeyGetter.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Encryption;
+
+use Defuse\Crypto\Key;
+use Defuse\Crypto\KeyProtectedByPassword;
+use Proximum\Vimeet\Application\Adapter\UserEventProtectedKeyGetterInterface;
+use Proximum\Vimeet\Application\Adapter\UserEventProtectedKeyPasswordGetterInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\User;
+
+class UserEventKeyGetter
+{
+    /** @var UserEventProtectedKeyGetterInterface */
+    private $userEventProtectedKeyGetter;
+
+    /** @var UserEventProtectedKeyPasswordGetterInterface */
+    private $userEventProtectedKeyPasswordGetter;
+
+    public function __construct(
+        UserEventProtectedKeyGetterInterface $userEventProtectedKeyGetter,
+        UserEventProtectedKeyPasswordGetterInterface $userEventProtectedKeyPasswordGetter
+    ) {
+        $this->userEventProtectedKeyGetter = $userEventProtectedKeyGetter;
+        $this->userEventProtectedKeyPasswordGetter = $userEventProtectedKeyPasswordGetter;
+    }
+
+    public function getKeyByEventAndUser(Event $event, User $user): Key
+    {
+        $protectedKey = $this->userEventProtectedKeyGetter->getProtectedKeyByEventAndUser($event, $user);
+        $protectedKeyPassword = $this->userEventProtectedKeyPasswordGetter->getProtectedKeyPasswordByEventAndUser(
+            $event,
+            $user
+        );
+
+        $key = KeyProtectedByPassword::loadFromAsciiSafeString($protectedKey);
+
+        return $key->unlockKey($protectedKeyPassword);
+    }
+}
