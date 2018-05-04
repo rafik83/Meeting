96a98a0c9630358cf3f00943f75e5a475f2a1adc
Richard Hanna <richard.hanna@elao.com>
2018-05-04 15:57:23 +0200

    1572 - Do not show preview of encrypted image

--- src/Domain/Template/TemplateObject/UploadObject.php
+++ src/Domain/Template/TemplateObject/UploadObject.php
@@ -117,6 +117,11 @@ class UploadObject extends EditableObject implements UploadableObjectInterface
         return \in_array($this->getExtension(), MimeType::IMAGE_EXTENSIONS, true);
     }
 
+    public function isImageFormatAndNotCrypted(): bool
+    {
+        return $this->isImageFormat() && !$this->isCrypted();
+    }
+
     public function isUploadAndHasPath(): bool
     {
         return $this->isUpload() && $this->getPath();
