0c2dee8b56e83e84656360757e286f16769de83d
Richard Hanna <richard.hanna@elao.com>
2018-05-09 16:32:30 +0200

    1538 - Add participate to happenings by product service

---
+++ src/Domain/Happening/ParticipateToHappeningsByProduct.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Happening;
+
+use Proximum\Vimeet\Application\Command\Happening\Participate;
+use Proximum\Vimeet\Application\Command\Happening\ParticipateHandler;
+use Proximum\Vimeet\Application\Exception\Happening\HappeningException;
+use Proximum\Vimeet\Domain\Model\Happening;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\Product;
+use Proximum\Vimeet\Domain\Repository\HappeningRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
+
+class ParticipateToHappeningsByProduct
+{
+    /** @var HappeningRepositoryInterface */
+    private $happeningRepository;
+
+    /** @var ParticipantRepositoryInterface */
+    private $participantRepository;
+
+    /** @var ParticipateHandler */
+    private $participateHandler;
+
+    public function __construct(
+        HappeningRepositoryInterface $happeningRepository,
+        ParticipantRepositoryInterface $participantRepository,
+        ParticipateHandler $participateHandler
+    ) {
+        $this->happeningRepository = $happeningRepository;
+        $this->participantRepository = $participantRepository;
+        $this->participateHandler = $participateHandler;
+    }
+
+    public function handle(Product $product, Participant $participant): void
+    {
+        $happeningsByProduct = $this->happeningRepository->findByProduct($product);
+
+        foreach ($happeningsByProduct as $happening) {
+            $this->participateToHappening($participant, $happening);
+        }
+    }
+
+    private function participateToHappening(Participant $participant, Happening $happening): void
+    {
+        $participantsToHappening = $this->participantRepository->getParticipantsForHappening(
+            $participant->getSheet(),
+            $happening
+        );
+
+        // User already participate to this Happening
+        if (\in_array($participant, $participantsToHappening, true)) {
+            return;
+        }
+
+        // Add the current participant to all participants to this happening
+        $participantsToHappening[] = $participant;
+
+        try {
+            $this->participateHandler->handle(
+                new Participate(
+                    $happening,
+                    $participant->getSheet(),
+                    $participant->getUser(),
+                    $participantsToHappening,
+                    null,
+                    null,
+                    false,
+                    false
+                )
+            );
+        } catch (HappeningException $happeningException) {
+            // Nothing to do
+        }
+    }
+}
---
+++ tests/Domain/Happening/ParticipateToHappeningsByProductTest.php
@@ -0,0 +1,98 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Domain\Happening;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\Command\Happening\Participate;
+use Proximum\Vimeet\Application\Command\Happening\ParticipateHandler;
+use Proximum\Vimeet\Domain\Happening\ParticipateToHappeningsByProduct;
+use Proximum\Vimeet\Domain\Model\Happening;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\Product;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\HappeningRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\ParticipantRepositoryInterface;
+
+class ParticipateToHappeningsByProductTest extends TestCase
+{
+    public function testHandle()
+    {
+        $happeningAlreadyParticipated = $this->prophesize(Happening::class);
+        $happeningNotParticipated = $this->prophesize(Happening::class);
+
+        $product = $this->prophesize(Product::class);
+        $sheet = $this->prophesize(Sheet::class);
+        $user = $this->prophesize(User::class);
+
+        $participant = $this->prophesize(Participant::class);
+        $participant->getSheet()->willReturn($sheet->reveal());
+        $participant->getUser()->willReturn($user->reveal());
+
+        $otherParticipant = $this->prophesize(Participant::class);
+
+        $happeningRepository = $this->prophesize(HappeningRepositoryInterface::class);
+        $happeningRepository
+            ->findByProduct($product->reveal())
+            ->shouldBeCalled()
+            ->willReturn(
+                [
+                    $happeningAlreadyParticipated->reveal(),
+                    $happeningNotParticipated->reveal(),
+                ]
+            )
+        ;
+
+        $participantRepository = $this->prophesize(ParticipantRepositoryInterface::class);
+        $participantRepository
+            ->getParticipantsForHappening($sheet->reveal(), $happeningAlreadyParticipated->reveal())
+            ->shouldBeCalled()
+            ->willReturn(
+                [
+                    $otherParticipant->reveal(),
+                    $participant->reveal(),
+                ]
+            )
+        ;
+        $participantRepository
+            ->getParticipantsForHappening($sheet->reveal(), $happeningNotParticipated->reveal())
+            ->shouldBeCalled()
+            ->willReturn([$otherParticipant->reveal()])
+        ;
+
+        $participateHandler = $this->prophesize(ParticipateHandler::class);
+        $participateHandler
+            ->handle(
+                new Participate(
+                    $happeningNotParticipated->reveal(),
+                    $sheet->reveal(),
+                    $user->reveal(),
+                    [
+                        $otherParticipant->reveal(),
+                        $participant->reveal(),
+                    ],
+                    null,
+                    null,
+                    false,
+                    false
+                )
+            )
+            ->shouldBeCalled()
+        ;
+
+        $participateToHappeningsByProduct = new ParticipateToHappeningsByProduct(
+            $happeningRepository->reveal(),
+            $participantRepository->reveal(),
+            $participateHandler->reveal()
+        );
+        $participateToHappeningsByProduct->handle($product->reveal(), $participant->reveal());
+    }
+}
