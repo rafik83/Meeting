c5f8716a52b8cacb5c3c13e43a05c310a419e133
Richard Hanna <richard.hanna@elao.com>
2018-05-09 21:54:35 +0200

    1538 - Add HappeningsNotOverlapped service

---
+++ src/Domain/Happening/HappeningsNotOverlapped.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Happening;
+
+use Proximum\Vimeet\Domain\Model\Happening;
+use Proximum\Vimeet\Domain\Time\TimeOverlap;
+
+class HappeningsNotOverlapped
+{
+    /**
+     * @param Happening[] $happenings
+     *
+     * @return Happening[]
+     */
+    public function getHappeningsNotOverlapped(array $happenings): array
+    {
+        $happeningsOverlapped = $this->getHappeningsOverlapped($happenings);
+        $happeningsNotOverlapped = [];
+
+        foreach ($happenings as $happening) {
+            if (isset($happeningsOverlapped[$happening->getId()])) {
+                continue;
+            }
+
+            $happeningsNotOverlapped[] = $happening;
+        }
+
+        return $happeningsNotOverlapped;
+    }
+
+    /**
+     * @param Happening[] $happenings
+     *
+     * @return Happening[]
+     */
+    private function getHappeningsOverlapped(array $happenings): array
+    {
+        $happeningsOverlapped = [];
+
+        foreach ($happenings as $happening) {
+            foreach ($happenings as $otherHappening) {
+                if ($otherHappening === $happening) {
+                    continue;
+                }
+
+                if (TimeOverlap::overlap($happening, $otherHappening)) {
+                    $happeningsOverlapped[$happening->getId()] = $happening;
+                }
+            }
+        }
+
+        return $happeningsOverlapped;
+    }
+}
