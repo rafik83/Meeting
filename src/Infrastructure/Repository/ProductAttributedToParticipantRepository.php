db09fac34bc6579772d97440747961d4e8a4e54f
Richard Hanna <richard.hanna@elao.com>
2018-05-15 14:46:00 +0200

    1538 - Remove all attributed product by sheet and recalculate happening registration

--- src/Infrastructure/Repository/ProductAttributedToParticipantRepository.php
+++ src/Infrastructure/Repository/ProductAttributedToParticipantRepository.php
@@ -14,7 +14,6 @@ use Doctrine\ORM\EntityManager;
 use Proximum\Vimeet\Domain\Model\Participant;
 use Proximum\Vimeet\Domain\Model\Product;
 use Proximum\Vimeet\Domain\Model\ProductAttributedToParticipant;
-use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Repository\ProductAttributedToParticipantRepositoryInterface;
 
 class ProductAttributedToParticipantRepository implements ProductAttributedToParticipantRepositoryInterface
@@ -60,24 +59,6 @@ class ProductAttributedToParticipantRepository implements ProductAttributedToPar
         }
 
         $this->entityManager->flush($productAttributedToParticipants);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function removeForSheet(Sheet $sheet): void
-    {
-        $productAttributedToParticipants = $this->entityManager
-            ->createQueryBuilder()
-            ->select('productAttributedToParticipant')
-            ->from(ProductAttributedToParticipant::class, 'productAttributedToParticipant')
-            ->join('productAttributedToParticipant.participant', 'participant', 'WITH', 'participant.sheet = :sheet')
-            ->setParameter('sheet', $sheet)
-            ->getQuery()
-            ->getResult()
-        ;
-
-        $this->removeBatch($productAttributedToParticipants);
     }
 
     /**
