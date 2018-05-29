46b4b739baec2f350a77bc4fe67748ed03f92e5a
Nico <nicolas.dievart@elao.com>
2018-05-29 16:20:33 +0200

    1587 - Add TemplateDataFileDuplicator

---
+++ src/Application/Components/TemplateData/TemplateDataFileDuplicator.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Components\TemplateData;
+
+use Proximum\Vimeet\Application\Adapter\FileStorageInterface;
+use Proximum\Vimeet\Application\Components\Sheet\Template\Tag;
+use Proximum\Vimeet\Domain\Template\TemplateData;
+use Proximum\Vimeet\Domain\Template\TemplateObject\Image;
+
+class TemplateDataFileDuplicator
+{
+    /** @var FileStorageInterface */
+    private $fileStorage;
+
+    public function __construct(FileStorageInterface $fileStorage)
+    {
+        $this->fileStorage = $fileStorage;
+    }
+
+    public function handle(TemplateData $templateData): TemplateData
+    {
+        foreach ($templateData->getObjects() as $object) {
+            if ($object instanceof Image
+                && !empty($object->getContentValue())
+                && $object->hasTag(Tag::SHEET_LOGO)
+            ) {
+                $newFilePath = $this->fileStorage->copyAndRename($object->getContentValue());
+
+                $object->setContentValue($newFilePath);
+            }
+        }
+
+        return $templateData;
+    }
+}
