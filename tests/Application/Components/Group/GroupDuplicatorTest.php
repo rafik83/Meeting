fb0a3f10782f01f43d3e9643badbcd8aa48ba50e
Nico <nicolas.dievart@elao.com>
2018-05-29 14:27:50 +0200

    1586 - Add GroupDuplicator

---
+++ tests/Application/Components/Group/GroupDuplicatorTest.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Components\Group;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
+use Proximum\Vimeet\Application\Command\Group\DuplicateToEvent;
+use Proximum\Vimeet\Application\Command\Group\DuplicateToEventHandler;
+use Proximum\Vimeet\Application\Components\Group\GroupDuplicator;
+use Proximum\Vimeet\Domain\Exception\Group\Duplicate\CanNotDuplicateToTheSameEventException;
+use Proximum\Vimeet\Domain\Exception\Group\Duplicate\GroupAlreadyDuplicatedInGivenEventException;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Sheet\Group;
+
+class GroupDuplicatorTest extends TestCase
+{
+    /** @var ObjectProphecy */
+    private $event;
+
+    /** @var ObjectProphecy */
+    private $group;
+
+    /** @var ObjectProphecy */
+    private $duplicateToEventHandler;
+
+    public function setUp()
+    {
+        $this->group = $this->prophesize(Group::class);
+        $this->event = $this->prophesize(Event::class);
+        $this->duplicateToEventHandler = $this->prophesize(DuplicateToEventHandler::class);
+    }
+
+    public function testDuplicateToEventWithSameEvent(): void
+    {
+        $exception = new CanNotDuplicateToTheSameEventException();
+        $this->duplicateToEventHandler->handle(new DuplicateToEvent($this->group->reveal(), $this->event->reveal()))
+            ->shouldBeCalled()
+            ->willThrow($exception)
+        ;
+
+        $groupDuplicator = new GroupDuplicator(
+            $this->duplicateToEventHandler->reveal()
+        );
+
+        $result = $groupDuplicator->duplicateToEvent($this->group->reveal(), $this->event->reveal());
+
+        $this->assertEquals($this->group->reveal(), $result);
+    }
+
+    public function testDuplicateToEventWithAlreadyDuplicatedGroup(): void
+    {
+        $duplicatedGroup = $this->prophesize(Group::class);
+        $exception = new GroupAlreadyDuplicatedInGivenEventException($duplicatedGroup->reveal());
+        $this->duplicateToEventHandler->handle(new DuplicateToEvent($this->group->reveal(), $this->event->reveal()))
+            ->shouldBeCalled()
+            ->willThrow($exception)
+        ;
+
+        $groupDuplicator = new GroupDuplicator(
+            $this->duplicateToEventHandler->reveal()
+        );
+
+        $result = $groupDuplicator->duplicateToEvent($this->group->reveal(), $this->event->reveal());
+
+        $this->assertEquals($duplicatedGroup->reveal(), $result);
+    }
+
+    public function testDuplicateToEvent(): void
+    {
+        $duplicatedGroup = $this->prophesize(Group::class);
+        $this->duplicateToEventHandler->handle(new DuplicateToEvent($this->group->reveal(), $this->event->reveal()))
+            ->shouldBeCalled()
+            ->willReturn($duplicatedGroup)
+        ;
+
+        $groupDuplicator = new GroupDuplicator(
+            $this->duplicateToEventHandler->reveal()
+        );
+
+        $result = $groupDuplicator->duplicateToEvent($this->group->reveal(), $this->event->reveal());
+
+        $this->assertEquals($duplicatedGroup->reveal(), $result);
+    }
+}
