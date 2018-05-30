2e8da3a65db35e6166f8d9de8a5b8d747455c0a2
Nico <nicolas.dievart@elao.com>
2018-05-30 16:41:44 +0200

    1587 - Add TemplateDataDuplicator and plug it into the SheetDuplicator process

--- src/Domain/Template/TemplateDataFactory.php
+++ src/Domain/Template/TemplateDataFactory.php
@@ -92,7 +92,7 @@ class TemplateDataFactory
      *
      * @return TemplateData
      */
-    public function createFromSheet(Sheet $sheet, $locale)
+    public function createFromSheet(Sheet $sheet, ?string $locale): TemplateData
     {
         return $this
             ->loadNomenclatures($sheet->getEvent())
