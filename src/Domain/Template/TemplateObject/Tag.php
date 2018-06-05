b7ba6657ac9102ac581e3e2f9a0774055b889a7e
Richard Hanna <richard.hanna@elao.com>
2018-06-05 17:26:09 +0200

    MV-58 - Index content from Tag object in sheet template

--- src/Domain/Template/TemplateObject/Tag.php
+++ src/Domain/Template/TemplateObject/Tag.php
@@ -11,7 +11,20 @@
 namespace Proximum\Vimeet\Domain\Template\TemplateObject;
 
 use Proximum\Vimeet\Domain\Template\TemplateObject;
+use Proximum\Vimeet\Domain\View\Template\TaggedDataView;
 
-class Tag extends TemplateObject
+class Tag extends TemplateObject implements SearchableObjectInterface
 {
+    /**
+     * @return array|string
+     */
+    public function getSearchableContent()
+    {
+        return array_map(
+            function (TaggedDataView $taggedDataView) {
+                return $taggedDataView->content;
+            },
+            $this->taggedDataViews
+        );
+    }
 }
--- tests/Infrastructure/Elastica/Transformer/SheetElasticTransformerTest.php
+++ tests/Infrastructure/Elastica/Transformer/SheetElasticTransformerTest.php
@@ -10,66 +10,272 @@
 
 namespace Proximum\Vimeet\Tests\Infrastructure\Elastica\Transformer;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Elastica\Document;
 use PHPUnit\Framework\TestCase;
 use Proximum\Vimeet\Application\Components\Sheet\SheetInfoGuesser;
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Model\Category;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\MeetingSlot;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Model\Spot;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Order\Balance;
 use Proximum\Vimeet\Domain\Repository\CartRowRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\HappeningParticipationRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\Invoice\InvoiceRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\Meeting\RequestRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\MeetingRepositoryInterface;
+use Proximum\Vimeet\Domain\Sheet\Availability\ConfirmationStatus;
+use Proximum\Vimeet\Domain\Sheet\CommercialStatus;
+use Proximum\Vimeet\Domain\Sheet\Phone\ValidationStatus;
+use Proximum\Vimeet\Domain\Template\Block;
 use Proximum\Vimeet\Domain\Template\ParticipantInfoGuesser;
+use Proximum\Vimeet\Domain\Template\TaggedDataFactory;
 use Proximum\Vimeet\Domain\Template\TemplateData;
 use Proximum\Vimeet\Domain\Template\TemplateDataFactory;
+use Proximum\Vimeet\Domain\Template\TemplateObject\EditableText;
+use Proximum\Vimeet\Domain\Template\TemplateObject\Tag;
+use Proximum\Vimeet\Domain\View\Template\TaggedDataView;
 use Proximum\Vimeet\Infrastructure\Elastica\Transformer\SheetElasticTransformer;
-use Proximum\Vimeet\Tests\Factory\EventFactory;
-use Proximum\Vimeet\Tests\Factory\SheetFactory;
 
 class SheetElasticTransformerTest extends TestCase
 {
     public function testTransform()
     {
-        $locale = 'fr';
+        $category1 = $this->prophesize(Category::class);
+        $category1->getId()->willReturn(999);
 
-        $event = EventFactory::createEvent();
-        $event->setLocales(['fr']);
+        $category2 = $this->prophesize(Category::class);
+        $category2->getId()->willReturn(888);
 
-        $sheet = SheetFactory::create($event);
+        $type = $this->prophesize(Type::class);
+        $type->getId()->willReturn(1337);
+        $type->getCategories()->willReturn(
+            new ArrayCollection(
+                [
+                    $category1->reveal(),
+                    $category2->reveal(),
+                ]
+            )
+        );
 
-        $registrationTemplateData = new TemplateData('type', [], 'fr', 'fr');
-        $sheetTemplateData = new TemplateData('type', [], 'fr', 'fr');
+        $account = $this->prophesize(User\Account::class);
+        $account->getLastName()->willReturn('Scarface');
+
+        $owner = $this->prophesize(User::class);
+        $owner->getId()->willReturn(13);
+        $owner->getEmail()->willReturn('owner@email.com');
+        $owner->getAccount()->willReturn($account->reveal());
+
+        $user1 = $this->prophesize(User::class);
+        $user1->getId()->willReturn(42);
+        $user1->getEmail()->willReturn('user1@email.com');
+
+        $user2 = $this->prophesize(User::class);
+        $user2->getEmail()->willReturn('user2@email.com');
+
+        $event = $this->prophesize(Event::class);
+        $event->getId()->willReturn(66);
+        $event->getLocales()->willReturn(['fr', 'en']);
+        $event->getFallback()->willReturn('fr');
+
+        $spot = $this->prophesize(Spot::class);
+
+        $meetingSlot1 = $this->prophesize(MeetingSlot::class);
+        $meetingSlot1->getId()->willReturn(11);
+        $availableSlot1 = $this->prophesize(Sheet\AvailableSlot::class);
+        $availableSlot1->getSlot()->willReturn($meetingSlot1->reveal());
+
+        $meetingSlot2 = $this->prophesize(MeetingSlot::class);
+        $meetingSlot2->getId()->willReturn(12);
+        $availableSlot2 = $this->prophesize(Sheet\AvailableSlot::class);
+        $availableSlot2->getSlot()->willReturn($meetingSlot2->reveal());
+
+        $participant1 = $this->prophesize(Participant::class);
+        $participant1->getUser()->willReturn($user1);
+
+        $participant2 = $this->prophesize(Participant::class);
+        $participant2->getUser()->willReturn($user2);
+
+        $follower = $this->prophesize(Admin::class);
+        $follower->getId()->willReturn(777);
+
+        $sheet = $this->prophesize(Sheet::class);
+        $sheet->getId()->willReturn(1);
+        $sheet->getType()->willReturn($type->reveal());
+        $sheet->getEvent()->willReturn($event->reveal());
+        $sheet->getParticipantsArray()->willReturn(
+            [
+                $participant1->reveal(),
+                $participant2->reveal(),
+            ]
+        )
+        ;
+        $sheet->getOwner()->willReturn($owner->reveal());
+        $sheet->hasUserParticipant($owner->reveal())->willReturn(false);
+        $sheet->getState()->willReturn(Sheet::STATE_ACCEPTED);
+        $sheet->getValidationState()->willReturn(Sheet::STATE_VALIDATION_PENDING);
+        $sheet->getAgendaConfirmedStatus()->willReturn(Sheet::AGENDA_ALL_CONFIRMED);
+        $sheet->getPhoneValidationStatus()->willReturn(ValidationStatus::NOT_CONCERNED);
+        $sheet->getAvailabilityConfirmationStatus()->willReturn(ConfirmationStatus::NONE_CONFIRMED);
+        $sheet->isEnabled()->willReturn(true);
+        $sheet->isCompleted()->willReturn(false);
+        $sheet->getCompleteness()->willReturn(88);
+        $sheet->getFollower()->willReturn($follower->reveal());
+        $sheet->getCommercialStatus()->willReturn(CommercialStatus::STATUS_INTEREST);
+        $sheet->countParticipants()->willReturn(2);
+        $sheet->isImported()->willReturn(false);
+        $sheet->isInCatalog()->willReturn(true);
+        $sheet->attend()->willReturn(true);
+        $sheet->hasGroup()->willReturn(true);
+        $sheet->getSpot()->willReturn($spot->reveal());
+        $sheet->getAvailableSlots()->willReturn(
+            [
+                $availableSlot1->reveal(),
+                $availableSlot2->reveal(),
+            ]
+        )
+        ;
+        $sheet->getLastLoginAt()->willReturn(new \DateTime('2016-06-18 20:45:00'));
+        $sheet->getCreatedAt()->willReturn(new \DateTime('2016-05-11 09:13:13'));
+        $sheet->getInCatalogAt()->willReturn(new \DateTime('2016-08-12 01:23:33'));
+        $sheet->getReminderDate()->willReturn(new \DateTime('2016-06-18 09:30:00'));
+
+        $registrationEditableText = new EditableText(
+            'registration-123',
+            'editable-text',
+            [
+                'translatable' => false,
+                'tags' => ['sheet_generic_tag_1', 'sheet_data']
+            ],
+            'fr',
+            'fr'
+        );
+        $registrationEditableText->setContentValue('Registration whatever text');
+
+        $registrationBlock = new Block('12', [], 'fr', 'fr');
+        $registrationBlock->addChild(1, '69b3cde3', $registrationEditableText);
+        $registrationTemplateData = new TemplateData('root', [], 'fr', 'fr');
+        $registrationTemplateData->addChild(0, '811f6edf', $registrationBlock);
+
+        $editableTextSheetFr = new EditableText(
+            '69b3cde3',
+            'editable-text',
+            [
+                'translatable' => true,
+            ],
+            'fr',
+            'fr'
+        );
+        $editableTextSheetFr->setContentValue('Ma description');
+
+        $blockFr = new Block('12', [], 'fr', 'fr');
+        $blockFr->addChild(1, '69b3cde3', $editableTextSheetFr);
+        $sheetTemplateWithTaggedDataFr = new TemplateData('root', [], 'fr', 'fr');
+        $sheetTemplateWithTaggedDataFr->addChild(0, '811f6edf', $blockFr);
+        $sheetTemplateWithTaggedDataFr->setTaggedDataViews(
+            [new TaggedDataView('editable-text', false, [], 'sheet_generic_tag_1', 'Tagged data from registration', false)]
+        );
+
+        $editableTextSheetEn = new EditableText(
+            '69b3cde3',
+            'editable-text',
+            [
+                'translatable' => true,
+            ],
+            'en',
+            'fr'
+        );
+        $editableTextSheetEn->setContentValue('My description');
+        $blockEn = new Block('12', [], 'en', 'fr');
+        $blockEn->addChild(1, '69b3cde3', $editableTextSheetEn);
+        $sheetTemplateWithTaggedDataEn = new TemplateData('root', [], 'en', 'fr');
+        $sheetTemplateWithTaggedDataEn->addChild(0, '811f6edf', $blockEn);
+        $sheetTemplateWithTaggedDataEn->setTaggedDataViews(
+            [new TaggedDataView('editable-text', false, [], 'sheet_generic_tag_1', 'Tagged data from registration', false)]
+        );
 
         $sheetInfoGuesser = $this->prophesize(SheetInfoGuesser::class);
-        $sheetInfoGuesser->guessSheetTitle($sheet, $locale)->shouldBeCalled();
+        $sheetInfoGuesser->guessSheetTitle($sheet->reveal(), 'fr')->shouldBeCalled()->willReturn('Hollywood Studio');
 
         $participantInfoGuesser = $this->prophesize(ParticipantInfoGuesser::class);
+        $participantInfoGuesser
+            ->guessParticipantLastName($participant1->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn('Willis')
+        ;
+        $participantInfoGuesser
+            ->guessParticipantPosition($participant1->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn('Acteur')
+        ;
+        $participantInfoGuesser
+            ->guessParticipantLastName($participant2->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn('Streep')
+        ;
+        $participantInfoGuesser
+            ->guessParticipantPosition($participant2->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn('Actrice')
+        ;
 
         $cartRowRepository = $this->prophesize(CartRowRepositoryInterface::class);
-        $cartRowRepository->findBySheet($sheet)->shouldBeCalled();
+        $cartRowRepository->findBySheet($sheet->reveal())->shouldBeCalled()->willReturn([]);
 
         $happeningParticipationRepository = $this->prophesize(HappeningParticipationRepositoryInterface::class);
-        $happeningParticipationRepository->hasParticipationsBySheet($sheet)->shouldBeCalled();
+        $happeningParticipationRepository
+            ->hasParticipationsBySheet($sheet->reveal())
+            ->shouldBeCalled()
+            ->willReturn(true)
+        ;
 
         $requestRepository = $this->prophesize(RequestRepositoryInterface::class);
-        $requestRepository->hasRequestSentBySheet($sheet)->shouldBeCalled();
-        $requestRepository->hasPendingPropositionReceivedBySheet($sheet)->shouldBeCalled();
+        $requestRepository->hasRequestSentBySheet($sheet->reveal())->shouldBeCalled()->willReturn(false);
+        $requestRepository->hasPendingPropositionReceivedBySheet($sheet->reveal())->shouldBeCalled()->willReturn(true);
 
         $templateDataFactory = $this->prophesize(TemplateDataFactory::class);
         $templateDataFactory
-            ->createRegistrationFromSheet($sheet, $locale)
+            ->createRegistrationFromSheet($sheet->reveal(), 'fr')
             ->shouldBeCalled()
-            ->willReturn($registrationTemplateData);
-        $templateDataFactory->createFromSheet($sheet, $locale)->shouldBeCalled()->willReturn($sheetTemplateData);
+            ->willReturn($registrationTemplateData)
+        ;
+        $templateDataFactory
+            ->createFromSheet($sheet->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn($sheetTemplateWithTaggedDataFr)
+        ;
+        $templateDataFactory
+            ->createFromSheet($sheet->reveal(), 'en')
+            ->shouldBeCalled()
+            ->willReturn($sheetTemplateWithTaggedDataEn)
+        ;
 
         $balance = $this->prophesize(Balance::class);
-        $balance->getNotCancelledOrderVatViews($sheet)->shouldBeCalled();
-        $balance->getRemainingToPay($sheet)->shouldBeCalled();
+        $balance->getNotCancelledOrderVatViews($sheet->reveal())->shouldBeCalled();
+        $balance->getRemainingToPay($sheet->reveal())->shouldBeCalled();
 
         $meetingRepository = $this->prophesize(MeetingRepositoryInterface::class);
-        $meetingRepository->hasScheduledMeeting($sheet)->shouldBeCalled();
+        $meetingRepository->hasScheduledMeeting($sheet->reveal())->shouldBeCalled()->willReturn(true);
 
         $invoiceRepository = $this->prophesize(InvoiceRepositoryInterface::class);
-        $invoiceRepository->hasInvoice($sheet)->shouldBeCalled();
+        $invoiceRepository->hasInvoice($sheet->reveal())->shouldBeCalled()->willReturn(false);
+
+        $taggedDataFactory = $this->prophesize(TaggedDataFactory::class);
+        $taggedDataFactory
+            ->buildTaggedDataView($sheet->reveal(), 'fr')
+            ->shouldBeCalled()
+            ->willReturn($sheetTemplateWithTaggedDataFr)
+        ;
+        $taggedDataFactory
+            ->buildTaggedDataView($sheet->reveal(), 'en')
+            ->shouldBeCalled()
+            ->willReturn($sheetTemplateWithTaggedDataEn)
+        ;
 
         $transformer = new SheetElasticTransformer(
             $sheetInfoGuesser->reveal(),
@@ -78,11 +284,89 @@ class SheetElasticTransformerTest extends TestCase
             $happeningParticipationRepository->reveal(),
             $requestRepository->reveal(),
             $templateDataFactory->reveal(),
+            $taggedDataFactory->reveal(),
             $balance->reveal(),
             $meetingRepository->reveal(),
             $invoiceRepository->reveal()
         );
 
-        $transformer->transform($sheet, []);
+        $expectedDocument = new Document(
+            1,
+            [
+                'id' => 1,
+                'sheetName' => 'Hollywood Studio',
+                'state' => 'accepted',
+                'validationState' => 'pending',
+                'agendaConfirmedStatus' => 'all_agenda_confirmed',
+                'phoneValidationStatus' => 'phone_not_concerned',
+                'availabilityConfirmationStatus' => 'none_confirmed',
+                'enabled' => true,
+                'completed' => false,
+                'completeness' => 88,
+                'type' => 1337,
+                'categories' => [
+                    ['id' => 999],
+                    ['id' => 888],
+                ],
+                'followUp' => 777,
+                'commercialStatus' => 'interest',
+                'participantNumber' => 2,
+                'participants' => [
+                    [
+                        'email' => 'user1@email.com',
+                        'lastname' => 'Willis',
+                        'position' => 'Acteur',
+                    ],
+                    [
+                        'email' => 'user2@email.com',
+                        'lastname' => 'Streep',
+                        'position' => 'Actrice',
+                    ],
+                    [
+                        'email' => 'owner@email.com',
+                        'lastname' => 'Scarface',
+                    ],
+                ],
+                'event' => 66,
+                'owner' => 13,
+                'ownerEmail' => 'owner@email.com',
+                'remainingToPay' => 0,
+                'imported' => false,
+                'lastLoginAt' => '2016-06-18T20:45:00+00:00',
+                'createdAt' => '2016-05-11T09:13:13+00:00',
+                'inCatalog' => true,
+                'inCatalogAt' => '2016-08-12T01:23:33+00:00',
+                'booleanFilter' => [],
+                'orderStatus' => 'no_order',
+                'hasCart' => false,
+                'organizationCategory' => null,
+                'city' => null,
+                'zipcode' => null,
+                'country' => [],
+                'countryCode' => null,
+                'nomenclatureItems' => [],
+                'nomenclatureItemsSupply' => [],
+                'nomenclatureItemsNeeds' => [],
+                'keywords' => [],
+                'hasHappeningParticipation' => true,
+                'hasMeetingRequest' => false,
+                'hasPendingMeetingProposition' => true,
+                'hasScheduledMeeting' => true,
+                'hasInvoice' => false,
+                'attend' => true,
+                'hasGroup' => true,
+                'hasSpot' => true,
+                'availableSlotIds' => [
+                    ['id' => 11],
+                    ['id' => 12],
+                ],
+                'reminderDate' => '2016-06-18T09:30:00+00:00',
+                'content' => 'Ma description My description',
+                'content_fr' => 'Ma description',
+                'content_en' => 'My description',
+            ]
+        );
+
+        $this->assertEquals($expectedDocument, $transformer->transform($sheet->reveal(), []));
     }
 }
