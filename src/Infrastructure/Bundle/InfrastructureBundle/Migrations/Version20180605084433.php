8049a48ac87590d2c3cdb9717a19e8dc47a23133
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-06-05 12:01:27 +0200

    1615 - Fix migration title

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180605084433.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180605084433.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add filled_template_filter table
  */
 class Version20180605084433 extends AbstractMigration
 {
