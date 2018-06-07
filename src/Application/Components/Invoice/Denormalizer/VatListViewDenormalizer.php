320664e325c8dfe3f136cb6f1e73c5ccc5185829
Richard Hanna <richard.hanna@elao.com>
2018-06-07 15:14:50 +0200

    1473 - Add VatListView denormalizer

---
+++ src/Application/Components/Invoice/Denormalizer/VatListViewDenormalizer.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Components\Invoice\Denormalizer;
+
+use Proximum\Vimeet\Application\View\Invoice\Vat\VatListView;
+use Proximum\Vimeet\Application\View\Invoice\Vat\VatView;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class VatListViewDenormalizer implements DenormalizerInterface, DenormalizerAwareInterface
+{
+    use DenormalizerAwareTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        $vatViews = [];
+
+        foreach ($data['vatViews'] as $vatViewData) {
+            $vatViews[] = $this->denormalizer->denormalize($vatViewData, VatView::class, $format, $context);
+        }
+
+        return new VatListView(
+            $data['total'],
+            $data['totalWithVat'],
+            $data['vatApplicable'],
+            $data['vatMode'],
+            $vatViews
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return VatListView::class === $type;
+    }
+}
