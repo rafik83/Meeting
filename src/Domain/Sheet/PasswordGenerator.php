5ed13718e6dd9682992e56f2cf7f3d902a5df6b4
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-06-19 09:45:58 +0200

    1614 - Password generator

---
+++ src/Domain/Sheet/PasswordGenerator.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Sheet;
+
+class PasswordGenerator
+{
+    public static function generate(int $length = 10): string
+    {
+        return substr(
+            str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?'),
+            0,
+            $length
+        );
+    }
+}
