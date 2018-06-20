373410bf1ddc7194c1bf6b230eb039ae5043676c
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-06-20 11:50:32 +0200

    1614 - Add async task & ui button to launch task

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Command/ExportUploadedObjectsBySheetsCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+/*
+ * This file is part of the PhpStorm project.
+ *
+ * Copyright (C) PhpStorm
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command;
+
+use Proximum\Vimeet\Domain\Model\Admin;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Event\ExtraData;
+use Proximum\Vimeet\Domain\Repository\AdminRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\Event\ExtraDataRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExportUploadedObjectsBySheetsCommand extends Command
+{
+    public const NAME = 'vimeet:export:uploaded-objects-by-sheets';
+
+    /** @var EventRepositoryInterface */
+    private $eventRepository;
+
+    /** @var AdminRepositoryInterface */
+    private $adminRepository;
+
+    /** @var SheetRepositoryInterface */
+    private $sheetRepository;
+
+    /** @var ExtraDataRepositoryInterface */
+    private $extraDataRepository;
+
+    public function __construct(
+        EventRepositoryInterface $eventRepository,
+        AdminRepositoryInterface $adminRepository,
+        SheetRepositoryInterface $sheetRepository,
+        ExtraDataRepositoryInterface $extraDataRepository
+    ) {
+        parent::__construct(self::NAME);
+
+        $this->eventRepository = $eventRepository;
+        $this->adminRepository = $adminRepository;
+        $this->sheetRepository = $sheetRepository;
+        $this->extraDataRepository = $extraDataRepository;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Export uploaded objects by sheets')
+            ->addArgument('eventId', InputArgument::REQUIRED, 'Event ID')
+            ->addArgument('extraDataId', InputArgument::REQUIRED, 'ExtraData ID')
+            ->addArgument('adminId', InputArgument::REQUIRED, 'Admin ID');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): void
+    {
+        $event = $this->eventRepository->getById($input->getArgument('eventId'));
+
+        if (!$event instanceof Event) {
+            throw new \InvalidArgumentException('Event not found.');
+        }
+
+        $extraData = $this->extraDataRepository->findById($input->getArgument('extraDataId'));
+
+        if (!$extraData instanceof ExtraData) {
+            throw new \InvalidArgumentException('Extra data not found.');
+        }
+
+        $admin = $this->adminRepository->findById($input->getArgument('adminId'));
+
+        if (!$admin instanceof Admin) {
+            throw new \InvalidArgumentException('Admin not found.');
+        }
+
+        $sheetIds = explode(',', $extraData->getValue());
+        $sheets = $this->sheetRepository->findByIds($sheetIds);
+
+        // handler
+    }
+}
---
+++ src/Ui/Bundle/AdminBundle/Controller/Sheet/ExportUploadedObjectsBySheetsAction.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Sheet;
+
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Application\Adapter\JobQueueInterface;
+use Proximum\Vimeet\Application\Adapter\QueryBusInterface;
+use Proximum\Vimeet\Application\Query\Sheet\GetSheetIdsByFiltersQuery;
+use Proximum\Vimeet\Application\View\Sheet\SheetIdsView;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Event\ExtraData\Type;
+use Proximum\Vimeet\Domain\Model\Event\ExtraData;
+use Proximum\Vimeet\Domain\Repository\Event\ExtraDataRepositoryInterface;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Filter\SheetFilterSubmittedDataGetter;
+use Proximum\Vimeet\Ui\Bundle\AdminBundle\ValueResolver\AdminDomain;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class ExportUploadedObjectsBySheetsAction
+{
+    /** @var SheetFilterSubmittedDataGetter */
+    private $sheetFilterSubmittedDataGetter;
+
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationCheckerAdapter;
+
+    /** @var ExtraDataRepositoryInterface */
+    private $extraDataRepository;
+
+    /** @var UrlGeneratorInterface */
+    private $urlGenerator;
+
+    /** @var FlashBagInterface */
+    private $flashBag;
+
+    /** @var JobQueueInterface */
+    private $jobQueue;
+
+    /** @var QueryBusInterface */
+    private $queryBus;
+
+    /** var \DateTimeInterface */
+    private $datetime;
+
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
+        SheetFilterSubmittedDataGetter $sheetFilterSubmittedDataGetter,
+        ExtraDataRepositoryInterface $extraDataRepository,
+        UrlGeneratorInterface $urlGenerator,
+        FlashBagInterface $flashBag,
+        JobQueueInterface $jobQueue,
+        QueryBusInterface $queryBus,
+        \DateTimeInterface $datetime
+    ) {
+        $this->sheetFilterSubmittedDataGetter = $sheetFilterSubmittedDataGetter;
+        $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
+        $this->extraDataRepository = $extraDataRepository;
+        $this->urlGenerator = $urlGenerator;
+        $this->flashBag = $flashBag;
+        $this->jobQueue = $jobQueue;
+        $this->queryBus = $queryBus;
+        $this->datetime = $datetime;
+    }
+
+    public function __invoke(Request $request, Event $event, AdminDomain $adminDomain): RedirectResponse
+    {
+        if (!$this->authorizationCheckerAdapter->isGranted('ROLE_ALLOWED_TO_ORGANIZE') ||
+            !$this->authorizationCheckerAdapter->isGranted('PERMISSION_EVENT_ACCESS', $event)) {
+            throw new AccessDeniedException();
+        }
+
+        $url = $this->urlGenerator->generate('admin_sheet', ['event' => $event->getId()]);
+        $locale = $event->getAvailableLocale($request->getLocale());
+        $filters = $this->sheetFilterSubmittedDataGetter->handle($event, $adminDomain->getAdmin(), $locale);
+
+        /** @var SheetIdsView $sheetIdsView */
+        $sheetIdsView = $this->queryBus->handle(new GetSheetIdsByFiltersQuery($event, $filters, $locale));
+
+        if (empty($sheetIdsView->sheetIds)) {
+            $this->flashBag->add('error', 'flash.admin.event.export.uploaded_objects.error');
+
+            return new RedirectResponse($url);
+        }
+
+        $extraData = new ExtraData(
+            $event,
+            Type::ADMIN_SHEET_BATCH_IDS,
+            implode(', ', $sheetIdsView->sheetIds),
+            $this->datetime
+        );
+
+        $this->extraDataRepository->add($extraData);
+        $this->jobQueue->exportUploadedObjectsBySheets($event, $adminDomain->getAdmin(), $extraData);
+        $this->flashBag->add('success', 'flash.admin.event.export.uploaded_objects.success');
+
+        return new RedirectResponse($url);
+    }
+}
