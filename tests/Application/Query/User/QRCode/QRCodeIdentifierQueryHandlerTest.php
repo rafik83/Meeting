260d878ddab21e3446bb628576e55e53781f90ea
Richard Hanna <richard.hanna@elao.com>
2018-06-25 22:02:01 +0200

    1613 - Add QRCodeIdentifier

---
+++ tests/Application/Query/User/QRCode/QRCodeIdentifierQueryHandlerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Query\User\QRCode;
+
+use Proximum\Vimeet\Application\Query\User\QRCode\QRCodeIdentifierQuery;
+use Proximum\Vimeet\Application\Query\User\QRCode\QRCodeIdentifierQueryHandler;
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\User;
+
+class QRCodeIdentifierQueryHandlerTest extends TestCase
+{
+    public function testHandle()
+    {
+        $event = $this->prophesize(Event::class);
+        $event->getId()->willReturn(1337);
+        $user = $this->prophesize(User::class);
+        $user->getId()->willReturn(42);
+
+        $qrCodeIdentifierQueryHandler = new QRCodeIdentifierQueryHandler();
+        $this->assertEquals(
+            '0000004201337',
+            $qrCodeIdentifierQueryHandler->handle(new QRCodeIdentifierQuery($event->reveal(), $user->reveal()))
+        );
+    }
+}
