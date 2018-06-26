9641809e56902d3f6be1afc5f5dfca95efd3b9d6
Nico <nicolas.dievart@elao.com>
2018-06-26 16:02:08 +0200

    1613 - Add AvailableChecker specification for sheet badge

---
+++ src/Domain/Badge/AvailableChecker.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Badge;
+
+use Proximum\Vimeet\Domain\KeyDates\Checker\EnableBadgeForParticipantDateAccessChecker;
+use Proximum\Vimeet\Domain\Model\Badge;
+use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Domain\Repository\BadgeRepositoryInterface;
+use Proximum\Vimeet\Domain\Sheet\HasRemainingToPay;
+
+/**
+ * Specification to determine if Badge is available for Sheet
+ */
+class AvailableChecker
+{
+    /** @var EnableBadgeForParticipantDateAccessChecker */
+    private $enableBadgeForParticipantDateAccessChecker;
+
+    /** @var BadgeRepositoryInterface */
+    private $badgeRepository;
+
+    /** @var HasRemainingToPay */
+    private $hasRemainingToPay;
+
+    public function __construct(
+        EnableBadgeForParticipantDateAccessChecker $enableBadgeForParticipantDateAccessChecker,
+        BadgeRepositoryInterface $badgeRepository,
+        HasRemainingToPay $hasRemainingToPay
+    ) {
+        $this->enableBadgeForParticipantDateAccessChecker = $enableBadgeForParticipantDateAccessChecker;
+        $this->badgeRepository = $badgeRepository;
+        $this->hasRemainingToPay = $hasRemainingToPay;
+    }
+
+    public function isSatisfiedBy(Sheet $sheet): bool
+    {
+        if (!$this->enableBadgeForParticipantDateAccessChecker->allowedToAccess($sheet->getEvent())) {
+            return false;
+        }
+
+        $badge = $this->badgeRepository->findByType($sheet->getType());
+
+        if ($badge instanceof Badge) {
+            if (!$badge->isActivated()) {
+                return false;
+            }
+
+            if ($badge->isConditioned()) {
+                if ($badge->isConditionedByPackage()
+                    && $this->hasRemainingToPay->isSatisfiedBy($sheet)
+                ) {
+                    return false;
+                }
+
+                if (!empty($badge->getConditionedByStates())
+                    && !\in_array($sheet->getState(), $badge->getConditionedByStates(), true)
+                ) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
