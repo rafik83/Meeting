21b6c015ad5fcf6bad21ccbfdee9862f95a27b4a
Richard Hanna <richard.hanna@elao.com>
2018-07-19 11:35:18 +0200

    mv-90 - Show image when package is not available or products attached to object are not buyable

--- src/Domain/Template/TemplateObject/Image.php
+++ src/Domain/Template/TemplateObject/Image.php
@@ -11,6 +11,7 @@
 namespace Proximum\Vimeet\Domain\Template\TemplateObject;
 
 use Proximum\Vimeet\Domain\MimeType\MimeType;
+use Proximum\Vimeet\Domain\Model\Sheet;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class Image extends EditableObject implements UploadableObjectInterface
@@ -100,6 +101,18 @@ class Image extends EditableObject implements UploadableObjectInterface
     {
         if (!$this->getProducts()) {
             return true;
+        }
+
+        if ($this->getSheet() instanceof Sheet) {
+            $package = $this->getSheet()->getPackage();
+
+            if (!$package->isPassable()) {
+                return true;
+            }
+
+            if (!$package->hasAtLeastOneProduct($this->getProducts())) {
+                return true;
+            }
         }
 
         if ($this->isBuyable() && null === $this->getSelectedProduct()) {
