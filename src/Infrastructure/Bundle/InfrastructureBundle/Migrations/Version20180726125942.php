25120e1696ab0f36deadf6d45db787f1e94e622a
Nico <nicolas.dievart@elao.com>
2018-07-26 15:04:24 +0200

    1687 - Add status on Meeting

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180726125942.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add status on badge
+ */
+class Version20180726125942 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting ADD status VARCHAR(255) DEFAULT \'not_confirmed\' NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE meeting DROP status');
+    }
+}
