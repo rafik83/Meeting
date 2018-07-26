29983162de9fd2c5c5a67c89e3a7b377f59b4a19
Nico <nicolas.dievart@elao.com>
2018-07-26 18:23:29 +0200

    1687 - Add unit test

---
+++ tests/Application/Command/Meeting/Admin/UpdateStatusHandlerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Application\Command\Meeting\Admin;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Application\Command\Meeting\Admin\UpdateStatus;
+use Proximum\Vimeet\Application\Command\Meeting\Admin\UpdateStatusHandler;
+use Proximum\Vimeet\Domain\Model\Meeting;
+use Proximum\Vimeet\Domain\Repository\MeetingRepositoryInterface;
+
+class UpdateStatusHandlerTest extends TestCase
+{
+    public function testHandle(): void
+    {
+        $meeting = $this->prophesize(Meeting::class);
+        $status = Meeting::STATUS_CANCELED;
+
+        $meeting->setStatus($status)->shouldBeCalled();
+        $meetingRepository = $this->prophesize(MeetingRepositoryInterface::class);
+        $meetingRepository->set($meeting->reveal())->shouldBeCalled();
+
+        $handler = new UpdateStatusHandler($meetingRepository->reveal());
+        $handler->handle(new UpdateStatus($meeting->reveal(), $status));
+    }
+}
