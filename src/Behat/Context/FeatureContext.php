8ec29e79f3bd8fc089554e55024246f546c69fdf
Nico <nicolas.dievart@elao.com>
2018-07-27 09:54:50 +0200

    1687 - Reset before populate

--- src/Behat/Context/FeatureContext.php
+++ src/Behat/Context/FeatureContext.php
@@ -112,6 +112,7 @@ class FeatureContext extends MinkContext implements KernelAwareContext, SnippetA
      */
     public function eslaticaIsPopulate()
     {
+        exec('bin/console fos:elastica:reset --env=test --no-debug');
         exec('bin/console fos:elastica:populate --env=test --quiet --no-interaction --no-debug');
     }
 
