28746de8e90a6b6583cb4f0a0e5fbd2cd96c1ad3
Nico <nicolas.dievart@elao.com>
2018-08-21 11:55:28 +0200

    1657 - add items classes for better integration

--- src/Ui/Bundle/AdminBundle/Resources/assets/js/components/_ButtonGroupDefaultStateChanger.js
+++ src/Ui/Bundle/AdminBundle/Resources/assets/js/components/_ButtonGroupDefaultStateChanger.js
@@ -4,6 +4,9 @@ function ButtonGroupDefaultStateChanger(element) {
     this.element = element;
 
     this.items = element.querySelectorAll('[data-btn-group-default-state-item]');
+    this.itemsClasses = Array.prototype.map.call(this.items, function (item) {
+        return item.getAttribute('data-btn-group-default-state-item');
+    });
 
     this.element.addEventListener('click', function (event) {
         setTimeout(function(){
@@ -11,7 +14,9 @@ function ButtonGroupDefaultStateChanger(element) {
                 if (item.classList.contains('active')) {
                     item.classList.add(item.getAttribute('data-btn-group-default-state-item'));
                 } else {
-                    item.classList.remove('btn-success', 'btn-danger');
+                    for (var itemClass = 0, length = this.itemsClasses.length; itemClass < length; itemClass++) {
+                        item.classList.remove(this.itemsClasses[itemClass]);
+                    }
                 }
             }.bind(this));
         }.bind(this), 1);
