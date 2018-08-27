7401bc7f4c2d2d2ac2e61a4d4b90fc83495835b9
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-08-27 11:30:25 +0200

    1689 - Add ParticipantExtraData model

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20180827093424.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+
+/**
+ * Add meeting_participant_extra_data table
+ */
+final class Version20180827093424 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE meeting_participant_extra_data (id INT AUTO_INCREMENT NOT NULL, participant_id INT NOT NULL, meeting_id INT NOT NULL, date DATETIME DEFAULT NULL, type VARCHAR(255) NOT NULL, INDEX IDX_6648EEDC9D1C3019 (participant_id), INDEX IDX_6648EEDC67433D9C (meeting_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE meeting_participant_extra_data ADD CONSTRAINT FK_6648EEDC9D1C3019 FOREIGN KEY (participant_id) REFERENCES participant (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE meeting_participant_extra_data ADD CONSTRAINT FK_6648EEDC67433D9C FOREIGN KEY (meeting_id) REFERENCES meeting (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE meeting_participant_extra_data');
+    }
+}
