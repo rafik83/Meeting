1d950f75f167f96dac275b26339b07f88c5f238f
Nico <nicolas.dievart@elao.com>
2018-09-27 14:26:05 +0200

    1679 - Add return type

--- src/Ui/Bundle/AdminBundle/Controller/Transactional/Mail/ListAction.php
+++ src/Ui/Bundle/AdminBundle/Controller/Transactional/Mail/ListAction.php
@@ -16,6 +16,7 @@ use Proximum\Vimeet\Application\Query\Transactional\Mail\TransactionalMailListVi
 use Proximum\Vimeet\Domain\Model\Event;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ListAction
@@ -39,7 +40,7 @@ class ListAction
         $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
     }
 
-    public function __invoke(Request $request, Event $event)
+    public function __invoke(Request $request, Event $event): Response
     {
         if (!$this->authorizationCheckerAdapter->isGranted('ROLE_ALLOWED_TO_ORGANIZE')) {
             throw new AccessDeniedException('Access denied');
