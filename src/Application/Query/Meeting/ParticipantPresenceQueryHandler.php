d2459adcfa69cc469606749fa885953d534da83d
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-10-04 15:52:11 +0200

    1729 - Check participant presence every 30s

---
+++ src/Application/Query/Meeting/ParticipantPresenceQueryHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the PhpStorm project.
+ *
+ * Copyright (C) PhpStorm
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Application\Query\Meeting;
+
+use Proximum\Vimeet\Application\View\Meeting\Admin\ListMeeting\ParticipantPresenceView;
+use Proximum\Vimeet\Domain\Meeting\IsParticipantPresentToMeeting;
+use Proximum\Vimeet\Domain\Model\Meeting;
+use Proximum\Vimeet\Domain\Model\Participant;
+use Proximum\Vimeet\Domain\Participant\IsParticipantVisio;
+use Proximum\Vimeet\Domain\Repository\MeetingRepositoryInterface;
+
+class ParticipantPresenceQueryHandler
+{
+    /** @var IsParticipantPresentToMeeting */
+    private $isParticipantPresentToMeeting;
+
+    /** @var MeetingRepositoryInterface */
+    private $meetingRepository;
+
+    /** @var IsParticipantVisio */
+    private $isParticipantVisio;
+
+    public function __construct(
+        IsParticipantPresentToMeeting $isParticipantPresentToMeeting,
+        MeetingRepositoryInterface $meetingRepository,
+        IsParticipantVisio $isParticipantVisio
+    ) {
+        $this->isParticipantPresentToMeeting = $isParticipantPresentToMeeting;
+        $this->meetingRepository = $meetingRepository;
+        $this->isParticipantVisio = $isParticipantVisio;
+    }
+
+    public function handle(ParticipantPresenceQuery $query): array
+    {
+        $meetings = $this->meetingRepository->findByMeetingSlot($query->meetingSlot);
+        $participantPresence = [];
+
+        /** @var Meeting $meeting */
+        foreach ($meetings as $meeting) {
+            $participants = array_merge(
+                $meeting->getToParticipants()->toArray(),
+                $meeting->getFromParticipants()->toArray()
+            );
+
+            /** @var Participant $participant */
+            foreach ($participants as $participant) {
+                if (!$this->isParticipantVisio->isSatisfiedBy($participant)) {
+                    continue;
+                }
+
+                $participantPresence[$participant->getId()] = new ParticipantPresenceView(
+                    $participant->getId(),
+                    $this->isParticipantPresentToMeeting->isSatisfiedBy($participant, $meeting)
+                );
+            }
+        }
+
+        return $participantPresence;
+    }
+}
