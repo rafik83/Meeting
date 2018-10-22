7257ab881f039505e16ef545079d963dea51f2a7
Mathieu MARCHOIS <mathieu.marchois@gmail.com>
2018-10-22 16:51:30 +0200

    1723 - Create a new campaign on BatchCampaignHandler

--- src/Application/Command/User/Batch/BatchCampaignHandler.php
+++ src/Application/Command/User/Batch/BatchCampaignHandler.php
@@ -2,10 +2,51 @@
 
 namespace Proximum\Vimeet\Application\Command\User\Batch;
 
+use Proximum\Vimeet\Application\Components\Sheet\SheetGuesser;
+use Proximum\Vimeet\Domain\Model\Messaging\Campaign;
+use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\UserRepositoryInterface;
+
 class BatchCampaignHandler
 {
+    /** @var SheetGuesser */
+    private $sheetGuesser;
+
+    /** @var UserRepositoryInterface */
+    private $userRepository;
+
+    /** @var \DateTimeInterface */
+    private $datetime;
+
+    public function __construct(
+        SheetGuesser $sheetGuesser,
+        UserRepositoryInterface $userRepository,
+        \DateTimeInterface $datetime
+    ) {
+        $this->sheetGuesser = $sheetGuesser;
+        $this->userRepository = $userRepository;
+        $this->datetime = $datetime;
+    }
+
     public function handle(BatchCampaign $batch): BatchCampaignResult
     {
-        dump($batch);die;
+        $campaign = new Campaign($batch->event, $batch->campaignTitle, [], $this->datetime);
+
+        foreach ($batch->ids as $id) {
+            $user = $this->userRepository->findOneById($id);
+            if (!$user instanceof User) {
+                continue;
+            }
+
+            try {
+                $sheet = $this->sheetGuesser->getUserSheet($user, $batch->event, $batch->locale);
+            } catch (\Exception $exception) {
+                continue;
+            }
+
+            $campaign->addSheet($sheet);
+        }
+
+        return new BatchCampaignResult($campaign);
     }
 }
