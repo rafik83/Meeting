2dfd6aafa0a7c53661a07ad2f334d1b3635f5b59
Nico <nicolas.dievart@elao.com>
2018-10-29 16:36:59 +0100

    Introduce a command to prepare jobs to calculate available slots on current event

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Command/Aggregate/Event/AvailableSlotCalculatorForCurrentEventCommand.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command\Aggregate\Event;
+
+use Proximum\Vimeet\Application\Adapter\JobQueueInterface;
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AvailableSlotCalculatorForCurrentEventCommand extends Command
+{
+    private const NAME = 'vimeet:event:available-slots-calculator-for-current-event';
+
+    /** @var EventRepositoryInterface */
+    private $eventRepository;
+
+    /** @var JobQueueInterface */
+    private $jobQueue;
+
+    /** @var \DateTimeInterface */
+    private $dateTime;
+
+    /**
+     * @param EventRepositoryInterface $eventRepository
+     * @param JobQueueInterface        $jobQueue
+     * @param \DateTimeInterface       $dateTime
+     */
+    public function __construct(
+        EventRepositoryInterface $eventRepository,
+        JobQueueInterface $jobQueue,
+        \DateTimeInterface $dateTime
+    ) {
+        parent::__construct(self::NAME);
+
+        $this->eventRepository = $eventRepository;
+        $this->jobQueue = $jobQueue;
+        $this->dateTime = $dateTime;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Prepare command to aggregate the slots available for the current events')
+        ;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $events = $this->eventRepository->getEventThatOccursDuringTheGivenDay($this->dateTime);
+
+        if (empty($events)) {
+            $output->writeln('No event occur now, aborted');
+
+            return;
+        }
+
+        foreach ($events as $event) {
+            $output->writeln(
+                sprintf('Prepare the calculating of available slots for the event %s with id %s', $event->getTitle(), $event->getId())
+            );
+            $this->jobQueue->aggregateAvailableSlot($event);
+        }
+    }
+}
