a170883a78ac9764feb4c4fa7c37af3279d84d1e
Nico <nicolas.dievart@elao.com>
2018-10-31 15:34:53 +0100

    1768 - Introduce type content

---
+++ src/Domain/Model/Type/Content.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Model\Type;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Proximum\Vimeet\Domain\Model\Type;
+
+class Content
+{
+    public const TYPE_TERMS_OF_SALE = 'terms-of-sale';
+
+    /** @var int|null */
+    private $id;
+
+    /** @var string */
+    private $type;
+
+    /** @var Type */
+    private $associatedParticipationType;
+
+    /** @var ArrayCollection of ContentTranslation */
+    private $translations;
+
+    public function __construct(Type $associatedParticipationType, string $type)
+    {
+        $this->type = $type;
+        $this->translations = new ArrayCollection();
+        $this->associatedParticipationType = $associatedParticipationType;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getAssociatedParticipationType(): Type
+    {
+        return $this->associatedParticipationType;
+    }
+
+    /**
+     * @return ContentTranslation[]
+     */
+    public function getTranslations(): array
+    {
+        return $this->translations->toArray();
+    }
+
+    public function hasTranslation(string $locale): bool
+    {
+        return $this->translations->containsKey($locale);
+    }
+
+    private function getTranslation(string $locale): ?ContentTranslation
+    {
+        return $this->translations->get($locale);
+    }
+
+    public function getValue(string $locale): string
+    {
+        return $this->hasTranslation($locale)
+            ? $this->getTranslation($locale)->getValue() : ''
+        ;
+    }
+
+    public function translate(string $locale, string $value): void
+    {
+        if ($this->hasTranslation($locale)) {
+            $this->getTranslation($locale)->update($value);
+        } else {
+            $this->translations->set($locale, new ContentTranslation($this, $locale, $value));
+        }
+    }
+}
---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20181031142855.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Add Type Content and Type Content Translation tables
+ */
+class Version20181031142855 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE type_content (id INT AUTO_INCREMENT NOT NULL, associated_participation_type_id INT NOT NULL, type VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_D6E45F5CCBD407F0 (associated_participation_type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type_content_translation (id INT AUTO_INCREMENT NOT NULL, content_id INT DEFAULT NULL, locale VARCHAR(255) NOT NULL, value LONGTEXT NOT NULL, INDEX IDX_61F75C4684A0A3ED (content_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE type_content ADD CONSTRAINT FK_D6E45F5CCBD407F0 FOREIGN KEY (associated_participation_type_id) REFERENCES type (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE type_content_translation ADD CONSTRAINT FK_61F75C4684A0A3ED FOREIGN KEY (content_id) REFERENCES type_content (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE type_content_translation DROP FOREIGN KEY FK_61F75C4684A0A3ED');
+        $this->addSql('DROP TABLE type_content');
+        $this->addSql('DROP TABLE type_content_translation');
+    }
+}
