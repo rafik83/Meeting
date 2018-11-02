6004a1355d4b4a1b071095b6775f1b50783ed345
Nico <nicolas.dievart@elao.com>
2018-11-02 10:17:57 +0100

    1768 - Fix rights and add unit tests

--- src/Ui/Bundle/AdminBundle/Controller/Type/Content/DefineTermsOfSaleAction.php
+++ src/Ui/Bundle/AdminBundle/Controller/Type/Content/DefineTermsOfSaleAction.php
@@ -10,6 +10,7 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Type\Content;
 
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
 use Proximum\Vimeet\Application\Adapter\CommandBusInterface;
 use Proximum\Vimeet\Application\Command\Type\Content\DefineTermsOfSale;
 use Proximum\Vimeet\Domain\Model\Event;
@@ -23,6 +24,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
 use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Templating\EngineInterface;
 
 class DefineTermsOfSaleAction
@@ -48,7 +50,11 @@ class DefineTermsOfSaleAction
     /** @var EventContentRepositoryInterface */
     private $eventContentRepository;
 
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationCheckerAdapter;
+
     public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
         EventContentRepositoryInterface $eventContentRepository,
         ContentRepositoryInterface $contentRepository,
         CommandBusInterface $commandBus,
@@ -64,10 +70,18 @@ class DefineTermsOfSaleAction
         $this->engine = $engine;
         $this->formFactory = $formFactory;
         $this->eventContentRepository = $eventContentRepository;
+        $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
     }
 
     public function __invoke(Request $request, Event $event, Type $type)
     {
+        if (!$this->authorizationCheckerAdapter->isGranted('ROLE_ALLOWED_TO_ORGANIZE')
+            || !$this->authorizationCheckerAdapter->isGranted('PERMISSION_EVENT_ACCESS', $event)
+            || $type->getEvent() !== $event
+        ) {
+            throw new AccessDeniedException('Access denied');
+        }
+
         $content = $this->contentRepository->findByTypeAndAssociatedParticipationType(Type\Content::TYPE_TERMS_OF_SALE, $type);
         $contentFallback = $this->eventContentRepository->findByEventAndType($event, Event\Content::TYPE_TERMS_OF_SALE);
 
