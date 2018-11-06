4410f94823444809e9c68bcb93d6bca800377a82
Nico <nicolas.dievart@elao.com>
2018-11-06 17:48:52 +0100

    1778 - On Invoice show the type or event payment conditions

--- src/Application/Components/Invoice/Denormalizer/InvoiceViewDenormalizer.php
+++ src/Application/Components/Invoice/Denormalizer/InvoiceViewDenormalizer.php
@@ -16,6 +16,7 @@ use Proximum\Vimeet\Application\View\Invoice\SummaryView;
 use Proximum\Vimeet\Application\View\Invoice\Vat\VatListView;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Invoice\Invoice;
+use Proximum\Vimeet\Domain\Model\Type\PaymentConditions;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
 use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
@@ -36,8 +37,20 @@ class InvoiceViewDenormalizer implements DenormalizerInterface, DenormalizerAwar
 
         /** @var Event $event */
         $event = $invoice->getEvent();
-
         $eventDefaultLocale = $event->getFallback();
+        $paymentConditions = $invoice->getSheet()->getType()->getPaymentConditions();
+
+        if ($paymentConditions instanceof PaymentConditions) {
+            $billingAddress = $paymentConditions->getBillingAddress($eventDefaultLocale);
+            $bankInfo = $paymentConditions->getBankInfo($eventDefaultLocale);
+            $paymentCondition = $paymentConditions->getPaymentCondition($eventDefaultLocale);
+            $paymentFooter = $paymentConditions->getPaymentFooter($eventDefaultLocale);
+        } else {
+            $billingAddress = $event->getBillingAddress($eventDefaultLocale);
+            $bankInfo = $event->getBankInfo($eventDefaultLocale);
+            $paymentCondition = $event->getPaymentCondition($eventDefaultLocale);
+            $paymentFooter = $event->getPaymentFooter($eventDefaultLocale);
+        }
 
         return new InvoiceView(
             $invoice->getNumber(),
@@ -53,10 +66,10 @@ class InvoiceViewDenormalizer implements DenormalizerInterface, DenormalizerAwar
             $invoice->getCreatedAt(),
             $eventDefaultLocale,
             $event->getTimeZone(),
-            $event->getBillingAddress($eventDefaultLocale),
-            $event->getBankInfo($eventDefaultLocale),
-            $event->getPaymentCondition($eventDefaultLocale),
-            $event->getPaymentFooter($eventDefaultLocale),
+            $billingAddress,
+            $bankInfo,
+            $paymentCondition,
+            $paymentFooter,
             $this->denormalizer->denormalize($data['summaryView'], SummaryView::class, $format, $context),
             $this->denormalizer->denormalize($data['billingInfosView'], BillingInfosView::class, $format, $context),
             isset($data['vatListView'])
--- tests/Application/Components/Invoice/Denormalizer/InvoiceViewDenormalizerTest.php
+++ tests/Application/Components/Invoice/Denormalizer/InvoiceViewDenormalizerTest.php
@@ -37,6 +37,7 @@ use Proximum\Vimeet\Application\View\Invoice\Vat\VatView;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Invoice\Invoice;
 use Proximum\Vimeet\Domain\Model\Invoice\Prefix;
+use Proximum\Vimeet\Domain\Model\Type;
 use Proximum\Vimeet\Tests\Factory\EventFactory;
 use Proximum\Vimeet\Tests\Factory\SheetFactory;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
@@ -54,7 +55,7 @@ class InvoiceViewDenormalizerTest extends TestCase
     public function testDenormalizeInvoiceView()
     {
         $event  = EventFactory::createEvent('My Event', 'fr');
-        $sheet  = SheetFactory::create();
+        $sheet  = SheetFactory::create($event);
         $prefix = new Prefix('Vimeet', 'Vi');
         $date   = new \DateTime('2016-12-25 08:00:00');
 
@@ -292,7 +293,7 @@ class InvoiceViewDenormalizerTest extends TestCase
     public function testDenormalizeInvoiceViewWithVatListView()
     {
         $event  = EventFactory::createEvent('My Event', 'fr');
-        $sheet  = SheetFactory::create();
+        $sheet  = SheetFactory::create($event);
         $prefix = new Prefix('Vimeet', 'Vi');
         $date   = new \DateTime('2016-12-25 08:00:00');
 
@@ -386,6 +387,218 @@ class InvoiceViewDenormalizerTest extends TestCase
             '',
             '',
             '',
+            new SummaryView(
+                new GroupsView(
+                    [
+                        new GroupView(
+                            'Full entry ticket',
+                            'plan',
+                            null,
+                            [
+                                new RowView(
+                                    'Full entry ticket',
+                                    1,
+                                    35000,
+                                    35000,
+                                    1668
+                                ),
+                            ],
+                            []
+                        ),
+                        new GroupView(
+                            'Participant 2 days with dinner',
+                            'participant',
+                            null,
+                            [
+                                new RowView(
+                                    'Participant 2 days with dinner',
+                                    1,
+                                    9900,
+                                    9900,
+                                    1662
+                                ),
+                            ]
+                        ),
+                        new GroupView(
+                            'A business meetings planning is aleady included in your registration package',
+                            'planning',
+                            null,
+                            [
+                                new RowView(
+                                    'A business meetings planning is aleady included in your registration package',
+                                    1,
+                                    0,
+                                    0,
+                                    1512
+                                ),
+                            ]
+                        ),
+                        new GroupView(
+                            'Options',
+                            'option',
+                            272,
+                            [
+                                new RowView(
+                                    'CHAIR',
+                                    1,
+                                    1900,
+                                    1900,
+                                    1745,
+                                    [
+                                        new CustomRowView(
+                                            'Remise exceptionnelle',
+                                            -230,
+                                            1,
+                                            -230
+                                        ),
+                                    ]
+                                ),
+                            ]
+                        ),
+                    ]
+                ),
+                new PromotionCodesView([]),
+                'et',
+                'EUR'
+            ),
+            new BillingInfosView(
+                'man',
+                'DUPOND',
+                'Laurent',
+                'Directeur',
+                '+33122334455',
+                '+33611223344',
+                'my-email@example.net',
+                'My company',
+                '10 rue Saint Marc',
+                '75002',
+                'Paris',
+                'NL',
+                null,
+                'My reference'
+            ),
+            new VatListView(
+                46570,
+                57843,
+                true,
+                'et',
+                [
+                    new VatView(25, 'et', 44670, 11168),
+                    new VatView(20, 'et', 0, 0),
+                    new VatView(5.5, 'et', 1900, 105),
+                ]
+            ),
+            57843
+        );
+
+        $this->assertEquals($expectedInvoiceView, $invoiceView);
+    }
+
+    public function testDenormalizeInvoiceViewWithVatListViewAndTypePaymentConditions()
+    {
+        $event  = EventFactory::createEvent('My Event', 'fr');
+        $type = $this->prophesize(Type::class);
+        $paymentConditions = $this->prophesize(Type\PaymentConditions::class);
+        $type->getPaymentConditions()->shouldBeCalled()->willReturn($paymentConditions);
+        $paymentConditions->getBillingAddress('fr')->shouldBeCalled()->willReturn('BillingAddress');
+        $paymentConditions->getBankInfo('fr')->shouldBeCalled()->willReturn('BankInfo');
+        $paymentConditions->getPaymentCondition('fr')->shouldBeCalled()->willReturn('PaymentCondition');
+        $paymentConditions->getPaymentFooter('fr')->shouldBeCalled()->willReturn('PaymentFooter');
+
+        $sheet  = SheetFactory::create($event, null, null, $type->reveal());
+        $prefix = new Prefix('Vimeet', 'Vi');
+        $date   = new \DateTime('2016-12-25 08:00:00');
+
+        /**
+         * This json data must not changes because it is an extract of real invoice data
+         */
+        $data = '{"summaryView":{"groups":{"groups":[{"groupId":null,"label":"Full entry ticket","type":"plan","products":[{"label":"Full entry ticket","quantity":1,"price":35000,"total":35000,"productId":1668,"vatRate":25,"customRows":[],"includedProducts":[]}],"customRows":[]},{"groupId":null,"label":"Participant 2 days with dinner","type":"participant","products":[{"label":"Participant 2 days with dinner","quantity":1,"price":9900,"total":9900,"productId":1662,"vatRate":25,"customRows":[],"includedProducts":[]}],"customRows":[]},{"groupId":null,"label":"A business meetings planning is aleady included in your registration package","type":"planning","products":[{"label":"A business meetings planning is aleady included in your registration package","quantity":1,"price":0,"total":0,"productId":1512,"vatRate":20,"customRows":[],"includedProducts":[]}],"customRows":[]},{"groupId":272,"label":"Options","type":"option","products":[{"label":"CHAIR","quantity":1,"price":1900,"total":1900,"productId":1745,"vatRate":5.5,"customRows":[{"label":"Remise exceptionnelle","quantity":1,"price":-230,"total":-230}],"includedProducts":[]}],"customRows":[]}]},"promotionCodes":{"promotionCodes":[]},"vatMode":"et","currency":"EUR"},"billingInfosView":{"gender":"man","lastname":"Philipse","firstname":"Esther","function":"CEO","phone":"+31615012306","mobile":"+31615012306","email":"email-70900@example.net","company":"AERspire","vatNumber":null,"street":"Tesla 1","zipcode":"6422 RG","city":"Heerlen","country":"NL","reference":null},"vatListView":{"vatAmount":11273,"total":46570,"totalWithVat":57843,"vatApplicable":true,"vatMode":"et","vatViews":{"vat_25":{"vatRate":25,"vatMode":"et","total":44670,"totalVat":11168},"vat_20":{"vatRate":20,"vatMode":"et","total":0,"totalVat":0},"vat_5.5":{"vatRate":5.5,"vatMode":"et","total":1900,"totalVat":105}}},"amountRemainToPay":57843}';
+
+        $invoice = new Invoice(
+            $event,
+            $sheet,
+            $prefix,
+            'Vi',
+            2016,
+            7,
+            true,
+            Event::VAT_MODE_ET,
+            25,
+            339300,
+            407160,
+            67860,
+            'EUR',
+            $data,
+            $date
+        );
+
+        $billingInfosViewOfSheet = new BillingInfosView(
+            'man',
+            'DUPOND',
+            'Laurent',
+            'Directeur',
+            '+33122334455',
+            '+33611223344',
+            'my-email@example.net',
+            'My company',
+            '10 rue Saint Marc',
+            '75002',
+            'Paris',
+            'EN',
+            'My vat changed number',
+            'My reference'
+        );
+
+        $serializer = new Serializer(
+            [
+                new InvoiceViewDenormalizer(),
+                new SummaryViewDenormalizer(),
+                new BillingInfosViewDenormalizer(),
+                new GroupsViewDenormalizer(),
+                new GroupViewDenormalizer(),
+                new RowViewDenormalizer(),
+                new PromotionCodesViewDenormalizer(),
+                new PromotionCodeViewDenormalizer(),
+                new PromotionProductRowViewDenormalizer(),
+                new VatListViewDenormalizer(),
+                new ObjectNormalizer(),
+            ],
+            [
+                new JsonEncoder(),
+            ]
+        );
+
+        $invoiceView = $serializer->deserialize(
+            $invoice->getData(),
+            InvoiceView::class,
+            'json',
+            [
+                'invoice'                 => $invoice,
+                'billingInfosViewOfSheet' => $billingInfosViewOfSheet,
+            ]
+        );
+
+        /**
+         * This InvoiceView should never changed because it is the representation of Invoice data
+         */
+        $expectedInvoiceView = new InvoiceView(
+            'Vi2016-0007',
+            true,
+            'et',
+            25,
+            339300,
+            407160,
+            67860,
+            'EUR',
+            'My Event',
+            null,
+            $date,
+            'fr',
+            $event->getTimeZone(),
+            'BillingAddress',
+            'BankInfo',
+            'PaymentCondition',
+            'PaymentFooter',
             new SummaryView(
                 new GroupsView(
                     [
