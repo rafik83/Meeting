32153a78f347e45fc25f5ccced4e699696b1883d
Cindy Elao <cindy.liwenge@elao.com>
2018-11-15 16:34:10 +0100

    1786 - Change table name, from FormTemplate-Type to TemplateForm-Type

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20181115151123.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20181115151123.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Create table FormTemplate - Type
+ * Create table TemplateForm - Type
  */
 class Version20181115151123 extends AbstractMigration
 {
@@ -17,9 +17,9 @@ class Version20181115151123 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE form_template_type (form_template_id INT NOT NULL, type_id INT NOT NULL, INDEX IDX_FAC63A7BF2B19FA9 (form_template_id), INDEX IDX_FAC63A7BC54C8C93 (type_id), PRIMARY KEY(form_template_id, type_id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE form_template_type ADD CONSTRAINT FK_FAC63A7BF2B19FA9 FOREIGN KEY (form_template_id) REFERENCES template_form (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE form_template_type ADD CONSTRAINT FK_FAC63A7BC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE template_form_type (form_template_id INT NOT NULL, type_id INT NOT NULL, INDEX IDX_FAC63A7BF2B19FA9 (form_template_id), INDEX IDX_FAC63A7BC54C8C93 (type_id), PRIMARY KEY(form_template_id, type_id)) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE template_form_type ADD CONSTRAINT FK_FAC63A7BF2B19FA9 FOREIGN KEY (form_template_id) REFERENCES template_form (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE template_form_type ADD CONSTRAINT FK_FAC63A7BC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id) ON DELETE CASCADE');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20181115151123 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE form_template_type');
+        $this->addSql('DROP TABLE template_form_type');
     }
 }
