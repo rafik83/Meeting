bf7ae5bb29fe1c48c10888e8b457b07a5ac23eb2
Nico <nicolas.dievart@elao.com>
2018-11-15 12:25:38 +0100

    1785 - Add FormTemplate repository

---
+++ src/Infrastructure/Repository/Template/FormTemplateRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Repository\Template;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Template\FormTemplate;
+use Proximum\Vimeet\Domain\Repository\Template\FormTemplateRepositoryInterface;
+
+class FormTemplateRepository implements FormTemplateRepositoryInterface
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function findByEvent(Event $event): array
+    {
+        return $this
+            ->entityManager
+            ->createQueryBuilder()
+            ->select('formTemplate')
+            ->from(FormTemplate::class, 'formTemplate')
+            ->where('formTemplate.event = :event')
+            ->setParameter('event', $event)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
