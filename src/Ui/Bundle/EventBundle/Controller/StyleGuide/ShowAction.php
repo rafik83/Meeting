8142d287acfd25146707b9dd3c27ce79e0c79da6
Richard Hanna <supertanuki@gmail.com>
2018-11-16 15:06:21 +0100

    Add styleguide page

---
+++ src/Ui/Bundle/EventBundle/Controller/StyleGuide/ShowAction.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\StyleGuide;
+
+use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Templating\EngineInterface;
+
+class ShowAction
+{
+    /** @var EngineInterface */
+    private $engine;
+
+    public function __construct(EngineInterface $engine)
+    {
+        $this->engine = $engine;
+    }
+
+    public function __invoke(Request $request, EventDomain $eventDomain): Response
+    {
+        $event = $eventDomain->getEvent();
+
+        return new Response(
+            $this->engine->render(
+                'EventBundle:StyleGuide:show.html.twig',
+                [
+                    'event' => $event,
+                ]
+            )
+        );
+    }
+}
