c8514cd08435af608ba54370b5ba4102ebb41a5a
Richard Hanna <supertanuki@gmail.com>
2018-11-16 16:33:16 +0100

    1790 - Add getFormTemplateViewByType query

--- src/Domain/Repository/Template/FormTemplateRepositoryInterface.php
+++ src/Domain/Repository/Template/FormTemplateRepositoryInterface.php
@@ -11,8 +11,19 @@
 namespace Proximum\Vimeet\Domain\Repository\Template;
 
 use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\Template\FormTemplate;
+use Proximum\Vimeet\Domain\Model\Type;
+use Proximum\Vimeet\Domain\Template\Form\FormTemplateView;
 
 interface FormTemplateRepositoryInterface
 {
+    /**
+     * @return FormTemplate[]
+     */
     public function findByEvent(Event $event): array;
+
+    /**
+     * @return FormTemplateView[]
+     */
+    public function getFormTemplateViewByType(Type $type, string $locale): array;
 }
