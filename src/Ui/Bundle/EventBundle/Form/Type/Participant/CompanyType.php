05dfd539cac28309de2170edf76aec73652e9802
Nico <nicolas.dievart@elao.com>
2018-11-27 12:08:02 +0100

    1791 - Introduce AbstractBlockType

--- src/Ui/Bundle/EventBundle/Form/Type/Participant/CompanyType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Participant/CompanyType.php
@@ -11,212 +11,30 @@
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Participant;
 
 use Proximum\Vimeet\Domain\Template;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\BooleanDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\CountryDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\DatetimeDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\EditableTextInputDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\EditableTextTranslationType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\FileDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\NomenclatureDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\TelephoneDataType;
-use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Sheet\Data\UrlDataType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Template\AbstractBlockType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CompanyType extends AbstractType
+class CompanyType extends AbstractBlockType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        /** @var Template\TemplateData $template */
-        $template = $options['template'];
-
-        foreach ($template->getEditableSheetDataExceptedImageObjects() as $key => $object) {
-            if ($object instanceof Template\TemplateObject\EditableText) {
-                $this->addText($key, $builder, $object, $options['locale'], $options['locales']);
-            } elseif ($object instanceof Template\TemplateObject\Nomenclature) {
-                $this->addNomenclature($key, $builder, $object, $options['locale']);
-            } elseif ($object instanceof Template\TemplateObject\Telephone) {
-                $this->addTelephone($key, $builder, $object, $options['locale'], $options['country']);
-            } elseif ($object instanceof Template\TemplateObject\Url) {
-                $this->addUrl($key, $builder, $object, $options['locale']);
-            } elseif ($object instanceof Template\TemplateObject\Country) {
-                $this->addCountry($key, $builder, $object, $options['locale']);
-            } elseif ($object instanceof Template\TemplateObject\BooleanObject) {
-                $this->addBoolean($key, $builder, $object, $options['locale']);
-            } elseif ($object instanceof Template\TemplateObject\UploadObject) {
-                $this->addFile($key, $builder, $object, $options['locale']);
-            } elseif ($object instanceof Template\TemplateObject\DateTime) {
-                $this->addDatetime($key, $builder, $object, $options['locale']);
-            }
-        }
-    }
-
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver): void
     {
+        parent::configureOptions($resolver);
         $resolver->setDefaults([
             'data_class'        => Template\TemplateData::class,
             'validation_groups' => ['Default', 'company'],
         ]);
-        $resolver->setRequired(['template', 'locale', 'country', 'locales']);
-        $resolver->setAllowedTypes('locale', 'string');
-        $resolver->setAllowedTypes('locales', 'array');
+        $resolver->setRequired(['template']);
         $resolver->setAllowedTypes('template', Template\TemplateData::class);
-        $resolver->setAllowedTypes('country', 'string');
     }
 
-    /**
-     * @param string                  $key
-     * @param FormBuilderInterface    $builder
-     * @param Template\TemplateObject $object
-     * @param string                  $locale
-     * @param array                   $locales
-     */
-    private function addText(
-        $key,
-        FormBuilderInterface $builder,
-        Template\TemplateObject $object,
-        $locale,
-        $locales
-    ): void {
-        if ($object->isTranslatable()) {
-            $builder->add($key, EditableTextTranslationType::class, [
-                'locales' => $locales,
-                'object'  => $object,
-                'label'   => $object->getOption('label', $locale),
-            ]);
-        } else {
-            $builder->add($key, EditableTextInputDataType::class, [
-                'label'  => false,
-                'locale' => $locale,
-                'object' => $object,
-            ]);
-        }
-    }
-
-    /**
-     * @param string                      $key
-     * @param FormBuilderInterface        $builder
-     * @param Template\TemplateObject\Url $object
-     * @param string                      $locale
-     */
-    private function addUrl($key, FormBuilderInterface $builder, Template\TemplateObject\Url $object, $locale): void
+    protected  function getObjects(array $options): array
     {
-        $builder->add($key, UrlDataType::class, [
-            'label'  => false,
-            'locale' => $locale,
-            'object' => $object,
-        ]);
-    }
+        /** @var Template\TemplateData $template */
+        $template = $options['template'];
 
-    /**
-     * @param string                  $key
-     * @param FormBuilderInterface    $builder
-     * @param Template\TemplateObject $object
-     * @param string                  $locale
-     */
-    private function addFile(string $key, FormBuilderInterface $builder, Template\TemplateObject $object, string $locale): void
-    {
-        $builder->add($key, FileDataType::class, [
-            'showLabel' => true,
-            'locale' => $locale,
-            'object' => $object,
-        ]);
-    }
-
-    /**
-     * @param string                  $key
-     * @param FormBuilderInterface    $builder
-     * @param Template\TemplateObject $object
-     * @param string                  $locale
-     */
-    private function addBoolean(
-        $key,
-        FormBuilderInterface $builder,
-        Template\TemplateObject $object,
-        $locale
-    ): void {
-        $builder->add($key, BooleanDataType::class, [
-            'object' => $object,
-            'locale' => $locale,
-            'label'  => false,
-        ]);
-    }
-
-    /**
-     * @param string                  $key
-     * @param FormBuilderInterface    $builder
-     * @param Template\TemplateObject $object
-     * @param string                  $locale
-     * @param string                  $country
-     */
-    private function addTelephone(
-        $key,
-        FormBuilderInterface $builder,
-        Template\TemplateObject $object,
-        $locale,
-        $country
-    ): void {
-        $builder->add($key, TelephoneDataType::class, [
-            'label'   => false,
-            'locale'  => $locale,
-            'object'  => $object,
-            'country' => $country,
-        ]);
-    }
-
-    /**
-     * @param string                  $key
-     * @param FormBuilderInterface    $builder
-     * @param Template\TemplateObject $object
-     * @param string                  $locale
-     */
-    private function addCountry($key, FormBuilderInterface $builder, Template\TemplateObject $object, $locale): void
-    {
-        $builder->add($key, CountryDataType::class, [
-            'label'  => false,
-            'locale' => $locale,
-            'object' => $object,
-        ]);
-    }
-
-    /**
-     * @param string                               $key
-     * @param FormBuilderInterface                 $builder
-     * @param Template\TemplateObject\Nomenclature $object
-     * @param string                               $locale
-     */
-    private function addNomenclature(
-        $key,
-        FormBuilderInterface $builder,
-        Template\TemplateObject\Nomenclature $object,
-        $locale
-    ): void {
-        $builder->add($key, NomenclatureDataType::class, [
-            'label'                                   => false,
-            'locale'                                  => $locale,
-            'object'                                  => $object,
-            'placeholder'                             => $object->getOption('label')[$locale],
-            'onMultipleUseSinglesInsteadOfCheckboxes' => true,
-        ]);
-    }
-
-    private function addDatetime(
-        $key,
-        FormBuilderInterface $builder,
-        Template\TemplateObject $object,
-        $locale
-    ): void {
-        $builder->add($key, DatetimeDataType::class, [
-            'object'  => $object,
-            'locale'  => $locale,
-            'label'   => false,
-        ]);
+        return $template->getEditableSheetDataExceptedImageObjects();
     }
 }
