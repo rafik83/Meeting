dc3126e81bac07b3818b76f09c4eec48cafd84a2
Richard Hanna <supertanuki@gmail.com>
2018-11-29 17:06:03 +0100

    Hotfix: Select the begin date of the event

--- src/Application/Command/Event/UpdateEventDatesToCurrentDate.php
+++ src/Application/Command/Event/UpdateEventDatesToCurrentDate.php
@@ -17,11 +17,15 @@ class UpdateEventDatesToCurrentDate
     /** @var Event */
     public $event;
 
+    /** @var \DateTime */
+    public $beginDate;
+
     /**
      * @param Event $event
      */
-    public function __construct(Event $event)
+    public function __construct(Event $event, \DateTime $beginDate)
     {
         $this->event = $event;
+        $this->beginDate = $beginDate;
     }
 }
