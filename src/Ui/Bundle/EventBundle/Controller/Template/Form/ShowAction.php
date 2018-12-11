8e70b8714e76c96b2196823201dd65a5268d531b
Cindy Elao <cindy.liwenge@elao.com>
2018-12-11 10:07:03 +0100

    1816 - Handle access to FormTemplate

--- src/Ui/Bundle/EventBundle/Controller/Template/Form/ShowAction.php
+++ src/Ui/Bundle/EventBundle/Controller/Template/Form/ShowAction.php
@@ -9,10 +9,65 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\Template\Form;
 
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Application\Components\Sheet\SheetGuesser;
+use Proximum\Vimeet\Application\Exception\Sheet\SheetNotFoundException;
+use Proximum\Vimeet\Domain\Model\Template\FormTemplate;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\ValueResolver\UserDomain;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
 class ShowAction
 {
-    public function __invoke()
-    {
-        return '';
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationChecker;
+
+    /** @var RouterInterface */
+    private $router;
+
+    /** @var SheetGuesser */
+    private $sheetGuesser;
+
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationChecker,
+        RouterInterface $router,
+        SheetGuesser $sheetGuesser
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->router = $router;
+        $this->sheetGuesser = $sheetGuesser;
+    }
+
+    public function __invoke(
+        Request $request,
+        EventDomain $eventDomain,
+        ?UserDomain $userDomain,
+        FormTemplate $formTemplate
+    ): RedirectResponse {
+        if (!$this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            throw new AccessDeniedException();
+        }
+
+        $event = $eventDomain->getEvent();
+
+        try {
+            $sheet = $this->sheetGuesser->getUserSheet($userDomain->getUser(), $event, $event->getFallback());
+        } catch (SheetNotFoundException $exception) {
+            return new RedirectResponse($this->router->generate('login'));
+        }
+
+        $participant = $sheet->getUserParticipant($userDomain->getUser());
+
+        return new RedirectResponse($this->router->generate('event_participant_fill_form',
+            [
+                'formTemplate' => $formTemplate->getId(),
+                'sheet' => $sheet->getId(),
+                'participant' => $participant->getId(),
+                'step' => 1,
+            ])
+        );
     }
 }
