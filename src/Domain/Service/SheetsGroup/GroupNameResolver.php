e973523ff59bf11da7e8e1602987c2f79cc86c10
Nico <nicolas.dievart@elao.com>
2019-01-17 14:26:35 +0100

    1849 - Display sheet title if sheetTitleForced on Group

--- src/Domain/Service/SheetsGroup/GroupNameResolver.php
+++ src/Domain/Service/SheetsGroup/GroupNameResolver.php
@@ -15,15 +15,22 @@ use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
+use Proximum\Vimeet\Domain\User\Sheet\FirstParticipantSheetOfUserGetter;
 
 class GroupNameResolver
 {
     /** @var SheetRepositoryInterface */
     private $sheetRepository;
 
-    public function __construct(SheetRepositoryInterface $sheetRepository)
-    {
+    /** @var FirstParticipantSheetOfUserGetter */
+    private $firstParticipantSheetOfUserGetter;
+
+    public function __construct(
+        SheetRepositoryInterface $sheetRepository,
+        FirstParticipantSheetOfUserGetter $firstParticipantSheetOfUserGetter
+    ) {
         $this->sheetRepository = $sheetRepository;
+        $this->firstParticipantSheetOfUserGetter = $firstParticipantSheetOfUserGetter;
     }
 
     public function resolve(Event $event, User $user, array $sheets = []): ?string
@@ -37,9 +44,17 @@ class GroupNameResolver
         }
 
         foreach ($sheets as $sheet) {
-            if (null !== $sheet->getGroup()) {
-                return $sheet->getGroup()->getTitle();
+            $group = $sheet->getGroup();
+
+            if ($group instanceof Sheet\Group && !$group->hasSheetTitleForced()) {
+                return $group->getTitle();
             }
+        }
+
+        $sheet = $this->firstParticipantSheetOfUserGetter->getFirstParticipantSheet($user, $sheets);
+
+        if ($sheet instanceof Sheet) {
+            return $sheet->getTitle();
         }
 
         $sheet = reset($sheets);
