d6dfa7985313ff5ef37fd511c452cc9ac49e3e42
Christophe Jolas <christophe.jolas@gmail.com>
2019-02-07 17:09:30 +0100

    1866 - fix method

--- src/Application/Query/Product/ProductsListViewQueryHandler.php
+++ src/Application/Query/Product/ProductsListViewQueryHandler.php
@@ -3,6 +3,7 @@
 namespace Proximum\Vimeet\Application\Query\Product;
 
 use Proximum\Vimeet\Application\View\Product\ProductsListView;
+use Proximum\Vimeet\Domain\Repository\Order\RowRepositoryInterface;
 use Proximum\Vimeet\Domain\Repository\ProductRepositoryInterface;
 
 class ProductsListViewQueryHandler
@@ -10,12 +11,19 @@ class ProductsListViewQueryHandler
     /** @var ProductRepositoryInterface */
     private $productRepository;
 
+    /** @var RowRepositoryInterface */
+    private $rowRepository;
+
     /**
      * @param ProductRepositoryInterface $productRepository
+     * @param RowRepositoryInterface $rowRepository
      */
-    public function __construct(ProductRepositoryInterface $productRepository)
-    {
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        RowRepositoryInterface $rowRepository
+    ) {
         $this->productRepository = $productRepository;
+        $this->rowRepository = $rowRepository;
     }
 
     /**
@@ -25,18 +33,43 @@ class ProductsListViewQueryHandler
      */
     public function handle(ProductsListViewQuery $query): array
     {
+        $bought = [];
         $productListViews = [];
-        $products = $this->productRepository->findByEventOrderedByProductTypeAndProductname($query->event);
+        $productIncludedBought = [];
+        $products = $this->productRepository->findByEventOrderedByProductTypeAndProductName($query->event);
 
         foreach ($products as $product) {
+
+            $bought[$product->getId()] = $this->rowRepository->boughtByProduct($product);
+
+            if ('plan' === $product->getType()) {
+                if (count($product->getIncludedProducts()) > 0) {
+                    $includedProducts = $product->getIncludedProducts();
+
+                    foreach ($includedProducts as $includedProduct) {
+                        if (isset($productIncludedBought[$includedProduct->getIncluded()->getId()])) {
+                            $productIncludedBought[$includedProduct->getIncluded()->getId()] += $includedProduct->getQuantity() * $bought[$product->getId()];
+                        } else {
+                            $productIncludedBought[$includedProduct->getIncluded()->getId()] = $includedProduct->getQuantity() * $bought[$product->getId()];
+                        }
+                    }
+                }
+            }
+        }
+
+        foreach ($products as $product) {
+            $unitPrice = $product->getUnitPrice();
+            $total = $bought[$product->getId()] + $productIncludedBought[$product->getId()];
+            $promotion = 0;
+
             $productListViews[] = new ProductsListView(
                 $product->getName(),
-                $product->getUnitPrice(),
-                '',
-                '',
-                '',
-                '',
-                ''
+                $unitPrice,
+                $bought[$product->getId()],
+                $productIncludedBought[$product->getId()],
+                $total,
+                $promotion,
+                ($unitPrice * $total) - $promotion
             );
         }
 
