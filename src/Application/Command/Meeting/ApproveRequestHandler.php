109542b1627941d5d389662603a41b85373b0875
taner <tamer@crea2.pro>
2019-04-02 15:52:07 +0200

    1913 - add all participants on approve meeting request

--- src/Application/Command/Meeting/ApproveRequestHandler.php
+++ src/Application/Command/Meeting/ApproveRequestHandler.php
@@ -108,6 +108,12 @@ class ApproveRequestHandler
             throw new IsNotAllowedToApproveMeetingRequestException();
         }
 
+        if($approveRequest->sheet->getType()->areAllSheetParticipantsAssignedToMeeting()) {
+            foreach ($approveRequest->sheet->getParticipantsArray() as $participant) {
+                    $approveRequest->request->addToParticipant($participant);
+                }
+        }
+
         foreach ($approveRequest->request->getToParticipants() as $oldToParticipant) {
             if (!in_array($oldToParticipant, $approveRequest->participants)) {
                 $approveRequest->request->removeToParticipant($oldToParticipant);
