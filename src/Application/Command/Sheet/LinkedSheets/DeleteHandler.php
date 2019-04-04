be410bd9f721ea63853f0261e3e922c05de8b0d7
Ricardo Coelho <coelho.ric@gmail.com>
2019-04-04 14:09:44 +0200

    1906 - change check owner

--- src/Application/Command/Sheet/LinkedSheets/DeleteHandler.php
+++ src/Application/Command/Sheet/LinkedSheets/DeleteHandler.php
@@ -2,6 +2,8 @@
 
 namespace Proximum\Vimeet\Application\Command\Sheet\LinkedSheets;
 
+use Proximum\Vimeet\Application\Criteria\LinkedSheets\AreRemovableLinkedSheetsCriteria;
+use Proximum\Vimeet\Domain\Exception\DomainException;
 use Proximum\Vimeet\Domain\Repository\Sheet\LinkedSheetsRepositoryInterface;
 
 class DeleteHandler
@@ -9,13 +11,23 @@ class DeleteHandler
     /** @var LinkedSheetsRepositoryInterface */
     private $linkedSheetsRepository;
 
-    public function __construct(LinkedSheetsRepositoryInterface $linkedSheetsRepository)
-    {
+    /** @var AreRemovableLinkedSheetsCriteria */
+    private $areRemovableLinkedSheetsCriteria;
+
+    public function __construct(
+        LinkedSheetsRepositoryInterface $linkedSheetsRepository,
+        AreRemovableLinkedSheetsCriteria $areRemovableLinkedSheetsCriteria
+    ) {
         $this->linkedSheetsRepository = $linkedSheetsRepository;
+        $this->areRemovableLinkedSheetsCriteria = $areRemovableLinkedSheetsCriteria;
     }
 
     public function handle(Delete $command)
     {
+        if (count($this->areRemovableLinkedSheetsCriteria->meetCriteria([$command->linkedSheets])) === 0) {
+            throw new DomainException('Linked sheets can\'t be removed');
+        }
+
         $this->linkedSheetsRepository->remove($command->linkedSheets);
     }
 }
--- src/Ui/Bundle/AdminBundle/Controller/LinkedSheets/DeleteAction.php
+++ src/Ui/Bundle/AdminBundle/Controller/LinkedSheets/DeleteAction.php
@@ -35,31 +35,22 @@ class DeleteAction
     /** @var CommandBusInterface */
     private $commandBus;
 
-    /** @var AreRemovableLinkedSheetsCriteria */
-    private $removableLinkedSheetsFilter;
-
     public function __construct(
         AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
         CommandBusInterface $commandBus,
         RouterInterface $router,
-        FlashBagInterface $flashBag,
-        AreRemovableLinkedSheetsCriteria $removableLinkedSheetsFilter
+        FlashBagInterface $flashBag
     ) {
         $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
         $this->router = $router;
         $this->flashBag = $flashBag;
         $this->commandBus = $commandBus;
-        $this->removableLinkedSheetsFilter = $removableLinkedSheetsFilter;
     }
 
     public function __invoke(Event $event, LinkedSheets $linkedSheets)
     {
         if (!$this->authorizationCheckerAdapter->isGranted('PERMISSION_EVENT_ACCESS', $event)) {
             throw new AccessDeniedException('Access denied!');
-        }
-
-        if (count($this->removableLinkedSheetsFilter->meetCriteria([$linkedSheets])) === 0) {
-            throw new AccessDeniedException();
         }
 
         $this->commandBus->handle(new Delete($linkedSheets));
