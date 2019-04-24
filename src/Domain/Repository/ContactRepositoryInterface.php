844d4d463abf9bde7f4cfdc3c80a1931c31f66cc
Richard Hanna <supertanuki@gmail.com>
2019-04-24 17:28:42 +0200

    1945 - Add contact repository

---
+++ src/Domain/Repository/ContactRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Repository;
+
+use Proximum\Vimeet\Domain\Model\Contact;
+
+interface ContactRepositoryInterface
+{
+    public function add(Contact $contact);
+}
---
+++ src/Infrastructure/Repository/ContactRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Infrastructure\Repository;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Model\Contact;
+use Proximum\Vimeet\Domain\Repository\ContactRepositoryInterface;
+
+class ContactRepository implements ContactRepositoryInterface
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function add(Contact $contact)
+    {
+        $this->entityManager->persist($contact);
+        $this->entityManager->flush($contact);
+    }
+}
