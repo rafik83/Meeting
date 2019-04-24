a28b25e187428fbe68a7811c897ad1c04f93697b
Richard Hanna <supertanuki@gmail.com>
2019-04-24 17:23:19 +0200

    1945 - Add contact entity

---
+++ src/Domain/Model/Contact.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Proximum\Vimeet\Domain\Model;
+
+class Contact
+{
+    /** @var Event */
+    private $event;
+
+    /** @var User */
+    private $user;
+
+    /** @var User */
+    private $contact;
+
+    /** @var \DateTimeInterface */
+    private $createdAt;
+
+    public function __construct(Event $event, User $user, User $contact, \DateTimeInterface $createdAt)
+    {
+        $this->event = $event;
+        $this->user = $user;
+        $this->contact = $contact;
+        $this->createdAt = $createdAt;
+    }
+
+    public function getEvent(): Event
+    {
+        return $this->event;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function getContact(): User
+    {
+        return $this->contact;
+    }
+
+    public function getCreatedAt(): \DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+}
