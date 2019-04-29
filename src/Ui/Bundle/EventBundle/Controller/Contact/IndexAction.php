691fd5d3fca866d8a2e65efdcf4eadac5d559d7a
Ricardo Coelho <coelho.ric@gmail.com>
2019-04-29 11:30:17 +0200

    1951 - add security checks

--- src/Ui/Bundle/EventBundle/Controller/Contact/IndexAction.php
+++ src/Ui/Bundle/EventBundle/Controller/Contact/IndexAction.php
@@ -2,14 +2,17 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\Contact;
 
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
 use Proximum\Vimeet\Application\Adapter\QueryBusInterface;
 use Proximum\Vimeet\Application\Query\Contact\ContactPreviewView;
 use Proximum\Vimeet\Application\Query\Contact\GetContactListViewQuery;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\Security\SheetVoter;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ValueResolver\UserDomain;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Templating\EngineInterface;
 
 class IndexAction
@@ -17,17 +20,20 @@ class IndexAction
     /** @var EngineInterface */
     private $engine;
 
-    /**
-     * @var QueryBusInterface
-     */
+    /** @var QueryBusInterface */
     private $queryBus;
+
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationCheckerAdapter;
 
     public function __construct(
         QueryBusInterface $queryBus,
-        EngineInterface $engine
+        EngineInterface $engine,
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter
     ) {
         $this->engine = $engine;
         $this->queryBus = $queryBus;
+        $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
     }
 
     public function __invoke(
@@ -36,6 +42,12 @@ class IndexAction
         UserDomain $userDomain,
         Sheet $sheet
     ) {
+        if (!$this->authorizationCheckerAdapter->isGranted('IS_AUTHENTICATED_REMEMBERED')
+            || !$this->authorizationCheckerAdapter->isGranted(SheetVoter::EDIT, $sheet)
+        ) {
+            throw new AccessDeniedException();
+        }
+
         $participant = $sheet->getUserParticipant($userDomain->getUser());
 
         /** @var ContactPreviewView[] $contactListView */
