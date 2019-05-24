0953a7f3150ce037ac346d562672e11d730092f4
taner <tamer@crea2.pro>
2019-05-24 09:45:50 +0200

    1968 - code review

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20190523123530.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20190523123530.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * is payment required for type ?
  */
 class Version20190523123530 extends AbstractMigration
 {
