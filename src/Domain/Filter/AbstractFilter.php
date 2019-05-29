77803147a11b425e2dd7bccf90e43b1ba2e4b902
Richard <supertanuki@gmail.com>
2019-05-29 11:27:58 +0200

    1940 - Depend on SessionInterface

---
+++ src/Domain/Filter/AbstractFilter.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\Filter;
+
+use Proximum\Vimeet\Application\Adapter\SessionInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+
+abstract class AbstractFilter
+{
+    /** @var SessionInterface */
+    private $session;
+
+    abstract function getName(): string;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    /**
+     * @param Event $event
+     *
+     * @return array|null
+     */
+    public function get(Event $event)
+    {
+        return $this->session->get($this->getKey($event));
+    }
+
+    /**
+     * @param Event $event
+     * @param array $filters
+     */
+    public function add(Event $event, array $filters)
+    {
+        $this->session->set($this->getKey($event),
+            array_filter($filters, function ($filter) {
+                return null !== $filter;
+            })
+        );
+    }
+
+    /**
+     * @param Event $event
+     */
+    public function clear(Event $event)
+    {
+        $this->session->remove($this->getKey($event));
+    }
+
+    /**
+     * @param Event $event
+     *
+     * @return string
+     */
+    private function getKey(Event $event)
+    {
+        return sprintf('%s_%s', $this->getName(), $event->getId());
+    }
+}
