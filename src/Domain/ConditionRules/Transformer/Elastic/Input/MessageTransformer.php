304e91fe124237e139fe46bfd5320ad1fefe50b3
taner <tamer@crea2.pro>
2019-06-07 14:36:19 +0200

    1250 - Message Transform

---
+++ src/Domain/ConditionRules/Transformer/Elastic/Input/MessageTransformer.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Domain\ConditionRules\Transformer\Elastic\Input;
+
+use Proximum\Vimeet\Application\Adapter\ElasticSearch\TypesMapping;
+use Proximum\Vimeet\Domain\ConditionRules\View\ComparisonOperator\ComparisonOperatorNotIn;
+use Proximum\Vimeet\Domain\ConditionRules\View\Field;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class MessageTransformer implements InputTransformerInterface
+{
+    /** @var Event */
+    private $event;
+
+    /** @var string */
+    private $locale;
+
+    public function setEventAndLocale(Event $event, string $locale): void
+    {
+        $this->event = $event;
+        $this->locale = $locale;
+    }
+
+    public function transform(Field $field): array
+    {
+        if (!$this->supports($field)) {
+            return [];
+        }
+
+        $messageIds = $field->getValue();
+
+        $query = $this->buildNestedMessageIdsQuery($messageIds);
+
+        if ($this->isContraryComparisonOperator($field)) {
+            return [
+                'bool' => [
+                    'must_not' => $query,
+                ],
+            ];
+        }
+
+        return $query;
+    }
+
+    private function buildNestedMessageIdsQuery(array $messageIds): array
+    {
+        $query = [];
+
+        foreach ($messageIds as $messageId) {
+            $query['bool']['should'][] = [
+                'match' => [
+                    sprintf('%s.id', TypesMapping::SHEET_MESSAGES_RECEIVED) => $messageId,
+                ],
+            ];
+        }
+
+        return [
+            'nested' => [
+                'path'  => TypesMapping::SHEET_MESSAGES_RECEIVED,
+                'query' => $query,
+            ],
+        ];
+    }
+
+    public function supports(Field $field): bool
+    {
+        return 'messagesReceived' === $field->getField();
+    }
+
+    private function isContraryComparisonOperator(Field $field): bool
+    {
+        return $field->getComparisonOperator() instanceof ComparisonOperatorNotIn;
+    }
+}
---
+++ tests/Domain/ConditionRules/Transformer/Elastic/Input/MessageTransformerTest.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Tests\Domain\ConditionRules\Transformer\Elastic\Input;
+
+use PHPUnit\Framework\TestCase;
+use Proximum\Vimeet\Domain\ConditionRules\Transformer\Elastic\Input\MessageTransformer;
+use Proximum\Vimeet\Domain\ConditionRules\View\ComparisonOperator\ComparisonOperatorIn;
+use Proximum\Vimeet\Domain\ConditionRules\View\Field;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class MessageTransformerTest extends TestCase
+{
+    public function testTransform(): void
+    {
+        $field = new Field('messagesReceived', new ComparisonOperatorIn(), 'select', [1, 9]);
+        $event = $this->prophesize(Event::class);
+
+        $messageTransformer = new MessageTransformer();
+        $messageTransformer->setEventAndLocale($event->reveal(), 'fr');
+
+        $expectedResult = [
+            'nested' => [
+                'path' => 'messagesReceived',
+                'query' => [
+                    'bool' => [
+                        'should' => [
+                            [
+                                'match' => [
+                                    'messagesReceived.id' => 1
+                                ]
+                            ],
+                            [
+                                'match' => [
+                                    'messagesReceived.id' => 9
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+        $this->assertSame($messageTransformer->transform($field), $expectedResult);
+    }
+}
