5444d3f37589ac9a4e31afd64a05ebe725130800
Ricardo Coelho <coelho.ric@gmail.com>
2019-06-24 13:55:52 +0200

    2001 - message if checkin already done

--- src/Ui/Bundle/AdminBundle/Controller/Event/FastCheckin/CheckinActionsAction.php
+++ src/Ui/Bundle/AdminBundle/Controller/Event/FastCheckin/CheckinActionsAction.php
@@ -5,6 +5,7 @@ namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Event\FastCheckin;
 use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
 use Proximum\Vimeet\Domain\Model\Event;
 use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Repository\ScanRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -17,12 +18,22 @@ class CheckinActionsAction
      */
     private $authorizationCheckerAdapter;
 
+    /** @var ScanRepositoryInterface */
+    private $scanRepository;
+
+    /** @var \DateTimeInterface */
+    private $dateTime;
+
     public function __construct(
         EngineInterface $engine,
-        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
+        ScanRepositoryInterface $scanRepository,
+        \DateTimeInterface $dateTime
     ) {
         $this->engine = $engine;
         $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
+        $this->scanRepository = $scanRepository;
+        $this->dateTime = $dateTime;
     }
 
     public function __invoke(Event $event, User $user)
@@ -31,12 +42,22 @@ class CheckinActionsAction
             throw new AccessDeniedException('Access denied');
         }
 
+        $checkinDate = $this->getUserCheckinDate($event, $user);
+
         return $this->engine->renderResponse(
             '@Admin/Event/checkinUser.html.twig',
             [
                 'event' => $event,
                 'user' => $user,
+                'checkinDate' => $checkinDate,
             ]
         );
+    }
+
+    private function getUserCheckinDate(Event $event, User $user): ?\DateTime
+    {
+        $checkinDates = $this->scanRepository->getScanDateByUsersAndEvent([$user], $event, $this->dateTime);
+
+        return isset($checkinDates[$user->getId()]) ? $checkinDates[$user->getId()]->getScannedAt() : null;
     }
 }
