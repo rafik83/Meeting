27151903873649a1362c6e91dc6a44034f214480
Richard <supertanuki@gmail.com>
2019-09-04 10:59:10 +0200

    2019 - Impersonate as every user of a participation sheet

--- src/Application/Query/Sheet/Detail/SheetDetailQuery.php
+++ src/Application/Query/Sheet/Detail/SheetDetailQuery.php
@@ -11,6 +11,7 @@
 namespace Proximum\Vimeet\Application\Query\Sheet\Detail;
 
 use Proximum\Vimeet\Application\Query\Query;
+use Proximum\Vimeet\Domain\Model\Admin;
 use Proximum\Vimeet\Domain\Model\Sheet;
 
 class SheetDetailQuery implements Query
@@ -21,13 +22,13 @@ class SheetDetailQuery implements Query
     /** @var string */
     public $locale;
 
-    /**
-     * @param Sheet  $sheet
-     * @param string $locale
-     */
-    public function __construct(Sheet $sheet, string $locale)
+    /** @var Admin */
+    public $admin;
+
+    public function __construct(Admin $admin, Sheet $sheet, string $locale)
     {
         $this->sheet  = $sheet;
         $this->locale = $locale;
+        $this->admin = $admin;
     }
 }
