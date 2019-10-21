758a0accde20d31b1a62acdde25869d074e6ea69
Richard <supertanuki@gmail.com>
2019-10-21 17:10:15 +0200

    1998 - Replace old dashboard controller by ADR pattern

---
+++ src/Ui/Bundle/AdminBundle/Controller/DashboardAction.php
@@ -0,0 +1,64 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller;
+
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Application\Query\Dashboard\DashboardViewQuery;
+use Proximum\Vimeet\Application\Query\Dashboard\DashboardViewQueryHandler;
+use Proximum\Vimeet\Domain\Model\Event;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Templating\EngineInterface;
+
+class DashboardAction
+{
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationChecker;
+
+    /** @var DashboardViewQueryHandler */
+    private $dashboardViewQueryHandler;
+
+    /** @var EngineInterface */
+    private $engine;
+
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationChecker,
+        DashboardViewQueryHandler $dashboardViewQueryHandler,
+        EngineInterface $engine
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
+        $this->dashboardViewQueryHandler = $dashboardViewQueryHandler;
+        $this->engine = $engine;
+    }
+
+    /**
+     * @param Request $request
+     * @param Event   $event
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Event $event): Response
+    {
+        if (!$this->authorizationChecker->isGranted('PERMISSION_EVENT_ACCESS', $event)) {
+            throw new AccessDeniedException();
+        }
+
+        $view = $this->dashboardViewQueryHandler->handle(
+            new DashboardViewQuery($event, $event->getAvailableLocale($request->getLocale()))
+        );
+
+        return new Response($this->engine->render('AdminBundle:Event/Dashboard:index.html.twig', [
+            'event'     => $event,
+            'dashboard' => $view,
+        ]));
+    }
+}
