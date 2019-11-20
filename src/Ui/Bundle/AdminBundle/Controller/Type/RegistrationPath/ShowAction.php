35c0284253ce40690ebabdce35d8234cc03b42a7
Richard <supertanuki@gmail.com>
2019-11-20 11:02:35 +0100

    2028 - Add route and controller for registration path config

---
+++ src/Ui/Bundle/AdminBundle/Controller/Type/RegistrationPath/ShowAction.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\Type\RegistrationPath;
+
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Application\Adapter\QueryBusInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class ShowAction
+{
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationCheckerAdapter;
+
+    /** @var QueryBusInterface */
+    private $queryBus;
+
+    /** @var EngineInterface */
+    private $engine;
+
+    /**
+     * @param AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter
+     * @param QueryBusInterface                    $queryBus
+     * @param EngineInterface                      $engine
+     */
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
+        QueryBusInterface $queryBus,
+        EngineInterface $engine
+    ) {
+        $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
+        $this->queryBus = $queryBus;
+        $this->engine = $engine;
+    }
+
+    /**
+     * @param Request $request
+     * @param Event   $event
+     *
+     * @throws AccessDeniedException
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Event $event): Response
+    {
+        if (!$this->authorizationCheckerAdapter->isGranted('PERMISSION_EVENT_ACCESS', $event)) {
+            throw new AccessDeniedException('Access denied');
+        }
+
+        //$typeListsView = $this->queryBus->handle($typeViewQuery);
+
+        return $this->engine->renderResponse('@Admin/Type/RegistrationPath/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+}
