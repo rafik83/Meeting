967b5405b49022fd9030c9da165d62a73883b307
Richard <supertanuki@gmail.com>
2019-11-26 16:15:04 +0100

    2028 - Question previous answer can be null

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20191126151234.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20191126151234.php
@@ -6,15 +6,15 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Add previous_answer to question
+ * Add previous_answer_id in Question
  */
-class Version20191126105543 extends AbstractMigration
+class Version20191126151234 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE registration_path_question ADD previous_answer_id INT NOT NULL');
+        $this->addSql('ALTER TABLE registration_path_question ADD previous_answer_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE registration_path_question ADD CONSTRAINT FK_682D9F65AD416706 FOREIGN KEY (previous_answer_id) REFERENCES registration_path_answer (id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_682D9F65AD416706 ON registration_path_question (previous_answer_id)');
     }
