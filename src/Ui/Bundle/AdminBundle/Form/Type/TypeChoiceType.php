e0335008db0756730b5c4a6e1205e7616fc1e3bc
Richard <supertanuki@gmail.com>
2019-12-19 17:04:50 +0100

    2026 - Avoid to select hidden types

--- src/Ui/Bundle/AdminBundle/Form/Type/TypeChoiceType.php
+++ src/Ui/Bundle/AdminBundle/Form/Type/TypeChoiceType.php
@@ -39,9 +39,10 @@ class TypeChoiceType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setRequired(['event', 'locale', 'user']);
+        $resolver->setRequired(['event', 'locale', 'user', 'exceptHidden']);
         $resolver->setDefined(['orderByTitle']);
         $resolver->addAllowedTypes('orderByTitle', 'bool');
+        $resolver->addAllowedTypes('exceptHidden', 'bool');
         $resolver->setDefaults([
             'orderByTitle' => false,
             'choice_label' => function (Options $options) {
@@ -82,6 +83,16 @@ class TypeChoiceType extends AbstractType
                                 mb_strtolower($typeB->getTitle($locale));
                         }
                     );
+                }
+
+                if ($options['exceptHidden']) {
+                    foreach ($types as $key => $type) {
+                        if ($type->isHidden()) {
+                            unset($types[$key]);
+                        }
+                    }
+
+                    $types = array_values($types);
                 }
 
                 return $types;
