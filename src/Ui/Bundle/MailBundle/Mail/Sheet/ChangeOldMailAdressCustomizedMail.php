cf1652e8e6916514b2bb48a5d914abef25f45c36
Thomas <thomas77139@gmail.com>
2020-02-06 15:02:08 +0100

    send old mail Okay

---
+++ src/Ui/Bundle/MailBundle/Mail/Sheet/ChangeOldMailAdressCustomizedMail.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * This file is part of the Proximum Vimeet project.
+ *
+ * Copyright (C) Proximum
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace Proximum\Vimeet\Ui\Bundle\MailBundle\Mail\Sheet;
+
+use Proximum\Vimeet\Application\Components\Mail\AbstractCustomizedMail;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class ChangeOldMailAdressCustomizedMail extends AbstractCustomizedMail
+{
+    public const TEMPLATE = 'MailBundle:Mail:base.html.twig';
+
+    /** @var string */
+    protected $template = self::TEMPLATE;
+
+    /**
+     * @var string
+     */
+    private $newMail;
+
+    /**
+     * @var bool
+     */
+    protected $sendToEmailTeam = false;
+
+    public function __construct(
+        Event $event,
+        string $sender,
+        string $receiver,
+        string $locale,
+        string $newMail,
+        string $subject,
+        string $content
+    ) {
+        parent::__construct($event, $sender, $receiver, $locale);
+
+        $this->newMail = $newMail;
+        $this->subject = $subject;
+        $this->content = $content;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNewMail()
+    {
+        return $this->newMail;
+    }
+}
