91e09e4af1f9713236939124700f9ac20a0b82db
Richard <supertanuki@gmail.com>
2020-02-17 17:24:34 +0100

    2039 - Fix meeting modal handle

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/agenda/_AgendaAllSheet.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/agenda/_AgendaAllSheet.js
@@ -2,12 +2,13 @@ var Meet = require('./_Meet');
 
 function AgendaAllSheet(element) {
     this.element = element;
-    this.moveMeetingModal = document.getElementById('meeting-modal');
+    this.meetingModal = document.getElementById('meeting-modal');
+    this.addMeet = this.addMeet.bind(this);
     Array.prototype.forEach.call(this.element.querySelectorAll('.meet'), this.addMeet);
 }
 
 AgendaAllSheet.prototype.addMeet = function (element) {
-    new Meet(null, element, this.moveMeetingModal);
+    new Meet(null, element, this.meetingModal);
 };
 
 module.exports = AgendaAllSheet;
