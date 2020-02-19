e87f10593034d7adb6a43d88f0259e65ff78e8d1
Richard <supertanuki@gmail.com>
2020-02-19 16:31:21 +0100

    2039 - Add controller access checks

--- src/Ui/Bundle/EventBundle/Controller/Agenda/SheetAgendaAction.php
+++ src/Ui/Bundle/EventBundle/Controller/Agenda/SheetAgendaAction.php
@@ -2,28 +2,39 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\Agenda;
 
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
 use Proximum\Vimeet\Application\Adapter\QueryBusInterface;
 use Proximum\Vimeet\Application\Query\Agenda\AgendaViewQuery;
 use Proximum\Vimeet\Application\Query\Tip\TipTranslationViewQuery;
 use Proximum\Vimeet\Application\Query\Tip\TipTranslationViewQueryHandler;
 use Proximum\Vimeet\Application\View\Agenda\AgendaView;
 use Proximum\Vimeet\Domain\Model\Sheet;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Security\Voter\AgendaAccessVoter;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\Security\SheetVoter;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ValueResolver\UserDomain;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Templating\EngineInterface;
 
 class SheetAgendaAction
 {
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationChecker;
+
     /** @var QueryBusInterface */
     private $queryBus;
 
     /** @var EngineInterface */
     private $engine;
 
-    public function __construct(QueryBusInterface $queryBus, EngineInterface $engine)
-    {
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationChecker,
+        QueryBusInterface $queryBus,
+        EngineInterface $engine
+    ) {
+        $this->authorizationChecker = $authorizationChecker;
         $this->queryBus = $queryBus;
         $this->engine = $engine;
     }
@@ -35,6 +46,13 @@ class SheetAgendaAction
         UserDomain $userDomain
     ): Response {
         $event = $eventDomain->getEvent();
+
+        if (!$this->authorizationChecker->isGranted(SheetVoter::EDIT, $sheet)
+            || !$this->authorizationChecker->isGranted(AgendaAccessVoter::PERMISSION, $event)
+        ) {
+            throw new AccessDeniedException();
+        }
+
         $user = $userDomain->getUser();
         $participant = $sheet->getUserParticipant($user);
 
