ca0837d6f7bed8f86117183d2c214ac36a6fd751
Nico <nicolas.dievart@elao.com>
2020-04-15 15:29:25 +0200

    2078 : Rewrite keywords transformer to search on content and mutualize event and admin code

--- src/Domain/ConditionRules/Transformer/Elastic/Input/KeywordTransformer.php
+++ src/Domain/ConditionRules/Transformer/Elastic/Input/KeywordTransformer.php
@@ -3,43 +3,33 @@
 
 namespace Proximum\Vimeet\Domain\ConditionRules\Transformer\Elastic\Input;
 
-use Proximum\Vimeet\Application\Adapter\ElasticSearch\TypesMapping;
 use Proximum\Vimeet\Domain\ConditionRules\View\Field;
+use Proximum\Vimeet\Domain\Search\ContentQueryBuilderInterface;
 
+/**
+ * This class is used to search of keyword on the sheets.
+ * It creates a query on the sheet name, the content and the content localized
+ * based on the equivalent request made by the Catalog Search.
+ */
 class KeywordTransformer implements InputTransformerInterface
 {
+    /**
+     * @var ContentQueryBuilderInterface
+     */
+    private $contentQueryBuilder;
+
+    public function __construct(ContentQueryBuilderInterface $contentQueryBuilder)
+    {
+        $this->contentQueryBuilder = $contentQueryBuilder;
+    }
+
     public function transform(Field $field): array
     {
         if (!$this->supports($field)) {
             return [];
         }
 
-        return $this->getQuery($field);
-    }
-
-    private function getQuery(Field $field): array
-    {
-        $query = [
-            'query_string' => [
-                'default_field' => (TypesMapping::SEARCH_MAPPING[$field->getField()]['path'] ?? '').'.label',
-                'query' => $this->getFilterQuery($field),
-                'default_operator' => 'AND',
-            ],
-        ];
-
-        return [
-            'nested' => [
-                'path' => TypesMapping::SEARCH_MAPPING[$field->getField()]['path'] ?? '',
-                'query' => $query,
-            ],
-        ];
-    }
-
-    private function getFilterQuery(Field $field): string
-    {
-        $cleanValue = preg_replace(TextTransformer::CLEAN_SEARCH_FIELD_REGEX, ' ', $field->getValue());
-
-        return '*'.$cleanValue.'*';
+        return $this->contentQueryBuilder->getQuery($field->getValue(), $field->getLocale());
     }
 
     public function supports(Field $field): bool
