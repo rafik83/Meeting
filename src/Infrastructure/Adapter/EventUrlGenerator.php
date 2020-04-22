ae90de5fbfbedb68495d185cb270165476c883ee
Thomas <thomas77139@gmail.com>
2020-04-22 16:21:31 +0200

    VIMEET-2050 - add image on email

--- src/Infrastructure/Adapter/EventUrlGenerator.php
+++ src/Infrastructure/Adapter/EventUrlGenerator.php
@@ -45,6 +45,16 @@ class EventUrlGenerator implements EventUrlGeneratorInterface
         );
     }
 
+    public function generateImageAbsoluteUrl(Event $event, string $locale)
+    {
+        return sprintf(
+            '%s://%s%s',
+            $this->scheme,
+            $event->getDomain(),
+            $event->getLocalizedNotificationImage($locale)
+        );
+    }
+
     public function generateBaseEventAbsoluteUrl(Event $event): string
     {
         return sprintf(
