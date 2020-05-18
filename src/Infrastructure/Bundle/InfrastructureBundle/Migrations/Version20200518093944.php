3a0e5047aa91961aa8393551b29e36fd69c63758
Richard <supertanuki@gmail.com>
2020-05-18 11:50:28 +0200

    2088 - Add rank to Participant

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20200518093944.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Add rank to Participant
+ */
+final class Version20200518093944 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE participant ADD rank INT DEFAULT 0 NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE participant DROP rank');
+    }
+}
