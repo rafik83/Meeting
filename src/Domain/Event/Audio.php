b6ca91fec6594ceb8d721bac4122cd0a36549b50
Nico <nicolas.dievart@elao.com>
2020-05-19 17:44:25 +0200

    2101 - Allow wav file

--- src/Domain/Event/Audio.php
+++ src/Domain/Event/Audio.php
@@ -5,6 +5,8 @@ namespace Proximum\Vimeet\Domain\Event;
 final class Audio
 {
     public const SUPPORTED_MIME_TYPE = [
+        'audio/wav',
+        'audio/x-wav',
         'audio/webm',
         'audio/mpeg',
         'audio/mp3',
