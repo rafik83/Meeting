828497c69640bfab5862a7bc550ff070cd65aec7
Richard <supertanuki@gmail.com>
2020-06-24 14:14:57 +0200

    mv-240 - Notify admin user with the file link

--- src/Application/Command/Happening/Export/NotifyHandler.php
+++ src/Application/Command/Happening/Export/NotifyHandler.php
@@ -3,32 +3,65 @@
 namespace Proximum\Vimeet\Application\Command\Happening\Export;
 
 use Proximum\Vimeet\Application\Adapter\MailerInterface;
-use Proximum\Vimeet\Ui\Bundle\MailBundle\Mail\OMZ\NotifyOMZExportMail;
+use Proximum\Vimeet\Application\Adapter\RouterInterface;
+use Proximum\Vimeet\Application\Adapter\TranslatorInterface;
+use Proximum\Vimeet\Ui\Bundle\MailBundle\Mail\Admin\Notification\NotifyAdmin;
 
 class NotifyHandler
 {
     /** @var MailerInterface */
     private $mailer;
 
+    /** @var RouterInterface */
+    private $router;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
     /** @var string */
     private $sender;
 
-    public function __construct(MailerInterface $mailer, string $sender)
-    {
+    public function __construct(
+        MailerInterface $mailer,
+        RouterInterface $router,
+        TranslatorInterface $translator,
+        string $sender
+    ) {
         $this->mailer = $mailer;
+        $this->router = $router;
+        $this->translator = $translator;
         $this->sender = $sender;
     }
 
     public function handle(Notify $command): void
     {
-        // @todo: change the mail and add a download file controller
-        $mail = new NotifyOMZExportMail(
-            $command->event,
+        $mail = new NotifyAdmin(
+            $this->translator->trans(
+                'admin.mail.export_happening_participants.subject',
+                ['%event%' => $command->event->getTitle()],
+                'mail',
+                $command->locale
+            ),
             $this->sender,
             $command->admin->getEmail(),
             $command->locale,
-            $command->file->getHash(),
-            $command->file->getId()
+            $this->translator->trans('admin.mail.export_happening_participants.content', [], 'mail', $command->locale),
+            $this->router->generate(
+                'admin_file_download',
+                [
+                    'event' => $command->event->getId(),
+                    'hash' => $command->file->getHash(),
+                    'file' => $command->file->getId()
+                ]
+            ),
+            $this->translator->trans(
+                'admin.mail.export_happening_participants.linkTitle',
+                [],
+                'mail',
+                $command->locale
+            ),
+            null,
+            $command->admin
         );
 
         $this->mailer->send($mail);
---
+++ src/Ui/Bundle/AdminBundle/Controller/File/DownloadAction.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Proximum\Vimeet\Ui\Bundle\AdminBundle\Controller\File;
+
+use Proximum\Vimeet\Application\Adapter\AuthorizationCheckerAdapterInterface;
+use Proximum\Vimeet\Application\Adapter\FileSystemAdapterInterface;
+use Proximum\Vimeet\Domain\Model\Event;
+use Proximum\Vimeet\Domain\Model\File;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\HttpFoundation\Response\CsvFileResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class DownloadAction
+{
+    /** @var AuthorizationCheckerAdapterInterface */
+    private $authorizationCheckerAdapter;
+
+    /** @var string */
+    private $path;
+
+    /** @var FileSystemAdapterInterface */
+    private $fileSystemAdapter;
+
+    public function __construct(
+        AuthorizationCheckerAdapterInterface $authorizationCheckerAdapter,
+        FileSystemAdapterInterface $fileSystemAdapter,
+        string $path
+    ) {
+        $this->authorizationCheckerAdapter = $authorizationCheckerAdapter;
+        $this->fileSystemAdapter = $fileSystemAdapter;
+        $this->path = $path;
+    }
+
+    public function __invoke(Event $event, string $hash, File $file): Response
+    {
+        if (!$this->authorizationCheckerAdapter->isGranted('PERMISSION_EVENT_ACCESS', $event)
+            || !$this->authorizationCheckerAdapter->isGranted('ROLE_ALLOWED_TO_ORGANIZE')
+        ) {
+            throw new AccessDeniedException('Access Denied for this page');
+        }
+
+        if ($file->getHash() !== $hash) {
+            throw new NotFoundHttpException(
+                sprintf('File %s has a different hash from the one given %s', $file->getId(), $hash)
+            );
+        }
+
+        $path = sprintf('%s%s', $this->path, $file->getPath());
+
+        if (!$this->fileSystemAdapter->exists($path)) {
+            throw new NotFoundHttpException(sprintf('File %s not found', $file->getId()));
+        }
+
+        if ('csv' === pathinfo($path, PATHINFO_EXTENSION)) {
+            return new CsvFileResponse(file_get_contents($path), basename($path));
+        }
+
+        $response = new Response(file_get_contents($path));
+        $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, basename($path));
+        $response->headers->set('Content-Disposition', $disposition);
+
+        return $response;
+    }
+}
