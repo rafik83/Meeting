5599a3add1a6c4682355b035c1cdd7fe0c4f04be
Nico <nicolas.dievart@elao.com>
2020-07-03 16:21:10 +0200

    2161 - add record archive

--- src/Application/Command/Happening/Webinar/Record/StopRecordHandler.php
+++ src/Application/Command/Happening/Webinar/Record/StopRecordHandler.php
@@ -3,16 +3,22 @@
 namespace Proximum\Vimeet\Application\Command\Happening\Webinar\Record;
 
 use Proximum\Vimeet\Application\Adapter\VideoConferenceAdapterInterface;
+use Proximum\Vimeet\Domain\Repository\Happening\RecordArchiveRepositoryInterface;
 
 class StopRecordHandler
 {
     /** @var VideoConferenceAdapterInterface */
     private $videoConferenceAdapter;
 
+    /** @var RecordArchiveRepositoryInterface */
+    private $recordArchiveRepository;
+
     public function __construct(
-        VideoConferenceAdapterInterface $videoConferenceAdapter
+        VideoConferenceAdapterInterface $videoConferenceAdapter,
+        RecordArchiveRepositoryInterface $recordArchiveRepository
     ) {
         $this->videoConferenceAdapter = $videoConferenceAdapter;
+        $this->recordArchiveRepository = $recordArchiveRepository;
     }
 
     public function handle(StopRecord $stopRecord): void
---
+++ src/Infrastructure/Repository/Happening/Webinar/RecordArchiveRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Repository\Happening\Webinar;
+
+use Doctrine\ORM\EntityManager;
+use Proximum\Vimeet\Domain\Happening\Webinar\RecordStatus;
+use Proximum\Vimeet\Domain\Model\Happening;
+use Proximum\Vimeet\Domain\Model\Happening\Webinar\RecordArchive;
+use Proximum\Vimeet\Domain\Repository\Happening\RecordArchiveRepositoryInterface;
+
+class RecordArchiveRepository implements RecordArchiveRepositoryInterface
+{
+    /** @var EntityManager */
+    private $entityManager;
+
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function add(RecordArchive $recordArchive): void
+    {
+        $this->entityManager->persist($recordArchive);
+        $this->entityManager->flush($recordArchive);
+    }
+
+    public function getStartedRecordArchiveForHappening(Happening $happening): array
+    {
+        return $this->entityManager
+            ->createQueryBuilder()
+            ->select('recordArchive')
+            ->from(RecordArchive::class, 'recordArchive')
+            ->where('recordArchive.happening = :happening')
+            ->andWhere('recordArchive.status = :status')
+            ->setParameter('happening', $happening)
+            ->setParameter('status', RecordStatus::STARTED)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+}
