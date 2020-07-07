6cbe73c9aa0d05c53a32deed295f459f777539a5
Nico <nicolas.dievart@elao.com>
2020-07-07 12:39:38 +0200

    2161 - Fix name change

--- src/Infrastructure/Bundle/InfrastructureBundle/Adapter/Happening/Webinar/RecordJobQueueAdapter.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Adapter/Happening/Webinar/RecordJobQueueAdapter.php
@@ -5,21 +5,21 @@ namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Adapter\Hap
 use JMS\JobQueueBundle\Entity\Job;
 use Proximum\Vimeet\Application\Adapter\Happening\Webinar\RecordJobQueueInterface;
 use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Adapter\AbstractJobQueueAdapter;
-use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command\Happening\Webinar\Record\PrepareReconciliationCommand;
+use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command\Happening\Webinar\Record\ReconciliationCommand;
 
 class RecordJobQueueAdapter extends AbstractJobQueueAdapter implements RecordJobQueueInterface
 {
     public function removeReconciliation(int $happeningId): void
     {
         $this->removeJob(
-            PrepareReconciliationCommand::NAME,
+            ReconciliationCommand::NAME,
             [$happeningId]
         );
     }
 
     public function prepareReconciliation(int $happeningId, \DateTimeInterface $reconciliationDate): void
     {
-        $command = PrepareReconciliationCommand::NAME;
+        $command = ReconciliationCommand::NAME;
         $args = [$happeningId];
 
         $this->removeJob($command, $args);
