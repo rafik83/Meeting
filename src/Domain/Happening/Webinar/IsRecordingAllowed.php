3744641dafcfae246cf2051147a3cc2a43aa4bb1
Nico <nicolas.dievart@elao.com>
2020-07-24 15:20:14 +0200

    2161 - Disable recording action on webinar after 30 minutes

---
+++ src/Domain/Happening/Webinar/IsRecordingAllowed.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Proximum\Vimeet\Domain\Happening\Webinar;
+
+use Proximum\Vimeet\Domain\Model\Happening;
+
+class IsRecordingAllowed
+{
+    /** @var \DateTimeInterface */
+    private $dateTime;
+
+    public function __construct(\DateTimeInterface $dateTime)
+    {
+        $this->dateTime = $dateTime;
+    }
+
+    public function isSatisfiedBy(Happening $happening): bool
+    {
+        if (!$happening->isWebinarRecorded()) {
+            return false;
+        }
+
+        $webinarEnd = $happening->getEnd();
+        $webinarEndPlus30Minutes = new \DateTime();
+        $webinarEndPlus30Minutes->setTimestamp($webinarEnd->getTimestamp());
+        $webinarEndPlus30Minutes->modify('+30 minutes');
+
+        return $webinarEndPlus30Minutes > $this->dateTime;
+    }
+}
