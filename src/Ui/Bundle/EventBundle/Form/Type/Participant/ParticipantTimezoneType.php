a4089df0cd1f477b75296b2fa90ca01aeb7b2e23
Richard <supertanuki@gmail.com>
2020-08-26 12:17:59 +0200

    2213 - Custome timezone label in form

--- src/Ui/Bundle/EventBundle/Form/Type/Participant/ParticipantTimezoneType.php
+++ src/Ui/Bundle/EventBundle/Form/Type/Participant/ParticipantTimezoneType.php
@@ -10,6 +10,7 @@
 
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Form\Type\Participant;
 
+use Proximum\Vimeet\Application\Adapter\TranslatorInterface;
 use Proximum\Vimeet\Application\Command\Participant\ParticipantTimezone;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TimezoneType;
@@ -18,9 +19,20 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ParticipantTimezoneType extends AbstractType
 {
+    /** @var TranslatorInterface */
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('timezone', TimezoneType::class, [
+            'choice_label' => function ($key, $label) {
+                return $this->translator->trans('timezone.' . strtolower(str_replace('/', '-', $key)));
+            },
             'attr' => [
                 'class' => 'select2',
             ]
