978f77e592e35c865bd07697c04185ca972b269c
Nico <nicolas.dievart@elao.com>
2020-09-02 19:23:04 +0200

    Media count init

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Command/MediaResourceCountCommand.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Command;
+
+use Proximum\Vimeet\Domain\Repository\EventRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\SheetRepositoryInterface;
+use Proximum\Vimeet\Domain\Repository\TypeRepositoryInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MediaResourceCountCommand extends Command
+{
+    public const NAME = 'vimeet:sheet:count-media-link';
+
+    /** @var SheetRepositoryInterface */
+    private $sheetRepository;
+
+    /** @var EventRepositoryInterface */
+    private $eventRepository;
+    /** @var TypeRepositoryInterface */
+    private $typeRepository;
+
+    public function __construct(
+        SheetRepositoryInterface $sheetRepository,
+        EventRepositoryInterface $eventRepository,
+        TypeRepositoryInterface $typeRepository
+    ) {
+        parent::__construct(self::NAME);
+
+        $this->sheetRepository = $sheetRepository;
+        $this->eventRepository = $eventRepository;
+        $this->typeRepository = $typeRepository;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Count the number of media link on the sheets')
+            ->addArgument(
+                'event',
+                InputArgument::REQUIRED,
+                'Count for the given event'
+            )
+            ->addArgument(
+                'type',
+                InputArgument::REQUIRED,
+                'The type to count on'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $eventId = $input->getArgument('event');
+        // 253
+        $event = $this->eventRepository->getById($eventId);
+
+        $typeId = $input->getArgument('type');
+        // 1552
+        $type = $this->typeRepository->getById($typeId);
+
+        if ($type === null || $type->getEvent() !== $event) {
+            return;
+        }
+
+        $sheets = $this->sheetRepository->getByTypes([$type]);
+
+        foreach ($sheets as $sheet) {
+            $data = $sheet->getData();
+
+            // id to check
+            // M2541Md657
+            // M199fMfc3c
+        }
+    }
+}
