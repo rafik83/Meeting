3e86299aaa999ef4aa1912141fbd7c9f3b71db9b
Volubyl <volubyl@pm.me>
2020-09-30 16:57:17 +0200

    2218 - Add UserConnectionRegister Frontend

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/_UserConnectionRegister.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/_UserConnectionRegister.js
@@ -1,12 +1,34 @@
+import { EventSourcePolyfill } from "event-source-polyfill";
 class UserConnectionRegister {
-    constructor() {
-        // chercher le snippet
-        //si present
-        //  faire son taff
+
+    constructor() { }
+
+    getRegistrationParameters() {
+        const rawData = document.getElementById("user-context");
+        if (!rawData) {
+            return null
+        }
+
+        return JSON.parse(rawData.innerHTML)
     }
 
-    init() {
+    connect() {
+        const registrationsParameters = this.getRegistrationParameters();
 
+        if (!registrationsParameters) {
+            return null
+        }
+
+        const { topic, subscriberKey, providerUrl } = registrationsParameters
+
+        const url = new URL(providerUrl);
+        url.searchParams.append('topic', topic);
+
+        new EventSourcePolyfill(url, {
+            headers: {
+                'Authorization': `Bearer ${subscriberKey}`
+            }
+        });
     }
 }
 export default UserConnectionRegister;
