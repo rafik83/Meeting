5c477c493996b7184e700f631de2b5c3b45e53d6
Nico <nicolas.dievart@elao.com>
2020-10-15 09:59:32 +0200

    2244 - Init configuration mapping with conditions

---
+++ src/Application/ThirdParty/TechEvent/Webservice/Configuration/MappingConfigurationChecker.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Proximum\Vimeet\Application\ThirdParty\TechEvent\Webservice\Configuration;
+
+class MappingConfigurationChecker
+{
+    public function isMappingConfigurationValid($mapping): bool
+    {
+
+    }
+}
