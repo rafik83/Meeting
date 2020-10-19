215815d16744d7770f35c0eac75d90b35a7bc5d7
Emilie <plouzeau@proximumgroup.com>
2020-10-19 08:45:01 +0200

    2223 - Fix code test unit

--- tests/Application/Query/Networking/PrivateChatQueryHandlerTest.php
+++ tests/Application/Query/Networking/PrivateChatQueryHandlerTest.php
@@ -7,6 +7,7 @@ use Proximum\Vimeet\Application\Adapter\NotificationSubscriberInterface;
 use Proximum\Vimeet\Application\Exception\Chat\PrivateChatInvalidToUser;
 use Proximum\Vimeet\Application\Query\Networking\PrivateChatQuery;
 use Proximum\Vimeet\Application\Query\Networking\PrivateChatQueryHandler;
+use Proximum\Vimeet\Domain\KeyDates\Checker\CallVisioPrivateChatAccessChecker;
 use Proximum\Vimeet\Domain\Model\Sheet;
 use Proximum\Vimeet\Domain\Model\User;
 use Proximum\Vimeet\Domain\Repository\ChatSessionRepositoryInterface;
@@ -18,7 +19,8 @@ class PrivateChatQueryHandlerTest extends TestCase
 
         $notificationSubscriber = $this->prophesize(NotificationSubscriberInterface::class);
         $chatSessionRepository = $this->prophesize(ChatSessionRepositoryInterface::class);
-        $privateChatQueryHandler = new PrivateChatQueryHandler($notificationSubscriber->reveal(), $chatSessionRepository->reveal());
+        $callVisioPrivateChatAccessChecker = $this->prophesize(CallVisioPrivateChatAccessChecker::class);
+        $privateChatQueryHandler = new PrivateChatQueryHandler($notificationSubscriber->reveal(), $chatSessionRepository->reveal(), $callVisioPrivateChatAccessChecker->reveal());
         $sheet = $this->prophesize(Sheet::class);
         $user = $this->prophesize(User::class);
         $user->getId()->shouldBeCalled()->willReturn(333);
