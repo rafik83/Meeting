d6640c1d883cf35abf96c5db6dc2a75f920db6b2
Emilie <plouzeau@proximumgroup.com>
2020-10-28 17:00:41 +0100

    2223 - Fix tests unit

--- tests/Application/Query/Networking/PrivateChatQueryHandlerTest.php
+++ tests/Application/Query/Networking/PrivateChatQueryHandlerTest.php
@@ -5,6 +5,7 @@ namespace Proximum\Vimeet\Tests\Application\Query\Networking;
 use PHPUnit\Framework\TestCase;
 use Proximum\Vimeet\Application\Adapter\NotificationSubscriberInterface;
 use Proximum\Vimeet\Application\Exception\Chat\PrivateChatInvalidToUser;
+use Proximum\Vimeet\Application\Query\Networking\IsUserInCallVisio;
 use Proximum\Vimeet\Application\Query\Networking\PrivateChatQuery;
 use Proximum\Vimeet\Application\Query\Networking\PrivateChatQueryHandler;
 use Proximum\Vimeet\Domain\KeyDates\Checker\AskCallVisioPrivateChatAccessChecker;
@@ -20,7 +21,8 @@ class PrivateChatQueryHandlerTest extends TestCase
         $notificationSubscriber = $this->prophesize(NotificationSubscriberInterface::class);
         $chatSessionRepository = $this->prophesize(ChatSessionRepositoryInterface::class);
         $callVisioPrivateChatAccessChecker = $this->prophesize(AskCallVisioPrivateChatAccessChecker::class);
-        $privateChatQueryHandler = new PrivateChatQueryHandler($notificationSubscriber->reveal(), $chatSessionRepository->reveal(), $callVisioPrivateChatAccessChecker->reveal());
+        $isUserInCallVisio = $this->prophesize(IsUserInCallVisio::class);
+        $privateChatQueryHandler = new PrivateChatQueryHandler($notificationSubscriber->reveal(), $chatSessionRepository->reveal(), $callVisioPrivateChatAccessChecker->reveal(), $isUserInCallVisio->reveal());
         $sheet = $this->prophesize(Sheet::class);
         $user = $this->prophesize(User::class);
         $user->getId()->shouldBeCalled()->willReturn(333);
