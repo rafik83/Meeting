3a4c6956a10f78c03cee317a131ec1059e233708
Ricardo Coelho <coelho.ric@gmail.com>
2020-10-29 11:43:31 +0100

    cs

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20200922131934.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20200922131934.php
@@ -6,13 +6,12 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Auto-generated Migration: Please modify to your needs!
+ * Add call visio dates into event configuration
  */
 final class Version20200922131934 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE event ADD configuration_call_visio_open_date DATETIME DEFAULT NULL, ADD configuration_call_visio_close_date DATETIME DEFAULT NULL');
@@ -20,7 +19,6 @@ final class Version20200922131934 extends AbstractMigration
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE event DROP configuration_call_visio_open_date, DROP configuration_call_visio_close_date');
