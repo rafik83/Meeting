9631915e22b7130b2f90133d4c5e979a511d1366
Gregory Pelletier <gregory.pelletier@fairness.coop>
2020-11-02 11:52:04 +0100

    add default value to type properties
    to allow type creation for non super-admin users

--- src/Application/Command/Type/Create.php
+++ src/Application/Command/Type/Create.php
@@ -88,10 +88,10 @@ class Create implements Command
     public $canSubmitValidation = true;
 
     /** @var bool */
-    public $displayAnalyticsOnSheet;
+    public $displayAnalyticsOnSheet = false;
 
     /** @var bool */
-    public $displayAnalyticsOnCatalog;
+    public $displayAnalyticsOnCatalog = false;
 
     public function __construct(Event $event, string $locale)
     {
