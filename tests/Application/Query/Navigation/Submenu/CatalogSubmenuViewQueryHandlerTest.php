5d787966024be2299cb9ce79c1d07858bca18925
BertrandD <volubyl@pm.me>
2020-11-20 16:01:27 +0100

    2261 - Add test to test catalog button

--- tests/Application/Query/Navigation/Submenu/CatalogSubmenuViewQueryHandlerTest.php
+++ tests/Application/Query/Navigation/Submenu/CatalogSubmenuViewQueryHandlerTest.php
@@ -310,7 +310,7 @@ class CatalogSubmenuViewQueryHandlerTest extends TestCase
         $canSeeOtherSheets->isSatisfiedBy($sheet->reveal())->shouldBeCalled()->willReturn(false);
 
         $requestRepository = $this->prophesize(RequestRepositoryInterface::class);
-        $requestRepository->countPendingPropositionReceivedBySheet($sheet->reveal())->shouldBeCalled()->willReturn(666);
+        $requestRepository->countPendingPropositionReceivedBySheet($sheet->reveal())->shouldBeCalled()->willReturn(null);
 
         $handler = new CatalogSubmenuViewQueryHandler($navigationBuilder->reveal(), $dateTime, $canSeeOtherSheets->reveal(), $requestRepository->reveal());
 
@@ -326,7 +326,90 @@ class CatalogSubmenuViewQueryHandlerTest extends TestCase
             $meetingTitle,
             'dummyRoute',
             false,
-            666,
+            null,
+            false
+        );
+
+        $this->assertEquals($result, $expectedResult);
+    }
+
+    public function testShouldReturnTwoSubMenuButtonView()
+    {
+        $sheetId = 666;
+        $sheet = $this->prophesize(Sheet::class);
+        $sheet->isInInternalCatalog()->willReturn(true);
+        $sheet->getId()->shouldBeCalled()->willReturn($sheetId);
+        $sheet->hasLinkedSheets()->shouldBeCalled()->willReturn(false);
+
+        $user = $this->prophesize(User::class);
+        $user->getId()->willReturn(42);
+
+        $configuration = $this->prophesize(Configuration::class);
+
+        $dateTime = new \DateTime('2020-03-14T03:14:15');
+        $catalogOnlineDate = new \DateTime('2019-01-14T03:14:15');
+
+
+        $configuration->getCatalogOnlineDate()->shouldBeCalled()->willReturn($catalogOnlineDate);
+
+        $event = $this->prophesize(Event::class);
+        $event->getConfiguration()->shouldBeCalled()->willReturn($configuration);
+
+        $meetingTitle = "WAZAAAAAAAAAAAAA";
+
+        $staticFormulation = $this->prophesize(StaticFormulation::class);
+        $locale = "fr";
+
+        $staticFormulation->getTitle($locale)->shouldBeCalled()->willReturn($meetingTitle);
+
+        $staticFormulationsIndexedByCategory = [Category::MEETING => $staticFormulation->reveal()];
+
+
+        $query = new CatalogSubmenuViewQuery($user->reveal(), $event->reveal(), $locale, $sheet->reveal(), '', $staticFormulationsIndexedByCategory);
+
+        $navigationBuilder = $this->prophesize(NavigationBuilderInterface::class);
+
+        $canSeeOtherSheets  = $this->prophesize(CanSeeOtherSheets::class);
+        $canSeeOtherSheets->isSatisfiedBy($sheet->reveal())->shouldBeCalled()->willReturn(true);
+
+        $requestRepository = $this->prophesize(RequestRepositoryInterface::class);
+        $requestRepository->countPendingPropositionReceivedBySheet($sheet->reveal())->shouldBeCalled()->willReturn(null);
+
+        $navigationBuilder->getRoute('event_meeting_list_request', [
+            'sheet' => $sheetId
+        ])->shouldBeCalled()->willReturn("dummyRoute");
+
+
+        $navigationBuilder->getRoute('event_catalog_index', [
+            'sheet' => $sheetId
+        ])->shouldBeCalled()->willReturn("otherDummyRoute");
+
+
+        $handler = new CatalogSubmenuViewQueryHandler($navigationBuilder->reveal(), $dateTime, $canSeeOtherSheets->reveal(), $requestRepository->reveal());
+
+        $result = $handler->handle(
+            $query
+        );
+
+        $expectedResult = [];
+
+        $catalogTitle = 'navigation.category.catalog';
+        $expectedResult[] = new SubmenuButtonView(
+            Category::CATALOG_ICON,
+            $catalogTitle,
+            'otherDummyRoute',
+            false,
+            null,
+            true
+        );
+
+
+        $expectedResult[] = new SubmenuButtonView(
+            Category::MEETING_ICON,
+            $meetingTitle,
+            'dummyRoute',
+            false,
+            null,
             false
         );
 
