51cd6ef487e0cc017251414ffa81567e3c8b25c9
Gregory Pelletier <gregory.pelletier@fairness.coop>
2020-11-23 08:38:44 +0100

    2234 - add api endpoint to get list of happenings

---
+++ src/Application/Query/Happening/GetApiListQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Proximum\Vimeet\Application\Query\Happening;
+
+use Proximum\Vimeet\Application\Query\Query;
+use Proximum\Vimeet\Domain\Model\Event;
+
+class GetApiListQuery implements Query
+{
+    /** @var Event */
+    public $event;
+
+    /** @var string */
+    public $locale;
+
+    /** @var string */
+    public $baseUrl;
+
+    /** @var array|null */
+    public $participantTypeIds;
+
+    public function __construct(Event $event, string $locale, string $baseUrl, ?array $participantTypeIds)
+    {
+        $this->event = $event;
+        $this->locale = $locale;
+        $this->baseUrl = $baseUrl;
+        $this->participantTypeIds = $participantTypeIds;
+    }
+}
