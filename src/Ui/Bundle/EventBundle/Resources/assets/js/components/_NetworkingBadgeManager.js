d2b03d5cbd980a30b513029d85c8c33f0dac2575
BertrandD <volubyl@pm.me>
2020-12-08 14:39:41 +0100

    2253 - remove usless functions

--- src/Ui/Bundle/EventBundle/Resources/assets/js/components/_NetworkingBadgeManager.js
+++ src/Ui/Bundle/EventBundle/Resources/assets/js/components/_NetworkingBadgeManager.js
@@ -145,34 +145,5 @@ export class NetworkingBadgeManager {
 
         this.chatMessageCountBadges[badgeType].textContent = newValue;
     }
-
-    countNewMessageFromPrivateChatIcons(dataSource) {
-        if (dataSource.length > 0) {
-            return Array.from(dataSource).reduce((prev, current) => {
-                return prev + parseInt(current.dataset.newMessagesCount, 10);
-            }, 0);
-        }
-
-        return 0;
-    }
-
-    appendNewMessageBadgeInHeaderSubmenu(datasource, destinationNodes) {
-        const newMessageCount = this.countNewMessageFromPrivateChatIcons(
-            datasource
-        );
-        if (newMessageCount === 0) {
-            return;
-        }
-
-        const newMessageBadge = document.createElement("span");
-        newMessageBadge.textContent =
-            newMessageCount >= 99 ? "99+" : newMessageCount;
-        newMessageBadge.classList.add("alert-notification");
-
-        destinationNodes.forEach((node) => {
-            const clone = newMessageBadge.cloneNode(true);
-            node.appendChild(clone);
-        });
-    }
 }
 export default NetworkingBadgeManager;
