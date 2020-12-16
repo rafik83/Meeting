bd47216c9629583efccbc0d9ab2ff98fc4b3d992
Gregory Pelletier <gregory.pelletier@fairness.coop>
2020-12-16 06:56:04 +0100

    2191 - add new tests and fix existing

--- tests/Application/Command/Happening/Webinar/Question/DeleteHappeningQuestionHandlerTest.php
+++ tests/Application/Command/Happening/Webinar/Question/DeleteHappeningQuestionHandlerTest.php
@@ -29,12 +29,8 @@ class DeleteHappeningQuestionHandlerTest extends TestCase
     /** @var DeleteHappeningQuestionHandler */
     private $deleteHappeningQuestion;
 
-    /** @var \DateTime */
-    private $datetime;
-
     protected function setUp()
     {
-        $this->datetime = new \DateTime('2020-06-02 12:00:00');
         $this->questionRepository = $this->prophesize(QuestionRepositoryInterface::class);
         $this->notificationPublisher = $this->prophesize(NotificationPublisherInterface::class);
         $this->deleteHappeningQuestion = new DeleteHappeningQuestionHandler(
@@ -56,7 +52,7 @@ class DeleteHappeningQuestionHandlerTest extends TestCase
             $happening->reveal(),
             $sheet,
             $createdBy->reveal(),
-            $this->datetime,
+            new \DateTime('2020-06-02 12:00:00'),
             'Can you develop your point about green IT?',
             true
         );
