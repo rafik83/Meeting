ddd391c93642baad26d69518708f0462238805f1
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-02-24 17:20:40 +0100

    2301 - fix dependency in helper service

--- src/Ui/Helper/CurrencyFormatter.php
+++ src/Ui/Helper/CurrencyFormatter.php
@@ -2,33 +2,19 @@
 
 namespace Proximum\Vimeet\Ui\Helper;
 
+use Twig\Extensions\IntlExtension;
+
 class CurrencyFormatter
 {
-    /**
-     * @var \Twig_Extensions_Extension_Intl
-     */
-    private $helper;
+    private IntlExtension $helper;
 
-    /**
-     * CurrencyFormatter constructor.
-     *
-     * @param \Twig_Extensions_Extension_Intl $helper
-     */
-    public function __construct($helper)
+    public function __construct(IntlExtension $helper)
     {
         $this->helper = $helper;
     }
 
-    /**
-     * @param float       $number
-     * @param string|null $currency
-     * @param string|null $locale
-     *
-     * @return string
-     */
-    public function format($number, $currency = null, $locale = null)
+    public function format(float $number, ?string $currency = null, ?string $locale = null): ?string
     {
-        //TODO migrate to new version of twig intl filter
         foreach ($this->helper->getFilters() as $filter) {
             if ('localizedcurrency' === $filter->getName()) {
                 $callbable = $filter->getCallable();
