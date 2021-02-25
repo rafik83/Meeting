1652b88a0deae8ed80337d532a996d1ddc3239d6
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-02-25 10:50:27 +0100

    2301 - remove unused updateJob and use messenger instead of jms job queue

--- src/Infrastructure/Bundle/InfrastructureBundle/Adapter/AbstractJobQueueAdapter.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Adapter/AbstractJobQueueAdapter.php
@@ -3,80 +3,41 @@
 namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Adapter;
 
 use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Adapter\BatchJobQueue\Message\Job;
 use Symfony\Component\Messenger\MessageBusInterface;
 
 abstract class AbstractJobQueueAdapter
 {
-    private EntityManagerInterface $entityManager;
     private MessageBusInterface $bus;
 
-    public function __construct(EntityManagerInterface $entityManager, MessageBusInterface $bus)
-    {
-        $this->entityManager = $entityManager;
+    public function __construct(
+        MessageBusInterface $bus
+    ) {
         $this->bus = $bus;
-    }
-
-    protected function hasAlreadyJobPending(string $command, array $args): bool
-    {
-        // TODO: refactor to use messenger
-        return false;
-
-        // $pendingJob = $this->entityManager
-        //     ->createQuery("SELECT j FROM JMSJobQueueBundle:Job j
-        //         WHERE j.command = :command
-        //         AND j.args = :args
-        //         AND j.state = :state
-        //     ")
-        //     ->setParameter('command', $command)
-        //     ->setParameter('args', $args, Types::JSON)
-        //     ->setParameter('state', Job::STATE_PENDING)
-        //     ->setMaxResults(1)
-        //     ->getOneOrNullResult()
-        // ;
-
-        // return null !== $pendingJob;
     }
 
     protected function setJob(Job $job): void
     {
-        $command = $job->getCommand();
-        $args = $job->getArgs();
-
-        // Avoid to set a job when the same job is already pending.
-        if ($this->hasAlreadyJobPending($command, $args)) {
-            return;
-        }
-
         $this->bus->dispatch($job);
-        // $this->entityManager->persist($job);
-        // $this->entityManager->flush($job);
     }
 
-    protected function updateJob(Job $job): void
+    protected function removeJob(Job $job): void
     {
-        throw new LogicException('TODO: refactor to use messenger');
-        $this->entityManager->flush($job);
-    }
+        throw new LogicException('Deprecated');
 
-    protected function removeJob(string $command, array $args): void
-    {
-        throw new LogicException('TODO: refactor to use messenger');
-
-         $this->entityManager
-             ->createQueryBuilder()
-             ->delete()
-             ->from(Job::class, 'job')
-             ->where('job.command = :command')
-             ->andWhere('job.args = :args')
-             ->andWhere('job.state = :state')
-            ->setParameter('command', $command)
-            ->setParameter('args', $args, Types::JSON)
-            ->setParameter('state', Job::STATE_PENDING)
-            ->getQuery()
-            ->execute()
-        ;
+        //  $this->entityManager
+        //      ->createQueryBuilder()
+        //      ->delete()
+        //      ->from(Job::class, 'job')
+        //      ->where('job.command = :command')
+        //      ->andWhere('job.args = :args')
+        //      ->andWhere('job.state = :state')
+        //     ->setParameter('command', $command)
+        //     ->setParameter('args', $args, Types::JSON)
+        //     ->setParameter('state', Job::STATE_PENDING)
+        //     ->getQuery()
+        //     ->execute()
+        // ;
     }
 }
