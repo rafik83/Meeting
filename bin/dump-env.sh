e938c74b0a833488c5be0cd437e84104da9527ef
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-03-10 15:06:42 +0100

    2301 - add script to dump env variables on prod

---
+++ bin/dump-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# dump environement variables, since they're not available for user www-data when application is deployed
+# this allows application to get values from root env
+if [ -z "$KUBERNETES_PORT" ]
+then
+    echo "This script should be not be run manually"
+else
+    echo "VIMEET_DATABASE_URL=${VIMEET_DATABASE_URL}" > .env.prod.local
+    echo "REDIS_URL=redis://${REDIS_HOST}:${REDIS_PORT}" >> .env.prod.local
+    composer dump-env prod
+fi
