16a9bd409cdf4383fc24257fdf9f48096687441a
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-04-22 16:38:37 +0200

    2512 - add sendEmailMinEvaluation field to entity

---
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20210422132047.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Proximum\Vimeet\Infrastructure\Bundle\InfrastructureBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Add sendEmailMinEvaluation to Rule entity
+ */
+final class Version20210422132047 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql('ALTER TABLE rule ADD send_email_min_evaluation SMALLINT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $this->addSql('ALTER TABLE rule DROP send_email_min_evaluation');
+    }
+}
