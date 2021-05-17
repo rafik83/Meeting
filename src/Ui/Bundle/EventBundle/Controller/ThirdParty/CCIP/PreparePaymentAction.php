9c098cc5ce19532bde6189e41409cd8d2a25888a
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-05-17 18:09:28 +0200

    2550 - prepare payment controller

--- src/Ui/Bundle/EventBundle/Controller/ThirdParty/CCIP/PreparePaymentAction.php
+++ src/Ui/Bundle/EventBundle/Controller/ThirdParty/CCIP/PreparePaymentAction.php
@@ -5,11 +5,12 @@ namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\ThirdParty\CCIP;
 
 
 use Proximum\Vimeet\Application\Adapter\QueryBusInterface;
-use Proximum\Vimeet\Application\ThirdParty\CCIP\OrderCCIPViewQuery;
 use Proximum\Vimeet\Domain\Model\Order;
-use Proximum\Vimeet\Domain\Model\User;
+use Proximum\Vimeet\Domain\Model\Transaction;
 use Proximum\Vimeet\Domain\Package\Funnel\FunnelFactory;
+use Proximum\Vimeet\Infrastructure\Payum\CCIP\PreparePayment;
 use Proximum\Vimeet\Ui\Bundle\EventBundle\ParamConverter\EventDomain;
+use Proximum\Vimeet\Ui\Bundle\EventBundle\ValueResolver\UserDomain;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -18,36 +19,43 @@ class PreparePaymentAction
 {
     public const TEMPLATE = 'EventBundle:ThirdParty:CCIP/preparePaymentCCIP.html.twig';
 
-    private QueryBusInterface $queryBus;
-
     private Environment $twig;
-
     private FunnelFactory $packageFunnelFactory;
+    private PreparePayment $prepareCcipPayment;
 
     public function __construct(
-        QueryBusInterface $queryBus,
         Environment $twig,
-        FunnelFactory $packageFunnelFactory
-    )
-    {
-        $this->queryBus = $queryBus;
+        FunnelFactory $packageFunnelFactory,
+        PreparePayment $prepareCcipPayment
+    ) {
         $this->twig = $twig;
         $this->packageFunnelFactory = $packageFunnelFactory;
+        $this->prepareCcipPayment = $prepareCcipPayment;
     }
 
-    public function __invoke(Request $request, Order $order, EventDomain $eventDomain, User $user): Response
-    {
+    public function __invoke(
+        Request $request,
+        Order $order,
+        EventDomain $eventDomain,
+        UserDomain $userDomain,
+        Transaction $transaction
+    ): Response {
+
+        // TODO: check that order and transaction belong to current user
 
         $event = $eventDomain->getEvent();
         $funnel = $this->packageFunnelFactory->create($order->getSheet(), $request->getLocale());
 
+        $captureToken = $this->prepareCcipPayment->process($transaction, $request->getLocale());
+
         return new Response($this->twig->render(self::TEMPLATE, [
             'order' => $order,
             'sheet' => $order->getSheet(),
-            'user' => $user,
+            'user' => $userDomain->getUser(),
             'domain' => $event->getDomain(),
             'view' => ['funnel' => $funnel],
             'event' => $eventDomain->getEvent(),
+            'payumToken' => $captureToken->getHash(),
         ]));
     }
 }
