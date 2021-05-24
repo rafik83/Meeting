debf10ac086cdb6d18afda5d758515113882ccf1
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-05-24 16:52:29 +0200

    2550 - code review

--- src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20210521131003.php
+++ src/Infrastructure/Bundle/InfrastructureBundle/Migrations/Version20210521131003.php
@@ -12,18 +12,16 @@ final class Version20210521131003 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return '';
+        return 'Add boolean flag to allow poll in webinar';
     }
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE happening ADD poll_allowed TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE happening DROP poll_allowed TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 }
