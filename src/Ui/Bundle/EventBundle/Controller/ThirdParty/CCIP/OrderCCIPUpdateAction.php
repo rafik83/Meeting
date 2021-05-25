d0d3f2130c80aee8b03f53b5ec6d743f729bb31c
Gregory Pelletier <gregory.pelletier@fairness.coop>
2021-05-25 13:17:41 +0200

    2550 - add update url support

--- src/Ui/Bundle/EventBundle/Controller/ThirdParty/CCIP/OrderCCIPUpdateAction.php
+++ src/Ui/Bundle/EventBundle/Controller/ThirdParty/CCIP/OrderCCIPUpdateAction.php
@@ -4,6 +4,7 @@
 namespace Proximum\Vimeet\Ui\Bundle\EventBundle\Controller\ThirdParty\CCIP;
 
 
+use Proximum\Vimeet\Application\ThirdParty\CCIP\Exception\InvalidPaymentNumber;
 use Proximum\Vimeet\Domain\Model\Transaction;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,13 +21,23 @@ class OrderCCIPUpdateAction
         $this->logger = $logger;
     }
 
-    public function __invoke(Request $request, Transaction $transaction): Response
+    public function __invoke(Request $request, Transaction $transaction, string $paymentNumber): Response
     {
+        if ($transaction->getPayment()->getNumber() !== $paymentNumber) {
+            throw new InvalidPaymentNumber(
+                'Invalid payment number for transaction #' . $transaction->getId() . ' while update endpoint is called'
+            );
+        }
+
         $this->logger->info(
             '[CCIP Payment] Update received transaction {transactionId}, status {status}',
             ['transactionId' => $transaction->getId(), 'status' => $request->request->get('update')]
         );
 
-        return new Response();
+        if ($request->request->get('update') === '00') {
+            $transaction->setPaid();
+        }
+
+        return new Response('Transaction validated');
     }
 }
